26f236d3bd01b2282333ca2376a6dbf6d52b1b5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:06 +0000

    Add package quotes-fetcher/0.4/quotes_fetcher-0.4.tar.gz

---
+++ packages/quotes-fetcher/quotes_fetcher-0.4.tar.gz/quotes_fetcher-0.4/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name = 'quotes_fetcher',
+  packages = ['quotes_fetcher'],
+  version = '0.4',
+  license='MIT',
+  description = 'quote_fetcher is a toy app that provides you with an option to fetch exchange market data and calculate some metrics ',
+  author = 'Ruslan Danila',
+  author_email = 'daniloruslan@yahoo.com',
+  url = 'https://github.com/user/daniloruslan',
+  download_url = 'https://github.com/daniloruslan/quotes_fetcher/archive/v0.4.tar.gz',
+  keywords = ['exchange', 'data', 'quotes'],
+  install_requires=[
+          'yfinance',
+          'pandas',
+          'numpy',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
