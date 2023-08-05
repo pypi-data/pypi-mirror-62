b543ba931c3ea78a540892148ab7218983c36d42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:30 +0000

    Add package chaosiq-cloud/0.9.0/chaosiq_cloud-0.9.0-py2.py3-none-any.whl

---
+++ packages/chaosiq-cloud/chaosiq_cloud-0.9.0-py2.py3-none-any.whl/chaoscloud/api/team.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from logzero import logger
+import requests
+
+__all__ = ["request_teams"]
+
+
+def request_teams(teams_url: str, token: str,
+                  verify_tls: bool = True) -> requests.Response:
+    try:
+        return requests.get(teams_url, headers={
+                "Authorization": "Bearer {}".format(token)
+            }, verify=verify_tls)
+    except requests.exceptions.SSLError as x:
+        logger.error(
+            "Failed to communicate over TLS with the ChaosIQ endpoint: "
+            "{}".format(str(x)))
