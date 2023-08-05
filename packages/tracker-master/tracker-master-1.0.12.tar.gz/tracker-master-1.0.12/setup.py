072d066e0fa38c0764f03191981f2163c7fd9d57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:04 +0000

    Add package tracker-master/1.0.12/tracker-master-1.0.12.tar.gz

---
+++ packages/tracker-master/tracker-master-1.0.12.tar.gz/tracker-master-1.0.12/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='tracker-master',
+    version='1.0.12',
+    packages=['tracker_master', 'tracker_master.model', 'tracker_master.app_master', 'tracker_master.app_server'],
+    url='',
+    license='',
+    author='Mikhail Astafurov',
+    author_email='',
+    package_data={'tracker_master': ['conf/*.yaml', 'conf/*.conf']},
+    description=''
+)
