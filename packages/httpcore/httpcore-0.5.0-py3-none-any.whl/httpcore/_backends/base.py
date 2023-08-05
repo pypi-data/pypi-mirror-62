0a0ede4be2c5686256389ca5f12b0d786116b721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:41 +0000

    Add package httpcore/0.5.0/httpcore-0.5.0-py3-none-any.whl

---
+++ packages/httpcore/httpcore-0.5.0-py3-none-any.whl/httpcore/_backends/base.py
@@ -0,0 +1,90 @@
+from ssl import SSLContext
+from types import TracebackType
+from typing import Dict, Optional, Type
+
+
+class AsyncSocketStream:
+    """
+    A socket stream with read/write operations. Abstracts away any asyncio-specific
+    interfaces into a more generic base class, that we can use with alternate
+    backends, or for stand-alone test cases.
+    """
+
+    def get_http_version(self) -> str:
+        raise NotImplementedError()  # pragma: no cover
+
+    async def start_tls(
+        self,
+        hostname: bytes,
+        ssl_context: SSLContext,
+        timeout: Dict[str, Optional[float]],
+    ) -> "AsyncSocketStream":
+        raise NotImplementedError()  # pragma: no cover
+
+    async def read(self, n: int, timeout: Dict[str, Optional[float]]) -> bytes:
+        raise NotImplementedError()  # pragma: no cover
+
+    async def write(self, data: bytes, timeout: Dict[str, Optional[float]]) -> None:
+        raise NotImplementedError()  # pragma: no cover
+
+    async def close(self) -> None:
+        raise NotImplementedError()  # pragma: no cover
+
+    def is_connection_dropped(self) -> bool:
+        raise NotImplementedError()  # pragma: no cover
+
+
+class AsyncLock:
+    """
+    An abstract interface for Lock classes.
+    """
+
+    async def __aenter__(self) -> None:
+        await self.acquire()
+
+    async def __aexit__(
+        self,
+        exc_type: Type[BaseException] = None,
+        exc_value: BaseException = None,
+        traceback: TracebackType = None,
+    ) -> None:
+        self.release()
+
+    def release(self) -> None:
+        raise NotImplementedError()  # pragma: no cover
+
+    async def acquire(self) -> None:
+        raise NotImplementedError()  # pragma: no cover
+
+
+class AsyncSemaphore:
+    """
+    An abstract interface for Semaphore classes.
+    Abstracts away any asyncio-specific interfaces.
+    """
+
+    async def acquire(self, timeout: float = None) -> None:
+        raise NotImplementedError()  # pragma: no cover
+
+    def release(self) -> None:
+        raise NotImplementedError()  # pragma: no cover
+
+
+class AsyncBackend:
+    async def open_tcp_stream(
+        self,
+        hostname: bytes,
+        port: int,
+        ssl_context: Optional[SSLContext],
+        timeout: Dict[str, Optional[float]],
+    ) -> AsyncSocketStream:
+        raise NotImplementedError()  # pragma: no cover
+
+    def create_lock(self) -> AsyncLock:
+        raise NotImplementedError()  # pragma: no cover
+
+    def create_semaphore(self, max_value: int, exc_class: type) -> AsyncSemaphore:
+        raise NotImplementedError()  # pragma: no cover
+
+    def time(self) -> float:
+        raise NotImplementedError()  # pragma: no cover
