28f0532d88de78336313f6c2e422a014031cf221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:13 +0000

    Add package dokklib-db/0.0.9/dokklib_db-0.0.9.tar.gz

---
+++ packages/dokklib-db/dokklib_db-0.0.9.tar.gz/dokklib_db-0.0.9/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='dokklib_db',
+    # TODO (abiro) get version from git tags
+    version='0.0.9',
+    author='Agost Biro',
+    author_email='agost+dokklib_db@dokknet.com',
+    description='DynamoDB Single Table Library',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://dokklib.com/libs/db/',
+    packages=['dokklib_db'],
+    install_requires=[
+        'boto3>=1.10.34,<2',
+        'botocore>=1.13.34,<2'
+    ],
+    data_files=[('', ['LICENSE', 'NOTICE'])],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Database'
+    ]
+)
