332af5ecb03060facf4d829234b2924b20df3305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:47 +0000

    Add package django-debug-toolbar-sqlalchemy/0.3.0/django_debug_toolbar_sqlalchemy-0.3.0-py3-none-any.whl

---
+++ packages/django-debug-toolbar-sqlalchemy/django_debug_toolbar_sqlalchemy-0.3.0-py3-none-any.whl/debug_toolbar_sqlalchemy/panel.py
@@ -0,0 +1,44 @@
+from debug_toolbar.panels import Panel
+from debug_toolbar_sqlalchemy.operation_tracker import OperationTracker
+
+from django.utils.translation import ugettext as _
+from django.template.loader import render_to_string
+
+
+class SqlAlchemyDebugPanel(Panel):
+    """ Panel that shows information about SQLAlchemy operations. """
+    name = 'SQLAlchemy'
+    has_content = True
+    template = 'sqlalchemy-panel.html'
+
+    def __init__(self, *args, **kwargs):
+        super(SqlAlchemyDebugPanel, self).__init__(*args, **kwargs)
+        self.tracker = OperationTracker()
+        self.tracker.register()
+
+    def nav_title(self):
+        return _('SQLAlchemy')
+
+    def nav_subtitle(self):
+        if self.tracker:
+            count = len(self.tracker.queries)
+            return "%d %s in %.2fms" % (
+                count,
+                "query" if count == 1 else "queries",
+                sum(map(lambda query: query.duration, self.tracker.queries)),
+            )
+
+        return "Unavailable"
+
+    def title(self):
+        return _('SQLAlchemy queries')
+
+    def url(self):
+        return ''
+
+    @property
+    def content(self):
+        return render_to_string(
+            self.template,
+            {'queries': self.tracker.queries}
+        )
