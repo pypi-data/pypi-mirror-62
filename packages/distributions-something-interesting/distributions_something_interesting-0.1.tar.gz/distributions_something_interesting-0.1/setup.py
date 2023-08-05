0e6d58f24749d42b7a85df9e18571a03a9b72fc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:30 +0000

    Add package distributions-something-interesting/0.1/distributions_something_interesting-0.1.tar.gz

---
+++ packages/distributions-something-interesting/distributions_something_interesting-0.1.tar.gz/distributions_something_interesting-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_something_interesting',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_something_interesting'],
+      author='Carl Shi',
+      zip_safe=False)
