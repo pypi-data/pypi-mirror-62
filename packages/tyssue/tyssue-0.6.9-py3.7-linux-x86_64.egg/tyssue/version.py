c896710cf27801a35c2e600558b84855a2f40542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:40:20 +0000

    Add package tyssue/0.6.9/tyssue-0.6.9-py3.7-linux-x86_64.egg

---
+++ packages/tyssue/tyssue-0.6.9-py3.7-linux-x86_64.egg/tyssue/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM tyssue SETUP.PY
+#
+short_version = '0.6.9'
+full_version = '0.6.9'
+git_revision = 'e85fdbad4462d2d5b0b97d82340ab19d01993f3b'
+release = True
+if release:
+    version = full_version
+else:
+    version = short_version
