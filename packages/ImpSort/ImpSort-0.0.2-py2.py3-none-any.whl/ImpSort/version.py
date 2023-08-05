d883730083bca6edcfc5a85d31b6b5b9f480da6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:36 +0000

    Add package ImpSort/0.0.2/ImpSort-0.0.2-py2.py3-none-any.whl

---
+++ packages/ImpSort/ImpSort-0.0.2-py2.py3-none-any.whl/ImpSort/version.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+__author__ = 'David Khudaverdyan'
+__copyright__ = 'Copyright 2020, David Khudaverdyan'
+__credits__ = ['David Khudaverdyan']
+__license__ = 'MIT'
+__version__ = '0.0.2'
+__maintainer__ = 'David Khudaverdyan'
+__email__ = 'khudaverdyan.david@gmail.com'
+__status__ = "Production"
+
+__all__ = ['__author__',
+           '__copyright__',
+           '__credits__',
+           '__license__',
+           '__version__',
+           '__maintainer__',
+           '__email__',
+           '__status__']
