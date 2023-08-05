4a430ddbbe6cb69e696d81e13b0f059a174f5beb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:29 +0000

    Add package threefive/2.0.47/threefive-2.0.47.tar.gz

---
+++ packages/threefive/threefive-2.0.47.tar.gz/threefive-2.0.47/examples/Time_Signal-Program_Start_End.py
@@ -0,0 +1,10 @@
+"""
+Example from the specification
+
+14.4. Time_Signal–Program_Start_End
+"""
+
+from threefive import decode
+
+Base64 = "/DBIAAAAAAAA///wBQb+ek2ItgAyAhdDVUVJSAAAGH+fCAgAAAAALMvDRBEAAAIXQ1VFSUgAABl/nwgIAAAAACyk26AQAACZcuND"
+decode(Base64)
