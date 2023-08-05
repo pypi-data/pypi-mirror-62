d727b846e719b5f21c2db55f088c9498ab9f2a77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:43 +0000

    Add package variableholder/0.0.0/VariableHolder-0.0.0.tar.gz

---
+++ packages/variableholder/VariableHolder-0.0.0.tar.gz/VariableHolder-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='VariableHolder',
+    version='0.0.0',
+    packages=['VariableHolder'],
+    url='',
+    license='MIT',
+    author='Yeachan-Heo',
+    author_email='hurrc04@gmail.com',
+    description=''
+)
