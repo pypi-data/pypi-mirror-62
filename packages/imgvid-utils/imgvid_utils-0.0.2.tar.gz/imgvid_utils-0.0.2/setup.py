631aac65e0778faa28c12b0b39e4a419ff33c06f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:17 +0000

    Add package imgvid-utils/0.0.2/imgvid_utils-0.0.2.tar.gz

---
+++ packages/imgvid-utils/imgvid_utils-0.0.2.tar.gz/imgvid_utils-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="imgvid_utils",
+    version="0.0.2",
+    author="Philippe Solodov",
+    author_email="solop1906@gmail.com",
+    description="A package that provides helpful utilities to interact with videos and images through OpenCV.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/philippeitis/imgvid_utils",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
