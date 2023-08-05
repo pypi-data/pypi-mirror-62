303cd7925ac58e125daabbba1a64f89287441c50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:14 +0000

    Add package pypdfy/0.1.1/pypdfy-0.1.1.tar.gz

---
+++ packages/pypdfy/pypdfy-0.1.1.tar.gz/pypdfy-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pypdfy',
+    version='0.1.1',
+    license='MIT',
+    description='An easy approach to metadata extraction for PDF files',
+    author='David Bros Campillo',
+    author_email='david_corp896@outlook.es',
+    maintainer='David Bros',
+    maintainer_email='david_corp896@outlook.es',
+    packages=find_packages(exclude=['README']),
+    download_url='https://github.com/TheHunter896/pypdfy',
+    keywords=['PDF', 'METADATA', 'NO DEPENDENCIES', 'PYTHON', 'OOP'],
+    )
