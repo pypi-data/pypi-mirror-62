d248ba4f8afef470e0f349fe38841ecf163f45aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:17 +0000

    Add package audiomate/5.2.0/audiomate-5.2.0-py3-none-any.whl

---
+++ packages/audiomate/audiomate-5.2.0-py3-none-any.whl/audiomate/corpus/validation/track.py
@@ -0,0 +1,64 @@
+import multiprocessing
+
+import audioread
+from tqdm import tqdm
+
+from . import base
+
+
+class TrackReadValidator(base.Validator):
+    """
+    Check if the track can be opened and read.
+    By reading the first few samples.
+    """
+
+    def __init__(self, num_workers=1):
+        self.num_workers = num_workers
+
+    def name(self):
+        return 'Track-Read'
+
+    def validate(self, corpus):
+        """
+        Perform the validation on the given corpus.
+
+        Args:
+            corpus (Corpus): The corpus to test/validate.
+
+        Returns:
+            InvalidItemsResult: Validation result.
+        """
+
+        with multiprocessing.pool.ThreadPool(self.num_workers) as p:
+            result = list(tqdm(
+                p.imap(
+                    self.validate_track,
+                    list(corpus.tracks.values())
+                ),
+                total=corpus.num_tracks,
+                desc='Validate tracks'
+            ))
+
+        invalid_tracks = {x[0]: x[1] for x in result if x[1] is not None}
+        passed = len(invalid_tracks) <= 0
+
+        return base.InvalidItemsResult(
+            passed,
+            invalid_tracks,
+            item_name='Tracks',
+            name=self.name()
+        )
+
+    def validate_track(self, track):
+        result = None
+
+        try:
+            track.read_samples(duration=0.001)
+        except EOFError:
+            result = 'EOFError'
+        except audioread.NoBackendError:
+            result = 'NoBackendError'
+        except Exception as ex:
+            result = str(ex)
+
+        return (track.idx, result)
