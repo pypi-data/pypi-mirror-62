fe77dc13caba4fcef9e320511b065e96fe884365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:27 +0000

    Add package ze2nb-cli/1.0.2/ze2nb_cli-1.0.2-py3-none-any.whl

---
+++ packages/ze2nb-cli/ze2nb_cli-1.0.2-py3-none-any.whl/ze2nb_cli/cli.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import sys
+import os
+import argparse
+
+from ze2nb import ze2nb
+
+def run_all(out_path=None, to_html=False, to_py=False):
+  for directory, folders, files in os.walk("."):
+    for f in files:
+      if os.path.splitext(f)[1] == ".json":
+        fullpath = os.path.join(directory, f)
+        ze2nb(
+            fullpath,
+            out_path=out_path,
+            to_html=to_html,
+            to_py=to_py)
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-i", "--input", help="The input file_name", type=str)
+  parser.add_argument("--run-all", help="If set, walks through this directory and converts all *.json to ipynb", action="store_true")
+  parser.add_argument("--out-path", help="Output path", type=str)
+  parser.add_argument("--to-html", help="Converts to HTML", action="store_true")
+  parser.add_argument("--to-py", help="Converts to .py", action="store_true")
+
+  args = parser.parse_args()
+
+  if not args.input and not args.run_all:
+    raise Exception("One of `--input` or `--run-all` must be included")
+
+  if args.run_all:
+    run_all(
+        out_path=args.out_path,
+        to_html=args.to_html,
+        to_py=args.to_py)
+  else:
+    ze2nb(
+        args.input,
+        out_path=args.out_path,
+        to_html=args.to_html,
+        to_py=args.to_py)
+
+
+if __name__ == "__main__":
+  main()
