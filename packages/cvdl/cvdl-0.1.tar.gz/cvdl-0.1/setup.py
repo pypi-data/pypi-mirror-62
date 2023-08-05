b91d1c3214fd8abe8317a2b44110fbb0e5076ea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:01 +0000

    Add package cvdl/0.1/cvdl-0.1.tar.gz

---
+++ packages/cvdl/cvdl-0.1.tar.gz/cvdl-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'cvdl',
+  packages = ['cvdl'],
+  version = '0.1',
+  license='MIT',
+  description = 'Computer Vision and Deep Learning',
+  author = 'Sarmad Gulzar',
+  author_email = 'sarmadgulzar@outlook.com',
+  url = 'https://github.com/cvdlai/cvdl',
+  download_url = 'https://github.com/cvdlai/cvdl/archive/v_01.tar.gz',
+  keywords = ['Computer Vision', 'Deep Learning'],
+)
