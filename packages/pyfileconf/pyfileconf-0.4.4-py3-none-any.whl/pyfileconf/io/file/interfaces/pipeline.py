cc1fafbaee732ce74483874acc5587d1f1b59f7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:50 +0000

    Add package pyfileconf/0.4.4/pyfileconf-0.4.4-py3-none-any.whl

---
+++ packages/pyfileconf/pyfileconf-0.4.4-py3-none-any.whl/pyfileconf/io/file/interfaces/pipeline.py
@@ -0,0 +1,8 @@
+
+from pyfileconf.io.file.load.lazy.pipeline import PipelineDictLoader
+
+class PipelineDictInterface(PipelineDictLoader):
+
+    def save(self):
+        # TODO [#9]: write pipeline dict to file
+        pass
