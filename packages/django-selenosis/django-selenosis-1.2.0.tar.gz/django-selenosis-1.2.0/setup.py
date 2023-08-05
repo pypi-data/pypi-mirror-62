0de0c3bed77b98ccca75c8370b31a004019cc0dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:33 +0000

    Add package django-selenosis/1.2.0/django-selenosis-1.2.0.tar.gz

---
+++ packages/django-selenosis/django-selenosis-1.2.0.tar.gz/django-selenosis-1.2.0/setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+from os import path
+import codecs
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+
+def read(*parts):
+    file_path = path.join(path.dirname(__file__), *parts)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+setup(
+    name='django-selenosis',
+    version='1.2.0',
+    license='BSD',
+    description='Helpers for writing selenium tests for Django',
+    long_description=read('README.rst'),
+    url='https://github.com/theatlantic/django-selenosis',
+    author='Frankie Dintino',
+    author_email='fdintino@theatlantic.com',
+    maintainer='Frankie Dintino',
+    maintainer_email='fdintino@theatlantic.com',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Django>=1.8',
+        'six',
+    ],
+    entry_points={
+        "pytest11": [
+            "selenosis = selenosis.pytest_plugin.selenosis",
+            "tags = selenosis.pytest_plugin.tags",
+        ],
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ])
