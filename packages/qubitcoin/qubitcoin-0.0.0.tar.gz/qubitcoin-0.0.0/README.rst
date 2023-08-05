a53b7c23ac098719c7f512c4d615e31423c7f5d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:27 +0000

    Add package qubitcoin/0.0.0/qubitcoin-0.0.0.tar.gz

---
+++ packages/qubitcoin/qubitcoin-0.0.0.tar.gz/qubitcoin-0.0.0/README.rst
@@ -0,0 +1,5 @@
+******
+ bitq
+******
+
+An ECDSA and XMSS interchangeable, decentralized P2P, mining-resistant, fully anonymized, self-regulating and backward stochastic python implementation of the bitcoin protocol
---
+++ packages/qubitcoin/qubitcoin-0.0.0.tar.gz/qubitcoin-0.0.0/qc/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 31 16:01:31 2020
+
+@author: shane
+
+Copyright 2020 Shane Jaroch
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+__title__ = "qubitcoin"
+__version__ = "0.0.0"
+__author__ = "Shane Jaroch"
+__license__ = "GPL v3"
+__copyright__ = "Copyright 2018-2020 Shane Jaroch"
