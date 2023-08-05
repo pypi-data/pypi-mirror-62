fffed7da404ad83bfac643153be50c833b71c44b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:20 +0000

    Add package treewatcher/0.2/treewatcher-0.2.tar.gz

---
+++ packages/treewatcher/treewatcher-0.2.tar.gz/treewatcher-0.2/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(
+    name     = 'treewatcher',
+    version  = '0.2',
+    packages = ['treewatcher'],
+    install_requires = [
+        'pyinotify'
+    ]
+)
+
