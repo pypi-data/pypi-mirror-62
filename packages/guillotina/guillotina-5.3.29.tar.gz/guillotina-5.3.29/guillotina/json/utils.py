ee5ff85dfa1e2200d9bed3d7e5bee5e71850740a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:40 +0000

    Add package guillotina/5.3.29/guillotina-5.3.29.tar.gz

---
+++ packages/guillotina/guillotina-5.3.29.tar.gz/guillotina-5.3.29/guillotina/json/utils.py
@@ -0,0 +1,24 @@
+from guillotina.component import get_multi_adapter
+from guillotina.exceptions import RequestNotFound
+from guillotina.interfaces import ISchemaSerializeToJson
+from guillotina.utils import get_current_request
+
+import logging
+
+
+logger = logging.getLogger("guillotina")
+
+
+def convert_interfaces_to_schema(interfaces):
+    properties = {}
+    try:
+        request = get_current_request()
+    except RequestNotFound:
+        from guillotina.tests.utils import get_mocked_request
+
+        request = get_mocked_request()
+
+    for iface in interfaces:
+        serializer = get_multi_adapter((iface, request), ISchemaSerializeToJson)
+        properties[iface.__identifier__] = serializer.serialize()
+    return properties
