2270f0e138574bf06dc7efe75f16cf867cc8c217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:55 +0000

    Add package dense-fusion/0.0.3/dense-fusion-0.0.3.tar.gz

---
+++ packages/dense-fusion/dense-fusion-0.0.3.tar.gz/dense-fusion-0.0.3/dense_fusion/models/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+import dense_fusion.models.resnet
+import dense_fusion.models.pspnet
+from dense_fusion.models.network import DenseFusion
+from dense_fusion.models.network import PoseNet
+from dense_fusion.models.network import PoseRefineNet
