4f00daea359e357ff1499fea88c47b9546e7d772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:38 +0000

    Add package isc-py-common/0.0.132/isc_py_common-0.0.132-py3-none-any.whl

---
+++ packages/isc-py-common/isc_py_common-0.0.132-py3-none-any.whl/tracker/views/message_confirm_reading.py
@@ -0,0 +1,50 @@
+import logging
+
+from crypto.models.upload_file import DSResponse_UploadFile
+from isc_common.datetime import DateToStr
+from isc_common.http.RPCResponse import RPCResponseConstant
+from isc_common.ws.webSocket import WebSocket
+from tracker.models.messages import Messages
+from twits import ChatData, ChatResponseData
+
+logger = logging.getLogger(__name__)
+
+
+class DSResponse__Messages_Confirm_reading(DSResponse_UploadFile):
+
+    def __init__(self, request):
+        json_data = request.POST.dict()
+        # logger.debug(f'json_data: {json_data}')
+
+        chat_data = ChatData(json_data)
+
+        try:
+            message = Messages.objects.get(guid=chat_data.guid,
+                                           to_whom_id=chat_data.user_id,
+                                           state__in=[
+                                               chat_data.message_state_new_id,
+                                               chat_data.message_state_delivered_id
+                                           ])
+            message.state_id = chat_data.message_state_readed_id
+            message.save()
+            date = DateToStr(message.lastmodified, '%d.%m.%Y, %H:%M:%S')
+            message = ChatResponseData(
+                channel=chat_data.channel,
+                date=date,
+                guid=str(message.guid).upper(),
+                message=message.message,
+                state__name=message.state.name,
+                to_whom_id=message.to_whom.id,
+                type=chat_data.type,
+                user__color=message.user.color,
+                user__short_name=message.user.get_short_name,
+                user_id=message.user.id,
+            )
+            WebSocket.send_message(host=chat_data.host, port=chat_data.port, channel=chat_data.channel, message=message, logger=logger)
+
+        except Messages.DoesNotExist:
+            pass
+        except Exception as ex:
+            raise ex
+
+        self.response = dict(status=RPCResponseConstant.statusSuccess)
