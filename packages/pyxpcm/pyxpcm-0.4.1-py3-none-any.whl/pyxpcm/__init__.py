30312ad259bdce77156af7879c5bc4f505c95070
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:08 +0000

    Add package pyxpcm/0.4.1/pyxpcm-0.4.1-py3-none-any.whl

---
+++ packages/pyxpcm/pyxpcm-0.4.1-py3-none-any.whl/pyxpcm/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.4.1"
+
+# Import class:
+from .models import pcm
+from .xarray import pyXpcmDataSetAccessor
+
+# Other Import
+from . import tutorial
+from . import utils
+from . import plot
+from . import stat
+from .io import load_netcdf
