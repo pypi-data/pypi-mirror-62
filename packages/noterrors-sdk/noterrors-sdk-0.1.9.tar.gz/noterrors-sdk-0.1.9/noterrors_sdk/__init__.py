b7b20657a6cdbbc0193c698649874b2a5d1a6efd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:39 +0000

    Add package noterrors-sdk/0.1.9/noterrors-sdk-0.1.9.tar.gz

---
+++ packages/noterrors-sdk/noterrors-sdk-0.1.9.tar.gz/noterrors-sdk-0.1.9/noterrors_sdk/__init__.py
@@ -0,0 +1,41 @@
+import traceback
+
+from .client import NotErrorsClient
+
+
+_default_client = None
+_clients = {}
+_config = {}
+
+
+def _get_client(type=None):
+    global _config, _clients, _default_client
+
+    if type:
+        client = _clients.get(type)
+        if client is None:
+            if not _config:
+                raise Exception('NotErrors SDK not initialized.')
+            client = _clients[type] = NotErrorsClient.init(*_config['args'], type=type, **_config['kwargs'])
+    else:
+        client = _default_client
+    return client
+
+
+def capture_message(message, type=None):
+    return _get_client(type).capture_message(message)
+
+
+def handle_exception(type=None):
+    try:
+        return _get_client(type).handle_exception()
+    except:
+        tb = traceback.format_exc()
+        print('NOTERRORS EXCEPTION:', tb)
+
+
+def noterrors_init(*args, type='basic', **kwargs):
+    global _config, _clients, _default_client
+
+    _config = {'args': args, 'kwargs': {**kwargs, 'type': type}}
+    _clients[type] = _default_client = NotErrorsClient.init(*args, type=type, **kwargs)
