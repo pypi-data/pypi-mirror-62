7e3ec486f4a568f0e75a2d6aa4534180ddfb2c5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:37 +0000

    Add package datamaestro-text/2020.2.24/datamaestro_text-2020.2.24-py3-none-any.whl

---
+++ packages/datamaestro-text/datamaestro_text-2020.2.24-py3-none-any.whl/datamaestro_text/data/embeddings.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from datamaestro.data import File, data, argument
+import numpy as np
+
+
+@argument("encoding", str, ignored=True, default="utf-8")
+@data(description="Word embeddings as a text word / values")
+class WordEmbeddingsText(File):
+    def load(self):
+        words = {}
+        vectors = []
+        dimension = None
+        with self.path.open("rt", encoding=self.encoding) as fp:
+            for ix, line in enumerate(fp):
+                word, *values = line.split(" ")
+                vectors.append([float(x) for x in values])
+                if dimension is None:
+                    dimension = len(values)
+                else:
+                    assert dimension == len(values)
+                words[word] = ix
+        return words, np.matrix(vectors)
