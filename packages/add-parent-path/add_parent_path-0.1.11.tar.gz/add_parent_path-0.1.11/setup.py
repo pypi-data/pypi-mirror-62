d51b8e46cdaa2e8eb8954437b4b4e2551b3992f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:20 +0000

    Add package add-parent-path/0.1.11/add_parent_path-0.1.11.tar.gz

---
+++ packages/add-parent-path/add_parent_path-0.1.11.tar.gz/add_parent_path-0.1.11/setup.py
@@ -0,0 +1,44 @@
+import io
+import os
+import re
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(filename):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    text_type = type(u"")
+    with io.open(filename, mode="r", encoding='utf-8') as fd:
+        return re.sub(text_type(r':[a-z]+:`~?(.*?)`'), text_type(r'``\1``'), fd.read())
+
+
+setup(
+    name="add_parent_path",
+    version="0.1.11",
+    url="https://github.com/fx-kirin/add_parent_path",
+    license='MIT',
+
+    author="fx-kirin",
+    author_email="fx.kirin@gmail.com",
+
+    description="",
+    long_description=read("README.rst"),
+
+    packages=find_packages(exclude=('tests',)),
+
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
