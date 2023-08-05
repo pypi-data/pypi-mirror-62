732ebee94de21edbec74b2369f3dceecc375f1e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:11 +0000

    Add package pysparklines/1.0/pysparklines-1.0.tar.gz

---
+++ packages/pysparklines/pysparklines-1.0.tar.gz/pysparklines-1.0/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='pysparklines',
+    version=1.0,
+    description="pysparklines is a unicode sparkline generation library.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Brandon Whaley",
+    author_email="redkrieg@gmail.com",
+    url="https://github.com/RedKrieg/pysparklines",
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': [
+            'sparkline = sparkline:main',
+        ]
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    python_requires='>=3.2',
+)
