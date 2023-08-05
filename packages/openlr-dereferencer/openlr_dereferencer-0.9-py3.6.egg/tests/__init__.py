22e63646cdbd9fb381fb04254f4e3299b5101e98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:18 +0000

    Add package openlr-dereferencer/0.9/openlr_dereferencer-0.9-py3.6.egg

---
+++ packages/openlr-dereferencer/openlr_dereferencer-0.9-py3.6.egg/tests/__init__.py
@@ -0,0 +1,5 @@
+"Test cases for this package"
+from openlr_dereferencer.a_star import AStarTests
+from openlr_dereferencer.example_mapformat import SQLiteMapTest
+from openlr_dereferencer.wgs84 import GeoTests
+from openlr_dereferencer.decode import DecodingTests
