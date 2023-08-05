d4375df5bbf7e33a94e020ba417e5af5bfdec692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:42 +0000

    Add package castle.theme/1.0.6/castle.theme-1.0.6.zip

---
+++ packages/castle.theme/castle.theme-1.0.6.zip/castle.theme-1.0.6/README.txt
@@ -0,0 +1,7 @@
+Introduction
+============
+
+This is the default theme for CastleCMS.
+
+Since CastleCMS does not use Diazo, this theme should be a good reference on how to
+write your own CastleCMS themes.
---
+++ packages/castle.theme/castle.theme-1.0.6.zip/castle.theme-1.0.6/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.6'
+
+setup(name='castle.theme',
+      version=version,
+      description="CastleCMS Theme Package",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Plone :: 5.0",
+        "Framework :: Plone :: 5.1",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Wildcard Corp.',
+      author_email='info@wildcardcorp.com',
+      url='https://github.com/castlecms/castle.theme',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['castle'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.theming',
+          'z3c.jbot'
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
