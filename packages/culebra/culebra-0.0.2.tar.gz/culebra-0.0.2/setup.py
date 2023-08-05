654afc75c309ca0533cbbd0f8f8ef1b57b9ab140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:52 +0000

    Add package culebra/0.0.2/culebra-0.0.2.tar.gz

---
+++ packages/culebra/culebra-0.0.2.tar.gz/culebra-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import sys
+
+from setuptools import setup
+
+# Require pytest-runner only when running tests
+pytest_runner = (['pytest-runner>=2.0,<3dev']
+                 if any(arg in sys.argv for arg in ('pytest', 'test'))
+                 else [])
+
+setup_requires = pytest_runner
+
+setup(
+    name="culebra",
+    version="0.0.2",
+    description="Web Framework ",
+    url='https://github.com/xerxes23/culebra',
+    author='Joseph Mendoza',
+    license="MIT",
+    packages=['culebra'],
+    tests_require=['pytest'],
+    setup_requires=setup_requires
+)
