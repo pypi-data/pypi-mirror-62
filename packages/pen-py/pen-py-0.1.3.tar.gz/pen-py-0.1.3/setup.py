afe88d833d20af2075790519fce78b51e10f4293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:45 +0000

    Add package pen-py/0.1.3/pen-py-0.1.3.tar.gz

---
+++ packages/pen-py/pen-py-0.1.3.tar.gz/pen-py-0.1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(name='pen-py',
+    version='0.1.3',
+    description='Penbox utils for python',
+    url='http://github.com/storborg/funniest',
+    author='Chris Castan',
+    author_email='chris@penbox.io',
+    license='MIT',
+    packages=find_packages(include=("penpy",)),
+    install_requires=['dotmap>=1.3'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
+    python_requires='>=3.6',
+    zip_safe=False
+)
