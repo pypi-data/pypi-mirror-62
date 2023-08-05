787eba0cecb6e2f9129b22c31a4c30f9449037ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:19 +0000

    Add package django-szuprefix-saas/0.7.5/django_szuprefix_saas-0.7.5-py2-none-any.whl

---
+++ packages/django-szuprefix-saas/django_szuprefix_saas-0.7.5-py2-none-any.whl/django_szuprefix_saas/comment/apis.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+from __future__ import unicode_literals
+
+from django_szuprefix.api.mixins import UserApiMixin
+from django_szuprefix_saas.saas.mixins import PartyMixin
+from django_szuprefix_saas.saas.permissions import IsSaasWorker
+
+from . import serializers, models
+from rest_framework import viewsets
+from django_szuprefix.api.helper import register
+
+__author__ = 'denishuang'
+
+
+class CommentViewSet(PartyMixin, UserApiMixin, viewsets.ModelViewSet):
+    serializer_class = serializers.CommentSerializer
+    queryset = models.Comment.objects.all()
+    permission_classes = [IsSaasWorker]
+    filter_fields = {
+        'content_type__app_label': ['exact'],
+        'content_type__model': ['exact'],
+        'content_type': ['exact'],
+        'object_id': ['exact'],
+        'user': ['exact']
+    }
+
+
+register(__package__, 'comment', CommentViewSet)
