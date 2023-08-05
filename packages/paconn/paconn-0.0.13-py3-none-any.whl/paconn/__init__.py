3e5f4192f8363286a53ea12bdff3e50eee6aebcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:11 +0000

    Add package paconn/0.0.13/paconn-0.0.13-py3-none-any.whl

---
+++ packages/paconn/paconn-0.0.13-py3-none-any.whl/paconn/__init__.py
@@ -0,0 +1,20 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# -----------------------------------------------------------------------------
+
+"""
+Initializer
+"""
+
+__VERSION__ = '0.0.13'
+__CLI_NAME__ = 'paconn'
+
+# Commands
+_COMMAND_GROUP = ''
+_LOGIN = 'login'
+_DOWNLOAD = 'download'
+_CREATE = 'create'
+_UPDATE = 'update'
+_VALIDATE = 'validate'
