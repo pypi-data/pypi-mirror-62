abfa3f11d54e4d76ff92dabf1e80a237fbfc9c11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:07 +0000

    Add package django-pivot/1.8.1/django-pivot-1.8.1.tar.gz

---
+++ packages/django-pivot/django-pivot-1.8.1.tar.gz/django-pivot-1.8.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+def read_file(name):
+    with open(name) as fd:
+        return fd.read()
+
+
+setup(
+    name='django-pivot',
+    version='1.8.1',
+    description='Create pivot tables and histograms from ORM querysets',
+    long_description=read_file('Readme.rst'),
+    url='https://github.com/martsberger/django-pivot',
+    download_url='https://github.com/martsberger/django-pivot/archive/1.8.1.tar.gz',
+    author='Brad Martsberger',
+    author_email='bmarts@lumere.com',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    packages=find_packages(),
+    install_requires=['django>=1.10', 'six']
+)
