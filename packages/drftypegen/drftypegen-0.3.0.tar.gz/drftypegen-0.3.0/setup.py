4b131f74bce948449091211027c13f020ec12cc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:25 +0000

    Add package drftypegen/0.3.0/drftypegen-0.3.0.tar.gz

---
+++ packages/drftypegen/drftypegen-0.3.0.tar.gz/drftypegen-0.3.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="drftypegen",  # Replace with your own username
+    version="0.3.0",
+    author="Utsob Roy",
+    include_package_data=True,
+    author_email="roy@codesign.com.bd",
+    description="An django app to generate type information for rest_framework.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="Mozilla Public License 2.0 (MPL 2.0)",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=["djangorestframework"],
+    classifiers=[
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+)
