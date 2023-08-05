5d37f4de0817ec7cb7aef208d394cb5953906dc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:58 +0000

    Add package asgara/0.0.4/asgara-0.0.4.tar.gz

---
+++ packages/asgara/asgara-0.0.4.tar.gz/asgara-0.0.4/example_pkg/pincopallo.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 25 08:42:31 2019
+
+@author: Riccardo Busti, Niccolò Tubini
+@license: creative commons 4.0
+"""
+
+import os
+import pandas as pd
+import numpy as np
+import matplotlib
+import math
+
+def sum(a,b):
+    return a+b
+
+def minus(a,b):
+    return a-b
+
+def pro(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
+
+# versione 0.0.3
+def sumabc(a,b,c):
+    return a+b+c
