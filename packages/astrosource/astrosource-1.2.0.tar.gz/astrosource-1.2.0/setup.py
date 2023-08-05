e6392f932510c96f72880188ebca8246d4a45440
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:59 +0000

    Add package astrosource/1.2.0/astrosource-1.2.0.tar.gz

---
+++ packages/astrosource/astrosource-1.2.0.tar.gz/astrosource-1.2.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(path.join(here, 'requirements.pip'), encoding='utf-8') as f:
+    requirements=[line.strip() for line in f.readlines()]
+
+setup(
+    name='astrosource',
+    version='1.2.0',
+    description='Analysis script for sources with variability in their brightness',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Michael Fitzgerald and Edward Gomez',
+    url="https://github.com/zemogle/astrosource",
+    install_requires=requirements,
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'astrosource=astrosource.main:main'
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
