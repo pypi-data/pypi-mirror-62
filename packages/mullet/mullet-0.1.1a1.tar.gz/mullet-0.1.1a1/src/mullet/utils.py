8bdcec1ebde3395c349959d3147869f33c7b7626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:51 +0000

    Add package mullet/0.1.1a1/mullet-0.1.1a1.tar.gz

---
+++ packages/mullet/mullet-0.1.1a1.tar.gz/mullet-0.1.1a1/src/mullet/utils.py
@@ -0,0 +1,23 @@
+"""
+Utility functions and classes.
+"""
+
+
+def add_example(first, second):
+    """
+    Add two inputs (example function to do so)
+
+    Parameters
+    ----------
+    first : float
+        First input to add
+
+    second : float
+        Other input to add
+
+    Returns
+    -------
+    float
+        Sum of first and second
+    """
+    return first + second
