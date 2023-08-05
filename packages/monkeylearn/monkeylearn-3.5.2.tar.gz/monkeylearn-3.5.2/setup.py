49a74b3f9006ccce0315097eca7b8a90f00c312a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:32 +0000

    Add package monkeylearn/3.5.2/monkeylearn-3.5.2.tar.gz

---
+++ packages/monkeylearn/monkeylearn-3.5.2.tar.gz/monkeylearn-3.5.2/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+from os import path
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name='monkeylearn',
+    version='3.5.2',
+    author='MonkeyLearn',
+    author_email='hello@monkeylearn.com',
+    description='Official Python client for the MonkeyLearn API',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/monkeylearn/monkeylearn-python',
+    download_url='https://github.com/monkeylearn/monkeylearn-python/tarball/v3.2.4',
+    keywords=['monkeylearn', 'machine learning', 'python'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    package_dir={'': '.'},
+    packages=find_packages('.'),
+    install_requires=[
+        # use "pip install requests[security]" for taking out the warnings
+        'requests>=2.8.1',
+        'six>=1.10.0',
+    ],
+)
