8ab12554f1ba1dd6a9d080398a36ec3bf04e246e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:13 +0000

    Add package tablip/1.0.6/tablip-1.0.6.tar.gz

---
+++ packages/tablip/tablip-1.0.6.tar.gz/tablip-1.0.6/README.md
@@ -0,0 +1,4 @@
+# tablip
+generate file such as json, excel ..  especially excel can auto fix column width.
+生成json，excel等文件。尤其是excel可以自动修复列宽。
+
---
+++ packages/tablip/tablip-1.0.6.tar.gz/tablip-1.0.6/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "tablip",      #这里是pip项目发布的名称
+    version = "1.0.6",  #版本号，数值大的会优先被pip
+    keywords = ("pip", "tablip","featureextraction"),
+    description='export data to file such as excel etc..',
+    long_description=(
+            open('README.md',encoding='utf-8').read()
+    ),
+    license = "MIT Licence",
+
+    url = "https://github.com/bn0901/tablip",     #项目相关文件地址，一般是github
+    author = "bn.zheng",
+    author_email = "zhengbingxian666@163.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+
+    install_requires=[
+        'xlrd',
+        'xlwt',
+    ]
+)
