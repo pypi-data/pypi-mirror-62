f19c1767ec537e9562f8a0d071abc9f94473e3fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:50 +0000

    Add package system-info/2.0.0/system_info-2.0.0.tar.gz

---
+++ packages/system-info/system_info-2.0.0.tar.gz/system_info-2.0.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="system_info",
+    version="2.0.0",
+    author="Suhanna CH",
+    author_email="suhanna52@gmail.com",
+    description="Package for finding system hardware specifications",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/suhanna/System_info",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['py-cpuinfo', 'psutil'],
+)
