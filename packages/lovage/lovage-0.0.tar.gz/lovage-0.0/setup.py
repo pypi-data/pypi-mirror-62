0f2570952509943bd0de7bb909984d7dda7d67b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:33 +0000

    Add package lovage/0.0/lovage-0.0.tar.gz

---
+++ packages/lovage/lovage-0.0.tar.gz/lovage-0.0/setup.py
@@ -0,0 +1,3 @@
+from distutils.core import setup
+setup(name="lovage", version="0.0",classifiers=['Development Status :: 1 - Planning'],    author='CloudSnorkel',
+    author_email='amir@cloudsnorkel.com',)
