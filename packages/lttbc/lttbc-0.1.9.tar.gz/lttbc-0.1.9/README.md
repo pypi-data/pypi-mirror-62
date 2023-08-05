6445c57fd1bff7037a79c24588466925f43b6fef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:48 +0000

    Add package lttbc/0.1.9/lttbc-0.1.9.tar.gz

---
+++ packages/lttbc/lttbc-0.1.9.tar.gz/lttbc-0.1.9/README.md
@@ -0,0 +1,4 @@
+# lttbc: Largest-Triangle-Three-Buckets (Python using a c implementation)
+This is a low-level implementation of the `Largest-Triangle-Three-Buckets` (LTTB) downsampling algorithm written in Python.
+
+The code has been translated from the work of Sveinn Steinarsson (https://github.com/sveinn-steinarsson/flot-downsample/).
---
+++ packages/lttbc/lttbc-0.1.9.tar.gz/lttbc-0.1.9/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import os.path as osp
+import sys
+
+from setuptools import setup, Extension
+
+import numpy
+
+
+def get_script_path():
+    return osp.dirname(osp.realpath(sys.argv[0]))
+
+
+def read(*parts):
+    return open(osp.join(get_script_path(), *parts)).read()
+
+
+lttbc_py = Extension('lttbc', sources=['lttbc.c'],
+                     define_macros=[
+                         ('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')],
+                     include_dirs=[numpy.get_include(),
+                                   get_script_path()],
+                     )
+
+setup(
+    name="lttbc",
+    author="European XFEL GmbH",
+    version="0.1.9",
+    include_dirs=[numpy.get_include(), get_script_path()],
+    ext_modules=[lttbc_py],
+    author_email="dennis.goeries@xfel.eu",
+    maintainer="Dennis Goeries",
+    url="https://github.com/dgoeries/lttbc/",
+    description="Largest triangle three buckets module for Python written in C",
+    license="MIT",
+    install_requires=[
+        'numpy'],
+    python_requires=">=3.5"
+)
