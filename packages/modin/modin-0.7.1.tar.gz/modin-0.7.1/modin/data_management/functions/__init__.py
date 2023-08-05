73c445310828fbefd7dbf193730d9707df3e19f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:53 +0000

    Add package modin/0.7.1/modin-0.7.1.tar.gz

---
+++ packages/modin/modin-0.7.1.tar.gz/modin-0.7.1/modin/data_management/functions/__init__.py
@@ -0,0 +1,6 @@
+from .mapfunction import MapFunction
+from .mapreducefunction import MapReduceFunction
+from .reductionfunction import ReductionFunction
+from .foldfunction import FoldFunction
+
+__all__ = ["MapFunction", "MapReduceFunction", "ReductionFunction", "FoldFunction"]
