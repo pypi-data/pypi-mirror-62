a785b39df4d9631f51e8ffe2220ca0fbf0e5e589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:51 +0000

    Add package ai-graphics-gpu/0.1.15/ai_graphics_gpu-0.1.15-py3-none-any.whl

---
+++ packages/ai-graphics-gpu/ai_graphics_gpu-0.1.15-py3-none-any.whl/graphics/__init__.py
@@ -0,0 +1,12 @@
+from .app import Graphics, VideoProcess, FontWriter, InsightFace, FaceDetector, FaceRecognition
+from .app import ImageCrop, Classifier, train_test_split
+
+__all__ = ['Graphics',
+           'VideoProcess',
+           'FontWriter',
+           'InsightFace',
+           'FaceDetector',
+           'FaceRecognition',
+           'ImageCrop',
+           'Classifier',
+           'train_test_split']
---
+++ packages/ai-graphics-gpu/ai_graphics_gpu-0.1.15-py3-none-any.whl/graphics/app/__init__.py
@@ -0,0 +1,5 @@
+from .InsightFace import InsightFace
+from .FaceDetector import FaceDetector
+from .FaceRecognition import FaceRecognition
+from .ImageCrop import ImageCrop
+from .Classifier import Classifier, train_test_split
