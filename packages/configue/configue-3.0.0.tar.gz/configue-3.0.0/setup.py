6857382aba54880d817489009fbf6f9a193bc82a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:14 +0000

    Add package configue/3.0.0/configue-3.0.0.tar.gz

---
+++ packages/configue/configue-3.0.0.tar.gz/configue-3.0.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+setuptools.setup(
+    name="configue",
+    version="3.0.0",
+    url="https://github.com/illuin-tech/configue/",
+
+    author="Illuin Technology",
+    author_email="contact@illuin.tech",
+
+    description="Helpers to load config files, based on logging.config",
+
+    zip_safe=False,
+    platforms="any",
+
+    install_requires=[
+        "pyyaml>=5.1.0,<6.0.0",
+    ],
+    python_requires=">=3.6,<4.0",
+    packages=setuptools.find_packages(include=["configue", "configue.*"]),
+
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
