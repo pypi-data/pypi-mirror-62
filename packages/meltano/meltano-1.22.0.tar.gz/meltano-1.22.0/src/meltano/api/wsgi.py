2400aab8bc8f478aa453f7b8e4f0be5012acf2a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:45 +0000

    Add package meltano/1.22.0/meltano-1.22.0.tar.gz

---
+++ packages/meltano/meltano-1.22.0.tar.gz/meltano-1.22.0/src/meltano/api/wsgi.py
@@ -0,0 +1,16 @@
+import os
+import warnings
+import meltano
+import importlib
+
+# disable the psycopg2 warning
+# this needs to run before `psycopg2` is imported
+warnings.filterwarnings("ignore", category=UserWarning, module="psycopg2")
+
+workers = int(os.getenv("WORKERS", 4))
+timeout = 600
+
+
+def on_reload(arbiter):
+    importlib.reload(meltano)
+    print(f"Meltano version reloaded to {meltano.__version__}")
