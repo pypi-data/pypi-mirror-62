2f2c2f2d2a46985556555c4b35bb196e5bf964a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:33 +0000

    Add package openlibrary-client/0.0.20/openlibrary-client-0.0.20.tar.gz

---
+++ packages/openlibrary-client/openlibrary-client-0.0.20.tar.gz/openlibrary-client-0.0.20/olclient/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    __init__.py
+    ~~~~~~~~~~~
+
+    :copyright: (c) 2016 by Internet Archive.
+    :license: see LICENSE for more details.
+"""
+
+__title__ = 'olclient'
+__version__ = '0.0.20'
+__author__ = 'Internet Archive'
+
+
+from .openlibrary import OpenLibrary
+from .marc import MARC
+from .common import Book, Author
