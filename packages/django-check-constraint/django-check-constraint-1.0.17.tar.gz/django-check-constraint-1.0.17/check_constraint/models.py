6274cb1f744d059e5fd5601b55423baada08a3c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:47 +0000

    Add package django-check-constraint/1.0.17/django-check-constraint-1.0.17.tar.gz

---
+++ packages/django-check-constraint/django-check-constraint-1.0.17.tar.gz/django-check-constraint-1.0.17/check_constraint/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+from django.db.models.sql import Query
+
+__all__ = ["AnnotatedCheckConstraint"]
+
+
+class AnnotatedCheckConstraint(models.CheckConstraint):
+    def __init__(self, *args, annotations=None, **kwargs):
+        self.annotations = annotations or {}
+        super(AnnotatedCheckConstraint, self).__init__(*args, **kwargs)
+
+    def _get_check_sql(self, model, schema_editor):
+        query = Query(model=model)
+
+        # Add annotations
+        for k, v in self.annotations.items():
+            query.add_annotation(v, k)
+
+        where = query.build_where(self.check)
+
+        compiler = query.get_compiler(connection=schema_editor.connection)
+
+        sql, params = where.as_sql(compiler, schema_editor.connection)
+
+        return sql % tuple(schema_editor.quote_value(p) for p in params)
+
+    def deconstruct(self):
+        path, args, kwargs = super(models.CheckConstraint, self).deconstruct()
+        kwargs["check"] = self.check
+        kwargs["annotations"] = self.annotations
+
+        return path, args, kwargs
