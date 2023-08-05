e01e5f506f6f9fea38c3c2cb8739eeb1cd28226a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:10 +0000

    Add package sphinx-typo3-theme/4.1.2/sphinx_typo3_theme-4.1.2-py2.py3-none-any.whl

---
+++ packages/sphinx-typo3-theme/sphinx_typo3_theme-4.1.2-py2.py3-none-any.whl/sphinx_typo3_theme/static/_version_info_GENERATED.py
@@ -0,0 +1,10 @@
+version_info = (
+{
+  "build_mtime": "1582622448",
+  "module_name": "sphinx_typo3_theme",
+  "version_scm": "4.1.2",
+  "version_scm_build": "",
+  "version_scm_core": "4.1.2",
+  "version_scm_pre_release": ""
+}
+)
