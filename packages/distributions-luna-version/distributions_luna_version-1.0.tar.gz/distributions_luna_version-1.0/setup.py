d2e21190e857d574a53de8192557b609acb929b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:40:08 +0000

    Add package distributions-luna-version/1.0/distributions_luna_version-1.0.tar.gz

---
+++ packages/distributions-luna-version/distributions_luna_version-1.0.tar.gz/distributions_luna_version-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_luna_version',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_luna_version'],
+      author='Yuemin Li',
+      author_email = 'cloverlym@gmail.com',
+      zip_safe=False)
