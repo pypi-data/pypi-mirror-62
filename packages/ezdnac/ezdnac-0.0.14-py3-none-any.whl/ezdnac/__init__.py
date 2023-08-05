766b5055e897fee8748dfe4b49e0a46b1d9fe75f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:59 +0000

    Add package ezdnac/0.0.14/ezdnac-0.0.14-py3-none-any.whl

---
+++ packages/ezdnac/ezdnac-0.0.14-py3-none-any.whl/ezdnac/__init__.py
@@ -0,0 +1,13 @@
+from ezdnac.functions import *
+
+
+class ezDNACError(Exception):
+    pass
+
+
+
+#####################################################
+## Written by Johan Lahti, CCIE60702                #
+## https://github.com/johan-lahti                   #
+## shiproute.net                                    #
+#####################################################
