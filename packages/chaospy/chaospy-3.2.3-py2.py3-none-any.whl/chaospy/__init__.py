f9abc8f94fc7ad0a862b3bb134b7b882642d70bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:30 +0000

    Add package chaospy/3.2.3/chaospy-3.2.3-py2.py3-none-any.whl

---
+++ packages/chaospy/chaospy-3.2.3-py2.py3-none-any.whl/chaospy/__init__.py
@@ -0,0 +1,42 @@
+"""
+Uncertainty Quantification Toolbox
+==================================
+
+This module contains tools for performing uncertainty quantification of models.
+"""
+import logging
+import os
+import pkg_resources
+
+import chaospy.bertran
+import chaospy.chol
+import chaospy.descriptives
+import chaospy.distributions
+import chaospy.orthogonal
+import chaospy.poly
+import chaospy.spectral
+import chaospy.quadrature
+import chaospy.saltelli
+import chaospy.regression
+
+from chaospy.distributions import *
+from chaospy.orthogonal import *
+from chaospy.poly import *
+from chaospy.spectral import *
+from chaospy.quadrature import *
+from chaospy.saltelli import *
+from chaospy.descriptives import *
+from chaospy.regression import *
+from chaospy.external import *
+
+LOGPATH = os.environ.get("CHAOSPY_LOGPATH", os.devnull)
+logging.basicConfig(level=logging.DEBUG, filename=LOGPATH, filemode="w")
+streamer = logging.StreamHandler()
+streamer.setLevel(logging.DEBUG)
+logger = logging.getLogger(__name__)
+logger.addHandler(streamer)
+
+try:
+    __version__ = pkg_resources.get_distribution("chaospy").version
+except pkg_resources.DistributionNotFound:
+    pass
