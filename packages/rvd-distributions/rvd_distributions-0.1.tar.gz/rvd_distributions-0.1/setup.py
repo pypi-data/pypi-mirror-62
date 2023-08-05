522aab563fff5dec8821f284746a008ac65fe15a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:44 +0000

    Add package rvd-distributions/0.1/rvd_distributions-0.1.tar.gz

---
+++ packages/rvd-distributions/rvd_distributions-0.1.tar.gz/rvd_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='rvd_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['rvd_distributions'],
+      author='Renan Vieira Dias',
+      author_email='renanvieiradias@gmail.com',
+      zip_safe=False)
