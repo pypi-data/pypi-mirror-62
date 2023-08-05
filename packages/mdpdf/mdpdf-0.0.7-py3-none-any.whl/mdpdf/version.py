566a9cf5a50f1b3f2e1eff8617255466cee2acd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:19 +0000

    Add package mdpdf/0.0.7/mdpdf-0.0.7-py3-none-any.whl

---
+++ packages/mdpdf/mdpdf-0.0.7-py3-none-any.whl/mdpdf/version.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This module contains project version information.
+
+.. currentmodule:: mdpdf.version
+.. moduleauthor:: Norman Lorrain <normanlorrain@gmail.com>
+"""
+
+__version__ = "0.0.7"  #: the working version
