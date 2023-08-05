ae210c3b020c2c4aa1a470527e0e51f522917550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:02 +0000

    Add package gawseed-threat-feed-tools/0.9.92/gawseed_threat_feed_tools-0.9.92-py3-none-any.whl

---
+++ packages/gawseed-threat-feed-tools/gawseed_threat_feed_tools-0.9.92-py3-none-any.whl/gawseed/threatfeed/events/__init__.py
@@ -0,0 +1,47 @@
+import sys
+
+from gawseed.threatfeed.config import Config
+
+class EventStream(Config):
+    def __init__(self, conf):
+        super().__init__(conf)
+        stream = self.config('stream')
+        if type(stream) == str:
+            self._stream_pattern = stream
+        else:
+            self._stream_pattern = None
+            if stream == None:
+                self._stream = sys.stdout
+            else:
+                self._stream = stream
+
+        self._output_type = "w"
+
+    def new_output(self, count):
+        if self._stream_pattern:
+            filename = self._stream_pattern % (count)
+            self._stream = open(filename, self._output_type)
+
+    def maybe_close_output(self):
+        if self._stream_pattern:
+            self._stream.close()
+
+    def output(self, something):
+        self._stream.write(something)
+
+    def maybe_convert(self, row):
+        new_row = {}
+        for key in row:
+            value = row[key]
+            if type(key) == bytes:
+                key = key.decode()
+            if type(value) == bytes:
+                new_row[key] = value.decode()
+            else:
+                new_row[key] = value
+        return new_row
+
+    def write(self, count, row, match, enrichments):
+        row = self.maybe_convert(row)
+        self.write_row(count, row, match, enrichments)
+        self.verbose("created output for event %d" % (count,))
