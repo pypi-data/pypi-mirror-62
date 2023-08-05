2300757f136fc3722b8573a74488fa49b85a1729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:02 +0000

    Add package pygorpho/0.6.0/pygorpho-0.6.0-cp35-cp35m-manylinux1_x86_64.whl

---
+++ packages/pygorpho/pygorpho-0.6.0-cp35-cp35m-manylinux1_x86_64.whl/pygorpho/constants.py
@@ -0,0 +1,17 @@
+"""One stop shop for constants."""
+
+from . import _thin
+
+#: Dilation
+DILATE = _thin.DILATE
+#: Erosion
+ERODE = _thin.ERODE
+
+#: Inside
+INSIDE = 0
+
+#: Best
+BEST = 1
+
+#: Outside
+OUTSIDE = 2
