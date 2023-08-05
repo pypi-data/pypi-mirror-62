8f0551757a595dc72b412e333824205f8e9e3afa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:04 +0000

    Add package hiss-repl/3.2.0/hiss_repl-3.2.0-py3-none-any.whl

---
+++ packages/hiss-repl/hiss_repl-3.2.0-py3-none-any.whl/hiss/cli/config.py
@@ -0,0 +1,106 @@
+import configparser
+import io
+import os
+
+from typing import Any, Dict, Union, Tuple, Optional  # noqa
+
+try:
+    from pathlib import Path
+except ImportError:
+    from pathlib2 import Path  # type: ignore
+
+from IPython.terminal.prompts import ClassicPrompts  # type: ignore
+
+from traitlets.config import Config as Config  # type: ignore
+from pygments.styles import get_style_by_name  # type: ignore
+from pygments.util import ClassNotFound  # type: ignore
+
+assert Path  # to shush flake8
+
+
+def _bool(value):
+    if not isinstance(value, bool):
+        return str(value).lower() in ("true", "t", "1")
+    return value
+
+
+def load_rc(path):
+    # type: (Path) -> Dict[str, str]
+    """This function loads the .hiss file represented as a dict"""
+    config = io.BytesIO()
+    config.write(b"[hiss]\n")
+
+    # attempt load of rc file
+    try:
+        with open(str(path)) as f:
+            config.write(f.read().replace("%", "%%").encode("utf8"))
+    except IOError:
+        # no rc file found
+        pass
+
+    config.seek(0, os.SEEK_SET)
+    parser = configparser.SafeConfigParser()
+    parser.read_string(config.read().decode())
+
+    return {key: str(value) for key, value in parser.items("hiss")}
+
+
+def get_theme(rc):
+    # type: (Dict[str, str]) -> Tuple[str, Optional[Any]]
+    pygments_style = None
+    theme = rc.get("theme", "legacy")
+
+    if theme != "legacy":
+        try:
+            pygments_style = get_style_by_name(theme)
+        except ClassNotFound:
+            theme = "legacy"
+
+    return theme, pygments_style
+
+
+def build_config(rc, banner):
+    # type: (Dict[str, str], str) -> Config
+
+    rc = rc
+
+    # initialize traitlets config obj
+    config = Config()
+
+    # hardcoded configs
+    config.TerminalInteractiveShell.prompts_class = ClassicPrompts
+    config.TerminalInteractiveShell.separate_in = ""
+    config.TerminalInteractiveShell.true_color = True
+    config.TerminalInteractiveShell.banner1 = banner
+    config.PrefilterManager.multi_line_specials = True
+
+    # overrideable configs
+    config.TerminalInteractiveShell.confirm_exit = _bool(rc.get("confirm_exit", False))
+    config.TerminalInteractiveShell.autoindent = _bool(rc.get("autoindent", True))
+    config.TerminalInteractiveShell.colors = rc.get("colors", "Linux")
+    config.TerminalInteractiveShell.editing_mode = rc.get("editing_mode", "emacs")
+
+    # auto reload
+    if _bool(rc.get("autoreload", False)):
+        config.InteractiveShellApp.extensions = ["autoreload"]
+        config.InteractiveShellApp.exec_lines = ["%autoreload 2"]
+
+    # themes
+    theme, pygments_style = get_theme(rc)
+    config.TerminalInteractiveShell.highlighting_style = theme
+    if pygments_style is not None:
+        config.TerminalInteractiveShell.highlighting_style_overrides = (
+            pygments_style.styles
+        )
+
+    # ensure magics will be loaded
+    config.InteractiveShellApp.exec_lines.extend(
+        [
+            "from hiss.cli.magic import HissMagics",
+            "get_ipython().register_magics(HissMagics)",
+            "import ipdb, sys",
+            "sys.breakpointhook = ipdb.set_trace",
+        ]
+    )
+
+    return config
---
+++ packages/hiss-repl/hiss_repl-3.2.0-py3-none-any.whl/hiss/cli/magic.py
@@ -0,0 +1,92 @@
+import json
+import os
+import site
+import sys
+import zipfile
+
+from imp import reload
+
+try:
+    from pathlib import Path
+except ImportError:
+    from pathlib2 import Path  # type: ignore
+
+from IPython.core.magic import Magics, magics_class, line_magic  # type: ignore
+from IPython import start_ipython  # type: ignore
+
+
+@magics_class
+class HissMagics(Magics):
+    @line_magic
+    def shiv(self, shiv_path):
+        if shiv_path:
+            sys.path.insert(0, shiv_path)
+            from _bootstrap import cache_path  # type: ignore
+
+            zf = zipfile.ZipFile(shiv_path)
+            env = json.loads(zf.read("environment.json"))
+            site.addsitedir(
+                cache_path(zf, Path.home() / ".shiv", env["build_id"]) / "site-packages"
+            )
+            print("Bootstrapped into shiv {shiv_path}".format(shiv_path=shiv_path))
+        else:
+            print("No pyz provided! Doing nothing.")
+
+    @line_magic
+    def pex(self, entry_point):
+        if entry_point:
+            sys.path[0] = os.path.abspath(sys.path[0])
+            sys.path.insert(0, entry_point)
+            sys.path.insert(0, os.path.abspath(os.path.join(entry_point, ".bootstrap")))
+            from _pex import pex_bootstrapper  # type: ignore
+
+            pex_bootstrapper.bootstrap_pex_env(entry_point)
+            self.reload_pkg_resources("")
+            print("Bootstrapped into pex {0}.".format(entry_point))
+        else:
+            print("No pex provided! Doing nothing.")
+
+    @line_magic
+    def reload_pkg_resources(self, line):
+        if "pkg_resources" in sys.modules:
+            import pkg_resources
+
+            reload(pkg_resources)
+
+    @line_magic
+    def debug(self, line):
+        import logging
+
+        logging.basicConfig(level=logging.DEBUG)
+        log = logging.getLogger(__name__)  # noqa: F841
+        print("Debug logging enabled.")
+
+    @line_magic
+    def flask(self, app):
+        """This is hella sketchy"""
+        try:
+            # we assume we have access to flask/werkzeug,
+            # if not, go ahead and raise the importerror
+            from flask import _request_ctx_stack  # type: ignore
+            from werkzeug import import_string  # type: ignore
+        except ImportError as e:
+            raise ImportError("You don't have flask/werkzeug! {err}".format(err=e))
+        else:
+            app = import_string(app)
+            with app.test_request_context():
+                banner = (
+                    "\nNow using flask context from: {app_repr}\n"
+                    "There is no escape! Exit the session when you are done.\n".format(
+                        app_repr=repr(app)
+                    )
+                )
+
+                # add our custom message
+                get_ipython().banner1 = banner  # noqa: F821
+
+                # starting a new session dorks up history, so end our sesh
+                get_ipython().history_manager.end_session()  # noqa: F821
+                get_ipython().history_manager.reset()  # noqa: F821
+
+                # respawn ipython with the flask context
+                start_ipython(user_ns=dict(app=_request_ctx_stack.top.app))
