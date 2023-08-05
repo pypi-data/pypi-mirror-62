498b4de516a7026950cd974e931c8055ad3f5d1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:21 +0000

    Add package async-request/0.181/async-request-0.181.tar.gz

---
+++ packages/async-request/async-request-0.181.tar.gz/async-request-0.181/async_request/spider.py
@@ -0,0 +1,25 @@
+from async_request.crawler import Crawler
+from async_request.request import Request
+
+
+class AsyncSpider(object):
+
+    start_urls = None
+
+    def __init__(self, **kwargs):
+        kwargs.setdefault('result_back', self.process_result)
+        self.crawler = Crawler(**kwargs)
+
+    def start_request(self):
+        for url in self.start_urls or []:
+            self.crawler.add_request(Request(url, callback=self.parse))
+
+    def parse(self, response):
+        pass
+
+    def process_result(self, result):
+        pass
+
+    def run(self, close_after_crawled=True):
+        self.start_request()
+        self.crawler.run(close_after_crawled)
