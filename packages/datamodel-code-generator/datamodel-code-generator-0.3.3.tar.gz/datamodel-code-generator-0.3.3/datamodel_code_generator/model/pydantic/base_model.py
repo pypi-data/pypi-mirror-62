d7feebefe9ac82754a76b40f8842997d95874a9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:56 +0000

    Add package datamodel-code-generator/0.3.3/datamodel-code-generator-0.3.3.tar.gz

---
+++ packages/datamodel-code-generator/datamodel-code-generator-0.3.3.tar.gz/datamodel-code-generator-0.3.3/datamodel_code_generator/model/pydantic/base_model.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+from typing import Any, DefaultDict, List, Optional
+
+from datamodel_code_generator.imports import Import
+from datamodel_code_generator.model import DataModel, DataModelField
+from datamodel_code_generator.model.pydantic.types import get_data_type
+from datamodel_code_generator.types import DataType, Types
+from pydantic import BaseModel as _BaseModel
+
+
+class Config(_BaseModel):
+    extra: Optional[str] = None
+
+
+class BaseModel(DataModel):
+    TEMPLATE_FILE_PATH = 'pydantic/BaseModel.jinja2'
+    BASE_CLASS = 'pydantic.BaseModel'
+
+    def __init__(
+        self,
+        name: str,
+        fields: List[DataModelField],
+        decorators: Optional[List[str]] = None,
+        base_classes: Optional[List[str]] = None,
+        custom_base_class: Optional[str] = None,
+        custom_template_dir: Optional[Path] = None,
+        extra_template_data: Optional[DefaultDict[str, Any]] = None,
+        auto_import: bool = True,
+        reference_classes: Optional[List[str]] = None,
+        imports: Optional[List[Import]] = None,
+    ):
+
+        super().__init__(
+            name=name,
+            fields=fields,
+            decorators=decorators,
+            base_classes=base_classes,
+            custom_base_class=custom_base_class,
+            custom_template_dir=custom_template_dir,
+            extra_template_data=extra_template_data,
+            auto_import=auto_import,
+            reference_classes=reference_classes,
+            imports=imports,
+        )
+
+        if 'additionalProperties' in self.extra_template_data:
+            self.extra_template_data['config'] = Config(extra='Extra.allow')
+            self.imports.append(Import(from_='pydantic', import_='Extra'))
+
+    @classmethod
+    def get_data_type(cls, types: Types, **kwargs: Any) -> DataType:
+        return get_data_type(types, **kwargs)
---
+++ packages/datamodel-code-generator/datamodel-code-generator-0.3.3.tar.gz/datamodel-code-generator-0.3.3/tests/model/pydantic/test_base_model.py
@@ -0,0 +1,57 @@
+from datamodel_code_generator.model import DataModelField
+from datamodel_code_generator.model.pydantic.base_model import BaseModel
+from datamodel_code_generator.types import DataType, Types
+
+
+def test_base_model():
+    field = DataModelField(name='a', data_types=[DataType(type='str')], required=True)
+
+    base_model = BaseModel(name='test_model', fields=[field])
+
+    assert base_model.name == 'test_model'
+    assert base_model.fields == [field]
+    assert base_model.decorators == []
+    assert base_model.render() == 'class test_model(BaseModel):\n' '    a: str'
+
+
+def test_base_model_optional():
+    field = DataModelField(
+        name='a', data_types=[DataType(type='str')], default="'abc'", required=False
+    )
+
+    base_model = BaseModel(name='test_model', fields=[field])
+
+    assert base_model.name == 'test_model'
+    assert base_model.fields == [field]
+    assert base_model.decorators == []
+    assert (
+        base_model.render() == 'class test_model(BaseModel):\n'
+        '    a: Optional[str] = \'abc\''
+    )
+
+
+def test_base_model_decorator():
+    field = DataModelField(
+        name='a', data_types=[DataType(type='str')], default="'abc'", required=False
+    )
+
+    base_model = BaseModel(
+        name='test_model',
+        fields=[field],
+        decorators=['@validate'],
+        base_classes=['Base'],
+    )
+
+    assert base_model.name == 'test_model'
+    assert base_model.fields == [field]
+    assert base_model.base_class == 'Base'
+    assert base_model.decorators == ['@validate']
+    assert (
+        base_model.render() == '@validate\n'
+        'class test_model(Base):\n'
+        '    a: Optional[str] = \'abc\''
+    )
+
+
+def test_base_model_get_data_type():
+    assert BaseModel.get_data_type(Types.integer) == DataType(type='int')
