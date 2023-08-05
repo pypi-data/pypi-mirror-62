7a897bdbd25ad23ae81985fa0a79992c737e10f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:57 +0000

    Add package nester20200226/1.1.1/nester20200226-1.1.1.tar.gz

---
+++ packages/nester20200226/nester20200226-1.1.1.tar.gz/nester20200226-1.1.1/nester20200226.py
@@ -0,0 +1,8 @@
+def print_list(the_list,level=0):
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_list(each_item,level+1)
+        else:
+            for num in range(level):
+                print("\t",end='')
+            print(each_item)
