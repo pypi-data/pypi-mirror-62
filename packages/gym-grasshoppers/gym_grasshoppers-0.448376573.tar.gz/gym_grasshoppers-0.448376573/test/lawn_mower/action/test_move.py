7ded90370ee6308fd5effb76a09092a38afd3600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:32 +0000

    Add package gym-grasshoppers/0.448376573/gym_grasshoppers-0.448376573.tar.gz

---
+++ packages/gym-grasshoppers/gym_grasshoppers-0.448376573.tar.gz/gym_grasshoppers-0.448376573/test/lawn_mower/action/test_move.py
@@ -0,0 +1,34 @@
+import os
+from unittest import TestCase
+from gym_grasshoppers.exception.invalid_action_exception import InvalidActionException
+from gym_grasshoppers.garden.garden import Garden
+from gym_grasshoppers.lawn_mower.action.move import Move
+from gym_grasshoppers.lawn_mower.lawn_mower import LawnMower
+
+TEST_DIR = os.path.join(os.path.join(os.path.dirname(__file__), '../..'))
+
+
+class TestMove(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        valid_geojson_json = open(os.path.join(TEST_DIR, 'resources/valid_geojson.json'))
+        valid_geojson_string = valid_geojson_json.read()
+        valid_geojson_json.close()
+        cls._garden = Garden(0.3, valid_geojson_string)
+        cls._lawn_mower = LawnMower(cls._garden.starting_point.x, cls._garden.starting_point.y, 0., 0.1, 0.5, 50., True)
+
+    def test_missing_distance(self):
+        with self.assertRaises(InvalidActionException):
+            Move(self._lawn_mower, None, self._garden)
+
+    def test_negative_distance(self):
+        with self.assertRaises(InvalidActionException):
+            Move(self._lawn_mower, -1., self._garden)
+
+    # TODO
+    def test_execute(self):
+        pass
+
+    # TODO
+    def test_calculate_new_position(self):
+        pass
