99b9beb4a79566ee8896c403c757b652bb11d637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:01 +0000

    Add package izza/0.1.0/izza-0.1.0-py3-none-any.whl

---
+++ packages/izza/izza-0.1.0-py3-none-any.whl/izza/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+name = "izza"
+
+from . import models
+from . import exploration
+from . import feature_selection
+
+# TODO: add version information here
+
+__all__ = ["models", "exploration", 'feature_selection']
