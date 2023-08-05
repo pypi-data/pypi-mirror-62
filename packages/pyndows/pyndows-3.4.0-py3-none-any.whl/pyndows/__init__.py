21b5d4577b1c1742b73f97de313264924e95cd37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:23 +0000

    Add package pyndows/3.4.0/pyndows-3.4.0-py3-none-any.whl

---
+++ packages/pyndows/pyndows-3.4.0-py3-none-any.whl/pyndows/__init__.py
@@ -0,0 +1,11 @@
+from pyndows._windows import (
+    connect,
+    get,
+    move,
+    rename,
+    get_file_desc,
+    check,
+    get_folder_content,
+)
+
+from pyndows.version import __version__
