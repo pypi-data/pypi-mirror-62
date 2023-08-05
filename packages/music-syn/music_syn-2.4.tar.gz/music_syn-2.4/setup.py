3e8ff29f09861080e186e8995836812cd3578561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:02 +0000

    Add package music-syn/2.4/music_syn-2.4.tar.gz

---
+++ packages/music-syn/music_syn-2.4.tar.gz/music_syn-2.4/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name="music_syn",
+      version="2.4",
+      description="A simple music synthesizer for Python 3",
+      author="Martin C. Doege",
+      author_email="mdoege@compuserve.com",
+      url="http://mdoege.github.io/PySynth/",
+      packages=["music_syn"],
+      install_requires=["numpy", "pyglet"])
