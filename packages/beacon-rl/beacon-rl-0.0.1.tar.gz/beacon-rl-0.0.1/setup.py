2f9798d2786a819a82fe0845e68adbbb3d83e1e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:04 +0000

    Add package beacon-rl/0.0.1/beacon-rl-0.0.1.tar.gz

---
+++ packages/beacon-rl/beacon-rl-0.0.1.tar.gz/beacon-rl-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="beacon-rl",
+    version="0.0.1",
+    description="",
+    long_description="",
+    url="https://www.example.com",
+    author="javier-m",
+    author_email="xavierm@fb.com",
+    packages=[],
+    classifiers=["Development Status :: 1 - Planning"],
+)
