955f8424edbb3dcc6eb87b908225a5d8b2c7145c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:41 +0000

    Add package frc1678-lime-plotter/0.6.0/frc1678-lime-plotter-0.6.0.tar.gz

---
+++ packages/frc1678-lime-plotter/frc1678-lime-plotter-0.6.0.tar.gz/frc1678-lime-plotter-0.6.0/setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="frc1678-lime-plotter",
+    version="0.6.0",
+    author="Wes Hardaker",
+    author_email="opensource@hardakers.net",
+    description="A matplotlib based plotter for FRC logs and networktables",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/frc1678/lime-plotter",
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': [
+            'lime-plotter.py = frc1678.limeplotter.main:main',
+            'lime-server.py = frc1678.limeplotter.networktablesserver:main',
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires = '>=3',
+    install_requires=[
+        'pynetworktables',
+        'numpy',
+        'pandas',
+        'pyyaml',
+        'matplotlib',
+        'svgpath2mpl',
+    ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    package_data = { 'frc1678.limeplotter.loader': ['svgs/2020map.svg',
+                                                    'svgs/2020map-rev.svg',
+                                                    'svgs/2020map-full.svg',
+                                                    'svgs/2019map.svg'] },
+)
