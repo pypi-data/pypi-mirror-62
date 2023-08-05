a708f1572e7371259d3db4db7e2796ec4bdb53a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:18 +0000

    Add package BiostarHandbook/0.0.1/BiostarHandbook-0.0.1.tar.gz

---
+++ packages/BiostarHandbook/BiostarHandbook-0.0.1.tar.gz/BiostarHandbook-0.0.1/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="BiostarHandbook",
+    version="0.0.1",
+    author="Istvan Albert",
+    author_email="istvan.albert@gmail.com",
+    description="Utilities for the Biostar Handbook ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/biostars/biostar-handbook-code",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    scripts=[
+        'scripts/foo.sh',
+        'scripts/r/foo.r',
+    ],
+    entry_points={
+        'console_scripts': [
+            'comm.py=handbook.comm:main',
+        ],
+    },
+
+    python_requires='>=3.6',
+)
