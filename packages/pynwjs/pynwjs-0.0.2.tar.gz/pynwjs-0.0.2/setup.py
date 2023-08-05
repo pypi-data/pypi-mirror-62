779a3c50bf8df3b58fe2cc946533ecf50dbd5c19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:52 +0000

    Add package pynwjs/0.0.2/pynwjs-0.0.2.tar.gz

---
+++ packages/pynwjs/pynwjs-0.0.2.tar.gz/pynwjs-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("Readme.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name = "pynwjs",
+    version = "0.0.2",
+    author = "Philipp Schillinger",
+    author_email = "schillin@kth.se",
+    description = "Use nwjs as GUI in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license = "BSD 3",
+    url = "https://github.com/pschillinger/pynwjs",
+    package_dir = {'': 'src'},
+    packages=['pynwjs'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+    ]
+)
