6c7502e58904504f6a1fbd00fb20cda52b5fc0e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:40 +0000

    Add package RoboSat.pink/0.0.2/RoboSat.pink-0.0.2.tar.gz

---
+++ packages/RoboSat.pink/RoboSat.pink-0.0.2.tar.gz/RoboSat.pink-0.0.2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+
+long_description = """
+
+Project moved to <a href="https://neat-EO.pink">https://neat-EO.pink</a>
+
+"""
+
+setup(
+    name="RoboSat.pink",
+    version="0.0.2",
+    url="https://neat-EO.pink",
+    download_url="https://datapink.io/datapink/neat-EO/releases",
+    license="MIT",
+    maintainer="DataPink",
+    maintainer_email="hello@datapink.com",
+    description="Computer Vision framework for GeoSpatial Imagery",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+        "Topic :: Scientific/Engineering :: GIS",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+    ],
+)
