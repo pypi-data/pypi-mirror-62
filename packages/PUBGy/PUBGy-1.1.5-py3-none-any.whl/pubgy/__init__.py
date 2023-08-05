f70de1adc372a906f14a1f2d77ac94d0b4fa18fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:33 +0000

    Add package PUBGy/1.1.5/PUBGy-1.1.5-py3-none-any.whl

---
+++ packages/PUBGy/PUBGy-1.1.5-py3-none-any.whl/pubgy/__init__.py
@@ -0,0 +1,11 @@
+__title__ = "PUBGy"
+__author__ = "Discordian"
+__license__ = "GNU"
+__version__ = "1.1.5"  # https://semver.org/
+
+
+from .client import *
+from .http import Query
+from .struct import Match, Player, Team, Filter
+from .constants import *
+#from .utils import filter
---
+++ packages/PUBGy/PUBGy-1.1.5-py3-none-any.whl/pubgy/constants.py
@@ -0,0 +1,11 @@
+SHARD_LIST = ["steam", "kakao", "tournament",  "psn", "xbox", "console"]
+PLATFORM_REGION = ["pc-as", "pc-eu", "pc-jp", "pc-kakao", "pc-krjp"]
+DEFAULT_SHARD = SHARD_LIST[0]
+BASE_URL = "https://api.pubg.com/shards/"
+DEBUG_URL = "https://api.pubg.com/status"
+MATCHES_ROUTE = "matches"
+PLAYERNAME_ROUTE = "players?filter[playerNames]="
+PLAYERID_ROUTE = "players"
+PLAYERIDLIST = "players?filter[playerIds]="
+SAMPLE_ROUTE = "samples"
+MATCH_TYPES = ["arcade", "custom", "event", "official", "training"]
