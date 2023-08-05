709cfbab30caf3c2fa0b185fa53b6d51f03ef1a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:27 +0000

    Add package threefive/2.0.43/threefive-2.0.43.tar.gz

---
+++ packages/threefive/threefive-2.0.43.tar.gz/threefive-2.0.43/examples/Time_Signal-Placement_Opportunity_End.py
@@ -0,0 +1,13 @@
+"""
+Example from the specification
+
+14.3. Time_Signal–Placement_Opportunity_End
+
+"""
+
+from threefive import decode
+
+Base64 = "/DAvAAAAAAAA///wBQb+dGKQoAAZAhdDVUVJSAAAjn+fCAgAAAAALKChijUCAKnMZ1g="
+
+decode(Base64)
+
