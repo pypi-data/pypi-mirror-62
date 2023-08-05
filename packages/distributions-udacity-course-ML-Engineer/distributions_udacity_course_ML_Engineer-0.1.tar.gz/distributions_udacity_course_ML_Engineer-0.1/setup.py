6ca38bbaa1fefa48df98fd6e660ed690d16eb78e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:10 +0000

    Add package distributions-udacity-course-ML-Engineer/0.1/distributions_udacity_course_ML_Engineer-0.1.tar.gz

---
+++ packages/distributions-udacity-course-ML-Engineer/distributions_udacity_course_ML_Engineer-0.1.tar.gz/distributions_udacity_course_ML_Engineer-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_udacity_course_ML_Engineer',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_udacity_course_ML_Engineer'],
+      zip_safe=False)
