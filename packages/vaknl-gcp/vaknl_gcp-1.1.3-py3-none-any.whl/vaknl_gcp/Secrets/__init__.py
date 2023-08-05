e6e3475278f3824a60ace6314c568e3a7e287a82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:30 +0000

    Add package vaknl-gcp/1.1.3/vaknl_gcp-1.1.3-py3-none-any.whl

---
+++ packages/vaknl-gcp/vaknl_gcp-1.1.3-py3-none-any.whl/vaknl_gcp/Secrets/__init__.py
@@ -0,0 +1,16 @@
+__author__ = "Wytze Bruinsma"
+
+import json
+import os
+
+from google.cloud import secretmanager_v1beta1 as secretmanager
+from vaknl_gcp import project_id
+
+SECRET_CLIENT = secretmanager.SecretManagerServiceClient()
+
+
+def get_secret(secret_id):
+    version_id = 'latest'
+    name = SECRET_CLIENT.secret_version_path(project_id, secret_id, version_id)
+    response = SECRET_CLIENT.access_secret_version(name)
+    return json.loads(response.payload.data.decode('UTF-8'))
