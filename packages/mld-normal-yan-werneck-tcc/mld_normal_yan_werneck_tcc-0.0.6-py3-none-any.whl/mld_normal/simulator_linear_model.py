f18365fd66a6588eca8154954fe5d37d6b5112cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:56 +0000

    Add package mld-normal-yan-werneck-tcc/0.0.6/mld_normal_yan_werneck_tcc-0.0.6-py3-none-any.whl

---
+++ packages/mld-normal-yan-werneck-tcc/mld_normal_yan_werneck_tcc-0.0.6-py3-none-any.whl/mld_normal/simulator_linear_model.py
@@ -0,0 +1,18 @@
+import numpy as np
+from scipy import stats
+import pymc3
+
+
+def gerador_dados_dinamicos(m0, w_a, w_b, v_a, v_a, sample_size = 1000):
+    
+    w_sample = stats.invgamma.rvs(w_a, scale = w_b, size = 1)
+    V_sample = stats.invgamma.rvs(v_a, scale = v_a, size = 1)
+
+    u = np.zeros(sample_size)
+    y = np.zeros(sample_size)
+    u[0] = m0
+    for iteration in range(1,sample_size):
+        u[iteration] = u[iteration-1] + stats.norm.rvs(loc = 0, scale = np.sqrt(w_sample))
+        y[iteration] = u[iteration] + stats.norm.rvs(loc = 0, scale = np.sqrt(V_sample))
+    
+    return y, u
