0a93540b5a856fffce6d8729cf5a34848bc3cdb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:05 +0000

    Add package pywin-env-shift/0.1.0/pywin_env_shift-0.1.0.tar.gz

---
+++ packages/pywin-env-shift/pywin_env_shift-0.1.0.tar.gz/pywin_env_shift-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='pywin_env_shift',
+   version='0.1.0',
+   author='ps neo',
+   license='LICENSE.txt',
+   packages=['pywin_env_shift'],
+   description='A package to help deploy your venv',
+   install_requires=[],
+)
