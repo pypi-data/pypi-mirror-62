41c6df2db83ec9d0ef0d371b9491e53c9e90e028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:14 +0000

    Add package zignal/0.6.0/zignal-0.6.0-py3-none-any.whl

---
+++ packages/zignal/zignal-0.6.0-py3-none-any.whl/zignal/__init__.py
@@ -0,0 +1,23 @@
+"""
+This is the zignal library
+
+@author: Ronny Andersson (ronny@andersson.tk)
+@copyright: (c) 2013 Ronny Andersson
+@license: MIT
+"""
+
+__version__ = "0.6.0"
+
+from .audio import *
+from . import filters
+from . import measure
+from . import music
+from . import sndcard
+
+__all__ = [
+           'filters',
+           'measure',
+           'music',
+           'sndcard',
+           ]
+__all__.extend(audio.__all__)       #@UndefinedVariable
