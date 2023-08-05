d71739ba13e10b42b8ce6f95fec96680705ab679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:38 +0000

    Add package django-okta-auth/0.3.1/django-okta-auth-0.3.1.tar.gz

---
+++ packages/django-okta-auth/django-okta-auth-0.3.1.tar.gz/django-okta-auth-0.3.1/okta_oauth2/tests/test_middleware.py
@@ -0,0 +1,83 @@
+from unittest.mock import Mock, patch
+
+from django.http import HttpResponse
+from django.test import RequestFactory
+from django.urls import reverse
+from okta_oauth2.exceptions import TokenExpired
+from okta_oauth2.middleware import OktaMiddleware
+from okta_oauth2.tests.utils import build_token
+
+rf = RequestFactory()
+
+
+def test_no_token_redirects_to_login():
+    """
+    If there's no token in the session then we should be
+    redirecting to the login.
+    """
+    request = rf.get("/")
+    request.session = {}
+    mw = OktaMiddleware(Mock(return_value=HttpResponse()))
+    response = mw(request)
+    assert response.status_code == 302
+    assert response.url == reverse("okta_oauth2:login")
+
+
+def test_invalid_token_redirects_to_login():
+    """
+    It there's a token but it's invalid we should be
+    redirecting to the login.
+    """
+    request = rf.get("/")
+    request.COOKIES["okta-oauth-nonce"] = "123456"
+    request.session = {"tokens": {}}
+    mw = OktaMiddleware(Mock(return_value=HttpResponse()))
+    response = mw(request)
+    assert response.status_code == 302
+    assert response.url == reverse("okta_oauth2:login")
+
+
+def test_valid_token_returns_response():
+    """
+    If we have a valid token we should be returning the normal
+    response from the middleware.
+    """
+
+    nonce = "123456"
+    # We're building a token here that we know will be valid
+    token = build_token(nonce=nonce)
+
+    with patch(
+        "okta_oauth2.middleware.TokenValidator._jwks", Mock(return_value="secret")
+    ):
+        request = rf.get("/")
+        request.COOKIES["okta-oauth-nonce"] = nonce
+        request.session = {"tokens": {"id_token": token}}
+        mw = OktaMiddleware(Mock(return_value=HttpResponse()))
+        response = mw(request)
+        assert response.status_code == 200
+
+
+def test_token_expired_triggers_refresh():
+    """
+    Test that an expired token triggers
+    an attempt at refreshing the token.
+    """
+    raises_token_expired = Mock()
+    raises_token_expired.side_effect = TokenExpired
+
+    with patch("okta_oauth2.middleware.TokenValidator.validate_token"), patch(
+        "okta_oauth2.middleware.TokenValidator.tokens_from_refresh_token"
+    ):
+
+        request = rf.get("/")
+        request.COOKIES["okta-oauth-nonce"] = "123456"
+        request.session = {
+            "tokens": {
+                "id_token": "imanexpiredtoken",
+                "refresh_token": "imsorefreshing",
+            }
+        }
+        mw = OktaMiddleware(Mock(return_value=HttpResponse()))
+        response = mw(request)
+        assert response.status_code == 200
---
+++ packages/django-okta-auth/django-okta-auth-0.3.1.tar.gz/django-okta-auth-0.3.1/okta_oauth2/views.py
@@ -0,0 +1,75 @@
+import logging
+
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.http import HttpResponseBadRequest, HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from django.urls.exceptions import NoReverseMatch
+
+from .conf import Config
+
+logger = logging.getLogger(__name__)
+
+
+def login(request):
+    config = Config()
+
+    okta_config = {
+        "clientId": config.client_id,
+        "url": config.org_url,
+        "redirectUri": str(config.redirect_uri),
+        "scope": config.scopes,
+        "issuer": config.issuer,
+    }
+    response = render(request, "okta_oauth2/login.html", {"config": okta_config})
+
+    _delete_cookies(response)
+
+    return response
+
+
+def callback(request):
+    config = Config()
+
+    if request.POST:
+        return HttpResponseBadRequest("Method not supported")
+
+    code = request.GET["code"]
+    state = request.GET["state"]
+
+    # Get state and nonce from cookie
+    cookie_state = request.COOKIES["okta-oauth-state"]
+    cookie_nonce = request.COOKIES["okta-oauth-nonce"]
+
+    # Verify state
+    if state != cookie_state:
+        return HttpResponseBadRequest(
+            "Value {} does not match the assigned state".format(state)
+        )
+
+    user = authenticate(request, auth_code=code, nonce=cookie_nonce)
+
+    if user is None:
+        return redirect(reverse("okta_oauth2:login"))
+
+    auth_login(request, user)
+
+    try:
+        redirect_url = reverse(config.login_redirect_url)
+    except NoReverseMatch:
+        redirect_url = config.login_redirect_url
+
+    return redirect(redirect_url)
+
+
+def logout(request):
+    auth_logout(request)
+    return HttpResponseRedirect(reverse("okta_oauth2:login"))
+
+
+def _delete_cookies(response):
+    # The Okta Signin Widget/Javascript SDK aka "Auth-JS" automatically generates
+    # state and nonce and stores them in cookies. Delete authJS/widget cookies
+    response.set_cookie("okta-oauth-state", "", max_age=1)
+    response.set_cookie("okta-oauth-nonce", "", max_age=1)
+    response.set_cookie("okta-oauth-redirect-params", "", max_age=1)
