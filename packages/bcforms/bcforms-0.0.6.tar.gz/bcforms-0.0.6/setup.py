2b9732921363df5f4102ed3cd18eaf631421526e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:42 +0000

    Add package bcforms/0.0.6/bcforms-0.0.6.tar.gz

---
+++ packages/bcforms/bcforms-0.0.6.tar.gz/bcforms-0.0.6/setup.py
@@ -0,0 +1,54 @@
+import setuptools
+try:
+    import pkg_utils
+except ImportError:
+    import subprocess
+    import sys
+    subprocess.check_call(
+        [sys.executable, "-m", "pip", "install", "pkg_utils"])
+    import pkg_utils
+import os
+
+name = 'bcforms'
+dirname = os.path.dirname(__file__)
+package_data = {
+    name: [
+        'grammar.lark',
+        'web/app.css',
+        'web/app.js',
+        'web/index.html',
+    ],
+}
+
+# get package metadata
+md = pkg_utils.get_package_metadata(dirname, name, package_data_filename_patterns=package_data)
+
+# install package
+setuptools.setup(
+    name=name,
+    version=md.version,
+    description='Toolkit for concretely describing macromolecular complexes',
+    long_description=md.long_description,
+    url="https://github.com/KarrLab/" + name,
+    download_url='https://github.com/KarrLab/' + name,
+    author="Karr Lab",
+    author_email="info@karrlab.org",
+    license="MIT",
+    keywords='complex post-translational modification crosslinks systems biology',
+    packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
+    package_data=md.package_data,
+    install_requires=md.install_requires,
+    extras_require=md.extras_require,
+    tests_require=md.tests_require,
+    dependency_links=md.dependency_links,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+    ],
+    entry_points={
+        'console_scripts': [
+            'bcforms = bcforms.__main__:main',
+        ],
+    },
+)
