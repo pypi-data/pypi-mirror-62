5aab23b4a1f9a863a410e97e09192ef8f4d1023c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:17 +0000

    Add package dummy-test-derek/0.0.0/dummy_test_derek-0.0.0-py3-none-any.whl

---
+++ packages/dummy-test-derek/dummy_test_derek-0.0.0-py3-none-any.whl/dummy/dummy.py
@@ -0,0 +1,11 @@
+class Dummy:
+  def __init__(self):
+    pass
+  
+  def printSomething(self, arg):
+    """Prints something.
+    
+    :param arg: Dummy object
+    :type arg: string
+    """      
+    print("test")
