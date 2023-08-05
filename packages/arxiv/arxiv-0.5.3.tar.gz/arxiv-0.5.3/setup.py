7a29dd61d02cab93d5d677b4447b732bfac89230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:50 +0000

    Add package arxiv/0.5.3/arxiv-0.5.3.tar.gz

---
+++ packages/arxiv/arxiv-0.5.3.tar.gz/arxiv-0.5.3/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+version = "0.5.3"
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="arxiv",
+    version=version,
+    packages=["arxiv"],
+
+    # dependencies
+    install_requires=[
+        'feedparser',
+        'requests',
+    ],
+    tests_require=[
+        "pytest",
+        "numpy",
+    ],
+    # metadata for upload to PyPI
+    author="Lukas Schwab",
+    author_email="lukas.schwab@gmail.com",
+    description="Python wrapper for the arXiv API: http://arxiv.org/help/api/",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    keywords="arxiv api wrapper academic journals papers",
+    url="https://github.com/lukasschwab/arxiv.py",
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
