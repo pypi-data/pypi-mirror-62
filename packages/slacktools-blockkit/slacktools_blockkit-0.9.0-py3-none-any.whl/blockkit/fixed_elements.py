3da0e68350c0bd82e12d6ad271e20de7bbcfbc0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:11 +0000

    Add package slacktools-blockkit/0.9.0/slacktools_blockkit-0.9.0-py3-none-any.whl

---
+++ packages/slacktools-blockkit/slacktools_blockkit-0.9.0-py3-none-any.whl/blockkit/fixed_elements.py
@@ -0,0 +1,30 @@
+from .composition import ConfirmationDialog
+from .elements import Button
+
+__all__ = ("FixedButton",)
+
+
+class FixedButton(Button):
+    """
+    Facilitates the definition of `Button` elements using fixed, referencable, attributes.
+    """
+
+    action_id: str = None
+    text: str = None
+    value: str = None
+    url: str = None
+    confirm: ConfirmationDialog = None
+    style: Button.Styles = None
+
+    def __init__(self, **kwargs):
+        super().__init__(
+            **{
+                "action_id": self.action_id,
+                "text": self.text,
+                "value": self.value,
+                "url": self.url,
+                "confirm": self.confirm,
+                "style": self.style,
+                **kwargs,
+            }
+        )
