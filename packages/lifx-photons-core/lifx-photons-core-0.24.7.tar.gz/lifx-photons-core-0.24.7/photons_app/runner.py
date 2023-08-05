24d63e5df4ae020e5526ad01cbdd28d33426b724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:25 +0000

    Add package lifx-photons-core/0.24.7/lifx-photons-core-0.24.7.tar.gz

---
+++ packages/lifx-photons-core/lifx-photons-core-0.24.7.tar.gz/lifx-photons-core-0.24.7/photons_app/runner.py
@@ -0,0 +1,125 @@
+from photons_app.errors import ApplicationCancelled
+
+from functools import partial
+import platform
+import asyncio
+import logging
+import signal
+import sys
+
+log = logging.getLogger("photons_app.runner")
+
+
+def on_done_task(final_future, res):
+    if res.cancelled():
+        if not final_future.done():
+            final_future.cancel()
+        return
+
+    exc = res.exception()
+    if exc:
+        if not final_future.done():
+            final_future.set_exception(exc)
+        return
+
+    res.result()
+
+    if not final_future.done():
+        final_future.set_result(None)
+
+
+def run(coro, photons_app, target_register):
+    """
+    Get the loop, then use runner with cleanup in a finally block
+    """
+    loop = photons_app.loop
+    final_future = photons_app.final_future
+
+    if platform.system() != "Windows":
+
+        def stop_final_fut():
+            final_future.cancel()
+
+        loop.add_signal_handler(signal.SIGTERM, stop_final_fut)
+
+    task = loop.create_task(coro)
+    task.add_done_callback(partial(on_done_task, final_future))
+
+    async def wait():
+        await final_future
+
+    waiter = loop.create_task(wait())
+
+    try:
+        loop.run_until_complete(waiter)
+    except asyncio.CancelledError:
+        raise ApplicationCancelled()
+    finally:
+        log.debug("CLEANING UP")
+
+        final_future.cancel()
+
+        try:
+            targets = target_register.used_targets
+            loop.run_until_complete(photons_app.cleanup(targets))
+            cancel_all_tasks(loop, task, waiter)
+            loop.run_until_complete(shutdown_asyncgens(loop))
+        finally:
+            loop.close()
+            del photons_app.loop
+            del photons_app.final_future
+
+
+async def shutdown_asyncgens(loop):
+    """Shutdown all active asynchronous generators."""
+    if not len(loop._asyncgens):
+        return
+
+    closing_agens = list(loop._asyncgens)
+    loop._asyncgens.clear()
+
+    # I would do an asyncio.tasks.gather but it would appear that just causes
+    # the asyncio loop to think it's shutdown, so I have to do them one at a time
+    for ag in closing_agens:
+        try:
+            await ag.aclose()
+        except asyncio.CancelledError:
+            pass
+        except:
+            exc = sys.exc_info()[1]
+            loop.call_exception_handler(
+                {
+                    "message": "an error occurred during closing of asynchronous generator",
+                    "exception": exc,
+                    "asyncgen": ag,
+                }
+            )
+
+
+def cancel_all_tasks(loop, *ignore_errors):
+    if hasattr(asyncio.tasks, "all_tasks"):
+        to_cancel = asyncio.tasks.all_tasks(loop)
+    else:
+        to_cancel = asyncio.Task.all_tasks(loop)
+
+    if not to_cancel:
+        return
+
+    for task in to_cancel:
+        task.cancel()
+
+    gathered = asyncio.tasks.gather(*to_cancel, loop=loop, return_exceptions=True)
+    loop.run_until_complete(gathered)
+
+    for task in to_cancel:
+        if task.cancelled():
+            continue
+
+        if task not in ignore_errors and task.exception() is not None:
+            loop.call_exception_handler(
+                {
+                    "message": "unhandled exception during shutdown",
+                    "exception": task.exception(),
+                    "task": task,
+                }
+            )
---
+++ packages/lifx-photons-core/lifx-photons-core-0.24.7.tar.gz/lifx-photons-core-0.24.7/photons_pytest.py
@@ -0,0 +1,190 @@
+"""pytest-cov: avoid already-imported warning: PYTEST_DONT_REWRITE."""
+
+__shortdesc__ = "Helpers for writing tests for photons in pytest"
+
+from textwrap import dedent
+from unittest import mock
+import tempfile
+import asyncio
+import socket
+import shutil
+import sys
+import re
+import os
+
+try:
+    import pytest
+except:
+
+    class FakePytest:
+        class fixture:
+            def __init__(self, **kwargs):
+                pass
+
+            def __call__(self, func):
+                return func
+
+    pytest = FakePytest()
+
+
+def run_pytest():
+    class EditConfig:
+        @pytest.hookimpl(hookwrapper=True)
+        def pytest_cmdline_parse(pluginmanager, args):
+            args.extend(
+                [
+                    "--tb=short",
+                    "-o",
+                    "console_output_style=classic",
+                    "-o",
+                    "default_alt_async_timeout=1",
+                    "-W",
+                    "ignore:Using or importing the ABCs:DeprecationWarning",
+                    "--log-level=INFO",
+                    "-p",
+                    "helpers_namespace",
+                ]
+            )
+            yield
+
+    sys.exit(pytest.main(plugins=[EditConfig()]))
+
+
+regexes = {}
+
+
+@pytest.fixture(scope="session")
+def a_temp_dir():
+    class TempDir:
+        def __enter__(self):
+            self.d = tempfile.mkdtemp()
+            return self.d, self.make_file
+
+        def __exit__(self, exc_type, exc, tb):
+            if hasattr(self, "d") and os.path.exists(self.d):
+                shutil.rmtree(self.d)
+
+        def make_file(self, name, contents):
+            location = os.path.join(self.d, name)
+            parent = os.path.dirname(location)
+            if not os.path.exists(parent):
+                os.makedirs(parent)
+
+            with open(location, "w") as fle:
+                fle.write(dedent(contents))
+
+            return location
+
+    return TempDir
+
+
+@pytest.fixture(scope="session")
+def FakeTime():
+    class FakeTime:
+        def __init__(self):
+            self.time = 0
+
+        def set(self, t):
+            self.time = t
+
+        def add(self, t):
+            self.time += t
+
+        def __enter__(self):
+            self.patch = mock.patch("time.time", self)
+            self.patch.start()
+            return self
+
+        def __exit__(self, exc_type, exc, tb):
+            self.patch.stop()
+
+        def __call__(self):
+            return self.time
+
+    return FakeTime
+
+
+def pytest_configure():
+    if not hasattr(pytest, "helpers"):
+        return
+
+    @pytest.helpers.register
+    def assert_regex(regex, value):
+        __tracebackhide__ = True
+
+        if isinstance(regex, str):
+            if regex not in regexes:
+                regexes[regex] = re.compile(regex)
+            regex = regexes[regex]
+
+        m = regex.match(value)
+        if m:
+            return
+
+        def indented(v):
+            return "\n".join(f"  {line}" for line in v.split("\n"))
+
+        lines = [
+            "Regex didn't match",
+            "Wanted:\n",
+            indented(regex.pattern),
+            "\nto match:",
+            indented(value),
+        ]
+
+        pytest.fail("\n".join(lines))
+
+    @pytest.helpers.register
+    def free_port():
+        """
+        Return an unused port number
+        """
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind(("0.0.0.0", 0))
+            return s.getsockname()[1]
+
+
+class MemoryDevicesRunner:
+    def __init__(self, devices):
+        self.final_future = asyncio.Future()
+        options = {
+            "devices": devices,
+            "final_future": self.final_future,
+            "protocol_register": __import__("photons_messages").protocol_register,
+        }
+
+        MemoryTarget = __import__("photons_transport.targets").targets.MemoryTarget
+        self.target = MemoryTarget.create(options)
+        self.devices = devices
+
+    async def __aenter__(self):
+        for device in self.devices:
+            await device.start()
+
+        self.afr = await self.target.args_for_run()
+        return self
+
+    async def __aexit__(self, typ, exc, tb):
+        if hasattr(self, "afr"):
+            await self.target.close_args_for_run(self.afr)
+
+        for device in self.target.devices:
+            await device.finish()
+
+        self.final_future.cancel()
+
+    async def reset_devices(self):
+        for device in self.devices:
+            await device.reset()
+
+    async def per_test(self):
+        await self.reset_devices()
+
+    @property
+    def serials(self):
+        return [device.serial for device in self.devices]
+
+
+@pytest.fixture(scope="session")
+def memory_devices_runner():
+    return MemoryDevicesRunner
