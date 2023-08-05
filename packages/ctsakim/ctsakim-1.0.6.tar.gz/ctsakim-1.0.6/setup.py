2e75a0ae67d8fd7a132a62a93592f307f389b1f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:06 +0000

    Add package ctsakim/1.0.6/ctsakim-1.0.6.tar.gz

---
+++ packages/ctsakim/ctsakim-1.0.6.tar.gz/ctsakim-1.0.6/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='ctsakim',
+      version='1.0.6',
+      description='Cts akim okuma',
+      url='https://gitlab.com/ctsyazilim/ctsakim.git',
+      author='Soner Akarsu',
+      author_email='soner.akarsu@cts.com.tr',
+      license='MPLv2',
+      packages=['ctsakim'],
+      scripts=['ctsakim/ctsakim.py'])
