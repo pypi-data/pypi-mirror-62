ee883858cd064d4675846987d2d7dff15e1a46fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:08 +0000

    Add package muss/0.1.1/muss-0.1.1.tar.gz

---
+++ packages/muss/muss-0.1.1.tar.gz/muss-0.1.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='muss',
+    version='0.1.1',
+    description='Python code execution module',
+    author='jalepi',
+    author_email='jalepi@live.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/jalepi/muss',
+    packages=['muss', 'muss.runners']
+)
