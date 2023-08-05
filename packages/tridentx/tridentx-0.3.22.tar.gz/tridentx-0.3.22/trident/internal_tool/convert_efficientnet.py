56e4ae693e7bd35079bcb95879eb7607c197c8e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:17 +0000

    Add package tridentx/0.3.22/tridentx-0.3.22.tar.gz

---
+++ packages/tridentx/tridentx-0.3.22.tar.gz/tridentx-0.3.22/trident/internal_tool/convert_efficientnet.py
@@ -0,0 +1,259 @@
+
+import os
+import cv2
+os.environ['TRIDENT_BACKEND'] = 'pytorch'
+#!pip install tridentx --upgrade
+import trident as T
+from trident import *
+import torch
+
+
+effb5_old=torch.load('C:/Users/Allan/Downloads/efficientnet-b5.pth')
+effb5=EfficientNetB5(pretrained=False)
+effb5_old.cpu()
+effb5.model.cpu()
+effb5.model.trainable=True
+
+paras_old=list(effb5_old.named_parameters())
+paras=list(effb5.model.named_parameters())
+
+print('paras_old',len(paras_old))
+print('paras',len(paras))
+
+buffer_old=list(effb5_old.named_buffers())
+buffer=list(effb5.model.named_buffers())
+
+
+buffer_old=[b for b in buffer_old  if 'running_mean' in b[0] or 'running_var' in b[0]]
+buffer=[b for b in buffer  if 'running_mean' in b[0] or 'running_var' in b[0]]
+
+print('buffer_old',len(buffer_old))
+print('buffer',len(buffer))
+
+for i in range(len(paras)):
+    p_old=paras_old[i]
+    p = paras[i]
+    if  p[1].shape==p_old[1].shape: #p[0]==p_old[0] and
+        p[1].data=p_old[1].data
+    else:
+        print(p[0], p_old[0])
+
+for i in range(len(buffer)):
+    b_old = buffer_old[i]
+    b = buffer[i]
+    if 'running_mean' in b[0] or 'running_var' in b[0]:
+        if  b[1].shape == b_old[1].shape: #b[0] == b_old[0] and
+            b[1].data = b_old[1].data
+    else:
+        pass
+
+
+
+
+effb5.model.eval()
+effb5.model.cuda()
+print(effb5.infer_single_image('cat.jpg',5))
+
+
+
+
+effb5.save_onnx('efficientnet-b5.onnx')
+
+#
+# example = torch.rand(1, 3, 224, 224).cuda()
+#
+# traced_script_module = torch.jit.trace(effb5.model, example)
+# traced_script_module.save("efficientnet-b5.pt")
+
+effb5.model.cpu()
+
+torch.save(effb5.model,'efficientnet-b5.pth')
+effb5.model.cuda()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+#
+#
+#
+# effb5=EfficientNet(1.0, 1.1, 240, 0.2, model_name='efficientnet-b5',include_top=True,num_classes=1000)
+# pretrain_weights1 =list(effb5.model.named_parameters())
+# buf=list(effb5.model.named_buffers())
+# buf=[ item for item in buf if  'running_mean' in  item[0] or  'running_var' in  item[0]]
+# pretrain_weights1.extend(buf)
+#
+#
+#
+# effnetb5=torch.load('C:/Users/Allan/Downloads/efficientnet-b5.pth')
+# pretrain_weights=OrderedDict()
+#
+# for k,v in effnetb5.items():
+#     if 'weight' in k or 'bias' in k or  'running_mean' in k or  'running_var' in k:
+#         pretrain_weights[k]=v
+#
+#
+# mapping=OrderedDict()
+# pretrain_weights_dict=OrderedDict()
+# for  item in pretrain_weights.item_list:
+#     mapping[item[0]]=''
+#     pretrain_weights_dict[item[0]]=item[1]
+#
+# keyword_map1={"_blocks.0.":"block1a",
+# "_blocks.1.":"block2a",
+# "_blocks.2.":"block2b",
+# "_blocks.3.":"block3a",
+# "_blocks.4.":"block3b",
+# "_blocks.5.":"block4a",
+# "_blocks.6.":"block4b",
+# "_blocks.7.":"block4c",
+# "_blocks.8.":"block5a",
+# "_blocks.9.":"block5b",
+# "_blocks.10.":"block5c",
+# "_blocks.11.":"block6a",
+# "_blocks.12.":"block6b",
+# "_blocks.13.":"block6c",
+# "_blocks.14.":"block6d",
+# "_blocks.15.":"block7a",
+# "fc":"fc"}
+# keyword_map={}
+# for k,v in keyword_map1.items():
+#     keyword_map[v]=k
+#
+#
+#
+# no_match=[]
+#
+# for  i in range(len(pretrain_weights1)):
+#     item=pretrain_weights1[i]
+#     k1 = None
+#     for k in keyword_map.keys():
+#         if k in item[0]:
+#             k1 = keyword_map[k]
+#
+#     if ('norm' in item[0] or 'bn' in  item[0] ) and 'weight'in item[0] :
+#         for k,v in pretrain_weights_dict.items():
+#             if (k1 is not None and k1 in k) or ( k1 is None and len([vv for vv in keyword_map.values() if (vv in k)]) == 0):
+#                 if mapping[k]=='' and  ('bn' in k or 'norm' in k) and 'weight'in k and item[1].shape==v.shape:
+#                     mapping[k]=item[0]
+#                     break
+#
+#     elif ('norm' in item[0] or 'bn' in  item[0] ) and 'bias' in item[0]:
+#         for k,v in pretrain_weights_dict.items():
+#             if (k1 is not None and k1 in k) or ( k1 is None and len([vv for vv in keyword_map.values() if (vv in k)]) == 0):
+#                 if mapping[k]==''and  ('bn' in k or 'norm' in k) and 'bias'in k and item[1].shape==v.shape:
+#                     mapping[k]=item[0]
+#                     break
+#
+#     elif  'running_mean'in item[0] :
+#         for k,v in pretrain_weights_dict.items():
+#             if (k1 is not None and k1 in k) or (k1 is None and len([vv for vv in keyword_map.values() if (vv in k)]) == 0):
+#                 if mapping[k]==''and   'running_mean'in k and item[1].shape==v.shape:
+#                     mapping[k]=item[0]
+#                     break
+#
+#     elif 'running_var' in item[0]:
+#         for k,v in pretrain_weights_dict.items():
+#             if (k1 is not None and k1 in k) or ( k1 is None and len([vv for vv in keyword_map.values() if (vv in k)]) == 0):
+#                 if mapping[k]==''  and 'running_var'in k and item[1].shape==v.shape:
+#                     mapping[k]=item[0]
+#                     break
+#
+#
+#     elif  'squeeze.weight' in item[0]:
+#         for k,v in pretrain_weights_dict.items():
+#             if (k1 is not None and k1 in k) or (k1 is None and len([vv for vv in keyword_map.values() if (vv in k)]) == 0):
+#                 if mapping[k]==''and   '_se_reduce.weight'in k and item[1].shape==v.shape:
+#                     mapping[k]=item[0]
+#                     break
+#
+#     elif 'squeeze.bias' in item[0]:
+#         for k, v in pretrain_weights_dict.items():
+#             if (k1 is not None and k1 in k) or (
+#                     k1 is None and len([vv for vv in keyword_map.values() if (vv in k)]) == 0):
+#                 if mapping[k] == '' and '_se_reduce.bias' in k and item[1].shape == v.shape:
+#                     mapping[k] = item[0]
+#                     break
+#
+#     elif 'excite.weight' in item[0]:
+#         for k, v in pretrain_weights_dict.items():
+#             if (k1 is not None and k1 in k) or (
+#                     k1 is None and len([vv for vv in keyword_map.values() if (vv in k)]) == 0):
+#                 if mapping[k] == '' and '_se_expand.weight'in k and item[1].shape == v.shape:
+#                     mapping[k] = item[0]
+#                     break
+#     elif 'excite.bias' in item[0]:
+#         for k, v in pretrain_weights_dict.items():
+#             if (k1 is not None and k1 in k) or (
+#                     k1 is None and len([vv for vv in keyword_map.values() if (vv in k)]) == 0):
+#                 if mapping[k] == '' and '_se_expand.bias' in k and item[1].shape == v.shape:
+#                     mapping[k] = item[0]
+#                     break
+#
+#     elif ('conv' in item[0]  or 'depthwise'in item[0]  ) and 'weight' in item[0]:
+#         for k,v in pretrain_weights_dict.items():
+#             if (k1 is not None and k1 in k) or ( k1 is None and len([vv for vv in keyword_map.values() if (vv in k)]) == 0):
+#                 if mapping[k]==''and ( 'conv' in k or 'feature' in k) and 'weight'in k and item[1].shape==v.shape:
+#                     mapping[k]=item[0]
+#                     break
+#
+#     elif ('conv' in item[0]  or 'depthwise'in item[0] )  and 'bias' in item[0]:
+#         for k, v in pretrain_weights_dict.items():
+#             if (k1 is not None and k1 in k) or ( k1 is None and len([vv for vv in keyword_map.values() if (vv in k)]) == 0):
+#                 if mapping[k] == '' and ( 'conv' in k or 'feature' in k)  and 'bias' in k and item[1].shape == v.shape:
+#                     mapping[k] = item[0]
+#                     break
+#
+#     elif 'fc' in item[0] and 'weight' in item[0]:
+#         for k, v in pretrain_weights_dict.items():
+#             if (k1 is not None and k1 in k) or ( k1 is None and len([vv for vv in keyword_map.values() if (vv in k)]) == 0):
+#                 if mapping[k] == '' and 'fc' in k and 'weight' in k and item[1].shape == v.shape:
+#                     mapping[k] = item[0]
+#                     break
+#
+#     elif 'fc' in item[0] and 'bias' in item[0]:
+#         for k, v in pretrain_weights_dict.items():
+#             if (k1 is not None and k1 in k) or ( k1 is None and len([vv for vv in keyword_map.values() if (vv in k)]) == 0):
+#                 if mapping[k] == '' and 'fc' in k and 'bias' in k and item[1].shape == v.shape:
+#                     mapping[k] = item[0]
+#                     break
+#
+#     else:
+#         no_match.append(item)
+#
+#
+# print(len(set(list(mapping.key_list))))
+# print(len(set(list(mapping.value_list))))
+# mapping1=OrderedDict()
+# for k,v in mapping.items():
+#     mapping1[v]=k
+#
+#
+#
+# for name,para in effb5.model.named_parameters():
+#     if name in mapping1:
+#         para.data=pretrain_weights_dict[mapping1[name]].data
+#
+# for name, buf in effb5.model.named_buffers():
+#     if name in mapping1:
+#         buf.data = pretrain_weights_dict[mapping1[name]].data
+#
+#
+# print(effb5.infer_single_image('cat.jpg',5))
+#
+# effb5.model.cpu()
+# #vgg19.save_model('vgg19.pth')
+# torch.save(effb5.model,'efficientnet-b5.pth')
+#
+#
