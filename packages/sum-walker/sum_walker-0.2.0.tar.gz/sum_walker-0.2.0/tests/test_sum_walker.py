f29122b755e3c13799daf55f096d7b46b2b4dabc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:30 +0000

    Add package sum-walker/0.2.0/sum_walker-0.2.0.tar.gz

---
+++ packages/sum-walker/sum_walker-0.2.0.tar.gz/sum_walker-0.2.0/tests/test_sum_walker.py
@@ -0,0 +1,463 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_sum_walker
+----------------------------------
+
+Tests for `sum_walker` module.
+"""
+
+import pytest
+
+from sum_walker import SumStream
+
+
+def test_sum_walker():
+    """Sample pytest test function with the pytest fixture as an argument.
+    """
+    assert False
+
+    seq = list(range(1, 100))
+    cnt = 2
+
+    def request_more():
+        nonlocal seq
+        seq.append(seq[-1] + 1)
+
+    w = SumStream(cnt, seq, request_more)
+
+    def _next():
+        sum_, coords, _ = next(w)
+        return (sum_, coords)
+
+    assert _next(w) == (1, [[0, 0]])
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_sum_walker
+----------------------------------
+
+Tests for `sum_walker` module.
+"""
+
+import pytest
+
+from sum_walker import SumStream
+
+
+def test_sum_walker():
+    """Sample pytest test function with the pytest fixture as an argument.
+    """
+    assert False
+
+    seq = list(range(1, 100))
+    cnt = 2
+
+    def request_more():
+        nonlocal seq
+        seq.append(seq[-1] + 1)
+
+    w = SumStream(cnt, seq, request_more)
+
+    def _next():
+        sum_, coords, _ = next(w)
+        return (sum_, coords)
+
+    assert _next(w) == (1, [[0, 0]])
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_sum_walker
+----------------------------------
+
+Tests for `sum_walker` module.
+"""
+
+import pytest
+
+from sum_walker import SumStream
+
+
+def test_sum_walker():
+    """Sample pytest test function with the pytest fixture as an argument.
+    """
+    assert False
+
+    seq = list(range(1, 100))
+    cnt = 2
+
+    def request_more():
+        nonlocal seq
+        seq.append(seq[-1] + 1)
+
+    w = SumStream(cnt, seq, request_more)
+
+    def _next():
+        sum_, coords, _ = next(w)
+        return (sum_, coords)
+
+    assert _next(w) == (1, [[0, 0]])
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_sum_walker
+----------------------------------
+
+Tests for `sum_walker` module.
+"""
+
+import pytest
+
+from sum_walker import SumStream
+
+
+def test_sum_walker():
+    """Sample pytest test function with the pytest fixture as an argument.
+    """
+    assert False
+
+    seq = list(range(1, 100))
+    cnt = 2
+
+    def request_more():
+        nonlocal seq
+        seq.append(seq[-1] + 1)
+
+    w = SumStream(cnt, seq, request_more)
+
+    def _next():
+        sum_, coords, _ = next(w)
+        return (sum_, coords)
+
+    assert _next(w) == (1, [[0, 0]])
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_sum_walker
+----------------------------------
+
+Tests for `sum_walker` module.
+"""
+
+import pytest
+
+from sum_walker import SumStream
+
+
+def test_sum_walker():
+    """Sample pytest test function with the pytest fixture as an argument.
+    """
+    assert False
+
+    seq = list(range(1, 100))
+    cnt = 2
+
+    def request_more():
+        nonlocal seq
+        seq.append(seq[-1] + 1)
+
+    w = SumStream(cnt, seq, request_more)
+
+    def _next():
+        sum_, coords, _ = next(w)
+        return (sum_, coords)
+
+    assert _next(w) == (1, [[0, 0]])
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_sum_walker
+----------------------------------
+
+Tests for `sum_walker` module.
+"""
+
+import pytest
+
+from sum_walker import SumStream
+
+
+def test_sum_walker():
+    """Sample pytest test function with the pytest fixture as an argument.
+    """
+    seq = list(range(1, 100))
+    cnt = 2
+
+    def request_more():
+        nonlocal seq
+        seq.append(seq[-1] + 1)
+
+    w = SumStream(cnt, seq, request_more)
+
+    def _next():
+        sum_, coords, _ = next(w)
+        return (sum_, coords)
+
+    assert _next(w) == (1, [[0, 0]])
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_sum_walker
+----------------------------------
+
+Tests for `sum_walker` module.
+"""
+
+import pytest
+
+from sum_walker import SumStream
+
+
+def test_sum_walker():
+    """Sample pytest test function with the pytest fixture as an argument.
+    """
+    seq = list(range(1, 100))
+    cnt = 2
+
+    def request_more():
+        nonlocal seq
+        seq.append(seq[-1] + 1)
+
+    def _next(w):
+        sum_, coords, _ = next(w)
+        return (sum_, coords)
+
+    w = SumStream(cnt, seq, request_more)
+
+    assert _next(w) == (1, [[0, 0]])
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_sum_walker
+----------------------------------
+
+Tests for `sum_walker` module.
+"""
+
+import pytest
+
+from sum_walker import SumStream
+
+
+def test_sum_walker():
+    """Sample pytest test function with the pytest fixture as an argument.
+    """
+    seq = list(range(1, 100))
+    cnt = 2
+
+    def request_more():
+        nonlocal seq
+        seq.append(seq[-1] + 1)
+
+    def _next(w):
+        sum_, coords, _ = next(w)
+        return (sum_, coords)
+
+    w = SumStream(cnt, seq, request_more)
+
+    assert _next(w) == (2, [[0, 0]])
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_sum_walker
+----------------------------------
+
+Tests for `sum_walker` module.
+"""
+
+import pytest
+
+from sum_walker import StreamCombiner, StreamGrouper, SumStream
+
+
+def test_sum_walker():
+    """Sample pytest test function with the pytest fixture as an argument.
+    """
+    seq = list(range(1, 100))
+    cnt = 2
+
+    def request_more():
+        nonlocal seq
+        seq.append(seq[-1] + 1)
+
+    def _next(w):
+        sum_, coords, _ = next(w)
+        return (sum_, coords)
+
+    w = StreamGrouper(StreamCombiner(SumStream(cnt, seq, request_more)))
+
+    assert _next(w) == (2, [[0, 0]])
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_sum_walker
+----------------------------------
+
+Tests for `sum_walker` module.
+"""
+
+import pytest
+
+from sum_walker import StreamCombiner, StreamGrouper, SumStream
+
+
+def test_sum_walker():
+    """Sample pytest test function with the pytest fixture as an argument.
+    """
+    seq = list(range(1, 100))
+    cnt = 2
+
+    def request_more():
+        nonlocal seq
+        seq.append(seq[-1] + 1)
+
+    def _next(w):
+        sum_, coords, _ = next(w)
+        return (sum_, coords)
+
+    w = StreamGrouper(StreamCombiner([SumStream(cnt, seq, request_more)]))
+
+    assert _next(w) == (2, [[0, 0]])
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_sum_walker
+----------------------------------
+
+Tests for `sum_walker` module.
+"""
+
+import pytest
+
+from sum_walker import StreamCombiner, StreamGrouper, SumStream
+
+
+def test_sum_walker():
+    """Sample pytest test function with the pytest fixture as an argument.
+    """
+    seq = list(range(1, 100))
+    cnt = 2
+
+    def request_more():
+        nonlocal seq
+        seq.append(seq[-1] + 1)
+
+    def _next(w):
+        sum_, coords = next(w)
+        return (sum_, coords)
+
+    w = StreamGrouper(StreamCombiner([SumStream(cnt, seq, request_more)]))
+
+    assert _next(w) == (2, [[0, 0]])
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_sum_walker
+----------------------------------
+
+Tests for `sum_walker` module.
+"""
+
+import pytest
+
+from sum_walker import StreamCombiner, StreamGrouper, SumStream
+
+
+def test_sum_walker():
+    """Sample pytest test function with the pytest fixture as an argument.
+    """
+    seq = list(range(1, 100))
+    cnt = 2
+
+    def request_more():
+        nonlocal seq
+        seq.append(seq[-1] + 1)
+
+    def _next(w):
+        sum_, coords = next(w)
+        return (sum_, coords)
+
+    w = StreamGrouper(StreamCombiner([SumStream(cnt, seq, request_more)]))
+
+    assert _next(w) == (2, [[0, 0]])
+    assert _next(w) == (3, [[0, 1]])
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_sum_walker
+----------------------------------
+
+Tests for `sum_walker` module.
+"""
+
+import pytest
+
+from sum_walker import StreamCombiner, StreamGrouper, SumStream
+
+
+def test_sum_walker():
+    """Sample pytest test function with the pytest fixture as an argument.
+    """
+    seq = list(range(1, 100))
+    cnt = 2
+
+    def request_more():
+        nonlocal seq
+        seq.append(seq[-1] + 1)
+
+    def _next(w):
+        sum_, coords = next(w)
+        return (sum_, coords)
+
+    w = StreamGrouper(StreamCombiner([SumStream(cnt, seq, request_more)]))
+
+    assert _next(w) == (2, [[0, 0]])
+    assert _next(w) == (3, [[0, 1]])
+    assert _next(w) == (4, [[0, 2], [1, 1], ])
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_sum_walker
+----------------------------------
+
+Tests for `sum_walker` module.
+"""
+
+import pytest
+
+from sum_walker import StreamCombiner, StreamGrouper, SumStream
+
+
+def test_sum_walker():
+    """Sample pytest test function with the pytest fixture as an argument.
+    """
+    seq = list(range(1, 100))
+    cnt = 2
+
+    def request_more():
+        nonlocal seq
+        seq.append(seq[-1] + 1)
+
+    def _next(w):
+        sum_, coords = next(w)
+        return (sum_, coords)
+
+    w = StreamGrouper(StreamCombiner([SumStream(cnt, seq, request_more)]))
+
+    assert _next(w) == (2, [[0, 0]])
+    assert _next(w) == (3, [[0, 1]])
+    assert _next(w) == (4, [[0, 2], [1, 1], ])
