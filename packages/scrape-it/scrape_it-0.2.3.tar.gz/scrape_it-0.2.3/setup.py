95ed1989005997d65774cd0d4e7266babc766fa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:40 +0000

    Add package scrape-it/0.2.3/scrape_it-0.2.3.tar.gz

---
+++ packages/scrape-it/scrape_it-0.2.3.tar.gz/scrape_it-0.2.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scrape_it",
+    version="0.2.3",
+    py_modules=["scrape_it"],
+    author="Valentyna Fihurska",
+    author_email="valentineburn@gmail.com",
+    description="Systemitized tool for scraping",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/erelin6613/Scrape_it",
+    license='Apache License 2.0',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: Apache Software License"
+    ],
+    python_requires='>=3.5',
+)
