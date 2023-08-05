073191e3e2261b091436fcdfb992a25eadaceb71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:40:11 +0000

    Add package flumine/1.0.0b3/flumine-1.0.0b3.tar.gz

---
+++ packages/flumine/flumine-1.0.0b3.tar.gz/flumine-1.0.0b3/flumine/strategy/strategy.py
@@ -0,0 +1,82 @@
+from betfairlightweight.resources import MarketBook, RaceCard, CurrentOrders
+
+
+class Strategies:
+    def __init__(self):
+        self._strategies = []
+
+    def __call__(self, strategy):
+        self._strategies.append(strategy)
+        strategy.start()
+
+    def __iter__(self):
+        return iter(self._strategies)
+
+    def __len__(self):
+        return len(self._strategies)
+
+
+class BaseStrategy:
+    def __init__(
+        self,
+        market_filter: dict,
+        market_data_filter: dict = None,
+        streaming_timeout: float = None,
+        conflate_ms: int = None,
+        raw_data: bool = False,
+        name: str = None,
+    ):
+        self.market_filter = market_filter
+        self.market_data_filter = market_data_filter
+        self.streaming_timeout = streaming_timeout
+        self.conflate_ms = conflate_ms
+        self.raw_data = raw_data
+        self._name = name
+
+        self.streams = []  # list of streams strategy is subscribed
+
+    def check_market(self, market_book: MarketBook) -> bool:
+        if market_book.streaming_unique_id not in self.stream_ids:
+            return False  # strategy not subscribed to market stream
+        elif self.check_market_book(market_book):
+            return True
+        else:
+            return False
+
+    def start(self) -> None:
+        # e.g. subscribe to streams
+        return
+
+    def check_market_book(self, market_book: MarketBook) -> bool:
+        # process_market_book only executed if this returns True
+        return False
+
+    def process_market_book(self, market_book: MarketBook) -> None:
+        # process marketBook; place/cancel/replace orders
+        return
+
+    def process_raw_data(self, publish_time: int, datum: dict) -> None:
+        return
+
+    def process_race_card(self, race_card: RaceCard) -> None:
+        # process raceCard object
+        return
+
+    def process_orders(self, orders: CurrentOrders) -> None:
+        # process currentOrders object
+        return
+
+    def finish(self) -> None:
+        # called before flumine ends
+        return
+
+    @property
+    def stream_ids(self) -> list:
+        return [stream.stream_id for stream in self.streams]
+
+    @property
+    def name(self):
+        return self._name or self.__class__.__name__
+
+    def __str__(self):
+        return "{0}".format(self.name)
---
+++ packages/flumine/flumine-1.0.0b3.tar.gz/flumine-1.0.0b3/flumine/streams/streams.py
@@ -0,0 +1,76 @@
+import logging
+from typing import Union, Type
+
+from ..strategy.strategy import BaseStrategy
+from .marketstream import MarketStream
+from .datastream import DataStream
+
+logger = logging.getLogger(__name__)
+
+
+class Streams:
+    def __init__(self, flumine):
+        self.flumine = flumine
+        self._streams = []
+        self._stream_id = 0
+
+    def __call__(self, strategy: BaseStrategy) -> None:
+        if strategy.raw_data:
+            stream = self.add_market_stream(strategy, DataStream)
+        else:
+            stream = self.add_market_stream(strategy, MarketStream)
+        strategy.streams.append(stream)
+
+    def add_market_stream(
+        self,
+        strategy: BaseStrategy,
+        stream_class: Union[Type[MarketStream], Type[DataStream]],
+    ) -> Union[MarketStream, DataStream]:
+        for stream in self:  # check if market stream already exists
+            if (
+                isinstance(stream, stream_class)
+                and stream.market_filter == strategy.market_filter
+                and stream.market_data_filter == strategy.market_data_filter
+                and stream.streaming_timeout == strategy.streaming_timeout
+                and stream.conflate_ms == strategy.conflate_ms
+            ):
+                logger.info(
+                    "Using stream ({0}) for strategy {1}".format(
+                        stream.stream_id, strategy
+                    )
+                )
+                return stream
+        else:  # nope? lets create a new one
+            stream_id = self._increment_stream_id()
+            logger.info(
+                "Creating new stream ({0}) for strategy {1}".format(stream_id, strategy)
+            )
+            stream = stream_class(
+                self.flumine,
+                stream_id,
+                strategy.market_filter,
+                strategy.market_data_filter,
+                strategy.streaming_timeout,
+                strategy.conflate_ms,
+            )
+            self._streams.append(stream)
+            return stream
+
+    def start(self) -> None:
+        logger.info("Starting streams..")
+        for stream in self:
+            stream.start()
+
+    def stop(self) -> None:
+        for stream in self:
+            stream.stop()
+
+    def _increment_stream_id(self) -> int:
+        self._stream_id += int(1e3)
+        return self._stream_id
+
+    def __iter__(self):
+        return iter(self._streams)
+
+    def __len__(self):
+        return len(self._streams)
