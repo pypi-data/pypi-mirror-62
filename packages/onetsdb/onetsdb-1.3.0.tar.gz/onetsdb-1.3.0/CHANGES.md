fea7294e6de8254ff90b45ae771104b888177006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:38 +0000

    Add package onetsdb/1.3.0/onetsdb-1.3.0.tar.gz

---
+++ packages/onetsdb/onetsdb-1.3.0.tar.gz/onetsdb-1.3.0/CHANGES.md
@@ -0,0 +1,15 @@
+CHANGES
+===============
+1.0.0
+
+- first version
+
+1.2.0
+
+- support mongodb, influxdb, sqlite3
+
+1.2.4
+- fix sqlite3 bug
+
+1.3.0
+- support tslite
---
+++ packages/onetsdb/onetsdb-1.3.0.tar.gz/onetsdb-1.3.0/onetsdb/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: UTF-8 -*
+'''
+Created on 2019-10-22
+'''
+
+from .base import connect, TSDBPoint, TSDBBase
+from . import influx, mongo, sqlite
+
+__version__ = '1.3.0'
