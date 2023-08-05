c28a93185304335e574b3d6d57957b3b124ad8ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:30 +0000

    Add package Flask-PonyManager/3.0/Flask_PonyManager-3.0-py3.6.egg

---
+++ packages/Flask-PonyManager/Flask_PonyManager-3.0-py3.6.egg/flask_ponymanager/__init__.py
@@ -0,0 +1,12 @@
+from .former import (
+    EntityForm,
+    EntityFormer
+)
+from .view import (
+    ManagerView,
+    EntityView
+)
+from .ponymanager import PonyManager
+
+__version__ = '3.0'
+__author__ = 'David Tláskal'
