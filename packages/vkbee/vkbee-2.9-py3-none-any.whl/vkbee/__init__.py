b96a1292f6b185c5fa6d4999bebb22469ac43600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:38 +0000

    Add package vkbee/2.9/vkbee-2.9-py3-none-any.whl

---
+++ packages/vkbee/vkbee-2.9-py3-none-any.whl/vkbee/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# author: asyncvk
+
+from .vkbee import VkApi
+from .exceptions import *
+
+__copyright__ = "2020"
+__version__ = "0.1"
+__authors__ = ["YamkaFox", "sergeyfillipov1"]
