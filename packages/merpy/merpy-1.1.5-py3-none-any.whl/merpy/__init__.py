c4dd3dee6ea5c1552d8bdb674295de18a718df7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:12 +0000

    Add package merpy/1.1.5/merpy-1.1.5-py3-none-any.whl

---
+++ packages/merpy/merpy-1.1.5-py3-none-any.whl/merpy/__init__.py
@@ -0,0 +1,30 @@
+from .merpy import (
+    get_entities,
+    generate_lexicon,
+    process_lexicon,
+    show_lexicons,
+    get_lexicons,
+    download_lexicon,
+    create_lexicon,
+    create_mappings,
+    download_mer,
+    download_lexicons,
+    mer_path,
+    get_entities_mp,
+)
+
+name = "merpy"
+__all__ = [
+    "get_entities",
+    "generate_lexicon",
+    "process_lexicon",
+    "show_lexicons",
+    "get_lexicons",
+    "download_lexicon",
+    "create_mappings",
+    "create_lexicon",
+    "download_mer",
+    "download_lexicons",
+    "mer_path",
+    "get_entities_mp",
+]
