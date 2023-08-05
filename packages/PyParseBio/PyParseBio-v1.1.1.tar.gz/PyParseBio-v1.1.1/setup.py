4c1e444ba0498f7dda96fa965e163b32f8f19f0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:27 +0000

    Add package PyParseBio/v1.1.1/PyParseBio-v1.1.1.tar.gz

---
+++ packages/PyParseBio/PyParseBio-v1.1.1.tar.gz/PyParseBio-v1.1.1/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+VERSION = 'v1.1.1'
+
+setup(
+  name = 'PyParseBio',
+  packages = ['PyParseBio'],
+  version = 'v1.1.1',
+  license='apache-2.0',
+  description = 'Pure-python parralelizable parsing of biological multipoint images',
+  author = 'Wolfgang Pernice',
+  author_email = 'wolfgang.pernice@gmail.com',
+  url = 'https://github.com/WMAPernice/PyParseBio',
+  download_url = 'https://github.com/WMAPernice/PyParseBio/archive/v1.1.1.tar.gz',
+  keywords = ['Biology', 'Microscopy', 'Images', 'Python', 'Parallel'],
+  install_requires=[
+          'numpy',
+          'scikit-image',
+		  'nd2reader>=3.2.3',
+		  'tqdm',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      	
+    'Intended Audience :: Science/Research',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: Apache Software License',  		
+    'Programming Language :: Python :: 3.6',
+  ],
+)
