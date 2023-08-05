761c39b1b50d99a8281f7ccd57d3850df263b888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:01 +0000

    Add package tableauserverclient/0.10/tableauserverclient-0.10.tar.gz

---
+++ packages/tableauserverclient/tableauserverclient-0.10.tar.gz/tableauserverclient-0.10/setup.py
@@ -0,0 +1,36 @@
+import sys
+import versioneer
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# Only install pytest and runner when test command is run
+# This makes work easier for offline installs or low bandwidth machines
+needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+pytest_runner = ['pytest-runner'] if needs_pytest else []
+
+setup(
+    name='tableauserverclient',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author='Tableau',
+    author_email='github@tableau.com',
+    url='https://github.com/tableau/server-client-python',
+    packages=['tableauserverclient', 'tableauserverclient.models', 'tableauserverclient.server',
+              'tableauserverclient.server.endpoint'],
+    license='MIT',
+    description='A Python module for working with the Tableau Server REST API.',
+    test_suite='test',
+    setup_requires=pytest_runner,
+    install_requires=[
+        'requests>=2.11,<3.0',
+        'urllib3>=1.24.3,<2.0'
+    ],
+    tests_require=[
+        'requests-mock>=1.0,<2.0',
+        'pytest',
+        'mock'
+    ]
+)
