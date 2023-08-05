90df98e2fb90b14460041306a28345f313419ed1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:50 +0000

    Add package cmd2-abbrev/1.1.0/cmd2_abbrev-1.1.0-py3-none-any.whl

---
+++ packages/cmd2-abbrev/cmd2_abbrev-1.1.0-py3-none-any.whl/cmd2_abbrev/__init__.py
@@ -0,0 +1,19 @@
+#
+# coding=utf-8
+# flake8: noqa F401
+"""An abbreviation plugin for cmd2
+
+Adds a user setting `abbrev` which defaults to false. When set to True
+any command can be abbreviated by typing the shortest unique prefix
+of that command.
+
+See examples/example.py
+"""
+from pkg_resources import get_distribution, DistributionNotFound
+
+from .abbrev import AbbrevMixin
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    __version__ = 'unknown'
