286528f930fa17687d4964f8df900416ee80c849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:14 +0000

    Add package morph-ai/1.0.8/morph-ai-1.0.8.tar.gz

---
+++ packages/morph-ai/morph-ai-1.0.8.tar.gz/morph-ai-1.0.8/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="morph-ai",
+    packages=['morph'],
+    description='Morph.ai Custom code SDK for python',
+    author="Ishan Jain",
+    author_email='ishan@morph.ai',
+    url='https://github.com/scupid-admin/morph-python-sdk',
+    download_url='https://github.com/scupid-admin/morph-python-sdk/archive/1.0.0.tar.gz',
+    keywords=['chatbots', 'custom code', 'morph.ai'],
+    version="1.0.8"
+)
