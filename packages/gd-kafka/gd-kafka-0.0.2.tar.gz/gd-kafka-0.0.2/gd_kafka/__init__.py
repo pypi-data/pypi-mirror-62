77cde59744babb9509735978f223f537bdae2c0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:40:37 +0000

    Add package gd-kafka/0.0.2/gd-kafka-0.0.2.tar.gz

---
+++ packages/gd-kafka/gd-kafka-0.0.2.tar.gz/gd-kafka-0.0.2/gd_kafka/__init__.py
@@ -0,0 +1,10 @@
+print("You are using Greendeck Kafka library!")
+# your package information
+name = "gd_kafka"
+__version__ = "0.0.1"
+author = "Rishabh Garg"
+author_email = "rg16335@gmail.com"
+url = ""
+
+# import sub packages
+from .src.kafka.GDKafka import GDKafka
