b70e087ed5395a8875d5811e11c68e30e5a39deb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:09 +0000

    Add package test-pypkg/1.1/test_pypkg-1.1.tar.gz

---
+++ packages/test-pypkg/test_pypkg-1.1.tar.gz/test_pypkg-1.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+   name='test_pypkg',
+   version='1.1',
+   description='A test python packaging',
+   author='Sangram Keshari Sahu',
+   author_email='sangram@bionivid.com',
+   packages=setuptools.find_packages(),
+   install_requires=['numpy', 'pandas'], #external packages as dependencies
+   python_requires='>=3.5', 
+)
