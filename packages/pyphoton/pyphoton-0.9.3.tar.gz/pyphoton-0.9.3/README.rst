e887e65d6f61c694395826d78806653eb90255c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:26 +0000

    Add package pyphoton/0.9.3/pyphoton-0.9.3.tar.gz

---
+++ packages/pyphoton/pyphoton-0.9.3.tar.gz/pyphoton-0.9.3/README.rst
@@ -0,0 +1,125 @@
+pyphoton
+========
+
+🗺 Light Photon client written in Python
+
+|Latest Version| |codecov| |Build Status| |License: MIT|
+
+Install
+-------
+
+.. code:: sh
+
+    pip install pyphoton
+
+Usage
+-----
+
+If you need some code ready to use,
+`spike.py <https://github.com/astagi/pyphoton/blob/master/spike.py>`__
+is a good starting point
+
+Execute queries
+~~~~~~~~~~~~~~~
+
+Python Photon client allows you to make queries to Photon service
+easily.
+
+.. code:: py
+
+    from pyphoton import Photon
+
+
+    client = Photon()
+    location = client.query('berlin', limit=1)
+
+    print (location.city)
+    print (location.latitude)
+    print (location.longitude)
+
+``Photon`` object accepts two parameters:
+
+-  ``host``: the url where Photon instance is running (default
+   ``https://photon.komoot.de``)
+-  ``language``: the preferred language (default is ``en``)
+
+You can pass the ``query`` method the following parameters along the
+query string:
+
+-  ``limit``: limit number of results
+-  ``language``: force language in the query
+-  ``latitude`` and ``longitude``: use them to search with priority to a
+   geo position
+-  ``location_bias_scale``: use to search with location bias
+-  ``osm_tags``: a ``string`` or ``list`` containing `osm tags
+   filters <https://github.com/komoot/photon#filter-results-by-tags-and-values>`__
+-  ``bbox``: a ``string`` with comma-separated values or ``list``
+   containing `bounding box
+   coordinates <https://github.com/komoot/photon#filter-results-by-bounding-box>`__
+
+``Location`` object (or objects if you don't set limit=1) is generated
+from the json returned and contains all the information you need: name,
+state, street, city, osm attributes, extent\_from.latitude,
+extent\_from.longitude, extent\_to.latitude, extent\_to.longitude ...
+
+Reverse search
+~~~~~~~~~~~~~~
+
+Python Photon client allows you to make reverse search.
+
+.. code:: py
+
+    from pyphoton import Photon
+
+
+    client = Photon()
+    locations = client.reverse(latitude=52, longitude=10)
+
+    for location in locations:
+        print ('🌉 Location #{0}\n{1}\n'.format(location.osm_id, location))
+
+You can pass to the ``reverse`` method the following parameters:
+
+-  ``latitude`` and ``longitude``: use them to search using a geo
+   position
+-  ``limit``: limit number of results
+
+Deal with errors
+~~~~~~~~~~~~~~~~
+
+If there's an error in your query, a ``PhotonException`` will be raised
+
+.. code:: py
+
+    from pyphoton import Photon
+    from pyphoton.errors import PhotonException
+
+
+    client = Photon()
+    try:
+        location = client.query('', limit=1)
+    except PhotonException as ex:
+        print (ex)
+
+Run tests
+---------
+
+.. code:: sh
+
+    pip install -r requirements-dev.txt
+    make test
+
+WIP Features
+------------
+
+-  Method to fetch `latest
+   data <http://download1.graphhopper.com/public/>`__
+
+.. |Latest Version| image:: https://img.shields.io/pypi/v/pyphoton.svg
+   :target: https://pypi.python.org/pypi/pyphoton/
+.. |codecov| image:: https://codecov.io/gh/astagi/pyphoton/branch/master/graph/badge.svg
+   :target: https://codecov.io/gh/astagi/pyphoton
+.. |Build Status| image:: https://travis-ci.org/astagi/pyphoton.svg?branch=master
+   :target: https://travis-ci.org/astagi/pyphoton
+.. |License: MIT| image:: https://img.shields.io/badge/License-MIT-blue.svg
+   :target: https://github.com/astagi/pyphoton/blob/master/LICENSE
---
+++ packages/pyphoton/pyphoton-0.9.3.tar.gz/pyphoton-0.9.3/pyphoton/client.py
@@ -0,0 +1,112 @@
+import json
+import requests
+
+from .errors import PhotonException
+from .models import Location, Point
+
+
+class Photon:
+
+    def __init__(
+            self,
+            host='https://photon.komoot.de',
+            language='en'
+    ):
+        self._host = host.strip('/')
+        self._language = language
+
+    def _execute_query(
+                self,
+                endpoint,
+                q=None,
+                limit=None,
+                lat=None,
+                lon=None,
+                lang=None,
+                location_bias_scale=None,
+                osm_tag=None,
+                bbox=None
+    ):
+        params = locals().items()
+        query_parameters = [
+            '{0}={1}'.format(k, v) for k, v in params if v and k not in ('self', 'endpoint', 'osm_tag', 'bbox')
+        ]
+        if osm_tag:
+            if not isinstance(osm_tag, (list, set, tuple)):
+                osm_tag = [osm_tag]
+            for osm_tag_el in osm_tag:
+                query_parameters.append('osm_tag={0}'.format(osm_tag_el))
+        if bbox:
+            if isinstance(bbox, (list, set, tuple)):
+                bbox = ','.join(map(str, bbox))
+            query_parameters.append('bbox={0}'.format(bbox))
+        parameters_query = '&'.join(query_parameters)
+        response = requests.get("{0}/{1}/?{2}".format(
+            self._host, endpoint, parameters_query
+        ))
+        if response.status_code != 200:
+            try:
+                json_response = response.json()
+            except json.decoder.JSONDecodeError:
+                json_response = {'message': 'Error ' + str(response.status_code)}
+            raise PhotonException(json_response['message'].capitalize())
+        return response.json()
+
+    def _transform_location(self, location):
+        new_location = Location()
+        location_point = Point(
+            location['geometry']['coordinates'][1],
+            location['geometry']['coordinates'][0]
+        )
+        setattr(new_location, '_point', location_point)
+        for property_name, property_value in location['properties'].items():
+            if property_name == 'extent':
+                extent_from = Point(
+                    property_value[1],
+                    property_value[0]
+                )
+                extent_to = Point(
+                    property_value[3],
+                    property_value[2]
+                )
+                setattr(new_location, 'extent_from', extent_from)
+                setattr(new_location, 'extent_to', extent_to)
+            else:
+                setattr(new_location, property_name, property_value)
+        return new_location
+
+    def query(
+                self,
+                query,
+                limit=None,
+                latitude=None,
+                longitude=None,
+                location_bias_scale=None,
+                osm_tags=None,
+                bbox=None,
+                language=None,
+    ):
+        if not language:
+            language = self._language
+        resp = self._execute_query('api', query, limit, latitude, longitude, language, location_bias_scale, osm_tags, bbox)
+        return self._transform_locations_from_resp(resp, limit)
+
+    def reverse(
+                self,
+                latitude=None,
+                longitude=None,
+                limit=None,
+                language=None,
+    ):
+        if not language:
+            language = self._language
+        resp = self._execute_query('reverse', limit=limit, lat=latitude, lon=longitude, lang=language)
+        return self._transform_locations_from_resp(resp, limit)
+
+    def _transform_locations_from_resp(self, resp, limit):
+        if limit == 1 and len(resp['features']):
+            return self._transform_location(resp['features'][0])
+        transformed_locations = []
+        for location in resp['features']:
+            transformed_locations.append(self._transform_location(location))
+        return transformed_locations
