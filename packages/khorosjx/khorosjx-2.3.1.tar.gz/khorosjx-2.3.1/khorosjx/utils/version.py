cb89a5d583ca67199e67ffaf8f007dafe9262500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:32 +0000

    Add package khorosjx/2.3.1/khorosjx-2.3.1.tar.gz

---
+++ packages/khorosjx/khorosjx-2.3.1.tar.gz/khorosjx-2.3.1/khorosjx/utils/version.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+:Module:            khorosjx.utils.version
+:Synopsis:          This simple script contains the package version
+:Usage:             ``from .utils import version``
+:Example:           ``__version__ = version.get_full_version()``
+:Created By:        Jeff Shurtliff
+:Last Modified:     Jeff Shurtliff
+:Modified Date:     24 Feb 2020
+"""
+
+__version__ = "2.3.1"
+
+
+def get_full_version():
+    """This function returns the current full version of the khorosjx package."""
+    return __version__
+
+
+def get_major_minor_version():
+    """This function returns the current major.minor (i.e. X.Y) version of the khorosjx package."""
+    return ".".join(__version__.split(".")[:2])
