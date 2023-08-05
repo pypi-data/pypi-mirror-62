0c4aefebbd74cebad79732fc83ba6341bf9818be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:15 +0000

    Add package pyfileconf/0.4.1/pyfileconf-0.4.1.tar.gz

---
+++ packages/pyfileconf/pyfileconf-0.4.1.tar.gz/pyfileconf-0.4.1/pyfileconf/io/file/interfaces/datadict.py
@@ -0,0 +1,8 @@
+
+from pyfileconf.io.file.load.lazy.datadict import SpecificClassDictLoader
+
+class SpecificClassInterface(SpecificClassDictLoader):
+
+    def save(self):
+        # TODO [#8]: write data dict to file
+        pass
