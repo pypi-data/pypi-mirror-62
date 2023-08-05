1b6fb0f074884352a7aeaaf0ed9708db508db91f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:50 +0000

    Add package pyfsnotif/1.1.1/pyfsnotif-1.1.1.tar.gz

---
+++ packages/pyfsnotif/pyfsnotif-1.1.1.tar.gz/pyfsnotif-1.1.1/pyfsnotif/command.py
@@ -0,0 +1,34 @@
+from os.path import exists
+from string import Template
+from subprocess import run
+from sys import argv
+
+from pyfsnotif import Watcher, MODIFY
+
+
+def on_change_run(path, cmd):
+
+    command = Template(cmd.replace('%','$path')).substitute(path=path).split(' ')
+
+    def do_cmd(event):
+        run(command)
+
+    with Watcher() as w:
+        w.add(path, MODIFY, do_cmd)
+
+
+def on_change():
+    path, *cmd = argv[1:]
+    cmd = ' '.join(cmd)
+    if exists(path):
+        on_change_run(path, cmd)
+        return
+    print(f'''Usage:
+    on_change {{path}} {{cmd}}
+
+    Use % in {{cmd}} as a placeholder for {{path}}.
+
+    ex : on_change readme.txt cat %
+
+    Use ^C to stop.
+    ''')
