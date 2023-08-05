dbeb152598917ac91dab1e152618cbb8a2d980e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:18 +0000

    Add package pyacryl2/0.1.4/pyacryl2-0.1.4-py3.7.egg

---
+++ packages/pyacryl2/pyacryl2-0.1.4-py3.7.egg/pyacryl2/__init__.py
@@ -0,0 +1,12 @@
+"""
+pyacryl2
+
+Acryl clients (sync and async) module
+"""
+
+__version__ = '0.1.4'
+__author__ = 'DPInvaders'
+__license__ = 'MIT'
+
+from .client import AcrylClient
+from .async_client import AcrylAsyncClient
