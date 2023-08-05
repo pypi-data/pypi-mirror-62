ab4a32afe4186535b746ed4c957e88f2e5f6a26d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:45 +0000

    Add package numerica/0.2.0/numerica-0.2.0.tar.gz

---
+++ packages/numerica/numerica-0.2.0.tar.gz/numerica-0.2.0/numerica/__init__.py
@@ -0,0 +1,6 @@
+from .utils import function
+from .utils.function import f, c
+from .nonlinear.bracketing.graph import graph
+from .nonlinear.bracketing.bisection import bisection
+from .nonlinear.bracketing.regulafalsi import regulafalsi
+from .nonlinear.iterative.basic import basic
