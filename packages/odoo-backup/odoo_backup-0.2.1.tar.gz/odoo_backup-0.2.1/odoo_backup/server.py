648c997b4f5e5130cbf721bc7bc06703ca10eb3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:10 +0000

    Add package odoo-backup/0.2.1/odoo_backup-0.2.1.tar.gz

---
+++ packages/odoo-backup/odoo_backup-0.2.1.tar.gz/odoo_backup-0.2.1/odoo_backup/server.py
@@ -0,0 +1,21 @@
+from flask import Flask, send_from_directory
+from odoo_backup.config import DATA_DIR
+import os
+from odoo_backup.utils import backup_datetime_from_name
+
+
+def create_server(server_name:str, database: str, file_name: str) -> Flask:
+    app = Flask('download_server')
+
+    @app.route('/')
+    def download_backup():
+        print('downloading', file_name)
+        return send_from_directory(
+            os.path.join(DATA_DIR, server_name, database),
+            file_name,
+            as_attachment=True,
+            attachment_filename='{} - {} - {}.zip'.format(server_name, database, backup_datetime_from_name(file_name).date().isoformat()),
+            cache_timeout=0,
+        )
+
+    return app
