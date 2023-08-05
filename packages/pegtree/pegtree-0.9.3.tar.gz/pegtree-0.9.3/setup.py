db2623a9b9bbd193e25395c38d4c26dd3fd05339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:02 +0000

    Add package pegtree/0.9.3/pegtree-0.9.3.tar.gz

---
+++ packages/pegtree/pegtree-0.9.3.tar.gz/pegtree-0.9.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent / 'tests'))
+
+setup(
+    name='pegtree',
+    version='0.9.3',
+    url='https://github.com/KuramitsuLab/pegtree.git',
+    license='MIT',
+    author='Kimio Kuramitsu',
+    description='PEGTree for Python and JavaScript',
+    install_requires=['setuptools'],
+        packages=['pegtree'],
+        package_data={'pegtree': ['grammar/*.tpeg',
+                                  'grammar/*/*.txt']},
+    entry_points={
+        'console_scripts': [
+            'pegtree = pegtree.main:main'
+        ]
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing',
+    ],
+    test_suite='test_all.suite'
+)
