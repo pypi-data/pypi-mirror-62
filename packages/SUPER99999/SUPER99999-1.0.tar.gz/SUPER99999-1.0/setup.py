c2caf61ecd8d0398ad9d0d06661f7cd399cf0432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:58 +0000

    Add package SUPER99999/1.0/SUPER99999-1.0.tar.gz

---
+++ packages/SUPER99999/SUPER99999-1.0.tar.gz/SUPER99999-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name="SUPER99999",#对外我们可模块名字
+    version='1.0', #版本号
+    description="这是用于数学测试的模块",#描述
+    author="liuzheng",#作者
+    author_email="liuzheng99999@yeah.net",#邮箱
+    py_modules=['SUPER99999.demo1','SUPER99999.demo2'] #要发布的模块
+
+)
