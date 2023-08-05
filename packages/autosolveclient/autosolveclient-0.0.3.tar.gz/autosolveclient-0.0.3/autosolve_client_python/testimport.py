6bcf646d7255a04e555a04b34a880dce1f4d7cbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:27 +0000

    Add package autosolveclient/0.0.3/autosolveclient-0.0.3.tar.gz

---
+++ packages/autosolveclient/autosolveclient-0.0.3.tar.gz/autosolveclient-0.0.3/autosolve_client_python/testimport.py
@@ -0,0 +1,20 @@
+from autosolveclient.autosolve import AutoSolve
+
+def receiver_function(json_message):
+    print("Task ID :: " + json_message['taskId'])
+    print("Site Key :: " + json_message['siteKey'])
+    print("Token :: " + json_message['token'])
+    print("Created At :: " + json_message['createdAt'])
+    print("Version :: " + json_message['version'])
+    print("Action :: " + json_message['action'])
+
+auto_solve = AutoSolve(
+    access_token="30144-4a843021-bb26-4a13-b442-f7ce4824da14",
+    api_key="2164d024-51a9-4401-a8f6-23a08e46d314",
+    client_key="test",
+    receiver_function=receiver_function,
+    debug=True)
+
+finished = auto_solve.initialized()
+
+
