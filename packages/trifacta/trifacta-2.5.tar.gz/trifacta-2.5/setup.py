cb372368f5d14276b2bbd1374aaa0a1ea63b7429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:24 +0000

    Add package trifacta/2.5/trifacta-2.5.tar.gz

---
+++ packages/trifacta/trifacta-2.5.tar.gz/trifacta-2.5/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="trifacta",
+    version="2.5",
+    author="Vijay Balasubramaniam",
+    author_email="vbalasu@gmail.com",
+    description="Trifacta client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vbalasu/trifacta",
+    packages=setuptools.find_packages(),
+    install_requires=['requests','pywebhdfs','pandas', 'boto3', 'ipywidgets', 'ijson', 'simplejson'],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ),
+)
