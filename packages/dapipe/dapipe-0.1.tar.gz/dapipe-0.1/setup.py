2f173a0f3a8cec3585d3a11efa69890232b8d7c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:07 +0000

    Add package dapipe/0.1/dapipe-0.1.tar.gz

---
+++ packages/dapipe/dapipe-0.1.tar.gz/dapipe-0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+requirements = [
+                'tensorflow==2.1.0',
+                'Pillow==7.0.0',
+                'scikit-video==1.1.11'
+                ]
+setup(name='dapipe',
+      version='0.1',
+      description='Creates dataset builder objects',
+      url='https://github.com/aiporre/multidataloader.git',
+      download_url='https://github.com/aiporre/multidataloader/archive/v0.1.tar.gz',
+      author='Ariel Iporre',
+      author_email='ariel.iporre.rivas@gmail.com',
+      long_description=open('README.md').read(),
+      long_description_content_type="text/markdown",
+      install_requires=requirements,
+      license='Apache License Version 2.0 ',
+      packages=find_packages(exclude=["*_dataset"]),
+      zip_safe=False,
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Build Tools',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 3.7',
+      ],
+      )
