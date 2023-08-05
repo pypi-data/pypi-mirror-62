3f2d9b4ff2d253611fa2f90ea94b529ad24b3b93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:38 +0000

    Add package fsetools/0.0.0.post20200225/fsetools-0.0.0.post20200225-py3-none-any.whl

---
+++ packages/fsetools/fsetools-0.0.0.post20200225-py3-none-any.whl/fsetools/gui/layout/make_nsh_files.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '/Users/ian/Documents/GitHub/fsetools/fsetools/gui/layout/ui/make_nsh_files.ui',
+# licensing of '/Users/ian/Documents/GitHub/fsetools/fsetools/gui/layout/ui/make_nsh_files.ui' applies.
+#
+# Created: Sun Feb 23 19:37:08 2020
+#      by: pyside2-uic  running on PySide2 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
+
