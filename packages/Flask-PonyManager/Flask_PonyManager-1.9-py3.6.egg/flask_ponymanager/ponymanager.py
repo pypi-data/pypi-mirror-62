33075032a8b432bfe3801fb04d71bf40b2303b0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:45 +0000

    Add package Flask-PonyManager/1.9/Flask_PonyManager-1.9-py3.6.egg

---
+++ packages/Flask-PonyManager/Flask_PonyManager-1.9-py3.6.egg/flask_ponymanager/ponymanager.py
@@ -0,0 +1,60 @@
+from .view import (
+    View,
+    UtilsView
+)
+
+import os
+from pony.orm import Database
+from flask import (
+    Flask,
+    Blueprint,
+
+)
+from typing import Dict
+
+base_path = os.path.dirname(os.path.abspath(__file__))
+
+
+class PonyManager:
+    def __init__(self, app: Flask = None, db: Database = None):
+        self.app = None
+
+        if app is not None:
+            self.init_app(app)
+
+        self.db = db
+        self.blueprint = Blueprint(
+            name=self.__class__.__name__.lower(),
+            import_name=__name__,
+
+            static_url_path='/',
+            static_folder=os.path.join(base_path, 'static'),
+            template_folder=os.path.join(base_path, 'templates'),
+
+            url_prefix='/manage'
+        )
+        self.views: Dict[str, View] = dict()
+        self.add_view(UtilsView(self))
+
+    def init_app(self, app: Flask):
+        """
+        It's a weird Flask way to do this.
+        """
+        self.app = app
+        self.app.context_processor(lambda: dict(manager=self))
+
+    def add_view(self, view: View) -> None:
+        """
+        Add View to manager.
+        """
+        self.views[view.name] = view
+        if not view.registered:
+            view.register()
+
+    def register(self):
+        """
+        Register manager blueprint.
+        """
+        self.app.register_blueprint(self.blueprint)
+
+
