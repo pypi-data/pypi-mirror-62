f1807740248821ce608f4b255f3212cf84b1d293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:49 +0000

    Add package compas-rcf/0.1.19/compas_rcf-0.1.19-py2.py3-none-any.whl

---
+++ packages/compas-rcf/compas_rcf-0.1.19-py2.py3-none-any.whl/compas_rcf/utils/__init__.py
@@ -0,0 +1,24 @@
+"""
+********************************************************************************
+compas_rcf.utils
+********************************************************************************
+
+.. currentmodule:: compas_rcf.utils
+
+RhinoCommon to COMPAS object conversions
+----------------------------------------
+Scripts for converting geometry objects between the two frameworks. Most of this
+has been integrated into COMPAS v0.15.0 but compas_fab needs earlier versions
+of COMPAS.
+
+JSON tools
+----------
+Utility functions for loading and dumping
+:class:`compas_rcf.fabrication.ClayBullet` objects.
+
+Assorted utilities
+------------------
+When there's no other good place for a bit of code..
+
+
+"""
