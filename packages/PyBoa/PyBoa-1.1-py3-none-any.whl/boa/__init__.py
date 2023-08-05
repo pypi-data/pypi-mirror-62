ff923c30f9ef55407702f0547f5311fc5f43bf75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:03 +0000

    Add package PyBoa/1.1/PyBoa-1.1-py3-none-any.whl

---
+++ packages/PyBoa/PyBoa-1.1-py3-none-any.whl/boa/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'PyBoa'
+__version__ = '1.1'
+__author__ = 'Pythux'
+
+# Version synonym
+VERSION = __version__
+
+from .boa_obj import boa
+from .boa_wraps import boa_wraps, BoaWraps
