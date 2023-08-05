5f41465d4eda9bcb91da0341b3a14eb174273953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:26 +0000

    Add package surfinFBA/0.8.3/surfinFBA-0.8.3.tar.gz

---
+++ packages/surfinFBA/surfinFBA-0.8.3.tar.gz/surfinFBA-0.8.3/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+try:
+    import cplex as cp
+except:
+    None
+
+try:
+    import gurobipy as gb
+except:
+    None
+
+setuptools.setup(
+    name="surfinFBA",
+    version="0.8.3",
+    author="James D. Brunner, Ph.D.",
+    author_email="brunner.james@mayo.edu",
+    description="Dynamic FBA for use with COBRApy metabolic models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jdbrunner/surfin_fba",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requres=['numpy','scipy.integrate','pandas','joblib','cobra']
+)
