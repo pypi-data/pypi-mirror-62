5451a1b66bf0eab6fce8f2f045ae6895696e8c69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:27 +0000

    Add package sida/0.0.8/sida-0.0.8.tar.gz

---
+++ packages/sida/sida-0.0.8.tar.gz/sida-0.0.8/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='sida',
+    version='0.0.8',
+    description='My first pip package',
+    py_modules=["sida"],
+    scripts=["bin/sida"],
+    package_dir={'':'src/sida'},
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'websocket-client'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    url="https://github.com/liusida/sida",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Sida Liu",
+    author_email="learner.sida.liu@gmail.com",
+)
