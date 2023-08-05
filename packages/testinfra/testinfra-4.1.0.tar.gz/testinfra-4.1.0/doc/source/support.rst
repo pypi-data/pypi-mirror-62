33ae4a913860a52fb1d736530cf9fe805dcc99bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:51 +0000

    Add package testinfra/4.1.0/testinfra-4.1.0.tar.gz

---
+++ packages/testinfra/testinfra-4.1.0.tar.gz/testinfra-4.1.0/doc/source/support.rst
@@ -0,0 +1,26 @@
+Support
+=======
+
+If you have questions or need help with testinfra please consider one of the
+following
+
+Issue Tracker
+~~~~~~~~~~~~~
+
+Checkout existing issues on `project issue tracker <https://github.com/philpep/testinfra/issues>`_
+
+IRC
+~~~
+
+You can also ask questions on IRC in `#testinfra <http://webchat.freenode.net/?randomnick=1&channels=%23testinfra>`_ channel on Freenode
+
+pytest documentation
+~~~~~~~~~~~~~~~~~~~~
+
+testinfra is implemented as pytest plugin so to get the most out of please
+read `pytest documentation <http://pytest.org>`_
+
+Community Contributions
+~~~~~~~~~~~~~~~~~~~~~~~
+
+* `Molecule <http://molecule.readthedocs.io>`_ is an Automated testing framework for Ansible roles, with native Testinfra support.
---
+++ packages/testinfra/testinfra-4.1.0.tar.gz/testinfra-4.1.0/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import setuptools
+
+setuptools.setup(
+    name='testinfra',
+    author='Philippe Pepiot',
+    author_email='phil@philpep.org',
+    description='Test infrastructures',
+    long_description=open(os.path.join(
+        os.path.dirname(__file__), 'README.rst')).read(),
+    url='https://github.com/philpep/testinfra',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Testing',
+        'Topic :: System :: Systems Administration',
+        'Framework :: Pytest',
+    ],
+    packages=setuptools.find_packages(),
+    entry_points={
+        'pytest11': {
+            'pytest11.testinfra=testinfra.plugin',
+        },
+    },
+    python_requires='>=3.4',
+    setup_requires=['setuptools_scm'],
+    use_scm_version=True,
+    install_requires=[
+        'pytest!=3.0.2',
+    ],
+)
