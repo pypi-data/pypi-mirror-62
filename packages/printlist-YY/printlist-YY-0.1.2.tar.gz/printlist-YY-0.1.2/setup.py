e94c8b9a9d3ee55b9ae584c3a1e71c1c7c70a782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:16 +0000

    Add package printlist-YY/0.1.2/printlist-YY-0.1.2.tar.gz

---
+++ packages/printlist-YY/printlist-YY-0.1.2.tar.gz/printlist-YY-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="printlist-YY", # Replace with your own username
+    version="0.1.2",
+    author="ZouYuan",
+    author_email="zouyuan@example.com",
+    description="print a list",
+    long_description=long_description,
+    url="https://github.com/pypa/HeadFirstProject",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
