2939cc03bbe9650846e876517cc57d1c3a60840e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:37 +0000

    Add package byter/0.0.2/byter-0.0.2-py3-none-any.whl

---
+++ packages/byter/byter-0.0.2-py3-none-any.whl/byter/constants.py
@@ -0,0 +1,18 @@
+# yapf: disable
+TYPES_TABLE = {
+    "char":               "c",
+    "signed_char":        "b",
+    "unsigned_char":      "B",
+    "bool":               "?",
+    "short":              "h",
+    "unsigned_short":     "H",
+    "int":                "i",
+    "unsigned_int":       "I",
+    "long":               "l",
+    "unsigned_long":      "L",
+    "long_long":          "q",
+    "unsigned_long_long": "Q",
+    "float":              "f",
+    "double":             "d"
+}
+# yapf: enable
