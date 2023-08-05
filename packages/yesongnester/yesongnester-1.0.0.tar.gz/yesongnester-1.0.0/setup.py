0848d94dc984295f72698d72ee2d01c7b46b82bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:08 +0000

    Add package yesongnester/1.0.0/yesongnester-1.0.0.tar.gz

---
+++ packages/yesongnester/yesongnester-1.0.0.tar.gz/yesongnester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='yesongnester',
+    version='1.0.0',
+    py_modules=['yesongnester'],
+    author='leranfeifan',
+    author_email='87785150@qq.com',
+    url='http://www.leranfeifan.com',
+    description='A simple printer of nested lists',
+)
