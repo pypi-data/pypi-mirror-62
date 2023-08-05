93c8e5f60deffc242defec5a7699612d609dad5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:14 +0000

    Add package FinMesh/1.7.8/FinMesh-1.7.8-py3-none-any.whl

---
+++ packages/FinMesh/FinMesh-1.7.8-py3-none-any.whl/FinMesh/iex/forex.py
@@ -0,0 +1,25 @@
+from ._common import *
+
+# Latest Rates
+FOREX_LATEST_URL = prepend_iex_url('fx') + 'latest?'
+def forex_latest_rate(symbols, vprint=False):
+    ## Returns the latest FOREX rate for the requested currency pair symbol.
+    url = FOREX_LATEST_URL + f'symbols={symbols}'
+    return get_iex_json_request(url, vprint=vprint)
+
+# Currency Conversion
+FOREX_CONVERSION_URL = prepend_iex_url('fx') + 'convert?'
+def forex_conversion(symbols, amount, vprint=False):
+    ## Converts one currency to another using up-to-date currency information.
+    url = FOREX_CONVERSION_URL + f'symbols={symbols}&amount={amount}'
+    return get_iex_json_request(url, vprint=vprint)
+
+# Historical Data
+FOREX_HISTORICAL_URL = prepend_iex_url('fx') + 'historical?'
+def forex_historical(symbols, vprint=False, **queries):
+    ## Returns historical FOREX rates for the requested currency pair symbol.
+    url = FOREX_HISTORICAL_URL + f'symbols={symbols}'
+    for key, value in queries.items():
+        url += (f"&{key}={value}")
+    url =+ '&'
+    return get_iex_json_request(url, vprint=vprint)
