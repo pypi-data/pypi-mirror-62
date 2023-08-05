659a311d483635641c277cb1f2e892895c0ea34a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:20 +0000

    Add package pythonfmu/0.5.0/pythonfmu-0.5.0-py3-none-any.whl

---
+++ packages/pythonfmu/pythonfmu-0.5.0-py3-none-any.whl/pythonfmu/fmi2slave.py
@@ -0,0 +1,270 @@
+"""Define the abstract facade class."""
+import json
+import datetime
+from abc import ABC, abstractmethod
+from collections import OrderedDict, namedtuple
+from typing import Any, ClassVar, Dict, List, Optional
+from uuid import uuid1
+from xml.etree.ElementTree import Element, SubElement
+
+from ._version import __version__ as VERSION
+from .enums import Fmi2Causality, Fmi2Initial, Fmi2Variability
+from .variables import Boolean, Integer, Real, ScalarVariable, String
+
+ModelOptions = namedtuple("ModelOptions", ["name", "value", "cli"])
+
+FMI2_MODEL_OPTIONS: List[ModelOptions] = [
+    ModelOptions("needsExecutionTool", True, "no-external-tool"),
+    ModelOptions("canHandleVariableCommunicationStepSize", True, "no-variable-step"),
+    ModelOptions("canInterpolateInputs", False, "interpolate-inputs"),
+    ModelOptions("canBeInstantiatedOnlyOncePerProcess", False, "only-one-per-process"),
+    ModelOptions("canGetAndSetFMUstate", False, "handle-state"),
+    ModelOptions("canSerializeFMUstate", False, "serialize-state"),
+    ModelOptions("canNotUseMemoryManagementFunctions", True, "use-memory-management"),
+]
+
+
+class Fmi2Slave(ABC):
+    """Abstract facade class to execute Python through FMI standard."""
+
+    guid: ClassVar[str] = uuid1()
+    author: ClassVar[Optional[str]] = None
+    license: ClassVar[Optional[str]] = None
+    version: ClassVar[Optional[str]] = None
+    copyright: ClassVar[Optional[str]] = None
+    modelName: ClassVar[Optional[str]] = None
+    description: ClassVar[Optional[str]] = None
+
+    def __init__(self, **kwargs):
+        self.vars = OrderedDict()
+        self.instance_name = kwargs["instance_name"]
+        self.resources = kwargs.get("resources", None)
+        self.visible = kwargs.get("visible", False)
+
+        if self.__class__.modelName is None:
+            self.__class__.modelName = self.__class__.__name__
+
+    def to_xml(self, model_options: Dict[str, str] = dict()) -> Element:
+        """Build the XML representation of the model.
+        
+        Args:
+            model_options (Dict[str, str]) : FMU model options
+        
+        Returns:
+            (xml.etree.TreeElement.Element) XML description of the FMU
+        """
+
+        t = datetime.datetime.now(datetime.timezone.utc)
+        date_str = t.isoformat(timespec="seconds")
+
+        attrib = dict(
+            fmiVersion="2.0",
+            modelName=self.modelName,
+            guid=f"{self.guid!s}",
+            generationTool=f"PythonFMU {VERSION}",
+            generationDateAndTime=date_str,
+            variableNamingConvention="structured",
+        )
+        if self.description is not None:
+            attrib["description"] = self.description
+        if self.author is not None:
+            attrib["author"] = self.author
+        if self.license is not None:
+            attrib["license"] = self.license
+        if self.version is not None:
+            attrib["version"] = self.version
+        if self.copyright is not None:
+            attrib["copyright"] = self.copyright
+
+        root = Element("fmiModelDescription", attrib)
+
+        options = dict()
+        for option in FMI2_MODEL_OPTIONS:
+            value = model_options.get(option.name, option.value)
+            options[option.name] = str(value).lower()
+        options["modelIdentifier"] = self.modelName
+
+        SubElement(root, "CoSimulation", attrib=options)
+
+        variables = SubElement(root, "ModelVariables")
+        for v in self.vars.values():
+            if ScalarVariable.requires_start(v):
+                self.__apply_start_value(v)
+            variables.append(v.to_xml())
+
+        structure = SubElement(root, "ModelStructure")
+        outputs = list(
+            filter(lambda v: v.causality == Fmi2Causality.output, self.vars.values())
+        )
+
+        if outputs:
+            outputs_node = SubElement(structure, "Outputs")
+            for i, v in enumerate(self.vars.values()):
+                if v.causality == Fmi2Causality.output:
+                    SubElement(outputs_node, "Unknown", attrib=dict(index=str(i + 1)))
+
+        return root
+
+    def __apply_start_value(self, var: ScalarVariable):
+        vrs = [var.value_reference]
+
+        if isinstance(var, Integer):
+            refs = self.get_integer(vrs)
+        elif isinstance(var, Real):
+            refs = self.get_real(vrs)
+        elif isinstance(var, Boolean):
+            refs = self.get_boolean(vrs)
+        elif isinstance(var, String):
+            refs = self.get_string(vrs)
+        else:
+            raise Exception(f"Unsupported type!")
+
+        var.start = refs[0]
+
+    def register_variable(self, var: ScalarVariable):
+        variable_reference = len(self.vars)
+        self.vars[variable_reference] = var
+        # Set the unique value reference
+        var.value_reference = variable_reference
+
+    def setup_experiment(self, start_time: float):
+        pass
+
+    def enter_initialization_mode(self):
+        pass
+
+    def exit_initialization_mode(self):
+        pass
+
+    @abstractmethod
+    def do_step(self, current_time: float, step_size: float) -> bool:
+        pass
+
+    def terminate(self):
+        pass
+
+    def get_value(self, name: str) -> Any:
+        """Generic variable getter.
+        
+        Args:
+            name (str): Name of the variable
+
+        Returns:
+            (Any) Value of the variable
+        """
+        return getattr(self, name)
+
+    def set_value(self, name: str, value: Any):
+        """Generic variable setter.
+        
+        Args:
+            name (str): Name of the variable
+            value (Any): Value of the variable
+        """
+        setattr(self, name, value)
+
+    def get_integer(self, vrs: List[int]) -> List[int]:
+        refs = list()
+        for vr in vrs:
+            var = self.vars[vr]
+            if isinstance(var, Integer):
+                refs.append(int(self.get_value(var.name)))
+            else:
+                raise TypeError(
+                    f"Variable with valueReference={vr} is not of type Integer!"
+                )
+        return refs
+
+    def get_real(self, vrs: List[int]) -> List[float]:
+        refs = list()
+        for vr in vrs:
+            var = self.vars[vr]
+            if isinstance(var, Real):
+                refs.append(float(self.get_value(var.name)))
+            else:
+                raise TypeError(
+                    f"Variable with valueReference={vr} is not of type Real!"
+                )
+        return refs
+
+    def get_boolean(self, vrs: List[int]) -> List[bool]:
+        refs = list()
+        for vr in vrs:
+            var = self.vars[vr]
+            if isinstance(var, Boolean):
+                refs.append(bool(self.get_value(var.name)))
+            else:
+                raise TypeError(
+                    f"Variable with valueReference={vr} is not of type Boolean!"
+                )
+        return refs
+
+    def get_string(self, vrs: List[int]) -> List[str]:
+        refs = list()
+        for vr in vrs:
+            var = self.vars[vr]
+            if isinstance(var, String):
+                refs.append(str(self.get_value(var.name)))
+            else:
+                raise TypeError(
+                    f"Variable with valueReference={vr} is not of type String!"
+                )
+        return refs
+
+    def set_integer(self, vrs: List[int], values: List[int]):
+        for vr, value in zip(vrs, values):
+            var = self.vars[vr]
+            if isinstance(var, Integer):
+                self.set_value(var.name, value)
+            else:
+                raise TypeError(
+                    f"Variable with valueReference={vr} is not of type Integer!"
+                )
+
+    def set_real(self, vrs: List[int], values: List[float]):
+        for vr, value in zip(vrs, values):
+            var = self.vars[vr]
+            if isinstance(var, Real):
+                self.set_value(var.name, value)
+            else:
+                raise TypeError(
+                    f"Variable with valueReference={vr} is not of type Real!"
+                )
+
+    def set_boolean(self, vrs: List[int], values: List[bool]):
+        for vr, value in zip(vrs, values):
+            var = self.vars[vr]
+            if isinstance(var, Boolean):
+                self.set_value(var.name, value)
+            else:
+                raise TypeError(
+                    f"Variable with valueReference={vr} is not of type Boolean!"
+                )
+
+    def set_string(self, vrs: List[int], values: List[str]):
+        for vr, value in zip(vrs, values):
+            var = self.vars[vr]
+            if isinstance(var, String):
+                self.set_value(var.name, value)
+            else:
+                raise TypeError(
+                    f"Variable with valueReference={vr} is not of type String!"
+                )
+
+    def _get_fmu_state(self) -> Dict[str, Any]:
+        state = dict()
+        for var in self.vars.values():
+            state[var.name] = self.get_value(var.name)
+        return state
+
+    def _set_fmu_state(self, state: Dict[str, Any]):
+        for name, value in state.items():
+            self.set_value(name, value)
+
+    @staticmethod
+    def _fmu_state_to_bytes(state: Dict[str, Any]) -> bytes:
+        return json.dumps(state).encode("utf-8")
+
+    @staticmethod
+    def _fmu_state_from_bytes(state: bytes) -> Dict[str, Any]:
+        return json.loads(state.decode("utf-8"))
---
+++ packages/pythonfmu/pythonfmu-0.5.0-py3-none-any.whl/pythonfmu/variables.py
@@ -0,0 +1,172 @@
+"""Classes describing interface variables."""
+from abc import ABC
+from enum import Enum
+from typing import Any, Optional
+from xml.etree.ElementTree import Element, SubElement
+
+from .enums import Fmi2Causality, Fmi2Initial, Fmi2Variability
+
+
+class ScalarVariable(ABC):
+    def __init__(
+        self,
+        name: str,
+        causality: Optional[Fmi2Causality] = None,
+        description: Optional[str] = None,
+        initial: Optional[Fmi2Initial] = None,
+        variability: Optional[Fmi2Variability] = None,
+    ):
+        self.__attrs = {
+            "name": name,
+            "valueReference": None,
+            "description": description,
+            "causality": causality,
+            "variability": variability,
+            "initial": initial,
+            # 'canHandleMultipleSetPerTimeInstant': # Only for ME
+        }
+
+    @property
+    def causality(self) -> Optional[Fmi2Causality]:
+        return self.__attrs["causality"]
+
+    @property
+    def description(self) -> Optional[str]:
+        return self.__attrs["description"]
+
+    @property
+    def initial(self) -> Optional[Fmi2Initial]:
+        return self.__attrs["initial"]
+
+    @property
+    def name(self) -> str:
+        return self.__attrs["name"]
+
+    @property
+    def value_reference(self) -> int:
+        return self.__attrs["valueReference"]
+
+    @value_reference.setter
+    def value_reference(self, value: int):
+        if self.__attrs["valueReference"] is not None:
+            raise RuntimeError("Value reference already set.")
+        self.__attrs["valueReference"] = value
+
+    @property
+    def variability(self) -> Optional[Fmi2Variability]:
+        return self.__attrs["variability"]
+
+    @staticmethod
+    def requires_start(v) -> bool:
+        return (
+            v.initial == Fmi2Initial.exact
+            or v.initial == Fmi2Initial.approx
+            or v.causality == Fmi2Causality.input
+            or v.causality == Fmi2Causality.parameter
+            or v.variability == Fmi2Variability.constant
+        )
+
+    def to_xml(self) -> Element:
+        attrib = dict()
+        for key, value in self.__attrs.items():
+            if value is not None:
+                attrib[key] = str(value.name if isinstance(value, Enum) else value)
+        return Element("ScalarVariable", attrib)
+
+
+class Real(ScalarVariable):
+    def __init__(self, name: str, start: Optional[Any] = None, **kwargs):
+        super().__init__(name, **kwargs)
+        self.__attrs = {"start": start}
+
+    @property
+    def start(self) -> Optional[Any]:
+        return self.__attrs["start"]
+
+    @start.setter
+    def start(self, value: float):
+        self.__attrs["start"] = value
+
+    def to_xml(self) -> Element:
+        attrib = dict()
+        for key, value in self.__attrs.items():
+            if value is not None:
+                # In order to not loose precision, a number of this type should be 
+                # stored on an XML file with at least 16 significant digits
+                attrib[key] = f"{value:.16g}"
+        parent = super().to_xml()
+        SubElement(parent, "Real", attrib)
+
+        return parent
+
+
+class Integer(ScalarVariable):
+    def __init__(self, name: str, start: Optional[Any] = None, **kwargs):
+        super().__init__(name, **kwargs)
+        self.__attrs = {"start": start}
+
+    @property
+    def start(self) -> Optional[Any]:
+        return self.__attrs["start"]
+
+    @start.setter
+    def start(self, value: float):
+        self.__attrs["start"] = value
+
+    def to_xml(self) -> Element:
+        attrib = dict()
+        for key, value in self.__attrs.items():
+            if value is not None:
+                attrib[key] = str(value)
+        parent = super().to_xml()
+        SubElement(parent, "Integer", attrib)
+
+        return parent
+
+
+class Boolean(ScalarVariable):
+    def __init__(self, name: str, start: Optional[Any] = None, **kwargs):
+        super().__init__(name, **kwargs)
+        self.__attrs = {"start": start}
+
+    @property
+    def start(self) -> Optional[Any]:
+        return self.__attrs["start"]
+
+    @start.setter
+    def start(self, value: float):
+        self.__attrs["start"] = value
+
+    def to_xml(self) -> Element:
+        attrib = dict()
+        for key, value in self.__attrs.items():
+            if value is not None:
+                attrib[key] = str(value).lower()
+        parent = super().to_xml()
+        SubElement(parent, "Boolean", attrib)
+
+        return parent
+
+
+class String(ScalarVariable):
+    def __init__(self, name: str, start: Optional[Any] = None, **kwargs):
+        super().__init__(name, **kwargs)
+        self.__attrs = {"start": start}
+
+    @property
+    def start(self) -> Optional[Any]:
+        return self.__attrs["start"]
+
+    @start.setter
+    def start(self, value: float):
+        self.__attrs["start"] = value
+
+    def to_xml(self) -> Element:
+        attrib = dict()
+        for key, value in self.__attrs.items():
+            if value is not None:
+                attrib[key] = str(value)
+        parent = super().to_xml()
+        SubElement(parent, "String", attrib)
+
+        return parent
