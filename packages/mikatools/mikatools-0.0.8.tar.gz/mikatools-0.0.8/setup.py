5b37a031f274ffe3103968909e91a39f4bd61f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:10 +0000

    Add package mikatools/0.0.8/mikatools-0.0.8.tar.gz

---
+++ packages/mikatools/mikatools-0.0.8.tar.gz/mikatools-0.0.8/setup.py
@@ -0,0 +1,24 @@
+#encoding: utf-8
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mikatools",
+    version="0.0.8",
+    author="Mika Hämäläinen",
+    author_email="mika.hamalainen@helsinki.fi",
+    description="Quick methods for every day Python programming",
+    long_description=long_description,
+    install_requires=['requests', "clint"],
+    long_description_content_type="text/markdown",
+    url="https://github.com/mikahama/mikatools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
