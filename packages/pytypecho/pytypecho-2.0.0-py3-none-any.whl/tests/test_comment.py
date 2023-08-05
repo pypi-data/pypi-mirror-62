700cbd376d11ee69b4a0f2b2d4f832b026d33a8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:55 +0000

    Add package pytypecho/2.0.0/pytypecho-2.0.0-py3-none-any.whl

---
+++ packages/pytypecho/pytypecho-2.0.0-py3-none-any.whl/tests/test_comment.py
@@ -0,0 +1,12 @@
+from pytypecho import Comment
+
+
+def test_get_comment(te):
+    r = te.get_comment(1)
+    assert r is None
+
+
+def test_get_comments(te):
+    r = te.get_comments()
+    assert r
+    assert r[0]['content'] == '欢迎加入 Typecho 大家族'
