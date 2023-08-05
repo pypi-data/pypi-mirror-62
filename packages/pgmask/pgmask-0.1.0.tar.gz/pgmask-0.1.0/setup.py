f6493959aa0830d34efc2cb4cf025f86eff404e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:08 +0000

    Add package pgmask/0.1.0/pgmask-0.1.0.tar.gz

---
+++ packages/pgmask/pgmask-0.1.0.tar.gz/pgmask-0.1.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pgmask']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pandas>=1.0.1,<2.0.0',
+ 'psycopg2-binary>=2.8.4,<3.0.0',
+ 'sqlalchemy>=1.3.13,<2.0.0']
+
+setup_kwargs = {
+    'name': 'pgmask',
+    'version': '0.1.0',
+    'description': 'A abstraction to handle of postgres database',
+    'long_description': '# Pgmask\n\nA postgres python abstraction\n',
+    'author': 'marcusmello',
+    'author_email': 'mandealista@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://gitlab.com/vintem/pgmask',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
