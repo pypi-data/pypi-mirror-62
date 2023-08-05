42814516604307f92903f485be972dc333b1fd8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:45 +0000

    Add package MemePy/1.1.0/MemePy-1.1.0.tar.gz

---
+++ packages/MemePy/MemePy-1.1.0.tar.gz/MemePy-1.1.0/MemePy/__init__.py
@@ -0,0 +1,25 @@
+import sys
+
+from .MemeGenerator import save_meme_to_disk, add_external_resource_dir
+
+
+def h():
+    print("Usage: MemePy <template> <path> <*args> <*options>\n"
+          "Visit https://github.com/julianbrandt/src for detailed instructions")
+    sys.exit()
+
+
+def main(args):
+    if len(args) <= 2:
+        h()
+    try:
+        args = args[1:]
+        template = args[0]
+        path = args[1]
+        add_external_resource_dir("C:/users/julia/desktop/resources")
+        save_meme_to_disk(template, path, args[2:])
+    except ValueError as e:
+        print(str(e))
+        h()
+    except FileNotFoundError:
+        print("No such directory: " + path)
