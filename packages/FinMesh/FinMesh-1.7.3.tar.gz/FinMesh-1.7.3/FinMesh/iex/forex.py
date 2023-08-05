fa4307a86c3d20913a97c1c4a7d5626bc77ad98b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:57 +0000

    Add package FinMesh/1.7.3/FinMesh-1.7.3.tar.gz

---
+++ packages/FinMesh/FinMesh-1.7.3.tar.gz/FinMesh-1.7.3/FinMesh/iex/forex.py
@@ -0,0 +1,32 @@
+from ._common import *
+
+# Latest Rates
+FOREX_LATEST_URL = prepend_iex_url('fx') + 'latest?'
+def forex_latest_rate(symbols, verbose=False):
+    ## Returns the latest FOREX rate for the requested currency pair symbol.
+    url = FOREX_LATEST_URL + f'symbols={symbols}'
+    if verbose: print(url)
+    return get_iex_json_request(url)
+
+# Currency Conversion
+FOREX_CONVERSION_URL = prepend_iex_url('fx') + 'convert?'
+def forex_conversion(symbols, amount, verbose=False):
+    ## Converts one currency to another using up-to-date currency information.
+    url = FOREX_CONVERSION_URL + f'symbols={symbols}&amount={amount}'
+    if verbose: print(url)
+    return get_iex_json_request(url)
+
+# Historical Data
+FOREX_HISTORICAL_URL = prepend_iex_url('fx') + 'historical?'
+def forex_historical(symbols, from=None, to=None, on=None, last=None, first=None, filter=None, format=None):
+    ## Returns historical FOREX rates for the requested currency pair symbol.
+    url = FOREX_HISTORICAL_URL f'symbols={symbols}'
+    if from: url =+ f'&from={from}'
+    if to: url =+ f'&to={to}'
+    if on: url =+ f'&on={on}'
+    if last: url =+ f'&last={last}'
+    if first: url =+ f'&first={first}'
+    if filter: url =+ f'&filter={filter}'
+    if format: url =+ f'&format={format}'
+    url =+ '&'
+    return get_iex_json_request(url)
---
+++ packages/FinMesh/FinMesh-1.7.3.tar.gz/FinMesh-1.7.3/FinMesh/iex/market.py
@@ -0,0 +1,35 @@
+from ._common import *
+
+IEX_DATA_POINT_URL = 'https://cloud.iexapis.com/stable/data-points/'
+IEX_TIME_SERIES_URL = 'https://cloud.iexapis.com/stable/time-series/'
+
+IEX_COMMODITIES_URL = IEX_DATA_POINT_URL + 'market/'
+def commodities(symbol):
+    ## Returns commodites data for the requested commodities symbol.
+    url = IEX_COMMODITIES_URL + f'{symbol}?'
+    return get_iex_json_request(url)
+
+IEX_ECONOMIC_URL = IEX_DATA_POINT_URL + 'market/'
+def economic_data(symbol):
+    ## Returns economic data for the requested economic indicator symbol.
+    url = IEX_ECONOMIC_URL + f'{symbol}?'
+    return get_iex_json_request(url)
+
+IEX_DATA_POINT_URL = 'https://cloud.iexapis.com/stable/data-points/'
+def generic_data_point(symbol, key=None):
+    ## Generic endpoint used to access all 'Data Point' data sets on IEX
+    url = IEX_DATA_POINT_URL + f'{symbol}'
+    if key:
+        url += f'/{key}?'
+    else:
+        url+= '?'
+    return get_iex_json_request(url)
+
+IEX_TIME_SERIES_URL = 'https://cloud.iexapis.com/stable/time-series/'
+def generic_time_series(symbol, *args):
+    ## Generic endpoint used to access all 'Time Series' data sets on IEX
+    url = IEX_TIME_SERIES_URL + f'{symbol}'
+    for a in args:
+        url += f'/{a}'
+    url += ?
+    return get_iex_json_request(url)
