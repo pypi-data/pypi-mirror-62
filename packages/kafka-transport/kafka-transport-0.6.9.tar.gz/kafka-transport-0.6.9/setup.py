7594d7be4583dd067cdef8f0caabfcc63c750fe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:32 +0000

    Add package kafka-transport/0.6.9/kafka-transport-0.6.9.tar.gz

---
+++ packages/kafka-transport/kafka-transport-0.6.9.tar.gz/kafka-transport-0.6.9/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='kafka-transport',
+      version='0.6.9',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/Karma-blockchain/kafka-transport',
+      author='Nozdrin-Plotnitsky Nikolay',
+      author_email='nozdrin.plotnitsky@karma.red',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+        "msgpack >= 0.5.6",
+        "kafka-python >= 1.4.7"
+      ])
