c4f95b048d54a415b10cc7ea26c7647e1a6e2830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:43 +0000

    Add package daltools/1.1/daltools-1.1.tar.gz

---
+++ packages/daltools/daltools-1.1.tar.gz/daltools-1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from daltools import __version__
+
+setup(
+    name="daltools",
+    author="Olav Vahtras",
+    author_email="olav.vahtras@gmail.com",
+    version=__version__,
+    url="https://github.com/vahtras/daltools",
+    packages=["daltools"],
+    install_requires=["blocked-matrix-utils", "fortran-binary"]
+    )
