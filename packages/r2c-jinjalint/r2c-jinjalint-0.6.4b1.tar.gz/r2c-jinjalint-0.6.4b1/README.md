a9107fcafe2bb17f41831cc66ae64e2534da4bfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:18 +0000

    Add package r2c-jinjalint/0.6.4b1/r2c-jinjalint-0.6.4b1.tar.gz

---
+++ packages/r2c-jinjalint/r2c-jinjalint-0.6.4b1.tar.gz/r2c-jinjalint-0.6.4b1/README.md
@@ -0,0 +1,93 @@
+# jinjalint
+
+A prototype linter which checks the indentation and the correctness of
+[Jinja][jinja]-like/HTML templates. Can [fix issues][django-commit].
+
+It works with [Django’s templates][djangotemplates] too, it should
+work with [Twig](https://twig.symfony.com/) and similar template languages.
+It should work fine with any kind of HTML 4 and 5, however XHTML is not
+supported.
+
+This linter parses both HTML and Jinja tags and will report mismatched
+tags and indentation errors:
+
+```html+jinja
+<div>
+    {% if something %}
+    </div>
+{% endif %}
+```
+
+```html+jinja
+<div>
+    <span>
+    </div>
+</span>
+```
+
+```html+jinja
+  {% if something %}
+ <div> not indented properly
+      </div>
+   {% endif %}
+```
+
+```html+jinja
+{% if something %}<a href="somewhere">{% endif %}
+    <p>something</p>
+{% if not something %}</a>{% endif %}
+```
+
+## Usage
+
+You need Python 3. Jinjalint doesn’t work with Python 2. Install it with
+`pip install jinjalint` (or `pip3 install jinjalint` depending on how `pip` is
+called on your system), then run it with:
+
+```sh
+$ jinjalint template-directory/
+```
+
+…or:
+
+```sh
+$ jinjalint some-file.html some-other-file.html
+```
+
+This is a work in progress. Feel free to contribute :upside_down_face:
+
+
+## Usage with [pre-commit](https://pre-commit.com) git hooks framework
+
+Add to your `.pre-commit-config.yaml`:
+
+```yaml
+-   repo: https://github.com/motet-a/jinjalint
+    rev: ''  # select a tag / sha to point at
+    hooks:
+    -   id: jinjalint
+```
+
+Make sure to fill in the `rev` with a valid revision.
+
+_Note_: by default this configuration will only match `.jinja` and `.jinja2`
+files.  To match by regex pattern instead, override `types` and `files` as
+follows:
+
+```yaml
+    -   id: jinjalint
+        types: [file]  # restore the default `types` matching
+        files: \.(html|sls)$
+```
+
+## Hacking
+
+Jinjalint is powered by [Parsy][parsy]. Parsy is an extremely powerful
+library and Jinjalint’s parser relies heavily on it. You have to read
+Parsy’s documentation in order to understand what’s going on in
+`parse.py`.
+
+[jinja]: http://jinja.pocoo.org/docs/2.9/
+[django-commit]: https://github.com/django/djangoproject.com/commit/14a964d626196c857809d9b3b492ff4cfa4b3f40
+[djangotemplates]: https://docs.djangoproject.com/en/1.11/ref/templates/language/
+[parsy]: https://github.com/python-parsy/parsy
---
+++ packages/r2c-jinjalint/r2c-jinjalint-0.6.4b1.tar.gz/r2c-jinjalint-0.6.4b1/jinjalint/cli.py
@@ -0,0 +1,106 @@
+"""jinjalint
+
+Usage:
+  jinjalint [-v | --verbose] [-p | --print] [-j | --json] [--config CONFIG]
+            [--parse-only] [--exit-zero] [--extension EXT | -e EXT]...
+            [--select CHK | -s CHK]... [--exclude CHK | -x CHK]... [INPUT ...]
+  jinjalint (-h | --help)
+  jinjalint --version
+
+Options:
+  -h --help             Show this help message and exit.
+  --version             Show version information and exit.
+  -v --verbose          Verbose mode.
+  -p --print            Print input files in tree format.
+  -j --json             Output results in JSON format.
+  -c --config CONFIG    Specify the configuration file.
+  --parse-only          Don’t lint, check for syntax errors and exit.
+  --exit-zero           Exit "0" even if there are issues.
+  -e --extension EXT    Extension of the files to analyze (used if INPUT
+                        contains directories to crawl).
+                        [default: html jinja twig]
+  -s --select CHK       List of checks to include when running.
+  -x --exclude CHK      List of checks to exclude when running.
+
+The configuration file must be a valid Python file.
+"""
+import json
+import sys
+
+from docopt import docopt
+
+from .lint import lint, resolve_file_paths
+from .config import parse_config
+from .version import version
+
+
+def print_issues(issues, config):
+    sorted_issues = sorted(
+        issues,
+        key=lambda i: (
+            i.location.file_path,
+            i.location.line,
+            i.location.column
+        ),
+    )
+
+    for issue in sorted_issues:
+        print(str(issue))
+
+
+def main():
+    arguments = docopt(__doc__)
+
+    input_names = arguments['INPUT'] or ['.']
+    extensions = ['.' + e for e in arguments['--extension']]
+    verbose = arguments['--verbose']
+
+    if arguments['--version']:
+        print(version)
+        return
+
+    if arguments['--config']:
+        if verbose:
+            print('Using configuration file {}'.format(arguments['--config']))
+        config = parse_config(arguments['--config'])
+    else:
+        config = {}
+
+    config['verbose'] = verbose
+    config['parse_only'] = arguments['--parse-only']
+    config['print'] = arguments['--print']
+    config['select'] = arguments['--select']
+    config['exclude'] = arguments['--exclude']
+
+    paths = list(resolve_file_paths(input_names, extensions=extensions))
+
+    if verbose:
+        print('Files being analyzed:')
+        print('\n'.join(str(p) for p in paths))
+        print()
+
+    issues = lint(paths, config)
+
+    if arguments['--json']:
+        result = [
+            {
+                'message': issue.message,
+                'context': issue.context,
+                'code': issue.code,
+                'file_path': str(issue.location.file_path),
+                'line': issue.location.line,
+                'column': issue.location.column,
+            }
+            for issue in issues
+        ]
+        json.dump(result, sys.stdout)
+    else:
+        print_issues(issues, config)
+
+    if any(issues):
+        code = 0 if arguments['--exit-zero'] else 1
+        sys.exit(code)
+
+
+if __name__ == '__main__':
+    main()
