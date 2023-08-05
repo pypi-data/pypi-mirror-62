7bd48d9df5f0cec84a62431d4e0ef573121d6414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:33 +0000

    Add package b22ao/0.0.3/b22ao-0.0.3.tar.gz

---
+++ packages/b22ao/b22ao-0.0.3.tar.gz/b22ao-0.0.3/b22ao/pvs.py
@@ -0,0 +1,12 @@
+
+AD_PVBASE = "BL22B-DI-CAM-10"  # Area Detector
+
+DM1_PVBASE = "BL22B-OP-ALPAO-01"  # Deformable mirror 1
+DM2_PVBASE = "BL22B-OP-ALPAO-02"  # Deformable mirror 2
+
+
+AD_ARRAY_COUNTER = "CAM:ArrayCounter_RBV"  # Frame counter
+AD_ACQUIRE = "CAM:Acquire"  # Acquire button - write with 'Acquire'
+AD_ARRAY_DATA = "ARR:ArrayData"  # Data
+
+DM_ACTUATOR_PREFIX = "ACT"  # individual actuator pv: pv base + ACT + actuator number
