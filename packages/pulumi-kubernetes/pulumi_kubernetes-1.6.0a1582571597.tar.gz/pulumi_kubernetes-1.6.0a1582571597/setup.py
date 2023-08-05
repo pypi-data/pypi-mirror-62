924060a07e33d525b9288441de6237ae3567e90b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:32 +0000

    Add package pulumi-kubernetes/1.6.0a1582571597/pulumi_kubernetes-1.6.0a1582571597.tar.gz

---
+++ packages/pulumi-kubernetes/pulumi_kubernetes-1.6.0a1582571597.tar.gz/pulumi_kubernetes-1.6.0a1582571597/setup.py
@@ -0,0 +1,39 @@
+from subprocess import check_call
+
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+
+class InstallPluginCommand(install):
+    def run(self):
+        install.run(self)
+        check_call(['pulumi', 'plugin', 'install', 'resource', 'kubernetes', 'v1.6.0-alpha.1582571597+g89fef6a9'])
+
+
+def readme():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+
+setup(name='pulumi_kubernetes',
+      version='1.6.0a1582571597',
+      description='A Pulumi package for creating and managing Kubernetes resources.',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
+      cmdclass={
+          'install': InstallPluginCommand,
+      },
+      keywords='pulumi kubernetes',
+      url='https://pulumi.io',
+      project_urls={
+          'Repository': 'https://github.com/pulumi/pulumi-kubernetes'
+      },
+      license='Apache-2.0',
+      packages=find_packages(),
+      install_requires=[
+          'pulumi>=1.4.0,<2.0.0',
+          'requests>=2.21.0,<2.22.0',
+          'semver>=2.8.1',
+          'parver>=0.2.1',
+      ],
+      zip_safe=False)
