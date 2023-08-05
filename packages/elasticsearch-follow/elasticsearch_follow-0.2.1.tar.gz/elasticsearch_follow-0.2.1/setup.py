8c61fb20d465611c44f1dbbb99bc95b0b2ca4b27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:24 +0000

    Add package elasticsearch-follow/0.2.1/elasticsearch_follow-0.2.1.tar.gz

---
+++ packages/elasticsearch-follow/elasticsearch_follow-0.2.1.tar.gz/elasticsearch_follow-0.2.1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+from elasticsearch_follow import __version__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="elasticsearch_follow",
+    version=__version__,
+    author="Marc Schiereck",
+    author_email="mdreem@fastmail.fm",
+    description="An Elasticsearch tail",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mdreem/elasticsearch_follow",
+    packages=setuptools.find_packages(exclude=['tests', 'examples']),
+    install_requires=[
+        'python-dateutil',
+        'elasticsearch',
+        'pytz'
+    ],
+    classifiers=[
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3"
+    ],
+    tests_require=['pytest',
+                   'pytest-cov',
+                   'pytest-html',
+                   'elasticsearch>7.0.0,<=8.0.0'],
+    test_suite="tests",
+)
