c063830e138070ddf0a4d6bcbd4bccae3d0545b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:10 +0000

    Add package pyconsoler/0.2.7/pyconsoler-0.2.7.tar.gz

---
+++ packages/pyconsoler/pyconsoler-0.2.7.tar.gz/pyconsoler-0.2.7/pyconsoler/const.py
@@ -0,0 +1,40 @@
+import logging
+
+from colorama import Fore, Style
+
+_LOGGER = logging.getLogger(__name__)
+
+bouncing_bar = {
+    "interval": 80,
+    "frames": [
+        "[    ]",
+        "[   =]",
+        "[  ==]",
+        "[ ===]",
+        "[====]",
+        "[=== ]",
+        "[==  ]",
+        "[=   ]",
+        "[    ]",
+        "[=   ]",
+        "[==  ]",
+        "[=== ]",
+        "[====]",
+        "[ ===]",
+        "[  ==]",
+        "[   =]",
+    ],
+}
+ERROR_COLOR = Fore.RED + Style.BRIGHT
+SUCCESS_COLOR = Fore.GREEN
+ATTENTION_COLOR = Fore.YELLOW + Style.BRIGHT
+
+COLOR_ERROR=ERROR_COLOR
+COLOR_SUCCESS=SUCCESS_COLOR
+COLOR_ATTENTION=ATTENTION_COLOR
+COLOR_LOW_FOCUS=Fore.WHITE + Style.DIM
+
+RIGHT_PAD = 10
+BIG_RIGHT_PAD = 25
+SMALL_RIGHT_PAD = 5
+PAGE_WIDTH = 80
