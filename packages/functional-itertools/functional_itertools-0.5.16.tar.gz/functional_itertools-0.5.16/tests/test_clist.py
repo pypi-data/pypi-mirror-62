01dcb1871fcc887d3456c2830fda35de87447179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:45 +0000

    Add package functional-itertools/0.5.16/functional_itertools-0.5.16.tar.gz

---
+++ packages/functional-itertools/functional_itertools-0.5.16.tar.gz/functional_itertools-0.5.16/tests/test_clist.py
@@ -0,0 +1,29 @@
+from typing import Callable
+from typing import List
+from typing import Optional
+
+from hypothesis import given
+from hypothesis import infer
+from hypothesis.strategies import none
+
+from functional_itertools.classes import CList
+from tests.test_utilities import assert_is_instance_and_equal_to
+from tests.test_utilities import int_to_int_funcs
+
+
+@given(x=infer)
+def test_reversed(x: List[int]) -> None:
+    assert_is_instance_and_equal_to(
+        CList(x).reversed(), CList, list(reversed(x)),
+    )
+
+
+@given(x=infer, key=none() | int_to_int_funcs, reverse=infer)
+def test_sort(
+    x: List[int], key: Optional[Callable[[int], int]], reverse: bool,
+) -> None:
+    assert_is_instance_and_equal_to(
+        CList(x).sort(key=key, reverse=reverse),
+        CList,
+        sorted(x, key=key, reverse=reverse),
+    )
