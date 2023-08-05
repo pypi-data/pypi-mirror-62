95125b574ba4906f44b855ba3e833084f63864b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:17 +0000

    Add package odsclient/0.2.0/odsclient-0.2.0-py3-none-any.whl

---
+++ packages/odsclient/odsclient-0.2.0-py3-none-any.whl/odsclient/__init__.py
@@ -0,0 +1,8 @@
+from odsclient.core import get_whole_dataset, get_whole_dataframe, ODSException
+
+__all__ = [
+    # submodules
+    'core',
+    # symbols
+    'get_whole_dataset', 'get_whole_dataframe', 'ODSException'
+]
