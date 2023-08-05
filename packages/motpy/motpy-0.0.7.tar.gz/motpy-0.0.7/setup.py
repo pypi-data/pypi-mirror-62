cc16da4140ae12df80dc8e991359a6d19908e8a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:38 +0000

    Add package motpy/0.0.7/motpy-0.0.7.tar.gz

---
+++ packages/motpy/motpy-0.0.7.tar.gz/motpy-0.0.7/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='motpy',
+    version='0.0.7',
+    url='https://github.com/wmuron/motpy.git',
+    download_url='https://github.com/wmuron/motpy/releases/tag/v0.0.7-alpha',
+    author='Wiktor Muron',
+    author_email='wiktormuron@gmail.com',
+    description='Library for track-by-detection multi object tracking implemented in python',
+    packages=find_packages(exclude=("tests",)),
+    python_requires='>3.6',
+    install_requires=['numpy',
+                      'scipy',
+                      'filterpy',
+                      'loguru'],
+    license='MIT',
+    keywords=['multi-object-tracking', 'object-tracking', 'kalman-filter'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License'
+    ]
+)
