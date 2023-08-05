9785f0afea7633a1f86a7fec12855869beaec5b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:29 +0000

    Add package nester20200226/1.2.2/nester20200226-1.2.2.tar.gz

---
+++ packages/nester20200226/nester20200226-1.2.2.tar.gz/nester20200226-1.2.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'nester20200226',
+    version="1.2.2",
+    py_modules = ['nester20200226'],
+    author = 'ZouYuan',
+    author_email='zouyuan@mail.ioa.ac.cn',
+    url="http://www.zouyuan.com",
+    description='a simple printer of nested lists',    
+)
