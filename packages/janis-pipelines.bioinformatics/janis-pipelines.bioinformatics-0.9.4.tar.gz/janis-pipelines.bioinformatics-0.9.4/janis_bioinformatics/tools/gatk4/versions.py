399bfcb8132408c3354c7c7a5e23916530e629a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:07 +0000

    Add package janis-pipelines.bioinformatics/0.9.4/janis-pipelines.bioinformatics-0.9.4.tar.gz

---
+++ packages/janis-pipelines.bioinformatics/janis-pipelines.bioinformatics-0.9.4.tar.gz/janis-pipelines.bioinformatics-0.9.4/janis_bioinformatics/tools/gatk4/versions.py
@@ -0,0 +1,36 @@
+from abc import ABC
+
+
+class Gatk_4_0_12(ABC):
+    def container(self):
+        return "broadinstitute/gatk:4.0.12.0"
+
+    def version(self):
+        return "4.0.12.0"
+
+
+class Gatk_4_1_2_0(ABC):
+    def container(self):
+        return "broadinstitute/gatk:4.1.2.0"
+
+    def version(self):
+        return "4.1.2.0"
+
+
+class Gatk_4_1_3_0(ABC):
+    def container(self):
+        return "broadinstitute/gatk:4.1.3.0"
+
+    def version(self):
+        return "4.1.3.0"
+
+
+class Gatk_4_1_4_0(ABC):
+    def container(self):
+        return "broadinstitute/gatk:4.1.4.0"
+
+    def version(self):
+        return "4.1.4.0"
+
+
+Gatk4Latest = Gatk_4_1_4_0
