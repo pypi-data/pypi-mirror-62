473cd12b366fe34a5d7a0099413d31c417a62184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:57 +0000

    Add package djangowechatapi/0.53/djangowechatapi-0.53.tar.gz

---
+++ packages/djangowechatapi/djangowechatapi-0.53.tar.gz/djangowechatapi-0.53/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(name='djangowechatapi',
+      version='0.53',
+      description='wechat api for web',
+      author='ivy',
+      author_email='919624032@qq.com',
+      url='https://github.com/Ivy-1996/wechatapi',
+      packages=['wechat', 'wechat.core', 'wechat.management', 'wechat.management.commands', 'wechat.migrations'],
+      python_requires='>=3.6',
+      license='Apache License',
+      install_requires=[
+          'djangorestframework==3.11.0',
+          'wxpy',
+          'django_redis',
+          'requests',
+          'django_filter',
+          'pillow',
+      ]
+      )
---
+++ packages/djangowechatapi/djangowechatapi-0.53.tar.gz/djangowechatapi-0.53/wechat/__init__.py
@@ -0,0 +1,5 @@
+from .__version__ import __version__, __url__, __author__, __author_email__, __license__, __description__
+
+
+def get_version():
+    return __version__
