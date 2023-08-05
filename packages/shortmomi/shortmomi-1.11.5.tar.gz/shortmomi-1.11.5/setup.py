f0068578f4785b5689e28ae4fb3283210b20f4c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:47 +0000

    Add package shortmomi/1.11.5/shortmomi-1.11.5.tar.gz

---
+++ packages/shortmomi/shortmomi-1.11.5.tar.gz/shortmomi-1.11.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="shortmomi",
+    version="1.11.5",
+    license="GPL3",
+    description="Bundle of useful functions and shortcuts for pyVmomi",
+    author="Philipp Schmitt",
+    author_email="philipp@schmitt.co",
+    url="https://github.com/pschmitt/shortmomi",
+    packages=find_packages(),
+    install_requires=[
+        "pyvmomi",
+        # "vsphere-automation-sdk-python @ git+https://github.com/vmware/vsphere-automation-sdk-python",
+    ],
+)
