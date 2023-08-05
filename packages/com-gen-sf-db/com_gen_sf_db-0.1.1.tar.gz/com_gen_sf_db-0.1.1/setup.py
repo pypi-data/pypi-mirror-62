10033bbfea286972d040f8cf8368362e90af0e1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:45 +0000

    Add package com-gen-sf-db/0.1.1/com_gen_sf_db-0.1.1.tar.gz

---
+++ packages/com-gen-sf-db/com_gen_sf_db-0.1.1.tar.gz/com_gen_sf_db-0.1.1/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name = 'com_gen_sf_db',         # How you named your package folder (MyLib)
+  packages = ['com_gen_sf_db'],   # Chose the same as "name"
+  version = '0.1.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
+  author = 'Jose Sanchez',                   # Type in your name
+  author_email = 'your.email@domain.com',      # Type in your E-Mail
+  url = 'https://source.developers.google.com/p/ent-dev-sales-flow/r/com-transformation-sales-flow-dbmanager-python',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/user/com-transformation-sales-flow-dbmanager-python/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'SQLAlchemy',
+          'PyMySQL',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
