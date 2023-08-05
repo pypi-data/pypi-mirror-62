34ad476bf2804174f7e191ddf705aa3c064915fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:06 +0000

    Add package pyphoton/0.9.1/pyphoton-0.9.1.tar.gz

---
+++ packages/pyphoton/pyphoton-0.9.1.tar.gz/pyphoton-0.9.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyphoton',
+    version='0.9.1',
+    url='',
+    install_requires=["requests==2.23.0"],
+    description="Photon Python client",
+    long_description=open('README.rst', 'r').read(),
+    license="MIT",
+    author="Andrea Stagi",
+    author_email="stagi.andrea@gmail.com",
+    packages=find_packages(exclude=("tests",)),
+    include_package_data=True,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3'
+    ]
+)
