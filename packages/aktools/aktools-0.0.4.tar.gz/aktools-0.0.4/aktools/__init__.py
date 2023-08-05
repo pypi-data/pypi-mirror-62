6a37a81ea2e016c28843fe78e82997582203a34b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:07 +0000

    Add package aktools/0.0.4/aktools-0.0.4.tar.gz

---
+++ packages/aktools/aktools-0.0.4.tar.gz/aktools-0.0.4/aktools/__init__.py
@@ -0,0 +1,19 @@
+"""AkTools 是 AkShare的配套金融工具, 主要用于金融数据科学的工具"""
+
+"""
+Changelog
+0.0.1
+for test
+
+0.0.2
+FastAPI test
+
+0.0.3
+FastAPI deployment test
+
+0.0.4
+add docs
+"""
+
+__version__ = "0.0.4"
+__author__ = "Albert King"
---
+++ packages/aktools/aktools-0.0.4.tar.gz/aktools-0.0.4/aktools/news/news_api.py
@@ -0,0 +1,85 @@
+# -*- coding:utf-8 -*-
+# /usr/bin/env python
+"""
+Author: Albert King
+date: 2020/2/27 16:19
+contact: jindaxiang@163.com
+desc: 
+"""
+import json
+
+from fastapi import FastAPI
+import akshare as ak
+
+app = FastAPI()
+
+
+@app.get("/charity_china_trust")
+async def root():
+    """
+    for charity_china_trust test
+    :return: 慈善中国数据
+    :rtype: json
+    """
+    return json.loads(ak.charity_china_trust().to_json())
+
+
+@app.get("/stock_js_weibo_report")
+async def root():
+    """
+    for charity_china_trust test
+    :return: 慈善中国数据
+    :rtype: json
+    """
+    return json.loads(ak.stock_js_weibo_report().to_json())
+
+
+@app.get("/stock_em_account")
+async def root():
+    """
+    for charity_china_trust test
+    :return: 慈善中国数据
+    :rtype: json
+    """
+    return json.loads(ak.stock_em_account().to_json())
+
+
+@app.get("/amac_manager_xxgs_hmd")
+async def root():
+    """
+    for charity_china_trust test
+    :return: 慈善中国数据
+    :rtype: json
+    """
+    return json.loads(ak.amac_manager_xxgs_hmd().to_json())
+
+
+@app.get("/amac_person_org_list")
+async def root():
+    """
+    for charity_china_trust test
+    :return: 中国基金协会数据-人
+    :rtype: json
+    """
+    return json.loads(ak.amac_person_org_list().to_json())
+
+
+@app.get("/amac_manager_xxgs_cxdj")
+async def root():
+    """
+    for charity_china_trust test
+    :return: 中国基金协会数据-人
+    :rtype: json
+    """
+    return json.loads(ak.amac_manager_xxgs_cxdj().to_json())
+
+
+@app.get("/amac_fund_abs")
+async def root():
+    """
+    for charity_china_trust test
+    :return: 中国基金协会数据-人
+    :rtype: json
+    """
+    return json.loads(ak.amac_fund_abs().to_json())
+
