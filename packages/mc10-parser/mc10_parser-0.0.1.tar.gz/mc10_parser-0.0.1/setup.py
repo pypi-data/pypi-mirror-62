1669512341f8c490f8f7e20f41298aab35375a53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:31 +0000

    Add package mc10-parser/0.0.1/mc10_parser-0.0.1.tar.gz

---
+++ packages/mc10-parser/mc10_parser-0.0.1.tar.gz/mc10_parser-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='mc10_parser',
+    version='0.0.1',
+    license='MIT,',
+    description='Data IO and parsing for MC10 BioStamp Sensors',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Sabar Dasgupta',
+    author_email='sdasgupta@epilepsyco.com',
+    install_requires=[
+        'boto3',
+        'numpy',
+        'pandas',
+        'pytz',
+        's3fs',
+    ],
+    py_modules=['mc10_parser'],
+    python_requires='>=3.7.3'
+)
