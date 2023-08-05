093cb70e63422ec2046ae52891567d6b0d12619c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:12 +0000

    Add package torch-optimizer/0.0.1a6/torch-optimizer-0.0.1a6.tar.gz

---
+++ packages/torch-optimizer/torch-optimizer-0.0.1a6.tar.gz/torch-optimizer-0.0.1a6/torch_optimizer/__init__.py
@@ -0,0 +1,23 @@
+from .accsgd import AccSGD
+from .adabound import AdaBound
+from .adamod import AdaMod
+from .diffgrad import DiffGrad
+from .lamb import Lamb
+from .novograd import NovoGrad
+from .radam import RAdam
+from .sgdw import SGDW
+from .yogi import Yogi
+
+
+__all__ = (
+    'AccSGD',
+    'AdaBound',
+    'AdaMod',
+    'DiffGrad',
+    'Lamb',
+    'NovoGrad',
+    'RAdam',
+    'SGDW',
+    'Yogi',
+)
+__version__ = '0.0.1a6'
