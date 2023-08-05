b43a2e5ab17d1a98ebfcf06a5290cb2de48771c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:53 +0000

    Add package miniKanren/1.0.1/miniKanren-1.0.1-py3-none-any.whl

---
+++ packages/miniKanren/miniKanren-1.0.1-py3-none-any.whl/kanren/__init__.py
@@ -0,0 +1,24 @@
+# flake8: noqa
+"""kanren is a Python library for logic and relational programming."""
+from unification import unify, reify, unifiable, var, isvar, vars, variables, Var
+
+from .core import run, eq, conde, lall, lany
+from .goals import (
+    heado,
+    tailo,
+    conso,
+    nullo,
+    itero,
+    appendo,
+    rembero,
+    permuteo,
+    permuteq,
+    membero,
+)
+from .facts import Relation, fact, facts
+from .term import arguments, operator, term, unifiable_with_term
+
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
