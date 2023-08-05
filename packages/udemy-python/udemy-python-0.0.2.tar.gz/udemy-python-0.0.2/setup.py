8d1f966a36219f049d04be4e8c0d1681b4e74bc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:41 +0000

    Add package udemy-python/0.0.2/udemy-python-0.0.2.tar.gz

---
+++ packages/udemy-python/udemy-python-0.0.2.tar.gz/udemy-python-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="udemy-python",
+    version="0.0.2",
+    author="Anthony Ebiner",
+    author_email="anthonyebiner@gmail.com",
+    description="A python wrapper for the Udemy Instructor API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/anthonyebiner/Python-Udemy",
+    packages=setuptools.find_packages(),
+    install_requires=['requests'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
