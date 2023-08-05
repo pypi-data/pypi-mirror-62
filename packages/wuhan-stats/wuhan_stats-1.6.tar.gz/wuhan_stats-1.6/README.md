7f320650a53df4d30ae25e751dd8b4f5dcfea4b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:17 +0000

    Add package wuhan-stats/1.6/wuhan_stats-1.6.tar.gz

---
+++ packages/wuhan-stats/wuhan_stats-1.6.tar.gz/wuhan_stats-1.6/README.md
@@ -0,0 +1,34 @@
+# wuhan_stats
+Simple alert on updates of Wuhan virus statistics.
+
+Generates alert using the Notifications feature on Mac or Windows as soon as new statistics are available at https://www.worldometers.info/coronavirus/  
+
+![Alert sample](https://github.com/vipervit/wuhan_stats/raw/master/snapshot.jpeg)
+
+## Downloads
+Mac: https://drive.google.com/file/d/1Kne4mmtAliNTgchhUsuLhaFdmEnIxkYZ/view?usp=sharing
+
+Win: TBD
+
+## Dependencies:
+- requests
+- BeautifulSoup
+- plyer
+
+## To install:
+*'pip install wuhan_stats'*
+
+## To run:
+
+*'nohup python -m wuhan-stats &'* (Mac)
+
+*'pythonw -m wuhan_stats'*        (Windows)
+
+## Notes
+Polling period is 1 hour (hardcoded).
+
+Alert is generated only when the script is launched and then only if it finds (while checking hourly) the 'Last updated' field on the source site updated.
+
+For Windows, the notification's timeout is 1 hour.
+
+For Mac, make sure desired notification style is choosen for Script Editor in System Preferences->Notifications.  
---
+++ packages/wuhan-stats/wuhan_stats-1.6.tar.gz/wuhan_stats-1.6/wuhan_stats/__main__.py
@@ -0,0 +1,117 @@
+import os
+import sys
+import time
+import logging
+
+from bs4 import BeautifulSoup
+import requests
+
+import plyer
+
+from . import __version__, logger
+
+if not __debug__:
+    logger.setLevel(logging.DEBUG)
+
+SLEEP = 3600
+SLEEP_MIN = 900
+WIN_NOTIFICATION_TIMEOUT = 3600
+
+SITES = { \
+    # 'jh': 'https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6',\
+    'wm': 'https://www.worldometers.info/coronavirus/' \
+    }
+
+# ------- Worldometer
+def info_get_wm():
+    r = requests.get(SITES['wm'])
+    soup = BeautifulSoup(r.text, features="html.parser")
+    tmp =soup.title.string.split(' ')
+    cases = tmp[3]
+    deaths = tmp[6]
+    time = soup.text.split('Last updated: ')[1].split('GMT')[0]
+    last_updated = soup.text.split('Last updated: ')[1].split('GMT')[0] + ' GMT'
+    critical = soup.text.split('Serious or Critical')[0].split('Mild Condition')[1].replace(' ', '')
+    return {'Cases': cases, 'Deaths': deaths, 'Critical': critical, 'As of': last_updated }
+
+# ---------- Johns Hopkins CSSE
+def info_get_jh():
+    raise NotImplementedError
+
+def info_get(site):
+    if site == 'wm':
+        return info_get_wm()
+    if site == 'jh':
+        return info_get_jh()
+
+def info_collect(sites):
+    complete_info = {}
+    for site in SITES:
+        complete_info.update({site: info_get(site)})
+    return complete_info
+
+def alert_wm(info):
+    txt = ''
+    info.pop('As of')
+    i_deaths = int(info['Deaths'].replace(',',''))
+    i_cases = int(info['Cases'].replace(',',''))
+    rate = round( 100 * i_deaths / i_cases, 2)
+    info['Deaths'] += '(' + str(rate) + '%)'
+    for i in info:
+        txt += i + ': ' + info[i] + '\n'
+    txt = list(txt)
+    txt = ''.join(txt)
+    return txt
+
+def alert_compose(info):
+    for site in info:
+        if site == 'wm':
+            return alert_wm(info[site])
+
+def get_platform():
+    platform = sys.platform
+    if platform == 'darwin':
+        return 'Mac'
+    elif platform == 'win32':
+        return 'Win'
+    elif platform == 'linux':
+        return 'Linux'
+    else:
+        raise NotImplementedError('Not designed for this platform: ' + platform)
+
+def output(stats, timestamp):
+    header = 'COVID-19 ' + timestamp + ' v' + __version__
+    if not __debug__: # running only once, not in loop
+        logger.debug(header + '\n' + stats)
+    else: # looped execution
+        platform = get_platform()
+        if platform == 'Mac':
+            os.system('osascript -e \'display notification \"' + stats + '\" with title \"' + header + '\"\'')
+        elif platform == 'Win':
+            plyer.notification.notify(header, stats, timeout=WIN_NOTIFICATION_TIMEOUT)
+        elif platform == 'Linux':
+            os.system('notify-send \"' + header + '\" \"' + stats + '\"')
+
+
+def main():
+
+    if SLEEP < SLEEP_MIN: # to restrict polling period in order not to abuse the sources
+        logger.info('Polling period may not be less than ' + SLEEP_MIN + ' min.')
+        raise SystemExit
+
+    prev = ''
+
+
+    while True:
+        info = info_collect(SITES)
+        last = info['wm']['As of']
+        if  prev < last:
+            output(alert_compose(info), last)
+        prev = last
+        if not __debug__:
+            logger.debug('Exiting due to debug mode.')
+            break
+        time.sleep(SLEEP)
+
+if __name__ == '__main__':
+    main()
