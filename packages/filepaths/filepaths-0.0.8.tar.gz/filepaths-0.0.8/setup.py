03ef37aa46535ad872f383b2685b9c06ad87297c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:10 +0000

    Add package filepaths/0.0.8/filepaths-0.0.8.tar.gz

---
+++ packages/filepaths/filepaths-0.0.8.tar.gz/filepaths-0.0.8/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='filepaths',
+    version="0.0.8",
+    author='Hunter Feiss',
+    author_email='hfeiss@gmail.com',
+    description='Root-agnostic filepath generation',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/hfeiss/filepaths',
+    packages=setuptools.find_packages(),
+    install_requires=['addict'],
+    license='MIT',
+    keywords='filepath files directories',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+)
