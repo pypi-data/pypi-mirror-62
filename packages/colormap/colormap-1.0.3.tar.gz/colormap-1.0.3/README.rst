90ab127afc37113219c1ccde08426f3160e4023b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:45 +0000

    Add package colormap/1.0.3/colormap-1.0.3.tar.gz

---
+++ packages/colormap/colormap-1.0.3.tar.gz/colormap-1.0.3/README.rst
@@ -0,0 +1,71 @@
+#############################
+COLORMAP documentation
+#############################
+
+
+Please see : http://colormap.readthedocs.io/ for an up-to-date documentation.
+
+.. image:: https://badge.fury.io/py/colormap.svg
+    :target: https://pypi.python.org/pypi/colormap
+
+.. image:: https://secure.travis-ci.org/cokelaer/colormap.png
+    :target: http://travis-ci.org/cokelaer/colormap
+
+.. image:: https://coveralls.io/repos/cokelaer/colormap/badge.png?branch=master
+    :target: https://coveralls.io/r/cokelaer/colormap?branch=master
+
+
+:version: Python 2.7, 3.4, 3.5, 3.6, 3.7
+:contributions: Please join https://github.com/cokelaer/colormap
+:issues: Please use https://github.com/cokelaer/colormap/issues
+:notebook: Please see https://github.com/cokelaer/colormap/tree/master/notebooks
+
+
+
+What is it ?
+################
+
+**colormap** package provides simple utilities to convert colors between
+RGB, HEX, HLS, HUV and a class to easily build colormaps for matplotlib. All
+matplotlib colormaps and some R colormaps are available altogether. The
+plot_colormap method (see below) is handy to quickly pick up a colormaps and
+the test_colormap is useful to see test a new colormap.
+
+
+Installation
+###################
+
+::
+
+    pip install colormap
+
+Example
+##########
+
+* Create your own colormap from red to green colors with intermediate color as
+  whitish (diverging map from red to green)::
+
+      c = Colormap()
+      mycmap = c.cmap( {'red':[1,1,0], 'green':[0,1,.39], 'blue':[0,1,0]})
+      cmap = c.test_colormap(mycmap)
+
+* Even simpler if the colormap is linear::
+
+      c = Colormap()
+      mycmap = c.cmap_linear('red', 'white', 'green(w3c)')
+      cmap = c.test_colormap(mycmap)
+
+.. image:: http://colormap.readthedocs.io/en/latest/_images/index-1.png
+    :width: 50%
+    :align: center
+
+* check out the available colormaps::
+
+      c = Colormap()
+      c.plot_colormap('diverging')
+
+.. image:: http://colormap.readthedocs.io/en/latest/_images/colormaps.png
+    :width: 50%
+    :align: center
+
+See online documentation for details: http://colormap.readthedocs.io/
---
+++ packages/colormap/colormap-1.0.3.tar.gz/colormap-1.0.3/setup.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+__revision__ = "$Id: setup.py 3170 2013-01-16 14:57:19Z cokelaer $"
+import sys
+import os
+from setuptools import setup, find_packages
+import glob
+
+_MAJOR               = 1
+_MINOR               = 0
+_MICRO               = 3
+version              = '%d.%d.%d' % (_MAJOR, _MINOR, _MICRO)
+release              = '%d.%d' % (_MAJOR, _MINOR)
+
+metainfo = {
+    'authors': {'Cokelaer':('Thomas Cokelaer','cokelaer@ebi.ac.uk')},
+    'version': version,
+    'license' : 'LGPL',
+    'download_url' : 'http://pypi.python.org/pypi/colormap',
+    'url' :  "http://github.com/cokelaer/colormap",
+    'description':'Utilities to ease manipulation of matplotlib colormaps and color codecs (e.g., hex2rgb)',
+    'platforms' : ['Linux', 'Unix', 'MacOsX', 'Windows'],
+    'keywords' : ["hex2web", "web2hex", "hex2rgb", "rgb2hex", "rgb2hsv", "hsv2rgb", 
+        "rgb2hls", "hls2rgb", "colormap", 'colors'],
+    'classifiers' : [
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+          ]
+    }
+
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd is True:  # only import and set the theme if we're building docs
+    install_requires = ['matplotlib', 'easydev', "numpydoc"]
+else:
+    install_requires = ['matplotlib', 'easydev']
+
+
+
+setup(
+    name             = 'colormap',
+    version          = version,
+    maintainer       = metainfo['authors']['Cokelaer'][0],
+    maintainer_email = metainfo['authors']['Cokelaer'][1],
+    author           = metainfo['authors']['Cokelaer'][0],
+    author_email     = metainfo['authors']['Cokelaer'][1],
+    long_description = open("README.rst").read(),
+    keywords         = metainfo['keywords'],
+    description = metainfo['description'],
+    license          = metainfo['license'],
+    platforms        = metainfo['platforms'],
+    url              = metainfo['url'],
+    download_url     = metainfo['download_url'],
+    classifiers      = metainfo['classifiers'],
+
+    # package installation
+    package_dir = {'':'src'},
+    packages = ['colormap'],
+    requires = install_requires,
+)
+
+
+
+
