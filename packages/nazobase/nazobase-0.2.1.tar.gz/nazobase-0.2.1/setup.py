cee25d05b2260081143ce90cb37723bdb1590e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:20 +0000

    Add package nazobase/0.2.1/nazobase-0.2.1.tar.gz

---
+++ packages/nazobase/nazobase-0.2.1.tar.gz/nazobase-0.2.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages, Distribution
+
+class BinaryDistribution(Distribution):
+    """Distribution which always forces a binary package with platform name"""
+    def has_ext_modules(foo):
+        return True
+
+with open("README.md", "r" ,encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name="nazobase", # Replace with your own username
+    version="0.2.1",
+    author="WEN",
+    description="NAZOrip basement where deposits some easy functions for Vapoursynth. - GoodManWEN/nazobase",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.nazorip.site",
+    packages=find_packages(),
+    package_data={
+        'nazobase': ['nazolib.cp37-win_amd64.pyd'],
+    },
+    install_requires=['pymediainfo'],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        'Programming Language :: Python :: 3.7',
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    python_requires='>=3.7',
+    keywords=["vapoursynth", "nazobase", "NAZOrip"]
+)                                                               
