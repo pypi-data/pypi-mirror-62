086a195ff30b4a72fe96c62fe64476111427bcb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:37 +0000

    Add package Demomgr/1.1.1/Demomgr-1.1.1-py3-none-any.whl

---
+++ packages/Demomgr/Demomgr-1.1.1-py3-none-any.whl/demomgr/dialogues/__init__.py
@@ -0,0 +1,9 @@
+from .bookmark_setter import BookmarkSetter
+from .cfg_error import CfgError
+from .deleter import Deleter
+from .first_run import FirstRun
+from .launch_tf2 import LaunchTF2
+from .settings import Settings
+
+__all__ = ["BookmarkSetter", "CfgError", "Deleter", "FirstRun",
+	"LaunchTF2", "Settings"]
---
+++ packages/Demomgr/Demomgr-1.1.1-py3-none-any.whl/demomgr/threads/__init__.py
@@ -0,0 +1,7 @@
+from .delete import ThreadDelete
+from .filter import ThreadFilter
+from .read_folder import ThreadReadFolder
+from .mark_demo import ThreadMarkDemo
+
+__all__ = ["ThreadDelete", "ThreadFilter", "ThreadReadFolder",
+	"ThreadMarkDemo"]
