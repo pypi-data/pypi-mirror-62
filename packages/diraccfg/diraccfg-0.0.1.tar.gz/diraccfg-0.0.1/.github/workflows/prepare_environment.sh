95fda079f8aefa864a503969f1c6a4706dd21554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:05 +0000

    Add package diraccfg/0.0.1/diraccfg-0.0.1.tar.gz

---
+++ packages/diraccfg/diraccfg-0.0.1.tar.gz/diraccfg-0.0.1/.github/workflows/prepare_environment.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+PYTHON_VERSION=$1
+PYTHON_VERSION=2.7.13
+conda create --quiet -c conda-forge -c free -n test-env \
+    python="$PYTHON_VERSION" "pytest>=4.6" pylint pytest-cov pycodestyle
+source "${CONDA}/bin/activate" test-env
+pip install ".[testing]"
---
+++ packages/diraccfg/diraccfg-0.0.1.tar.gz/diraccfg-0.0.1/src/diraccfg/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+__all__ = [
+    'CFG',
+]
+
+from .cfg import CFG
