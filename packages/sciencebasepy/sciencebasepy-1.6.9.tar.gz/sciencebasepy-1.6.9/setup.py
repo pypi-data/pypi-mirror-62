46d9c0080d9af32c3e8e1e3f90a3f98b91f3d08a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:33 +0000

    Add package sciencebasepy/1.6.9/sciencebasepy-1.6.9.tar.gz

---
+++ packages/sciencebasepy/sciencebasepy-1.6.9.tar.gz/sciencebasepy-1.6.9/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='sciencebasepy',
+    version='1.6.9',
+    author="USGS ScienceBase Development Team",
+    author_email="sciencebase@usgs.gov",
+    description="Python ScienceBase Utilities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/usgs/sciencebasepy',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+    install_requires=[
+        "requests"
+    ]
+)
