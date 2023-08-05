a075412426fa9f1f116782b0465f4695f3839340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:15 +0000

    Add package resolwe-bio/27.0.0a1/resolwe_bio-27.0.0a1-py3-none-any.whl

---
+++ packages/resolwe-bio/resolwe_bio-27.0.0a1-py3-none-any.whl/resolwe_bio/extensions.py
@@ -0,0 +1,20 @@
+"""Resolwe-bio extensions of Resolwe core."""
+from django_filters import rest_framework as filters
+
+
+class ExtendedDataFilter:
+    """Data filter extensions."""
+
+    def filter_output(self, queryset, name, value):
+        """Filter queryset by genome build."""
+        return queryset.filter(**{'output__{}'.format(name): value})
+
+    def filter_output_icontains(self, queryset, name, value):
+        """Filter queryset by genome build."""
+        return queryset.filter(**{'output__{}__icontains'.format(name): value})
+
+    # These filters use custom indexes defined in migrations.
+    build = filters.CharFilter(method='filter_output')
+    feature_type = filters.CharFilter(method='filter_output')
+    source = filters.CharFilter(method='filter_output')
+    species = filters.CharFilter(method='filter_output_icontains')
