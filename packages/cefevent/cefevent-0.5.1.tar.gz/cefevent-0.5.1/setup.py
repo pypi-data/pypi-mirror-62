0c4e9283a4f1c2c02c922fb4e4879073954c4f8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:14 +0000

    Add package cefevent/0.5.1/cefevent-0.5.1.tar.gz

---
+++ packages/cefevent/cefevent-0.5.1.tar.gz/cefevent-0.5.1/setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup
+
+long_description = 'ArcSight Common Event Format library'
+if os.path.exists('README.md'):
+    long_description = open('README.md', 'r').read()
+
+
+setup(
+  name = 'cefevent',
+  packages = ['cefevent'],
+  version = '0.5.1',
+  description = 'ArcSight Common Event Format library',
+  long_description = long_description,
+  long_description_content_type = "text/markdown",
+  author = 'Kamus Hadenes',
+  author_email = 'kamushadenes@hyadesinc.com',
+  url = 'https://github.com/kamushadenes/cefevent',
+  download_url = 'https://github.com/kamushadenes/cefevent/tarball/0.5.1',
+  keywords = ['logging', 'cef', 'arcsight', 'event', 'security'],
+)
