ddaa6059b2c5b7d5bd78e2db95237cceedbc5c38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:44 +0000

    Add package gpf3/0.3.2/gpf3-0.3.2-py3-none-any.whl

---
+++ packages/gpf3/gpf3-0.3.2-py3-none-any.whl/gpf/tools/__init__.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+#
+# Copyright 2019 Geocom Informatik AG / VertiGIS
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+The *tools* subpackage contains a set of general classes and functions that
+should make it a little easier to work with ArcGIS and ``arcpy``.
+
+Some classes are wrappers for well-known ``arcpy`` classes,
+created for a more user-friendly experience and/or better performance.
+"""
