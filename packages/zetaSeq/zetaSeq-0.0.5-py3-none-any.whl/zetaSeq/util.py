2f1ee163b76f6df0e301124d1e19e5a66e0d6f03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:36 +0000

    Add package zetaSeq/0.0.5/zetaSeq-0.0.5-py3-none-any.whl

---
+++ packages/zetaSeq/zetaSeq-0.0.5-py3-none-any.whl/zetaSeq/util.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 15 10:36:47 2018
+
+Coders who love to comment their code are unlikely to have bad luck.
+
+@author: Zewei Song
+@email: songzewei@genomics.cn
+"""
+#%
+from __future__ import print_function
+from __future__ import division
+
+def test_me():
+    from zetaSeq import io as seqIO
+    seqIO.test_me()
