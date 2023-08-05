d09d1c7ca1fab3f539ded519fe958a9e3ddc3da5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:23 +0000

    Add package matsim-tools/0.0.2/matsim-tools-0.0.2.tar.gz

---
+++ packages/matsim-tools/matsim-tools-0.0.2.tar.gz/matsim-tools-0.0.2/matsim/__init__.py
@@ -0,0 +1,6 @@
+from . import Events
+from . import Network
+from . import TripEventHandler
+
+read_network = Network.read_network
+event_reader = Events.event_reader
---
+++ packages/matsim-tools/matsim-tools-0.0.2.tar.gz/matsim-tools-0.0.2/setup.py
@@ -0,0 +1,29 @@
+import pathlib
+
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="matsim-tools",
+    version="0.0.2",
+    description="Tools for working with the MATSim Agent-Based Transportation Simulation framework",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/matsim-vsp/matsim-python-tools",
+    author="VSP-Berlin",
+    author_email="laudan@tu-berlin.de",
+    license="GPL-3",
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["matsim"],
+    install_requires=['pandas'], # ,'geopandas','shapely'],
+    entry_points={},
+)
