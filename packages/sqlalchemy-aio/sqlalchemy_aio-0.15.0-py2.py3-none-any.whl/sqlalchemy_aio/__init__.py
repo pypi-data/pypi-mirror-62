5de5e24054cb4b9a4efc8047c40abcfb3dc3c18b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:00 +0000

    Add package sqlalchemy-aio/0.15.0/sqlalchemy_aio-0.15.0-py2.py3-none-any.whl

---
+++ packages/sqlalchemy-aio/sqlalchemy_aio-0.15.0-py2.py3-none-any.whl/sqlalchemy_aio/__init__.py
@@ -0,0 +1,12 @@
+from .exc import AlreadyQuit, BlockingWarning
+from .strategy import ASYNCIO_STRATEGY, TRIO_STRATEGY
+
+__all__ = [
+    'ASYNCIO_STRATEGY',
+    'TRIO_STRATEGY',
+]
+
+__author__ = 'Frazer McLean <frazer@frazermclean.co.uk>'
+__version__ = '0.15.0'
+__license__ = 'MIT'
+__description__ = 'Async support for SQLAlchemy.'
