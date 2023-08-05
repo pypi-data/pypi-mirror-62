7d8a45902b7668242daac554c7d49d0bc964eb01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:31 +0000

    Add package broompy/2.1.9/broompy-2.1.9.tar.gz

---
+++ packages/broompy/broompy-2.1.9.tar.gz/broompy-2.1.9/broompy/__init__.py
@@ -0,0 +1,27 @@
+from .core import *
+
+# module level doc-string
+__doc__ = """
+broompy - a simple data cleaning tool for Python
+===================================================
+
+**broompy** is a Python library designed to help 
+data scientists observe a summary of their 
+Dataset and facilitate data cleaning. It reveals 
+features that require cleaning or inspection. 
+This library also displays charts to view the normal 
+approximation of variable distributions and generates 
+scatter plot to visualize correlation.
+
+https://github.com/juanduranc/broompy
+
+Main Features
+-------------
+  - Display count of null and available data.
+  - Display mean, median and 1.5 IQR outliers count.
+  - Identify data types of each variable.
+  - Identify count of unique values per variable.
+  - Identify the normal approximation using pvalue.
+  - Show a dataset sample to understand data content.
+  - Visualize trends with histograms and scatter plot.
+"""
