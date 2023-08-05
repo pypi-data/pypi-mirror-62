eb215e124460c8186612703fd3dc39650e7e0cde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:52 +0000

    Add package bfit/3.1.1/bfit-3.1.1.tar.gz

---
+++ packages/bfit/bfit-3.1.1.tar.gz/bfit-3.1.1/bfit/backend/colors.py
@@ -0,0 +1,17 @@
+# color definitions
+foreground      = 'white'
+background      = 'black'
+fieldbackground = 'grey30'
+focusbackground = 'grey'
+
+selected        = 'green2'
+
+readonly        = 'grey10'
+disabled        = 'grey40'
+
+entry_grey      = 'grey45'
+entry_white     = 'grey92'
+
+tab             = 'grey'
+
+font            = 'TkFixedFont 10'
