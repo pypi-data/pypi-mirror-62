1f0ff0499dc9b219223aa60fb7a6f5034ae1469c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:49 +0000

    Add package http-types/0.0.10/http_types-0.0.10-py2.py3-none-any.whl

---
+++ packages/http-types/http_types-0.0.10-py2.py3-none-any.whl/http_types/types.py
@@ -0,0 +1,148 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Any, Union, Mapping, Sequence, Optional
+import enum
+
+"""
+HTTP request or response headers. Array-valued header values can be represented with a comma-separated string.
+"""
+Headers = Mapping[str, Union[str, Sequence[str]]]
+
+"""
+HTTP request query parameters.
+"""
+Query = Mapping[str, Union[str, Sequence[str]]]
+
+"""
+HTTP request protocol.
+"""
+
+
+class Protocol(enum.Enum):
+    HTTP = "http"
+    HTTPS = "https"
+
+
+"""
+HTTP method.
+"""
+
+
+class HttpMethod(enum.Enum):
+    GET = "get"
+    PUT = "put"
+    POST = "post"
+    PATCH = "patch"
+    DELETE = "delete"
+    OPTIONS = "options"
+    TRACE = "trace"
+    HEAD = "head"
+    CONNECT = "connect"
+
+
+@dataclass
+class Request:
+    """
+    HTTP request.
+    """
+
+    """
+    HTTP request body as JSON. Could be dictionary, list, or string.
+    """
+    bodyAsJson: Optional[Any]
+
+    """
+    Timestamp when the request was initiated.
+    """
+    timestamp: Optional[datetime]
+
+    """
+    Request body. None if no body should exit (ie GET, DELETE). Empty string if empty.
+    """
+    body: Optional[str]
+
+    """
+    Request method.
+    """
+    method: HttpMethod
+
+    """
+    Request headers.
+    """
+    headers: Headers
+
+    """
+    Query parameters.
+    """
+    query: Query
+
+    """
+    Request host, possibly including port number.
+    """
+    host: str
+
+    """
+    Full request path.
+    Example value: '/v1/pets?id=234'
+    """
+    path: str
+
+    """
+    Request pathname, not containing query parameters etc.
+    Example value: '/v1/pets'
+    """
+    pathname: str
+
+    """
+    Request protocol.
+    """
+    protocol: Protocol
+
+
+@dataclass
+class Response:
+    """
+    HTTP response.
+    """
+
+    """
+    Response body as JSON. Could be dictionary, list, or string.
+    """
+    bodyAsJson: Optional[Any]
+
+    """
+    Timestamp when the response was sent.
+    """
+    timestamp: Optional[datetime]
+
+    """
+    Response body.
+    """
+    body: str
+
+    """ Response status code."""
+    statusCode: int
+
+    """ Response headers. """
+    headers: Headers
+
+
+@dataclass
+class HttpExchange:
+    """
+    HTTP request-response pair.
+    """
+
+    request: Request
+    response: Response
+
+
+__all__ = [
+    "Request",
+    "Response",
+    "HttpExchange",
+    "Headers",
+    "Query",
+    "Protocol",
+    "HttpMethod",
+]
