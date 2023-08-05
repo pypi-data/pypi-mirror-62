b0a6ff6a910791e29e114c5bcfd9261b0bf0b8b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:35 +0000

    Add package dicom-parser/0.1.0/dicom_parser-0.1.0.tar.gz

---
+++ packages/dicom-parser/dicom_parser-0.1.0.tar.gz/dicom_parser-0.1.0/dicom_parser/utils/siemens/private_tags.py
@@ -0,0 +1,16 @@
+"""
+Siemens specific private tags they may not be accessible by keyword using
+`pydicom <https://github.com/pydicom/pydicom>`_.
+
+"""
+
+
+# Based on `this <https://nipy.org/nibabel/dicom/siemens_csa.html>`_ article.
+SIEMENS_PRIVATE_TAGS = {
+    "CSAImageHeaderType": ("0029", "1008"),
+    "CSAImageHeaderVersion": ("0029", "1009"),
+    "CSAImageHeaderInfo": ("0029", "1010"),
+    "CSASeriesHeaderType": ("0029", "1018"),
+    "CSASeriesHeaderVersion": ("0029", "1019"),
+    "CSASeriesHeaderInfo": ("0029", "1020"),
+}
