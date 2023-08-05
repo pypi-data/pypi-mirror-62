2d23fdd05015a7cfb07d41c7043feaf7040e2289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:28 +0000

    Add package PyRastreamentoCorreios/0.2/PyRastreamentoCorreios-0.2.tar.gz

---
+++ packages/PyRastreamentoCorreios/PyRastreamentoCorreios-0.2.tar.gz/PyRastreamentoCorreios-0.2/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'PyRastreamentoCorreios',        
+  packages = ['PyRastreamentoCorreios'],   
+  version = '0.2',      
+  license='MIT',        
+  description = 'Biblioteca de rastreamento dos correios',   
+  author = 'Gabriell Huver',                   
+  author_email = 'gabriell.is.huver@gmail.com',    
+  url = 'https://github.com/gabriellhuver/PyRastreamento-correios', 
+  download_url = 'https://github.com/gabriellhuver/PyRastreamento-correios/archive/first.tar.gz', 
+  keywords = ['CORREIOS', 'RASTREAMENTO', 'LINKCORREIOS'],   
+  install_requires=[           
+          'requests',
+          'beautifulsoup4'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
