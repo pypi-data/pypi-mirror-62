22c0c9faddac1ebeb791aa19e19cac24e8f5fa3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:26 +0000

    Add package eme/1.5.1.4/eme-1.5.1.4.tar.gz

---
+++ packages/eme/eme-1.5.1.4.tar.gz/eme-1.5.1.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+
+setup(name='eme',
+      version='1.5.1.4',
+      description='Multi-purpose web framework',
+      url='https://github.com/oboforty/eme',
+      author='oboforty',
+      author_email='rajmund.csombordi@hotmail.com',
+      license='MIT',
+      zip_safe=False,
+      packages=['eme', 'eme/vendor', 'eme/auth', 'eme_tools', 'eme_tools/commands'],
+      package_data={'eme_tools': ['content/*.tpl', 'content/*.zip']},
+      entry_points={
+          'console_scripts': [
+              'eme = eme_tools.cli:main',
+          ],
+      },
+      install_requires=[
+          'flask',
+          'flask-login',
+          'flask-mail',
+          'bcrypt',
+
+          'sqlalchemy',
+          'redis',
+          'faker',
+          'inflect'
+      ])
