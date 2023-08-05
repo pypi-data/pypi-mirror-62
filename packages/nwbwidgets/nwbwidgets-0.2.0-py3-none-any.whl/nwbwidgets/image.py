e53696089d6926c4802b43d5b7ea2f3dac8ae4cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:42 +0000

    Add package nwbwidgets/0.2.0/nwbwidgets-0.2.0-py3-none-any.whl

---
+++ packages/nwbwidgets/nwbwidgets-0.2.0-py3-none-any.whl/nwbwidgets/image.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+import ipywidgets as widgets
+import pynwb
+from pynwb.image import GrayscaleImage, ImageSeries, RGBImage
+from .base import fig2widget
+
+
+def show_image_series(image_series: ImageSeries, neurodata_vis_spec: dict):
+
+    def show_image(index=0):
+        fig, ax = plt.subplots(subplot_kw={'xticks': [], 'yticks': []})
+        ax.imshow(image_series.data[index][:, :], cmap='gray')
+        return fig2widget(fig)
+
+    def on_index_change(change):
+        show_image(change.new)
+    slider = widgets.IntSlider(value=0, min=0,
+                               max=image_series.data.shape[0] - 1,
+                               orientation='horizontal')
+    slider.observe(on_index_change, names='value')
+    output = show_image()
+
+    return widgets.VBox([output, slider])
+
+
+def show_index_series(index_series, neurodata_vis_spec: dict):
+    show_timeseries = neurodata_vis_spec[pynwb.TimeSeries]
+    series_widget = show_timeseries(index_series)
+
+    indexed_timeseries = index_series.indexed_timeseries
+    image_series_widget = show_image_series(indexed_timeseries,
+                                            neurodata_vis_spec)
+
+    return widgets.VBox([series_widget, image_series_widget])
+
+
+def show_grayscale_image(grayscale_image: GrayscaleImage, neurodata_vis_spec=None):
+    fig, ax = plt.subplots()
+    plt.imshow(grayscale_image.data[:], 'gray')
+    plt.axis('off')
+
+    return fig
+
+
+def show_rbg_image(rgb_image: RGBImage, neurodata_vis_spec=None):
+    fig, ax = plt.subplots()
+    plt.imshow(rgb_image.data[:])
+    plt.axis('off')
+
+    return fig
---
+++ packages/nwbwidgets/nwbwidgets-0.2.0-py3-none-any.whl/nwbwidgets/utils/units.py
@@ -0,0 +1,126 @@
+import numpy as np
+import pynwb
+
+from bisect import bisect_right, bisect_left
+
+
+def get_spike_times(units: pynwb.misc.Units, index, in_interval):
+    """Use bisect methods to efficiently retrieve spikes from a given unit in a given interval
+
+    Parameters
+    ----------
+    units: pynwb.misc.Units
+    index: int
+    in_interval: start and stop times
+
+    Returns
+    -------
+
+    """
+    st = units['spike_times']
+    unit_start = 0 if index == 0 else st.data[index - 1]
+    unit_stop = st.data[index]
+    start_time, stop_time = in_interval
+
+    ind_start = bisect_left(st.target, start_time, unit_start, unit_stop)
+    ind_stop = bisect_right(st.target, stop_time, ind_start, unit_stop)
+
+    return np.asarray(st.target[ind_start:ind_stop])
+
+
+def get_min_spike_time(units: pynwb.misc.Units):
+    """Efficiently retrieve the first spike time across all units
+
+    Parameters
+    ----------
+    units: pynwb.misc.Units
+
+    Returns
+    -------
+
+    """
+    st = units['spike_times']
+    inds = [0] + list(st.data[:-1])
+    first_spikes = [st.target.data[i] for i in inds]
+    return np.min(first_spikes)
+
+
+def get_max_spike_time(units: pynwb.misc.Units):
+    """Efficiently retrieve the last spike time across all units
+
+    Parameters
+    ----------
+    units: pynwb.misc.Units
+
+    Returns
+    -------
+
+    """
+    st = units['spike_times']
+    inds = list(st.data[:] - 1)
+    last_spikes = [st.target.data[i] for i in inds]
+    return np.max(last_spikes)
+
+
+def align_by_times(units: pynwb.misc.Units, index, starts, stops):
+    """
+    Args:
+        units: pynwb.misc.Units
+        index: int
+        starts: array-like
+        stops: array-like
+    Returns:
+        np.array
+    """
+    return np.array([get_spike_times(units, index, [a, b]) - a for a, b in zip(starts, stops)])
+
+
+def align_by_trials(units: pynwb.misc.Units, index, start_label='start_time',
+                    stop_label=None, before=0., after=1.):
+    """
+    Args:
+        units
+        start_label: str
+            default: 'start_time'
+        stop_label: str
+            default: None (just align to start_time)
+        before: float
+            time after start_label in secs (positive goes back in time)
+        after: float
+            time after stop_label in secs (positive goes forward in time)
+    Returns:
+        np.array(shape=(n_trials, n_time, ...))
+    """
+    trials = units.get_ancestor('NWBFile').trials
+    return align_by_time_intervals(units, index, trials, start_label, stop_label, before, after)
+
+
+def align_by_time_intervals(units: pynwb.misc.Units, index, intervals, start_label='start_time',
+                            stop_label='stop_time', before=0., after=0., trials_select=None):
+    """
+    Args:
+        units: time-aware neurodata_type
+        index: int
+        intervals: pynwb.epoch.TimeIntervals
+        start_label: str
+            default: 'start_time'
+        stop_label: str
+            default: 'stop_time'
+        before: float
+            time after start_label in secs (positive goes back in time)
+        after: float
+            time after stop_label in secs (positive goes forward in time)
+        trials_select: array_like, optional
+            sub-selects specific trials
+    Returns:
+        np.array(shape=(n_trials, n_time, ...))
+    """
+    if stop_label is None:
+        stop_label = 'start_time'
+    if trials_select is None:
+        starts = np.array(intervals[start_label][:]) - before
+        stops = np.array(intervals[stop_label][:]) + after
+    else:
+        starts = np.array(intervals[start_label][trials_select]) - before
+        stops = np.array(intervals[stop_label][trials_select]) + after
+    return [x - before for x in align_by_times(units, index, starts, stops)]
