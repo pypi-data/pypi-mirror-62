7570935baa5ea491db2eee06203de40e9beb8cf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:05 +0000

    Add package neat-panda/0.9.5.0/neat_panda-0.9.5.0-py3-none-any.whl

---
+++ packages/neat-panda/neat_panda-0.9.5.0-py3-none-any.whl/neat_panda/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Neat Panda."""
+
+__author__ = """Henric Sundberg"""
+__email__ = "henric.sundberg@gmail.com"
+__version__ = "0.9.5.0"
+
+from ._tidy import spread, gather
+
+# from ._caretaker import clean_column_names, _clean_column_names
+from ._caretaker import clean_column_names
+from ._set_operations import difference, intersection, symmetric_difference, union
+from ._helpers import _get_version_from_toml
+
