f1e0885138e719be2dd8400be378ac7b07fe6ae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:11 +0000

    Add package segmentationextractors/0.1.0/segmentationextractors-0.1.0.tar.gz

---
+++ packages/segmentationextractors/segmentationextractors-0.1.0.tar.gz/segmentationextractors-0.1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+d = {}
+exec(open("segmentationextractors/version.py").read(), None, d)
+version = d['version']
+pkg_name = "segmentationextractors"
+with open('README.md') as rd:
+    long_description = rd.read()
+
+setup(
+    name=pkg_name,
+    version=version,
+    author="Saksham Sharda, Ben Dichter",
+    author_email="saksham20.sharda@gmail.com",
+    description="Python module for extracting optical physiology ROIs and traces for various file types and formats",
+    url="https://github.com/ben-dichter-consulting/segmentationextractors",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    package_data={},
+    install_requires=[
+        'pynwb',
+        'sima',
+        'dill',
+        'lazy_ops'
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research"
+    )
+)
