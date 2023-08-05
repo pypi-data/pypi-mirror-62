47d467b9204f389c0d6dc0eac84a7bfcfb1ef64d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:00 +0000

    Add package dryable/1.0.5/dryable-1.0.5-py3-none-any.whl

---
+++ packages/dryable/dryable-1.0.5-py3-none-any.whl/dryable/__init__.py
@@ -0,0 +1,35 @@
+import collections
+import functools
+import logging
+
+class Dryable:
+    _dryRun = collections.defaultdict( lambda: False )
+    logger = logging.getLogger( 'dryable' )
+
+    def __init__( self, value = None, *, label = 'default' ):
+        self._value = value
+        self._label = label
+
+    @classmethod
+    def set( cls, value, label = 'default' ):
+        if value:
+            cls._dryRun[ label ] = True
+        else:
+            cls._dryRun[ label ] = False
+
+    def __call__( self, function ):
+        @functools.wraps( function )
+        def _decorated( * args, ** kwargs ):
+            if self._dryRun[ self._label ]:
+                argsString = ', '.join( [ str( argument ) for argument in args ] )
+                kwargsString = ', '.join( [ '{}={}'.format( key, value ) for ( key, value ) in kwargs.items() ] )
+                if len( kwargs ) > 0:
+                    if len( args ) > 0:
+                        kwargsString = ', {}'.format( kwargsString )
+                Dryable.logger.info( 'dryable[{label}] skip: {function}( {args}{kwargs} )'.format( label = self._label, function = function.__qualname__, args = argsString, kwargs = kwargsString ) )
+                return self._value
+            return function( * args, ** kwargs )
+
+        return _decorated
+
+set = Dryable.set
