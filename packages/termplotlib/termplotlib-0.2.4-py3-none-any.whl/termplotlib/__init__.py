f6415e6bc4355d0b04596a2360bb206370553206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:44 +0000

    Add package termplotlib/0.2.4/termplotlib-0.2.4-py3-none-any.whl

---
+++ packages/termplotlib/termplotlib-0.2.4-py3-none-any.whl/termplotlib/__init__.py
@@ -0,0 +1,23 @@
+from .__about__ import (
+    __author__,
+    __copyright__,
+    __email__,
+    __license__,
+    __status__,
+    __version__,
+)
+from .figure import Figure, figure
+from .subplot import SubplotGrid, subplot_grid
+
+__all__ = [
+    "__author__",
+    "__email__",
+    "__copyright__",
+    "__license__",
+    "__version__",
+    "__status__",
+    "Figure",
+    "figure",
+    "SubplotGrid",
+    "subplot_grid",
+]
