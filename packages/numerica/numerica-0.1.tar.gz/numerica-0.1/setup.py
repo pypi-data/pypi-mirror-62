9911d95b5ee80a78962d1d0c1f411560f458465f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:57 +0000

    Add package numerica/0.1/numerica-0.1.tar.gz

---
+++ packages/numerica/numerica-0.1.tar.gz/numerica-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='numerica',
+      version='0.1',
+      description='Numerical Analysis methods with Python (experimental)',
+      url='http://github.com/ramesaliyev/numerica',
+      author='Rames Aliyev',
+      author_email='creator@ramesaliyev.com',
+      license='MIT',
+      packages=['numerica'],
+      zip_safe=False)
