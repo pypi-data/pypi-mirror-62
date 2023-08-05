85f14120887804adf1ede3a3766c21ddd35ffb00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:30 +0000

    Add package config-field/0.2.5/config_field-0.2.5.tar.gz

---
+++ packages/config-field/config_field-0.2.5.tar.gz/config_field-0.2.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+from config_field import __version__
+
+setup(
+    name='config_field',
+    version=__version__,
+    packages=find_packages(),
+    author='bzdvdn',
+    author_email='bzdv.dn@gmail.com',
+    url='https://github.com/bzdvdn/config_field',
+)
