e827baa717acf2c4e0f8d5f092890b23a8058498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:07 +0000

    Add package code-timer/v1.0.1/code_timer-v1.0.1.tar.gz

---
+++ packages/code-timer/code_timer-v1.0.1.tar.gz/code_timer-v1.0.1/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+
+with open('README.md') as f:
+    long_desc = f.read()
+
+setup(
+  name='code_timer',
+  packages=['code_timer'],
+  version='v1.0.1',
+  license='MIT License',
+  description='Custom timer for your Python coding pleasure',
+  long_description=long_desc,
+  long_description_content_type="text/markdown",
+  author='Stephen Gemin',
+  author_email='s.gemin88@gmail.com',
+  url='https://github.com/StephenGemin/code_timer',
+  download_url='https://github.com/StephenGemin/code_timer/archive/v1.0.1.tar.gz',
+  keywords=["decorator", "decorators", "code timer", "timing",
+            "code timing", "context manager", "profiling"],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
