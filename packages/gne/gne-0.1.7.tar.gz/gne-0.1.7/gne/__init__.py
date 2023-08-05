cc3144fa43218aa3661cab0fa5f048e35ab2384d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:21 +0000

    Add package gne/0.1.7/gne-0.1.7.tar.gz

---
+++ packages/gne/gne-0.1.7.tar.gz/gne-0.1.7/gne/__init__.py
@@ -0,0 +1,27 @@
+from .utils import pre_parse, remove_noise_node, config
+from gne.extractor import ContentExtractor, TitleExtractor, TimeExtractor, AuthorExtractor
+
+
+class GeneralNewsExtractor:
+    def extract(self,
+                html,
+                title_xpath='',
+                author_xpath='',
+                publish_time_xpath='',
+                host='',
+                noise_node_list=None,
+                with_body_html=False):
+        element = pre_parse(html)
+        remove_noise_node(element, noise_node_list)
+        content = ContentExtractor().extract(element, host, with_body_html)
+        title = TitleExtractor().extract(element, title_xpath=title_xpath)
+        publish_time = TimeExtractor().extractor(element, publish_time_xpath=publish_time_xpath)
+        author = AuthorExtractor().extractor(element, author_xpath=author_xpath)
+        result = {'title': title,
+                  'author': author,
+                  'publish_time': publish_time,
+                  'content': content[0][1]['text'],
+                  'images': content[0][1]['images']}
+        if with_body_html or config.get('with_body_html', False):
+            result['body_html'] = content[0][1]['body_html']
+        return result
