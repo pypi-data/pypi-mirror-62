d26c6a8f472e78683c3e51b7fd89cf94a2a40592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:44 +0000

    Add package droughtMoPro/0.1.3.3/droughtMoPro-0.1.3.3.tar.gz

---
+++ packages/droughtMoPro/droughtMoPro-0.1.3.3.tar.gz/droughtMoPro-0.1.3.3/README.md
@@ -0,0 +1,9 @@
+# droughtMoPro
+Set of scripts to generate drought monitoring products.
+
+## Dependencies
+This package requires the following modules in order to works.
+* dask
+* toolz
+* xarray
+
---
+++ packages/droughtMoPro/droughtMoPro-0.1.3.3.tar.gz/droughtMoPro-0.1.3.3/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+import os
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+     name='droughtMoPro',
+     version='v0.1.3.3',
+     author='R. A. Real-Rangel',
+     author_email='rrealr@iingen.unam.mx',
+     description='Generator of drought monitoring products.',
+     license='GPL-3.0',
+     keywords="drought monitoring mapping series",
+     url='https://github.com/rrealrangel/droughtMoPro',
+     packages=['droughtMoPro'],
+     long_description=read('README.md'),
+     classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Scientific/Engineering :: Hydrology",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+        ],
+     install_requires=[
+         'datetime',
+         'dask',
+         'netcdf4',
+         'numpy',
+         'openpyxl',
+         'pandas',
+         'pathlib2',
+         'scipy',
+         'toml',
+         'toolz',
+         'xarray',
+         ],
+     zip_safe=False
+     )
