e488f5e832ffa4785043c132afe773e17dcb2658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:01 +0000

    Add package bandaid/1.0.4/bandaid-1.0.4-py3-none-any.whl

---
+++ packages/bandaid/bandaid-1.0.4-py3-none-any.whl/bandaid/agent.py
@@ -0,0 +1,78 @@
+"""Gets latest band data for a particular band and adds to user watchlist."""
+from bs4 import BeautifulSoup as bs
+import requests
+import argparse
+
+__version__ = "1.0.4"
+
+
+def printlogo():
+    """
+    Prints logo
+    Returns nothing
+    """
+    print("")
+    print("     ;;;;;;;;;;;;;;;;;;; ")
+    print("     ;;;;;;;;;;;;;;;;;;; ")
+    print("     ;                 ; ")
+    print("     ;     bandaid     ; ")
+    print("     ;                 ; ")
+    print("     ;  +-----------+  ; ")
+    print("     ;  |by JC 2020 |  ; ")
+    print("     ;  +-----------+  ; ")
+    print("     ;                 ; ")
+    print(",;;;;;            ,;;;;; ")
+    print(";;;;;;            ;;;;;; ")
+    print("`;;;;'            `;;;;' ")
+    print("")
+
+
+def watchlist(bandname):
+    """
+    Add band to watchlist, initialize watchlist service and db is doesn't exist
+    """
+
+
+def getBand(bandname):
+    """
+    Get band page and related data
+    """
+    baseurl = "https://www.bandsintown.com/{}"
+    r = requests.get(baseurl.format(bandname))
+    if r.status_code == 200:
+        if "No upcoming events</div>" in r.text:
+            print(f"No upcoming events for {bandname}.")
+        else:
+            print(f"{bandname} is on tour!")
+        exit()
+    else:
+        exit(1)
+
+
+def prepper():
+    """
+    Process all runtime arguments
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-b', '--band', dest='bandname',
+                        help="band to lookup", type=str, nargs='+')
+    parser.add_argument('-w', '--watchlist', dest='watchlist',
+                        action='store_true', help="add to watchlist")
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    """
+    Main function that runs everything
+    """
+    args = prepper()
+    printlogo()
+    if args.bandname:
+        getBand(" ".join(args.bandname))
+    else:
+        exit('Must set band name -h for help.')
+
+
+if __name__ == "__main__":
+    main()
