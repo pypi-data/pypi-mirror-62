6faf255743ba64679c7014110218841e816dfcbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:20 +0000

    Add package botw-havok/0.1.1/botw_havok-0.1.1.tar.gz

---
+++ packages/botw-havok/botw_havok-0.1.1.tar.gz/botw_havok-0.1.1/botw_havok/classes/util/class_map.py
@@ -0,0 +1,17 @@
+from ..hkpBvCompressedMeshShape import hkpBvCompressedMeshShape
+from ..hkpPhysicsData import hkpPhysicsData
+from ..hkpPhysicsSystem import hkpPhysicsSystem
+from ..hkpRigidBody import hkpRigidBody
+from ..hkpStaticCompoundShape import hkpStaticCompoundShape
+from ..hkRootLevelContainer import hkRootLevelContainer
+from ..StaticCompoundInfo import StaticCompoundInfo
+
+hk_class_map = {
+    "hkpBvCompressedMeshShape": hkpBvCompressedMeshShape,
+    "hkpRigidBody": hkpRigidBody,
+    "hkpPhysicsData": hkpPhysicsData,
+    "hkpPhysicsSystem": hkpPhysicsSystem,
+    "hkRootLevelContainer": hkRootLevelContainer,
+    "StaticCompoundInfo": StaticCompoundInfo,
+    "hkpStaticCompoundShape": hkpStaticCompoundShape,
+}
