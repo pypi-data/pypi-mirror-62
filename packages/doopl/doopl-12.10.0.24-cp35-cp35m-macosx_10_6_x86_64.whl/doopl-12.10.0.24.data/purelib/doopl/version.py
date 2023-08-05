a5e251d270d7a86e7c2a27570520e5498190ed40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:18 +0000

    Add package doopl/12.10.0.24/doopl-12.10.0.24-cp35-cp35m-macosx_10_6_x86_64.whl

---
+++ packages/doopl/doopl-12.10.0.24-cp35-cp35m-macosx_10_6_x86_64.whl/doopl-12.10.0.24.data/purelib/doopl/version.py
@@ -0,0 +1,12 @@
+# --------------------------------------------------------------------------
+# Source file provided under Apache License, Version 2.0, January 2004,
+# http://www.apache.org/licenses/
+# (c) Copyright IBM Corp. 2018
+# --------------------------------------------------------------------------
+
+# gendoc: ignore
+# This file is generated !
+opl_version_major = 12
+opl_version_minor = 9
+opl_version_micro = 0
+opl_version_string = '12.9.0'
