4be7d5e60999e5482f9d523f318eeeb605aeb48b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:37 +0000

    Add package santodigital-bigquery/1.0.1/santodigital-bigquery-1.0.1.tar.gz

---
+++ packages/santodigital-bigquery/santodigital-bigquery-1.0.1.tar.gz/santodigital-bigquery-1.0.1/src/santodigital_bigquery/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Santo Digital BigQuery."""
+
+from __future__ import absolute_import
+from santodigital_bigquery import io
+
+
+
+__author__ = """Lucas Magalhaes"""
+__email__ = 'lucas.magalhaes@santodigital.com.br'
+__version__ = '1.0.1'
---
+++ packages/santodigital-bigquery/santodigital-bigquery-1.0.1.tar.gz/santodigital-bigquery-1.0.1/src/santodigital_bigquery/io/__init__.py
@@ -0,0 +1,3 @@
+from __future__ import absolute_import
+
+from santodigital_bigquery.io import *
