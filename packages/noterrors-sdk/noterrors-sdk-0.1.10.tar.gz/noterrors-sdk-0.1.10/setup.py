26c99f647e9f21d3697177cb54886f55ed1dc5f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:39 +0000

    Add package noterrors-sdk/0.1.10/noterrors-sdk-0.1.10.tar.gz

---
+++ packages/noterrors-sdk/noterrors-sdk-0.1.10.tar.gz/noterrors-sdk-0.1.10/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='noterrors-sdk',
+     version='0.1.10',
+     author="noterrors",
+     author_email="jarod@ukr.net",
+     description="noterrors sdk client",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/jarodut/noterrors-sdk",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+)
