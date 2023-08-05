35742a884ed96e5d4d94c56c0640ee4abfca5081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:39 +0000

    Add package grayskull/0.1.3/grayskull-0.1.3.tar.gz

---
+++ packages/grayskull/grayskull-0.1.3.tar.gz/grayskull-0.1.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="grayskull",
+    packages=find_packages(),
+    entry_points={"console_scripts": ["grayskull = grayskull.__main__:main"]},
+    use_scm_version={"write_to": "grayskull/_version.py"},
+    setup_requires=["setuptools-scm", "setuptools>=30.3.0"],
+    python_requires=">=3.7",
+    install_requires=[
+        "requests",
+        "ruamel.yaml >=0.15.3",
+        "ruamel.yaml.jinja2",
+        "stdlib-list",
+        "pip",
+        "setuptools>=30.3.0",
+        "fuzzywuzzy",
+        "python-Levenshtein",
+        "progressbar2",
+        "colorama",
+    ],
+    extras_require={"testing": ["pytest"]},
+    url="https://github.com/marcelotrevisani/grayskull",
+    license="MIT",
+    author="Marcelo Duarte Trevisani",
+    author_email="marceloduartetrevisani@gmail.com",
+    description="Project to generate recipes for conda packages. "
+    "Skeletor's main goal is to conquer the mysterious fortress of"
+    " Castle Grayskull. If he succeeds, Skeletor would be able to conquer not"
+    " only Eternia, but the whole universe.",
+)
