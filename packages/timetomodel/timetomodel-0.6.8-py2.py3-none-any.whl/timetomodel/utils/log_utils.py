21db15ea6cf99590d8b71aab0361ca733ce14c8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:39 +0000

    Add package timetomodel/0.6.8/timetomodel-0.6.8-py2.py3-none-any.whl

---
+++ packages/timetomodel/timetomodel-0.6.8-py2.py3-none-any.whl/timetomodel/utils/log_utils.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+import time
+
+
+class Timer(object):
+    """Usage example:
+
+    >>> with Timer("Calling max function"):
+    >>>     a = max(range(10**6))
+    <<<
+    [Calling max function] Starting (at Friday, October 18, 2019 16:16:18) ...
+    [Calling max function] Elapsed: 35 ms
+    """
+
+    def __init__(self, name=None, filename=None):
+        self.name = name
+        self.filename = filename
+
+    def __enter__(self):
+        self.tstart = time.time()
+        print(
+            "[%s] Starting (at %s) ..."
+            % (
+                self.name,
+                datetime.fromtimestamp(self.tstart).strftime("%A, %B %d, %Y %H:%M:%S"),
+            )
+        )
+
+    def __exit__(self, type, value, traceback):
+        duration = time.time() - self.tstart
+        if duration > 1:
+            message = "Elapsed: %.2f seconds" % duration
+        else:
+            message = "Elapsed: %.0f ms" % (duration * 1000)
+        if self.name:
+            message = "[%s] " % self.name + message
+        print(message)
+        if self.filename:
+            with open(self.filename, "a") as file:
+                print(str(datetime.now()) + ": ", message, file=file)
