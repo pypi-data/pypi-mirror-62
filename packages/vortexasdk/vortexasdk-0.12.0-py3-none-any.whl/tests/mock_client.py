365824d5ee99f64df98b6972f257b84a15719d75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:56 +0000

    Add package vortexasdk/0.12.0/vortexasdk-0.12.0-py3-none-any.whl

---
+++ packages/vortexasdk/vortexasdk-0.12.0-py3-none-any.whl/tests/mock_client.py
@@ -0,0 +1,40 @@
+from typing import List, Dict
+
+import jsons
+
+from vortexasdk.api import ID
+from vortexasdk.abstract_client import AbstractVortexaClient
+from vortexasdk.endpoints.endpoints import (
+    CORPORATIONS_REFERENCE,
+    VESSELS_REFERENCE,
+    PRODUCTS_REFERENCE,
+    VESSEL_MOVEMENTS_RESOURCE,
+)
+
+
+def _read(example_file) -> List[Dict]:
+    with open(f"tests/api/examples/{example_file}", "r") as f:
+        return jsons.loads(f.read(), List)
+
+
+example_corporations: List[Dict] = _read("corporations.json")
+example_vessels: List[Dict] = _read("vessels.json")
+example_products: List[Dict] = _read("products.json")
+example_vessel_movements: List[Dict] = _read("vessel_movements.json")
+example_cargo_movements: List[Dict] = _read("vessel_movements.json")
+
+
+class MockVortexaClient(AbstractVortexaClient):
+    _results = {
+        CORPORATIONS_REFERENCE: example_corporations,
+        VESSELS_REFERENCE: example_vessels,
+        PRODUCTS_REFERENCE: example_products,
+        VESSEL_MOVEMENTS_RESOURCE: example_vessel_movements,
+    }
+
+    def get_reference(self, resource: str, id: ID) -> List[Dict]:
+        entities = MockVortexaClient._results[resource]
+        return [e for e in entities if e["id"] == id]
+
+    def search(self, resource: str, **data) -> List:
+        return MockVortexaClient._results[resource]
