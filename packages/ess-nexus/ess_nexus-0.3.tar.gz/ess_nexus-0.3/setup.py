fb4b90726f4d4b04c6c7a544107939564a340673
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:27 +0000

    Add package ess-nexus/0.3/ess_nexus-0.3.tar.gz

---
+++ packages/ess-nexus/ess_nexus-0.3.tar.gz/ess_nexus-0.3/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
+setuptools.setup(
+
+     name='ess_nexus',
+
+     version='0.3',
+
+     author='Natalia Milas',
+
+     author_email='natalia.milas@ess.eu',
+
+     description='Utility class for saving nexus files on ESS data fromat',
+
+     long_description='Simple class that creates an hdf5 file according to a predifined format to be used internally at ESS Accelerator division',
+
+     long_description_content_type="text/markdown",
+
+     packages=setuptools.find_packages(),
+
+     classifiers=[
+
+         "Programming Language :: Python :: 3",
+
+         "License :: OSI Approved :: MIT License",
+
+         "Operating System :: OS Independent",
+
+     ],
+
+ )
