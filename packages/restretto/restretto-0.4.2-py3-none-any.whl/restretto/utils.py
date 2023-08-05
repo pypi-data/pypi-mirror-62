ce657d4388d7ab8db70022ffd6182585444dc6ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:25 +0000

    Add package restretto/0.4.2/restretto-0.4.2-py3-none-any.whl

---
+++ packages/restretto/restretto-0.4.2-py3-none-any.whl/restretto/utils.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import yaml
+from jinja2 import Template
+
+
+def apply_context(src, context={}):
+    """Apply context to dict"""
+    return yaml.full_load(Template(yaml.dump(src)).render(context))
+
+
+def json_path(path, data):
+    """Extract property by path"""
+    fragments = path.split(".")
+    src = data
+    for p in fragments:
+        src = src.get(p, {})
+    return src
