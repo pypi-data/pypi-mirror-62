cba092876428f9698d1f30f42b05336937dc7c8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:52 +0000

    Add package surfgen/0.2.2/surfgen-0.2.2.tar.gz

---
+++ packages/surfgen/surfgen-0.2.2.tar.gz/surfgen-0.2.2/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+from setuptools import setup
+setup(name='surfgen',
+version='0.2.2',
+description='Tools for creating slab models',
+author='Zheng-Da He',
+author_email='z.he@fz-juelich.de, jameshzd@mail.ustc.edu.cn, zhengdahe.electrocatalysis@gmail.com',
+license='GPL-3.0',
+packages=setuptools.find_packages(),
+requirements=['ase>=3.19.0', 'pymatgen>=2020.1.28'],
+zip_safe=False)
