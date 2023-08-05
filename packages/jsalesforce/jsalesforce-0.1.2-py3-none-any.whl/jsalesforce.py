e0df2fae1be192f1796dbc6afd3bb0ff03eb0278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:48 +0000

    Add package jsalesforce/0.1.2/jsalesforce-0.1.2-py3-none-any.whl

---
+++ packages/jsalesforce/jsalesforce-0.1.2-py3-none-any.whl/jsalesforce.py
@@ -0,0 +1,299 @@
+from simple_salesforce import Salesforce
+from pprint import pprint
+import pandas as pd
+from datetime import datetime
+
+
+def login(username, password, security_token):
+    return jSalesforce(
+        username=username, password=password, security_token=security_token,
+    )
+
+
+class SalesforceError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
+
+
+class MissingDataError(SalesforceError):
+    def __init__(self, keys, data, msg=None):
+        self.data = data
+        if msg == None:
+            msg = f"Missing data for {keys} on the data: \n{data}"
+        super().__init__(msg)
+
+
+class DuplicatesError(SalesforceError):
+    def __init__(self, n_items, msg=None):
+        self.n_items = n_items
+        if msg == None:
+            msg = f"There are {n_items} duplicates in salesforce."
+        super().__init__(msg)
+
+
+class jSalesforce(Salesforce):
+    def __init__(self, username, password, security_token):
+        super().__init__(username, password, security_token)
+        self.username = username
+
+    def active_users(self):
+        """Returns a list salesforce users that are active, usually the active
+        juniors.
+
+        Parameters
+        ----------
+        sf: Salesforce connector from login function
+
+        Returns
+        -------
+        list(dict): Returns a list of dictionaries. Each dict represents a user and has the following keys:
+            Id, Email, isActive, FirstName, LastName, isActive
+        """
+        users = self.query(
+            "SELECT Id, Email, isActive, FirstName, LastName FROM User WHERE isActive=True"
+        )["records"]
+        return users
+
+    def user_id(self, email):
+        """Returns the salesforce id from the user (usually a junior) with that
+        email."""
+        users = self.active_users()
+        try:
+            sf_id = next(user["Id"] for user in users if user["Email"] == email)
+        except StopIteration:
+            raise SalesforceError(f"No user with email {email} found.")
+        return sf_id
+
+    def create_lead(self, data):
+        """Creates a Lead from a pandas Series. Checks keys and data.
+
+        Parameters
+        ----------
+        sf:
+        data: pandas Series containing at least ['Email', 'FirstName', 'LastName', 'Company', 'OwnerId', 'Funktion__c']
+
+        Returns
+        -------
+        The result from saleselforce API
+
+        Raises
+        ------
+        MissingDataErro
+        """
+        allowed_keys = {
+            "Email",
+            "FirstName",
+            "LastName",
+            "Company",
+            "Funktion__c",
+            "LeadSource",
+            "OwnerId",
+        }
+        required_keys = {
+            "Email",
+            "FirstName",
+            "LastName",
+            "Company",
+            "Funktion__c",
+            "OwnerId",
+        }
+        data = data.dropna()
+        data_keys = set(data.keys())
+        missing_keys = required_keys - data_keys
+        if len(missing_keys) > 0:
+            raise MissingDataError(missing_keys, data)
+
+        not_allowed_keys = data_keys - allowed_keys
+        data = data.drop(labels=not_allowed_keys).to_dict()
+
+        return self.Lead.create(data)
+
+    def lead_id_from_email(self, email):
+        """Returns the salesforce Id as str or None."""
+        q = f"SELECT Id FROM Lead WHERE Email='{email}' AND IsConverted=False"
+        records = self.query(q)["records"]
+        if len(records) == 0:
+            return None
+        return records[0]["Id"]
+
+    def contact_id_from_email(self, email):
+        """Returns the salesforce Id as str or None."""
+        q = f"SELECT Id FROM Contact WHERE Email='{email}'"
+        records = self.query(q)["records"]
+        if len(records) == 0:
+            return None
+        return records[0]["Id"]
+
+    def create_new_leads(self, df):
+        """Creates all leads from the DataFrame.
+
+        Parameters
+        ----------
+        self:
+        df: pandas DataFrame with columns = [Company, FirstName, LastName, Funktion__c, Email, Subject, Owner Email, Sales_Aktivit_t__c, ActivityDate, Result]
+
+        Returns
+        -------
+        DataFrame: If a new Lead got created the column "Result" contains the new id. It also adds the WhoId and the Type to the new Leads
+
+        Raises
+        ------
+        MissingDataError
+        """
+        df["New Lead Id"] = ""
+        df["OwnerId"] = df["Owner Email"].transform(self.user_id)
+
+        is_not_object = (
+            lambda email: self.lead_id_from_email(email) is None
+            and self.contact_id_from_email(email) is None
+        )
+
+        df = df.drop_duplicates(subset=["Email"])
+        no_object = df["Email"].map(is_not_object)
+        df_new_leads = df[no_object]
+        for row in df_new_leads.iterrows():
+            data = row[1]
+            # TODO handle exception
+            try:
+                print(f"Creating Lead for: {data['Email']}")
+                result = self.create_lead(data)
+                lead_id = result["id"]
+                print(f"Created Lead with id {lead_id}")
+                df.at[row[0], "New Lead Id"] = lead_id
+                df.at[row[0], "Types"] = {"Lead"}
+            except MissingDataError as e:
+                raise
+        return df
+
+    def create_task(self, data):
+        """Creates a Task based on a pandas Series. Checks keys and data.
+        Parameters
+        ----------
+        self:
+        data: pandas Series containing at least ['WhoId','Subject','ActivityDate','Status','OwnerId','Sales_Aktivit_t__c']
+
+        Returns
+        -------
+        The result from salesforce API
+
+        Raises
+        ------
+        MissingDataError
+        """
+        allowed_keys = {
+            "WhoId",
+            "WhatId",
+            "Subject",
+            "ActivityDate",
+            "Status",
+            "Description",
+            "OwnerId",
+            "Sales_Aktivit_t__c",
+        }
+        required_keys = {
+            "WhoId",
+            "Subject",
+            "ActivityDate",
+            "Status",
+            "OwnerId",
+            "Sales_Aktivit_t__c",
+        }
+        data = data.dropna()
+        data_keys = set(data.keys())
+        missing_keys = required_keys - data_keys
+        if len(missing_keys) > 0:
+            raise MissingDataError(missing_keys, data)
+
+        not_allowed_keys = data_keys - allowed_keys
+        data = data.drop(labels=not_allowed_keys).to_dict()
+
+        return self.Task.create(data)
+
+    def create_tasks(self, df):
+        """Creates all tasks from the DataFrame.
+
+        Parameters
+        ----------
+        self: Salesforce connectore from login function
+        df: DataFrame containing at least the columns ['Email','Subject','ActivityDate','Status','Owner Email','Sales_Aktivit_t__c', 'Result']
+
+        Returns
+        -------
+        DataFrame: the same DataFrame with the Task id added to the column "Result"
+
+        Raises
+        ------
+        MissingDataError
+        """
+        df["New Task Id"] = ""
+
+        df["OwnerId"] = df["Owner Email"].transform(self.user_id)
+
+        df["LeadId"] = df["Email"].map(self.lead_id_from_email)
+        df["ContactId"] = df["Email"].map(self.contact_id_from_email)
+        is_lead = ~df["LeadId"].isna()
+        is_contact = ~df["ContactId"].isna()
+        df["WhoId"] = None
+        df.loc[is_lead, "WhoId"] = df["LeadId"]
+        df.loc[is_contact, "WhoId"] = df["ContactId"]
+
+        is_object = ~df["WhoId"].isna()
+        has_task = ~df["Status"].isna()
+
+        for row in df[is_object & has_task].iterrows():
+            data = row[1]
+            # TODO handle exception
+            try:
+                print(f"Creating Task for: {data['Email']}")
+                result = self.create_task(data)
+                task_id = result["id"]
+                print(f"Tracked Task with id {task_id}")
+                df.at[row[0], "New Task Id"] = task_id
+            except:
+                raise
+        return df
+
+    def salesforce_datetime(self, python_dt):
+        """Converts python datetime to the salesforce format.
+
+        Example: 2014-11-20T14:23:44.000+0000
+        """
+        return python_dt.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "+0000"
+
+    def get_objects_created_after(self, python_dt, object_type):
+        """Returns objects of specific type created by the logged in user after
+        that time."""
+        salesforce_dt = self.salesforce_datetime(python_dt)
+        user_id = self.user_id(self.username)
+        q = f"SELECT Id FROM {object_type} WHERE CreatedDate>{salesforce_dt} AND CreatedById='{user_id}'"
+        return self.query(q)["records"]
+
+    def delete_object(self, salesforce_id, object_type):
+        """Deletes salesforce object with that id and that type."""
+        delete_function = {"Lead": self.Lead.delete, "Task": self.Task.delete}
+        result = delete_function[object_type](salesforce_id)
+        return result
+
+    def delete_objects_created_after(self, python_dt, object_type, delete_many=False):
+        """Deletes objects of specific type created after some time.
+
+        Protects against mass deletions of more than 25 elements by default.
+        Set delete_many=True if you want to skip that protection.
+        """
+        records = self.get_objects_created_after(python_dt, object_type)
+        if len(records) > 25 and delete_many == False:
+            raise DuplicatesError(
+                len(records),
+                msg=f"You are about to delete {records} items. If that is correct, call the function with delete_many=True",
+            )
+
+        for record in records:
+            salesforce_id = record["Id"]
+            result = self.delete_object(salesforce_id, object_type)
+            # TODO: Use proper logging
+            print(
+                f"Deleted object with Type {object_type} and id {salesforce_id}. Result:\n{result}"
+            )
