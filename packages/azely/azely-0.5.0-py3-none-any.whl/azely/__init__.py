1bcc124a9347c0bd2a4d5e52dfd4760dd6711e78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:35 +0000

    Add package azely/0.5.0/azely-0.5.0-py3-none-any.whl

---
+++ packages/azely/azely-0.5.0-py3-none-any.whl/azely/__init__.py
@@ -0,0 +1,19 @@
+# flake8: noqa
+__version__ = "0.5.0"
+__author__ = "Akio Taniguchi"
+
+
+# submodules
+from . import consts
+from . import utils
+from . import location
+from . import object
+from . import time
+from . import azel
+
+
+# aliases
+from .location import get_location
+from .object import get_object
+from .time import get_time
+from .azel import compute
