1bf095e409ef4bf91f2e3e34c288e58474ae6fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:02 +0000

    Add package trax/1.2.3/trax-1.2.3.tar.gz

---
+++ packages/trax/trax-1.2.3.tar.gz/trax-1.2.3/trax/layers/metrics.py
@@ -0,0 +1,176 @@
+# coding=utf-8
+# Copyright 2020 The Trax Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python3
+"""Trax metrics layers.
+
+Trax computes metrics (loss functions and evaluation metrics) using layers.
+A metrics layer takes 2 or 3 batch inputs:
+
+  - output values (vectors)
+  - target values (vectors or scalars)
+  - weights [optional]
+
+and gives a single scalar as output. Trax reduces batch values to a scalar by
+taking the weighted (and often also masked) mean of those values:
+
+  - `L2Loss`: weighted masked mean of L2 of (prediction_vector - target_vector)
+
+  - `AccuracyScalar`: weighted masked mean of category predictions
+    (argmax(prediction_vector) vs. target_category)
+
+  - `CrossEntropyLoss`: weighted masked mean of pairwise cross entropy of
+    (prediction_vector, target_vector)
+
+
+TODO(jonni): Explain masks and weighting.
+"""
+
+import jax
+
+from trax import math
+from trax.layers import base
+from trax.layers import combinators as cb
+from trax.layers import core
+from trax.math import numpy as np
+
+
+# pylint: disable=no-value-for-parameter
+def L2Loss(id_to_mask=None, has_weights=False):
+  """Returns a layer to compute L2 loss."""
+  if id_to_mask is not None:
+    raise ValueError('L2Loss cannot mask by ID, provide weights if needed.')
+
+  @base.layer(n_in=2, n_out=1)
+  def RawL2Loss(inputs, **unused_kwargs):
+    y_hat, y = inputs
+    return np.mean((y_hat - y)**2)
+
+  @base.layer(n_in=3, n_out=1)
+  def MaskedL2Loss(inputs, **unused_kwargs):
+    y_hat, y, mask = inputs
+    l2 = mask * (y_hat - y)**2
+    return np.sum(l2) / np.sum(mask)
+
+  return MaskedL2Loss() if has_weights else RawL2Loss()
+
+
+def AccuracyScalar(id_to_mask=None, has_weights=False):
+  """Computes weighted masked mean of category prediction accuracy."""
+  return _WeightedMaskedMean(_Accuracy(), id_to_mask, has_weights)
+
+
+def SequenceAccuracyScalar(id_to_mask=None, has_weights=False):
+  """Computes weighted masked mean of sequence prediction accuracy."""
+  return _WeightedMaskedMean(_Accuracy(), id_to_mask, has_weights,
+                             final_layer_override=_WeightedSequenceMean())
+
+
+def CrossEntropyLoss(id_to_mask=None, has_weights=False):
+  """Computes weighted masked mean of prediction-target cross entropies."""
+  return _WeightedMaskedMean(_CrossEntropy(), id_to_mask, has_weights)
+
+
+def SumOfWeights(id_to_mask=None, has_weights=False):
+  """Returns a layer to compute sum of weights of all non-masked elements."""
+  multiply_by_weights = cb.Multiply() if has_weights else []
+  return cb.Serial(
+      cb.Drop(),  # Drop inputs.
+      _ElementMask(id_to_mask=id_to_mask),
+      multiply_by_weights,
+      core.Sum(axis=None)  # Sum all.
+  )
+# pylint: enable=no-value-for-parameter
+
+
+@base.layer(n_in=2, n_out=1)
+def _L2(inputs, **unused_kwargs):
+  """Returns a layer to compute L2 norms of predicted minus target vectors."""
+  y_hat, y = inputs
+  return np.sum((y_hat - y)**2, axis=-1)
+
+
+@base.layer(n_in=2, n_out=1)
+def _Accuracy(inputs, axis=-1, **unused_kwargs):
+  """Returns a layer to score matches of predicted versus target categories."""
+  y_hat, target_category = inputs
+  predicted_category = np.argmax(y_hat, axis=axis)
+  return np.equal(predicted_category, target_category).astype(np.float32)
+
+
+@base.layer(n_in=2, n_out=1)
+def _CrossEntropy(inputs, **unused_kwargs):
+  """Returns a layer to compute prediction-target cross entropies."""
+  y_hat, target_category = inputs
+  return -1.0 * np.sum(y_hat * one_hot(target_category, y_hat.shape[-1]),
+                       axis=-1)
+
+
+@base.layer()
+def _ElementMask(target, id_to_mask=0, **unused_kwargs):
+  """Returns a mask with zeros marking elements to exclude from calculations."""
+  if id_to_mask is None:
+    return np.ones_like(target)
+  return 1.0 - np.equal(target, id_to_mask).astype(np.float32)
+
+
+@base.layer(n_in=2, n_out=1)
+def _WeightedMean(inputs, **unused_kwargs):
+  """Returns a layer to compute weighted mean over all values in the input."""
+  values, weights = inputs
+  return np.sum(values * weights) / np.sum(weights)
+
+
+@base.layer(n_in=2, n_out=1)
+def _WeightedSequenceMean(inputs, **unused_kwargs):
+  """Returns a layer to compute weighted seqeunce accuracy mean."""
+  values, weights = inputs  # This function assumes weights are 0 or 1.
+  not_correct = (1.0 - values) * weights  # 1: not-correct, 0: correct or masked
+  axis_to_sum = list(range(1, len(not_correct.shape)))
+  # Summing not-correct on all axes but batch. We're summing 0s and 1s,
+  # so the sum is 0 if it's all 0 and >=1 in all other cases.
+  not_correct_seq = np.sum(not_correct, axis=axis_to_sum)
+  # Sequence is correct if not_correct_seq is 0, reverting here.
+  correct_seq = 1.0 - np.minimum(1.0, not_correct_seq)
+  return np.mean(correct_seq)  # Mean over batch.
+
+
+# pylint: disable=no-value-for-parameter
+def _WeightedMaskedMean(metric_layer, id_to_mask, has_weights,
+                        final_layer_override=None):
+  """Computes weighted masked mean of metric_layer(predictions, targets)."""
+  multiply_by_weights = cb.Multiply() if has_weights else []
+  final_layer = final_layer_override or _WeightedMean()  # For sequence acc.
+  # Create a layer with 2 or 3 inputs:
+  #   - predictions targets (weights)
+  # that applies the specified metric to a batch and gathers the results into
+  # a single scalar.
+  return cb.Serial(
+      cb.Select([0, 1, 1]),
+      cb.Parallel(metric_layer, _ElementMask(id_to_mask=id_to_mask)),
+      cb.Parallel([], multiply_by_weights),  # Stack now: metric_values weights
+      final_layer
+  )
+# pylint: enable=no-value-for-parameter
+
+
+# TODO(jonni): Figure out the right name and home for this function.
+def one_hot(x, n_categories, dtype=np.float32):  # pylint: disable=invalid-name
+  """Makes a one-hot array (n+1 dims) from an int-categorical array (n dims)."""
+  indices_less_than_n = np.arange(n_categories)
+  if math.backend_name() == 'jax':
+    # Work around a jax broadcasting issue.
+    indices_less_than_n = jax.lax.tie_in(x, indices_less_than_n)
+  return np.array(x[..., np.newaxis] == indices_less_than_n, dtype)
---
+++ packages/trax/trax-1.2.3.tar.gz/trax-1.2.3/trax/optimizers/base.py
@@ -0,0 +1,159 @@
+# coding=utf-8
+# Copyright 2020 The Trax Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python3
+"""Trax base optimizer class."""
+
+from trax.layers import base as layers
+from trax.math import numpy as np
+
+
+class Optimizer(object):
+  """Optimizer object, base class. Maps per-parameter functions to trees."""
+
+  def __init__(self, learning_rate, **init_opt_params):
+    """Initialize the optimizer.
+
+    Takes the initial optimizer parameters as positional arguments. They are fed
+    back to the optimizer in tree_update, in the same order. They can be changed
+    between updates, e.g. for learning rate schedules.
+
+    The constructor should be overridden in derived classes to give names to the
+    optimizer parameters, so the gin configuration can set them.
+
+    Args:
+      learning_rate: The initial learning rate.
+      **init_opt_params: Initial values of any additional optimizer parameters.
+    """
+    init_opt_params['learning_rate'] = learning_rate
+    self._init_opt_params = {
+        name: np.array(value) for (name, value) in init_opt_params.items()
+    }
+    self._slots = None
+
+  def init(self, params):
+    """Create optimizer slots for the given parameters."""
+    raise NotImplementedError
+
+  def update(self, step, grads, weights, slots, opt_params):
+    """Update a single parameter array.
+
+    Args:
+      step: Current step.
+      grads: Gradients.
+      weights: Trainable model weights.
+      slots: Optimizer slots (e.g. gradient moments).
+      opt_params: Optimizer (hyper)parameters (e.g. learning rate, momentum).
+
+    Returns:
+      (new_weights, new_slots)
+    """
+    raise NotImplementedError
+
+  # End subclass interface.
+
+  @property
+  def slots(self):
+    return self._slots
+
+  @slots.setter
+  def slots(self, slots):
+    self._slots = slots
+
+  def tree_init(self, weight_tree):
+    self._slots = [self.init(weight) for weight in _tree_flatten(weight_tree)]
+    return (
+        self._slots,
+        self._init_opt_params,
+    )
+
+  def _update_and_check(self, step, grads, weights, slots, opt_params):
+    """Update a single weight array and check types."""
+    new_weights, new_slots = self.update(
+        step, grads, weights, slots, opt_params)
+    if isinstance(weights, np.ndarray):
+      assert isinstance(new_weights, np.ndarray), (
+          'The type of the new weight values should be np.ndarray; got %s' %
+          type(new_weights))
+      assert new_weights.dtype == weights.dtype, (
+          'The dtype of the new weight values (%s) is not the same as the '
+          'old one (%s)' % (new_weights.dtype, weights.dtype))
+    return new_weights, new_slots
+
+  def tree_update(self, step, grad_tree, weight_tree, slots, opt_params):
+    grads_flat = _tree_flatten(grad_tree)
+    weights_flat = _tree_flatten(weight_tree)
+    updated_pairs = [
+        self._update_and_check(step, grad, weight, slot, opt_params)
+        for (grad, weight, slot) in zip(grads_flat, weights_flat, slots)
+    ]
+    new_weights_flat, self.slots = zip(*updated_pairs)
+    new_weights, _ = _tree_unflatten(new_weights_flat, weight_tree)
+    return new_weights, self.slots
+
+
+def _tree_flatten(tree):
+  """Flatten a tree into a list."""
+  if isinstance(tree, (list, tuple)):
+    # In python, sum of lists starting from [] is the concatenation.
+    return sum([_tree_flatten(t) for t in tree], [])
+  if isinstance(tree, dict):
+    # Only use the values in case of a dictionary node.
+    return sum([_tree_flatten(v) for v in tree.values()], [])
+  return [tree]
+
+
+def _tree_unflatten(flat, tree):
+  """Unflatten a list into a tree given the tree shape as second argument.
+
+  Args:
+    flat: a flat list of elements to be assembled into a tree.
+    tree: a tree with the structure we want to have in the new tree.
+
+  Returns:
+    A pair (new_tree, rest_of_flat) where the new tree that has the structure
+    of tree but with leaves from flat, and the remaining elements of flat if
+    more were provided than the number of leaves of tree (useful for recursion).
+  """
+  if isinstance(tree, (list, tuple)):
+    new_tree, rest = [], flat
+    for t in tree:
+      new_t, rest = _tree_unflatten(rest, t)
+      new_tree.append(new_t)
+    new_tree = tuple(new_tree) if isinstance(tree, tuple) else new_tree
+    return new_tree, rest
+  if isinstance(tree, dict):
+    new_tree, rest = {}, flat
+    for k in tree:
+      new_v, rest = _tree_unflatten(rest, tree[k])
+      new_tree[k] = new_v
+    return new_tree, rest
+  return flat[0], flat[1:]
+
+
+# Utilities.
+
+
+def l2_norm(tree):
+  """Compute the l2 norm of a pytree of arrays. Useful for weight decay."""
+  leaves = _tree_flatten(tree)
+  return np.sqrt(sum(np.vdot(x, x) for x in leaves))
+
+
+def clip_grads(grad_tree, max_norm):
+  """Clip gradients stored as a pytree of arrays to maximum norm `max_norm`."""
+  norm = l2_norm(grad_tree)
+  normalize = lambda g: np.where(norm < max_norm, g, g * (max_norm / norm))
+  return layers.nested_map(grad_tree, normalize)
