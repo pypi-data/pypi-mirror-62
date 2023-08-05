35e26c06aa64f4d194d4af1135de2a59fb63c73b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:50 +0000

    Add package ufo2ft/2.12.0/ufo2ft-2.12.0.zip

---
+++ packages/ufo2ft/ufo2ft-2.12.0.zip/ufo2ft-2.12.0/Lib/ufo2ft/constants.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import, unicode_literals
+
+SPARSE_TTF_MASTER_TABLES = frozenset(
+    ["glyf", "head", "hmtx", "loca", "maxp", "post", "vmtx"]
+)
+SPARSE_OTF_MASTER_TABLES = frozenset(["CFF ", "VORG", "head", "hmtx", "maxp", "vmtx"])
+
+UFO2FT_PREFIX = "com.github.googlei18n.ufo2ft."
+GLYPHS_PREFIX = "com.schriftgestaltung."
+
+USE_PRODUCTION_NAMES = UFO2FT_PREFIX + "useProductionNames"
+GLYPHS_DONT_USE_PRODUCTION_NAMES = GLYPHS_PREFIX + "Don't use Production Names"
+
+COLOR_LAYERS_KEY = "com.github.googlei18n.ufo2ft.colorLayers"
+COLOR_PALETTES_KEY = "com.github.googlei18n.ufo2ft.colorPalettes"
+COLOR_LAYER_MAPPING_KEY = "com.github.googlei18n.ufo2ft.colorLayerMapping"
