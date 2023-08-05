6fcc0e3281f667716217966b67118e0a56de8e60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:19 +0000

    Add package clitui/0.2.1/clitui-0.2.1.tar.gz

---
+++ packages/clitui/clitui-0.2.1.tar.gz/clitui-0.2.1/clitui/__init__.py
@@ -0,0 +1,19 @@
+from .display import (
+    draw_circle,
+    draw_rectangle,
+    draw_brectangle,
+    label,
+    print_at,
+    is_pressed,
+    safe_run,
+    getchar,
+    clear,
+)
+from .font import (
+    style_by,
+    fg_color_by,
+    bg_color_by,
+    bg_by,
+    fg_by,
+)
+from .menu import Menu
