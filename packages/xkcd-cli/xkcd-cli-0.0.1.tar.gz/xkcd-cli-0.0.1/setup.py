744927c7929be8b19d74c370316e5a6521e6c1c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:59 +0000

    Add package xkcd-cli/0.0.1/xkcd-cli-0.0.1.tar.gz

---
+++ packages/xkcd-cli/xkcd-cli-0.0.1.tar.gz/xkcd-cli-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages
+from distutils.core import setup
+
+setup(
+    name='xkcd-cli',
+    version='0.0.1',
+    packages=find_packages(),
+    license='MIT',
+    include_package_data=True,
+    author='Nikhil Deepak Shinde',
+    author_email='crenickshinde1996@gmail.com',
+    download_url='',
+    install_requires=[
+        'Click',
+    ],
+    keywords = ['web-comic', 'xkcd', 'cli'],
+    entry_points='''
+        [console_scripts]
+        xkcd=src.cli:cli
+    ''',
+)
