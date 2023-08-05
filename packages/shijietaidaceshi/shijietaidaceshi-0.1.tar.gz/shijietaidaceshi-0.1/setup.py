ee7fc58acb132ec282aa50571bbf0ca48d19979c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:40 +0000

    Add package shijietaidaceshi/0.1/shijietaidaceshi-0.1.tar.gz

---
+++ packages/shijietaidaceshi/shijietaidaceshi-0.1.tar.gz/shijietaidaceshi-0.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from setuptools import setup,find_packages
+
+
+setup(
+    name = "shijietaidaceshi",
+    version = "0.1",
+    author="qitian",
+    author_email="xzwgm81@163.com",
+    description = "make in chinese",
+    url = "https://www.baidu.com/",
+    packages = find_packages(),
+
+)
