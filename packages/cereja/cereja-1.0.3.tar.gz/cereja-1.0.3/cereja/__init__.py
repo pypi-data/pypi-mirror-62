4aa8fce12417522a92d54301b4fe093f3c1d7e01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:38 +0000

    Add package cereja/1.0.3/cereja-1.0.3.tar.gz

---
+++ packages/cereja/cereja-1.0.3.tar.gz/cereja-1.0.3/cereja/__init__.py
@@ -0,0 +1,12 @@
+from cereja.utils import get_version_pep440_compliant
+from . import utils
+from importlib import import_module
+
+VERSION = "1.0.3.final.0"
+
+__version__ = get_version_pep440_compliant(VERSION)
+
+cj_modules_dotted_path = utils.import_string('cereja.conf.cj_modules_dotted_path')
+
+for dot_module in cj_modules_dotted_path:
+    globals().update(utils.module_references(import_module(dot_module)))
