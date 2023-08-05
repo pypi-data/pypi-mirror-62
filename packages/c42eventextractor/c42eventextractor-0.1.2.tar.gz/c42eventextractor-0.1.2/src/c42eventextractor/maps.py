1a0072934a59bf50b463d033f6e45eff5805cccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:37 +0000

    Add package c42eventextractor/0.1.2/c42eventextractor-0.1.2.tar.gz

---
+++ packages/c42eventextractor/c42eventextractor-0.1.2.tar.gz/c42eventextractor-0.1.2/src/c42eventextractor/maps.py
@@ -0,0 +1,47 @@
+JSON_TO_CEF_MAP = {
+    "actor": "cs5",
+    "cloudDriveId": "aid",
+    "createTimestamp": "fileCreateTime",
+    "deviceUid": "deviceExternalId",
+    "deviceUserName": "suser",
+    "domainName": "dvchost",
+    "eventId": "externalId",
+    "eventTimestamp": "end",
+    "exposure": "reason",
+    "fileCategory": "fileType",
+    "fileName": "fname",
+    "filePath": "filePath",
+    "fileSize": "fsize",
+    "insertionTimestamp": "rt",
+    "md5Checksum": "fileHash",
+    "modifyTimestamp": "fileModificationTime",
+    "osHostName": "shost",
+    "processName": "sproc",
+    "processOwner": "spriv",
+    "publicIpAddress": "src",
+    "removableMediaBusType": "cs1",
+    "removableMediaCapacity": "cn1",
+    "removableMediaName": "cs3",
+    "removableMediaSerialNumber": "cs4",
+    "removableMediaVendor": "cs2",
+    "sharedWith": "duser",
+    "syncDestination": "destinationServiceName",
+    "url": "request",
+    "userUid": "suid",
+}
+
+CEF_CUSTOM_FIELD_NAME_MAP = {
+    "cn1Label": "Code42AEDRemovableMediaCapacity",
+    "cs1Label": "Code42AEDRemovableMediaBusType",
+    "cs2Label": "Code42AEDRemovableMediaVendor",
+    "cs3Label": "Code42AEDRemovableMediaName",
+    "cs4Label": "Code42AEDRemovableMediaSerialNumber",
+    "cs5Label": "Code42AEDEventActor",
+}
+
+AED_EVENT_TO_SIGNATURE_ID_MAP = {
+    "CREATED": "C42200",
+    "MODIFIED": "C42201",
+    "DELETED": "C42202",
+    "READ_BY_APP": "C42203",
+}
