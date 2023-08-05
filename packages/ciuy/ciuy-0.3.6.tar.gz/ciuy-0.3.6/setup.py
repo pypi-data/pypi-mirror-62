5b006d699a98bb4eb87d7e253e556bd7e893da9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:19 +0000

    Add package ciuy/0.3.6/ciuy-0.3.6.tar.gz

---
+++ packages/ciuy/ciuy-0.3.6.tar.gz/ciuy-0.3.6/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(name='ciuy',
+      version='0.3.6',
+      python_requires=">=3.5",
+      description='Python package for validating Uruguayan identity document numbers.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      keywords=["Uruguay", "dni", "id", "eid", "validation", "cédula", "cédula uruguaya"],
+      url='https://github.com/ismaelpadilla/ciuy_py',
+      project_urls={
+          "Documentation": "https://ciuy.readthedocs.io/"
+      },
+      author='Ismael Padilla',
+      author_email='padillaismael92@gmail.com',
+      license='MIT',
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "Programming Language :: Python :: 3.8"
+      ],
+      packages=['ciuy'],
+      include_package_data=True,
+      zip_safe=True)
