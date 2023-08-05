e527f66f8c9e612ddbe996e36e50c7c7df12b22a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:46 +0000

    Add package pynspawn/1.1.4/pynspawn-1.1.4.tar.gz

---
+++ packages/pynspawn/pynspawn-1.1.4.tar.gz/pynspawn-1.1.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pynspawn',
+      version='1.1.4',
+      description='Python wrapper for systemd-nspawn',
+      url='http://github.com/dfoulks1/pynspawn',
+      author='Drew Foulks',
+      author_email='dfoulks@gmail.com',
+      license='Apache v2 License',
+      packages=['pynspawn'],
+      zip_safe=False)
