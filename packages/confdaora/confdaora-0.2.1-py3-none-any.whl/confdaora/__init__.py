df0927ee6ca27887f710fa7978ca6645cb992ea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:31 +0000

    Add package confdaora/0.2.1/confdaora-0.2.1-py3-none-any.whl

---
+++ packages/confdaora/confdaora-0.2.1-py3-none-any.whl/confdaora/__init__.py
@@ -0,0 +1,9 @@
+"""confdaora"""
+
+__version__ = '0.2.1'
+
+from confdaora.confdaora import confdaora_env
+from confdaora.exceptions import ValidationError
+
+
+__all__ = ['confdaora_env', 'ValidationError']
