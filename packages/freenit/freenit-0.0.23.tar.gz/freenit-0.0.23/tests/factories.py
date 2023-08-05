41001d1f6b857f4a5aa575f68acf352a3cb162e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:36 +0000

    Add package freenit/0.0.23/freenit-0.0.23.tar.gz

---
+++ packages/freenit/freenit-0.0.23.tar.gz/freenit-0.0.23/tests/factories.py
@@ -0,0 +1,31 @@
+from importlib import import_module
+
+import factory
+from flask_security.utils import hash_password
+from name import app_name
+
+user = import_module(f'{app_name}.models.user')
+role = import_module(f'{app_name}.models.role')
+
+
+class UserFactory(factory.Factory):
+    class Meta:
+        model = user.User
+
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+    email = factory.Faker('email')
+    password = factory.LazyAttribute(lambda a: hash_password('Sekrit'))
+    username = factory.Faker('name')
+    active = True
+
+
+class AdminFactory(UserFactory):
+    admin = True
+
+
+class RoleFactory(factory.Factory):
+    class Meta:
+        model = role.Role
+
+    name = factory.Faker('first_name')
