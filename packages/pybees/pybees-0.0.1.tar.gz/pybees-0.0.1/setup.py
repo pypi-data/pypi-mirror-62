ba5f2eaf8ce458eb93501c4cc3ca03d86ce04265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:14 +0000

    Add package pybees/0.0.1/pybees-0.0.1.tar.gz

---
+++ packages/pybees/pybees-0.0.1.tar.gz/pybees-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+def setup_package():
+
+    setup(
+        name="pybees", 
+        version="0.0.1",
+        author="Joseph Moorhouse",
+        author_email="moorhouse@live.co.uk",
+        description="A research toolkit for the Bees algorithm in Python",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/jbmoorhouse/pybees",
+        packages=find_packages(include=["pybees", "pybees.*"]),
+        classifiers=[
+            "Programming Language :: Python :: 3.6",
+            "Operating System :: OS Independent",
+            "Topic :: Scientific/Engineering"
+        ],
+        python_requires='>=3.6',
+    )
+
+if __name__ == "__main__":
+    setup_package() 
