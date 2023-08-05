1377b9549050ed9bcaf13d03927bb640a2e9bd16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:57 +0000

    Add package google-play-scraper/0.0.2.1/google-play-scraper-0.0.2.1.tar.gz

---
+++ packages/google-play-scraper/google-play-scraper-0.0.2.1.tar.gz/google-play-scraper-0.0.2.1/google_play_scraper/features/reviews.py
@@ -0,0 +1,72 @@
+import json
+from typing import Optional
+
+from google_play_scraper import Sort
+from google_play_scraper.constants.element import ElementSpecs
+from google_play_scraper.constants.regex import Regex
+from google_play_scraper.constants.url import Formats
+from google_play_scraper.utils.request import post
+
+
+def _fetch_review_items(url, app_id, sort, count, filter_score_with, pagination_token):
+    dom = post(
+        url,
+        Formats.ReviewPayload.build(
+            app_id,
+            sort,
+            count,
+            "null" if filter_score_with is None else filter_score_with,
+            pagination_token,
+        ),
+        {"content-type": "application/x-www-form-urlencoded"},
+    )
+
+    match = json.loads(Regex.REVIEWS.findall(dom)[0])
+
+    return json.loads(match[0][2])[0], json.loads(match[0][2])[-1][-1]
+
+
+def reviews(
+    app_id, lang="en", country="us", sort=Sort.NEWEST, count=100, filter_score_with=None
+):
+    # type: (str, str, str, Sort, int, Optional[int]) -> list
+    # TODO filtering with device model
+    # TODO reply data
+    # TODO refactoring
+
+    url = Formats.Reviews.build(lang=lang, country=country)
+
+    if count < 200:
+        _count = count
+    else:
+        _count = 199
+
+    result = []
+
+    pagination_token = None
+
+    while True:
+        review_items, pagination_token = _fetch_review_items(
+            url, app_id, sort, _count, filter_score_with, pagination_token
+        )
+
+        for review in review_items:
+            review_dict = {}
+
+            for k, spec in ElementSpecs.Review.items():
+                review_dict[k] = spec.extract_content(review)
+
+            result.append(review_dict)
+
+        remaining_count_of_reviews_to_fetch = count - len(result)
+
+        if remaining_count_of_reviews_to_fetch == 0:
+            break
+
+        if remaining_count_of_reviews_to_fetch < 200:
+            _count = count - len(result)
+
+        else:
+            continue
+
+    return result
---
+++ packages/google-play-scraper/google-play-scraper-0.0.2.1.tar.gz/google-play-scraper-0.0.2.1/tests/e2e_tests/test_reviews.py
@@ -0,0 +1,72 @@
+from datetime import datetime
+from unittest import TestCase
+
+from google_play_scraper import Sort
+from google_play_scraper.features.reviews import reviews
+
+
+class TestApp(TestCase):
+    def test_e2e_scenario_1(self):
+        result = reviews("com.mojang.minecraftpe", sort=Sort.NEWEST, count=500)
+
+        self.assertEqual(500, len(result))
+
+        review_created_version_contained_review_count = 0
+
+        for r in result:
+            self.assertTrue(r["userName"])
+            self.assertTrue(r["userImage"])
+            self.assertTrue(r["content"])
+            self.assertTrue(r["score"] >= 1)
+            self.assertTrue(r["thumbsUpCount"] >= 0)
+            self.assertTrue(datetime(2019, 12, 1) < r["at"] < datetime(2040, 1, 1))
+
+            if r["reviewCreatedVersion"]:
+                review_created_version_contained_review_count += 1
+
+        self.assertTrue(review_created_version_contained_review_count > 100)
+
+        last_review_at = result[0]["at"]
+        well_sorted_review_count = 0
+
+        for r in result[1:]:
+            review_at = r["at"]
+
+            if review_at <= last_review_at:
+                well_sorted_review_count += 1
+
+            last_review_at = review_at
+
+        self.assertTrue(well_sorted_review_count > 490)
+
+    def test_e2e_scenario_2(self):
+        result = reviews("com.mojang.minecraftpe", sort=Sort.MOST_RELEVANT, count=30)
+
+        self.assertEqual(30, len(result))
+
+        review_count_has_thumbs_up_count_over_0 = 0
+
+        for r in result:
+            if 0 < r["thumbsUpCount"]:
+                review_count_has_thumbs_up_count_over_0 += 1
+
+        self.assertTrue(review_count_has_thumbs_up_count_over_0 > 25)
+
+    def test_e2e_scenario_3(self):
+        result = reviews("com.mojang.minecraftpe", sort=Sort.RATING, count=100)
+
+        self.assertEqual(100, len(result))
+
+        for r in result:
+            self.assertEqual(5, r["score"])
+
+    def test_e2e_scenario_4(self):
+        for score in {1, 2, 3, 4, 5}:
+            result = reviews(
+                "com.mojang.minecraftpe",
+                sort=Sort.NEWEST,
+                count=300,
+                filter_score_with=score,
+            )
+
+            self.assertEqual(score * 300, sum([r["score"] for r in result]))
