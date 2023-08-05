dde4c2ad3977294080cb2f19861d5cc95eb4633f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:46 +0000

    Add package pyffish/0.0.40/pyffish-0.0.40.tar.gz

---
+++ packages/pyffish/pyffish-0.0.40.tar.gz/pyffish-0.0.40/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, Extension
+from glob import glob
+import platform
+
+args = ["-Wno-date-time", "-flto", "-DLARGEBOARDS", "-DPRECOMPUTED_MAGICS", "-std=c++11"]
+
+if "64bit" in platform.architecture():
+    args.append("-DIS_64BIT")
+
+CLASSIFIERS = [
+    "Development Status :: 3 - Alpha",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+]
+
+with open("Readme.md", "r") as fh:
+    long_description = fh.read().strip()
+
+pyffish_module = Extension(
+    "pyffish",
+    sources=glob("src/*.cpp") + glob("src/syzygy/*.cpp"),
+    extra_compile_args=args)
+
+setup(name="pyffish", version="0.0.40",
+      description="Fairy-Stockfish Python wrapper",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author="Bajusz Tamás",
+      author_email="gbtami@gmail.com",
+      license="GPL3",
+      classifiers=CLASSIFIERS,
+      url="https://github.com/gbtami/Fairy-Stockfish",
+      ext_modules=[pyffish_module]
+      )
