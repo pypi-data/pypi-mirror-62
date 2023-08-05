bb812d00955255cff98825f76470e0c0b67d2b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:19 +0000

    Add package apio/0.5.1/apio-0.5.1.tar.gz

---
+++ packages/apio/apio-0.5.1.tar.gz/apio-0.5.1/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import json
+
+from setuptools import setup
+
+from apio import (__author__, __description__, __email__, __license__,
+                  __title__, __url__, __version__)
+
+# Load extras_require
+extras_require = {}
+filepath = os.path.join('apio', 'resources', 'distribution.json')
+with open(filepath, 'r') as f:
+    resource = json.loads(f.read())
+    pip_packages = resource.get('pip_packages', {})
+    extras_require = {k: [k + v] for k, v in pip_packages.items()}
+
+setup(
+    name=__title__,
+    version=__version__,
+    description=__description__,
+    author=__author__,
+    author_email=__email__,
+    url=__url__,
+    license=__license__,
+    packages=['apio'],
+    package_data={
+        'apio': ['commands/*.py',
+                 'managers/*.py',
+                 'resources/*',
+                 'resources/ice40/*',
+                 'resources/ecp5/*']
+    },
+    install_requires=[
+        'click>=5',
+        'semantic_version>=2.5.0,<3',
+        'requests>=2.4.0,<3',
+        'pyjwt>=1.5.3,<2',
+        'colorama',
+        'pyserial>=3,<4'
+    ],
+    extras_require=extras_require,
+    entry_points={
+        'console_scripts': ['apio=apio.__main__:cli']
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3']
+)
