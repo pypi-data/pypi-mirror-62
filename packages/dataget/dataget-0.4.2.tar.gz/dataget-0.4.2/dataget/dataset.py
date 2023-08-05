7c0044bc7f64d2041a301897c6cc0ec6415798c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:08 +0000

    Add package dataget/0.4.2/dataget-0.4.2.tar.gz

---
+++ packages/dataget/dataget-0.4.2.tar.gz/dataget-0.4.2/dataget/dataset.py
@@ -0,0 +1,69 @@
+import asyncio
+import os
+import shutil
+from abc import ABC, abstractmethod
+from pathlib import Path
+
+from dataget import utils
+
+
+class Dataset(ABC):
+    """
+    DATASETTTT
+    """
+
+    @property
+    @abstractmethod
+    def name(self):
+        pass
+
+    def __init__(self, path: Path = None, global_cache: bool = False):
+
+        if path and not isinstance(path, Path):
+            path = Path(path)
+
+        if path:
+            pass
+        elif global_cache:
+            path = Path("~").expanduser() / ".dataget" / self.name
+        else:
+            path = Path("data") / self.name
+
+        self.path = path
+
+    def get(self, clean_cache: bool = False, **kwargs):
+
+        if clean_cache or not self.is_valid(**kwargs):
+            shutil.rmtree(self.path, ignore_errors=True)
+            self.path.mkdir(parents=True)
+
+            # get data
+            coro = self.download(**kwargs)
+
+            if coro is not None:
+                asyncio.get_event_loop().run_until_complete(coro)
+
+            if not self.is_valid(**kwargs):
+                raise DownloadError(
+                    f"Failed download for '{self.name}' at '{self.path}'"
+                )
+
+        return self.load(**kwargs)
+
+    @abstractmethod
+    def download(self, **kwargs):
+        pass
+
+    @abstractmethod
+    def load(self, **kwargs):
+        pass
+
+    @abstractmethod
+    def is_valid(self, **kwargs):
+        pass
+
+
+class DownloadError(Exception):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
