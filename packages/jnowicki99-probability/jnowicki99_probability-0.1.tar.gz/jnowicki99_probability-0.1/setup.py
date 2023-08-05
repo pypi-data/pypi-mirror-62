b1d72c093a36e9236b65949f3f80a9628ae935d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:04 +0000

    Add package jnowicki99-probability/0.1/jnowicki99_probability-0.1.tar.gz

---
+++ packages/jnowicki99-probability/jnowicki99_probability-0.1.tar.gz/jnowicki99_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='jnowicki99_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['jnowicki99_probability'],
+      author='Jakub Nowicki',
+      author_email='j99nowicki@gmail.com',
+      zip_safe=False)
