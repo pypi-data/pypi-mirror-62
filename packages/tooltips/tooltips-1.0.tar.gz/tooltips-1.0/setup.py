67d557591550c22a86848548f7357b3e157aec0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:33 +0000

    Add package tooltips/1.0/tooltips-1.0.tar.gz

---
+++ packages/tooltips/tooltips-1.0.tar.gz/tooltips-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='tooltips', # 对外我们模块的名字
+    version='1.0', # 版本号
+    description='这是第一个气泡，测试哦', #描述
+    author='ez', # 作者
+    author_email='yihui1021@126.com',
+    py_modules=['ToolTip'] # 要发布的模块
+)
