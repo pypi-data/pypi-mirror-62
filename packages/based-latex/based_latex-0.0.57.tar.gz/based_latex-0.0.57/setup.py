a35be3c0472a8116d3795b6bbb4e89762395bc29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:46 +0000

    Add package based-latex/0.0.57/based_latex-0.0.57.tar.gz

---
+++ packages/based-latex/based_latex-0.0.57.tar.gz/based_latex-0.0.57/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+version = "0.0.57"
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name="based_latex",
+        version=version,
+        author="Adolfo Rodriguez",
+        author_email="adolfo@interoper.io",
+        description="Generates properly baselined images and HTML code from LaTeX formulas.",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/wircho/based_latex",
+        packages=["based_latex"],
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        python_requires='>=3.6',
+        install_requires=[]
+    )
