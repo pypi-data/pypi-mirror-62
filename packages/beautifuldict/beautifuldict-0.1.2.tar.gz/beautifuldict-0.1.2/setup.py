5ff56633e818fdd3bc3d99f813624382eb853255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:49 +0000

    Add package beautifuldict/0.1.2/beautifuldict-0.1.2.tar.gz

---
+++ packages/beautifuldict/beautifuldict-0.1.2.tar.gz/beautifuldict-0.1.2/setup.py
@@ -0,0 +1,28 @@
+"""Package setup."""
+
+from setuptools import find_packages, setup
+
+setup(
+    name='beautifuldict',
+    version='0.1.2',
+    author='Servicio de Informática y Banco de Datos',
+    author_email='sibd@cantabria.es',
+    packages=find_packages(),
+    url='https://github.com/icane/beautifuldict.git',
+    license='Apache License 2.0',
+    description='Utils for managing configurations',
+    long_description=open('README.rst').read(),
+    test_suite='beautifuldict.test',
+    keywords=['configuration', 'utils'],
+    classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Libraries'
+          ]
+)
