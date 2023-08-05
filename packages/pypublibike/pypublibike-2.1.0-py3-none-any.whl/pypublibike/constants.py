ebaaaa393ef33a4baf3a220d1f903b74dab79055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:58 +0000

    Add package pypublibike/2.1.0/pypublibike-2.1.0-py3-none-any.whl

---
+++ packages/pypublibike/pypublibike-2.1.0-py3-none-any.whl/pypublibike/constants.py
@@ -0,0 +1,16 @@
+
+class Constants():
+    ID = "id"
+    STATE = "state"
+    ACTIVE = "Active"
+    NAME = "name"
+    LATITUDE = "latitude"
+    LONGITUDE = "longitude"
+    ADDRESS = "address"
+    ZIP = "zip"
+    CITY = 'city'
+    VEHICLES = "vehicles"
+    TYPE = "type"
+    BATTERY_LEVEL = "ebike_battery_level"
+    BIKE = "Bike"
+    EBIKE = "E-Bike"
