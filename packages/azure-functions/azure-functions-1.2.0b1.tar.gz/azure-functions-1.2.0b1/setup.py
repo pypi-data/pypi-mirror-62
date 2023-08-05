00f5c40b8cffb9f7b75dab2a1e7db240f802b9fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:23 +0000

    Add package azure-functions/1.2.0b1/azure-functions-1.2.0b1.tar.gz

---
+++ packages/azure-functions/azure-functions-1.2.0b1.tar.gz/azure-functions-1.2.0b1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+
+setup(
+    name='azure-functions',
+    version='1.2.0b1',
+    description='Azure Functions for Python',
+    author='Microsoft Corporation',
+    author_email='azpysdkhelp@microsoft.com',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS :: MacOS X',
+        'Environment :: Web Environment',
+        'Development Status :: 5 - Production/Stable',
+    ],
+    license='MIT',
+    packages=['azure.functions'],
+    package_data={
+        'azure.functions': ['py.typed']
+    },
+    extras_require={
+        'dev': [
+            'flake8~=3.7.9',
+            'mypy',
+            'pytest',
+            'requests==2.*',
+        ]
+    },
+    include_package_data=True,
+    test_suite='tests'
+)
