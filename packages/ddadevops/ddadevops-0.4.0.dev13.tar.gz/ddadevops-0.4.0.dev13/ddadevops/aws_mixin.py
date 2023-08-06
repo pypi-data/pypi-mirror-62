0f10167bc75c827dbcce4e3d9b104d57c2991601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:54 +0000

    Add package ddadevops/0.4.0.dev13/ddadevops-0.4.0.dev13.tar.gz

---
+++ packages/ddadevops/ddadevops-0.4.0.dev13.tar.gz/ddadevops-0.4.0.dev13/ddadevops/aws_mixin.py
@@ -0,0 +1,40 @@
+from python_terraform import *
+from .credential import gopass_credential_from_env_path
+from .devops_terraform_build import DevopsTerraformBuild
+
+
+def add_aws_mixin_config(config, account_name):
+    config.update({'AwsMixin':
+                   {'account_name': account_name}})
+    return config
+
+
+class AwsMixin(DevopsTerraformBuild):
+
+    def __init__(self, project, config):
+        super().__init__(project, config)
+        aws_mixin_config = config['AwsMixin']
+        self.account_name = aws_mixin_config['account_name']
+
+    def backend_config(self):
+        return "backend." + self.account_name + "." + self.stage + ".properties"
+
+    def project_vars(self):
+        ret = super().project_vars()
+        ret.update({'account_name': self.account_name})
+        return ret
+
+    def init_client(self):
+        tf = Terraform(working_dir=self.build_path())
+        tf.init(backend_config=self.backend_config())
+        if self.use_workspace:
+            try:
+                tf.workspace('select', slef.stage)
+            except:
+                tf.workspace('new', self.stage)
+        return tf
+
+    def plan(self):
+        tf = self.init_client()
+        tf.plan(capture_output=False, var=self.project_vars(),
+                var_file=self.backend_config())
---
+++ packages/ddadevops/ddadevops-0.4.0.dev13.tar.gz/ddadevops-0.4.0.dev13/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from setuptools.command.install import install as _install
+
+class install(_install):
+    def pre_install_script(self):
+        pass
+
+    def post_install_script(self):
+        pass
+
+    def run(self):
+        self.pre_install_script()
+
+        _install.run(self)
+
+        self.post_install_script()
+
+if __name__ == '__main__':
+    setup(
+        name = 'ddadevops',
+        version = '0.4.0.dev13',
+        description = 'tools to support builds combining gopass, terraform, dda-pallet, aws & hetzner-cloud',
+        long_description = '',
+        author = 'meissa GmbH',
+        author_email = 'buero@meissa-gmbh.de',
+        license = 'Apache Software License',
+        url = 'https://github.com/DomainDrivenArchitecture/dda-devops-build',
+        scripts = [],
+        packages = ['ddadevops'],
+        namespace_packages = [],
+        py_modules = [],
+        classifiers = [
+            'License :: OSI Approved :: Apache Software License',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Topic :: Software Development :: Build Tools'
+        ],
+        entry_points = {},
+        data_files = [],
+        package_data = {},
+        install_requires = [],
+        dependency_links = [],
+        zip_safe = True,
+        cmdclass = {'install': install},
+        keywords = '',
+        python_requires = '',
+        obsoletes = [],
+    )
