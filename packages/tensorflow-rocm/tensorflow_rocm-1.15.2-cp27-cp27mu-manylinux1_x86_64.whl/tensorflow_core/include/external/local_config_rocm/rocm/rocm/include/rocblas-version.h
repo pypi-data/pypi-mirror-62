06466b6a4a4381ff06e5d361d4d999fec4ae4ba0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:34 +0000

    Add package tensorflow-rocm/1.15.2/tensorflow_rocm-1.15.2-cp27-cp27mu-manylinux1_x86_64.whl

---
+++ packages/tensorflow-rocm/tensorflow_rocm-1.15.2-cp27-cp27mu-manylinux1_x86_64.whl/tensorflow_core/include/external/local_config_rocm/rocm/rocm/include/rocblas-version.h
@@ -0,0 +1,18 @@
+/* ************************************************************************
+ * Copyright 2016-2020 Advanced Micro Devices, Inc.
+ * ************************************************************************ */
+
+/* the configured version and settings
+ */
+#ifndef ROCBLAS_VERSION_H_
+#define ROCBLAS_VERSION_H_
+
+// clang-format off
+#define ROCBLAS_VERSION_MAJOR       2
+#define ROCBLAS_VERSION_MINOR       14
+#define ROCBLAS_VERSION_PATCH       1
+#define ROCBLAS_VERSION_TWEAK       1861-rocm-rel-3.1-35-cc49425
+#define ROCBLAS_VERSION_COMMIT_ID   
+// clang-format on
+
+#endif
