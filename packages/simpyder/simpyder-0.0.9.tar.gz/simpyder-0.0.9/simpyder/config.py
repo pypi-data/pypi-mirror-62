7a936cf15743e40afd65d71d65857ea687099ca7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:31 +0000

    Add package simpyder/0.0.9/simpyder-0.0.9.tar.gz

---
+++ packages/simpyder/simpyder-0.0.9.tar.gz/simpyder-0.0.9/simpyder/config.py
@@ -0,0 +1,10 @@
+from simpyder.utils import DEFAULT_UA
+
+
+class SimpyderConfig():
+  USER_AGENT = DEFAULT_UA
+  COOKIE = ""
+  PARSE_THREAD_NUMER = 8
+  HEADERS = None
+  LOG_LEVEL = "INFO"
+  pass
