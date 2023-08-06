585a46b870aa685668f9e3b1ac50b3ffe262fa78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:50 +0000

    Add package plonetheme.classic/1.5.1/plonetheme.classic-1.5.1.tar.gz

---
+++ packages/plonetheme.classic/plonetheme.classic-1.5.1.tar.gz/plonetheme.classic-1.5.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+version = '1.5.1'
+
+setup(name='plonetheme.classic',
+      version=version,
+      description="The classic Plone 3 default theme.",
+      long_description=(open("README.txt").read() + "\n" +
+                        open("CHANGES.txt").read()),
+      classifiers=[
+          "Development Status :: 6 - Mature",
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Plone :: 4.3",
+          "Framework :: Zope2",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+      ],
+      keywords='web zope plone theme',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='https://github.com/plone/plonetheme.classic',
+      license='GPL version 2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plonetheme'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
