3c8859f3726c2dbb08f651be25e3b30b40a6bc2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:49 +0000

    Add package transparentai/0.1.1/transparentai-0.1.1.tar.gz

---
+++ packages/transparentai/transparentai-0.1.1.tar.gz/transparentai-0.1.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+fh.close()
+
+setup(
+    name='transparentai',
+    version='0.1.1',
+    description="Python tool to create or inspect a transparent and ethical AI.",
+    license='MIT',
+    author='Nathan LAUGA',
+    author_email='nathan.lauga@protonmail.com',
+    url='https://github.com/Nathanlauga/transparentai',
+    packages=[pkg for pkg in find_packages(
+    ) if pkg.startswith('transparentai')],
+    include_package_data=True,
+    install_requires=[
+        'pandas',
+        'numpy',
+        'matplotlib',
+        'seaborn',
+        'scipy',
+        'scikit-learn',
+        'ipython',
+        'shap'
+    ],
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    python_requires='>=3.5',
+    classifiers=[
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    entry_points={
+
+    },
+)
---
+++ packages/transparentai/transparentai-0.1.1.tar.gz/transparentai-0.1.1/transparentai/plots/__init__.py
@@ -0,0 +1,14 @@
+__all__ = [
+    'plot_or_save', 
+    'plot_stack',
+    'plot_stack_bar',
+    'plot_boxplot_cat_num_var',
+    'plot_correlation_matrix',
+    'plot_gauge',
+    'plot_bar',
+    'save_plot',
+    'save_dir',
+    'verbose'
+    ]
+
+from transparentai.plots.plots import *
