024d47a1604fb143edc82c64ffa60a651df704b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:15 +0000

    Add package pythermalcomfort/0.4.2/pythermalcomfort-0.4.2.tar.gz

---
+++ packages/pythermalcomfort/pythermalcomfort-0.4.2.tar.gz/pythermalcomfort-0.4.2/src/pythermalcomfort/psychrometrics.py
@@ -0,0 +1,99 @@
+import math
+
+
+def p_sat_torr(t):
+    """ Estimates the saturation vapor pressure in [torr]
+
+    Parameters
+    ----------
+    t : float
+        dry bulb air temperature, [C]
+
+    Returns
+    -------
+    p_sat  : float
+        saturation vapor pressure [torr]
+    """
+    return math.exp(18.6686 - 4030.183 / (t + 235.0))
+
+
+def v_relative(v, met):
+    """ Estimates the relative air velocity
+
+    Parameters
+    ----------
+    v : float
+        air velocity measured by the sensor, [m/s]
+    met : float
+        metabolic rate, [met]
+
+    Returns
+    -------
+    vr  : float
+        relative air velocity, [m/s]
+    """
+
+    if met > 1:
+        return 0.3 * (met - 1)
+    else:
+        return v
+
+
+def units_converter(from_units='ip', **kwargs):
+    """ Converts IP values to SI units
+
+    Parameters
+    ----------
+    from_units: str
+        specify system to convert from
+    **kwargs : [t, v]
+
+    Returns
+    -------
+    converted values in SI units
+    """
+    results = list()
+    if from_units == 'ip':
+        for key, value in kwargs.items():
+            if 'tmp' in key or key == 'tr' or key == 'ta':
+                results.append((value - 32) * 5 / 9)
+            if key in ['v', 'vr', 'vel']:
+                results.append(value / 3.281)
+            if key == 'area':
+                results.append(value / 10.764)
+            if key == 'pressure':
+                results.append(value * 101325)
+
+    elif from_units == 'si':
+        for key, value in kwargs.items():
+            if 'tmp' in key or key == 'tr' or key == 'ta':
+                results.append((value * 9 / 5) + 32)
+            if key in ['v', 'vr', 'vel']:
+                results.append(value * 3.281)
+            if key == 'area':
+                results.append(value * 10.764)
+            if key == 'pressure':
+                results.append(value / 101325)
+
+    return results
+
+
+def to_calc(ta, tr, v):
+    """ Calculates operative temperature in accordance with ISO 7726:1998 [5]_
+
+    Parameters
+    ----------
+    ta: float
+        air temperature, [°C]
+    tr: float
+        mean radiant temperature temperature, [°C]
+    v: float
+        air velocity, [m/s]
+
+    Returns
+    -------
+    to: float
+        operative temperature, [°C]
+    """
+
+    return (ta * math.sqrt(10 * v) + tr) / (1 + math.sqrt(10 * v))
