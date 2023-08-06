d4768e1da6bec4a071dd9e979aede8df2a4f4baf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:39 +0000

    Add package eve-optimizer/0.0.7/eve_optimizer-0.0.7.tar.gz

---
+++ packages/eve-optimizer/eve_optimizer-0.0.7.tar.gz/eve_optimizer-0.0.7/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("VERSION", "r") as ver:
+    version = ver.readlines()[-1]
+    print(version)
+
+setuptools.setup(
+    name="eve_optimizer", 
+    version=version,
+    author="Ajay Uppili Arasanipalai",
+    author_email="ajayuppili@gmail.com",
+    description="The EVer Evolving Optimizer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/iyaja/eve",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
