0398ad5f29e10c86f9e2b9cf99d9bb3fe9998542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:33 +0000

    Add package SATOSA/6.1.0/SATOSA-6.1.0-py3-none-any.whl

---
+++ packages/SATOSA/SATOSA-6.1.0-py3-none-any.whl/satosa/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+    satosa
+    ~~~~~~~~~~~~~~~~
+
+    An any to any Single Sign On (SSO) proxy.
+    Has support for SAML2, OpenID Connect and some OAUth2 variants.
+
+    :copyright: (c) 2016 by Umeå University.
+    :license: APACHE 2.0, see LICENSE for more details.
+"""
