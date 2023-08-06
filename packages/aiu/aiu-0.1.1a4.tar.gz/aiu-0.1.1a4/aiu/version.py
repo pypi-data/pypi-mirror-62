db1dc69295587189bccdef431a788d7a1f29cb49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:10 +0000

    Add package aiu/0.1.1a4/aiu-0.1.1a4.tar.gz

---
+++ packages/aiu/aiu-0.1.1a4.tar.gz/aiu-0.1.1a4/aiu/version.py
@@ -0,0 +1,7 @@
+__name__ = "aiu"
+__version__ = "0.1.1a4"
+
+name = __name__
+version = __version__
+
+user_agent_string = "{}/{} - See: https://github.com/oduwsdl/archiveit_utilities".format(name, version)
