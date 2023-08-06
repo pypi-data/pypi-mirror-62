4e4f5602ea913cb568752c86c3fd5c02d308ccda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:34 +0000

    Add package connect-sdk-haxe-port/18.0.8/connect-sdk-haxe-port-18.0.8.tar.gz

---
+++ packages/connect-sdk-haxe-port/connect-sdk-haxe-port-18.0.8.tar.gz/connect-sdk-haxe-port-18.0.8/connect/api/__init__.py
@@ -0,0 +1,20 @@
+from connect.autogen import connect_api_ConnectHelper as ConnectHelper
+from connect.autogen import connect_api_IApiClient as IApiClient
+from connect.autogen import connect_api_IFulfillmentApi as IFulfillmentApi
+from connect.autogen import connect_api_IGeneralApi as IGeneralApi
+from connect.autogen import connect_api_ITierApi as ITierApi
+from connect.autogen import connect_api_IUsageApi as IUsageApi
+from connect.autogen import connect_api_Query as Query
+from connect.autogen import connect_api_Response as Response
+
+
+__all__ = [
+    'ConnectHelper',
+    'IApiClient',
+    'IFulfillmentApi',
+    'IGeneralApi',
+    'ITierApi',
+    'IUsageApi',
+    'Query',
+    'Response',
+]
