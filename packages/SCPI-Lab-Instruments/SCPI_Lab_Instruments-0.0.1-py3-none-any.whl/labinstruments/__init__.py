ad8faeeba3d9081497cc203b9625ab54fdd4860d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:03 +0000

    Add package SCPI-Lab-Instruments/0.0.1/SCPI_Lab_Instruments-0.0.1-py3-none-any.whl

---
+++ packages/SCPI-Lab-Instruments/SCPI_Lab_Instruments-0.0.1-py3-none-any.whl/labinstruments/__init__.py
@@ -0,0 +1,12 @@
+"""SCPI-Lab-Instruments
+
+Control instruments in the receiver lab over Ethernet using SCPI commands.
+
+"""
+
+from labinstruments.hittite import Hittite, SignalGenerator
+# from labinstruments.keithley import Keithley2280
+# from labinstruments.microlambda import YigFilter
+
+__author__ = "John Garrett"
+__version__ = "0.0.1"
