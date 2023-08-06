74382bb86c2138cb6c4ebb2e272e31470f822060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:40 +0000

    Add package chromaterm/0.5.9/chromaterm-0.5.9.tar.gz

---
+++ packages/chromaterm/chromaterm-0.5.9.tar.gz/chromaterm-0.5.9/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""ChromaTerm setup"""
+
+from setuptools import setup
+
+REQUIRES = ['psutil', 'PyYAML']
+REQUIRES_PYTHON = '>=3.5.0'
+
+with open('.github/README.md', 'r') as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name='chromaterm',
+    author='hSaria',
+    author_email='ping@heysaria.com',
+    classifiers=[
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Telecommunications Industry',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3', 'Topic :: Terminals',
+        'Topic :: Utilities'
+    ],
+    description='Colorize your output using RegEx',
+    license='MIT',
+    install_requires=REQUIRES,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    packages=['chromaterm'],
+    python_requires=REQUIRES_PYTHON,
+    scripts=['ct'],
+    setup_requires=REQUIRES,
+    url='https://github.com/hSaria/ChromaTerm',
+    version='0.5.9',
+)
