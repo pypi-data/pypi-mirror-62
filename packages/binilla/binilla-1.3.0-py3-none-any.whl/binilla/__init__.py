1896d35f622a202ebffb39b2e9e87ca2fc9a71ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:56 +0000

    Add package binilla/1.3.0/binilla-1.3.0-py3-none-any.whl

---
+++ packages/binilla/binilla-1.3.0-py3-none-any.whl/binilla/__init__.py
@@ -0,0 +1,16 @@
+# ##############
+#   metadata   #
+# ##############
+__author__ = "Devin Bobadilla, Michelle van der Graaf"
+#           YYYY.MM.DD
+__date__ = "2020.02.29"
+__version__ = (1, 3, 0)
+__website__ = "https://github.com/Sigmmma/binilla"
+__all__ = (
+    'defs', 'widgets', 'windows',
+    'app_window', 'constants', 'edit_manager', 'editor_constants', 'handler',
+    )
+
+from binilla import constants
+
+constants.inject()
