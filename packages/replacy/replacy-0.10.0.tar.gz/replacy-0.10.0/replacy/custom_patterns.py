81ecaa8586b80f224016402a0b8efb8e9dc0fdea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:51 +0000

    Add package replacy/0.10.0/replacy-0.10.0.tar.gz

---
+++ packages/replacy/replacy-0.10.0.tar.gz/replacy-0.10.0/replacy/custom_patterns.py
@@ -0,0 +1,47 @@
+"""
+This module contains predicates which influence what counts as a match
+If the predicate (function) returns True, the match will be ignored
+"""
+import operator
+
+
+def compose(f, g):
+    return lambda doc, start, end: f(g(doc, start, end))
+
+
+def neg(f):
+    # function negatin, ex. neg(preceeded_by_pos(pos))
+    return compose(operator.not_, f)
+
+
+def succeeded_by_phrase(phrase):
+    return lambda doc, start, end: doc[end:].text.lower().startswith(phrase.lower())
+
+
+def preceeded_by_phrase(phrase):
+    return lambda doc, start, end: doc[:start].text.lower().endswith(phrase.lower())
+
+
+def succeeded_by_pos(pos):
+    return lambda doc, start, end: doc[end].pos_ == pos
+
+
+def preceeded_by_pos(pos):
+    return lambda doc, start, end: doc[start - 1].pos_ == pos
+
+
+def succeeded_by_dep(dep):
+    return lambda doc, start, end: doc[end].dep_ == dep
+
+
+def preceeded_by_dep(dep):
+    return lambda doc, start, end: doc[start - 1].dep_ == dep
+
+
+def surrounded_by_phrase(phrase):
+    def _surrounded_by_hook(doc, start, end):
+        preceeds = doc[:start].text.lower().endswith(phrase.lower())
+        follows = doc[end:].text.lower().startswith(phrase.lower())
+        return preceeds and follows
+
+    return _surrounded_by_hook
