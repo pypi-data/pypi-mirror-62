cc8c349be10da31acceca97d951c93e3b73d56df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:55 +0000

    Add package tap-pipedrive/1.0.6/tap-pipedrive-1.0.6.tar.gz

---
+++ packages/tap-pipedrive/tap-pipedrive-1.0.6.tar.gz/tap-pipedrive-1.0.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+setup(name="tap-pipedrive",
+      version="1.0.6",
+      description="Singer.io tap for extracting data from the Pipedrive API",
+      author="Stitch",
+      author_email="dev@stitchdata.com",
+      url="http://singer.io",
+      classifiers=["Programming Language :: Python :: 3 :: Only"],
+      py_modules=["tap_pipedrive"],
+      install_requires=[
+          "pendulum==2.0.4",
+          "requests==2.21.0",
+          "singer-python==5.4.1",
+      ],
+      entry_points="""
+          [console_scripts]
+          tap-pipedrive=tap_pipedrive.cli:main
+      """,
+      packages=["tap_pipedrive",
+                "tap_pipedrive.streams",
+                "tap_pipedrive.streams.recents",
+                "tap_pipedrive.streams.recents.dynamic_typing"],
+      include_package_data=True,
+)
