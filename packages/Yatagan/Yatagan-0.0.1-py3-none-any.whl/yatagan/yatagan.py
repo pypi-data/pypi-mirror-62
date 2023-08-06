0848fa93ba7191b54c50874790e09fd11eb13712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:43 +0000

    Add package Yatagan/0.0.1/Yatagan-0.0.1-py3-none-any.whl

---
+++ packages/Yatagan/Yatagan-0.0.1-py3-none-any.whl/yatagan/yatagan.py
@@ -0,0 +1,14 @@
+def help():
+    """
+    Shows help message
+    Args:
+        None
+    Returns:
+        None
+    """
+    helpmsg = "Usage: yatagan [OPTION] \n" + \
+                "Try \'yatagan --help\' for more information"
+
+    print(helpmsg)
+
+help()
