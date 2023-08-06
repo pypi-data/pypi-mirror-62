96098a77ed113d24cb7886ad68ef6caded5bd55d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:26 +0000

    Add package pmapper/0.4.0/pmapper-0.4.0-py3-none-any.whl

---
+++ packages/pmapper/pmapper-0.4.0-py3-none-any.whl/pmapper/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#==============================================================================
+# author          : Pavel Polishchuk
+# date            : 19-08-2019
+# version         : 
+# python_version  : 
+# copyright       : Pavel Polishchuk 2019
+# license         : 
+#==============================================================================
+
+__version__ = '0.4.0'
