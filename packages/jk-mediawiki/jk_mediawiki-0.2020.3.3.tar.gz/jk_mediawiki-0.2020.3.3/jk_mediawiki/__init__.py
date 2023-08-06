b448a4f93ffc2ceedba7d3bfa0014cf3bfb72a3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:03 +0000

    Add package jk-mediawiki/0.2020.3.3/jk_mediawiki-0.2020.3.3.tar.gz

---
+++ packages/jk-mediawiki/jk_mediawiki-0.2020.3.3.tar.gz/jk_mediawiki-0.2020.3.3/jk_mediawiki/__init__.py
@@ -0,0 +1,16 @@
+﻿
+
+
+__version__ = "0.2020.3.3"
+
+
+
+
+from .lang_support_php import PHPTokenizer, PHP
+from .MediaWikiLocalSettingsFile import MediaWikiLocalSettingsFile
+
+from .MediaWikiLocalUserServiceMgr import MediaWikiLocalUserServiceMgr
+from .MediaWikiLocalUserInstallationMgr import MediaWikiLocalUserInstallationMgr
+
+
+
