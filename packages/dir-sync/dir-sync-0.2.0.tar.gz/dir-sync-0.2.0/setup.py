00b37d9c29d803896dca539302cccfcb4213927f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:04 +0000

    Add package dir-sync/0.2.0/dir-sync-0.2.0.tar.gz

---
+++ packages/dir-sync/dir-sync-0.2.0.tar.gz/dir-sync-0.2.0/setup.py
@@ -0,0 +1,33 @@
+import os
+
+from setuptools import setup
+
+import sync
+
+if os.environ.get('CONVERT_README'):
+    import pypandoc
+
+    long_description = pypandoc.convert_file('README.md', 'rst')
+else:
+    long_description = ''
+
+setup(
+    name='dir-sync',
+    use_scm_version=True,
+    version=sync.VERSION,
+    description='Sync files in the local folder with files in the remote location, e.g. Google Drive.',
+    long_description=long_description,
+    author='Mingchao Liao',
+    author_email='mingchaoliao95@gmail.com',
+    url='https://github.com/mingchaoliao/dir-sync',
+    license='MIT',
+    packages=['sync', 'sync.google_drive', 'sync.local'],
+    zip_safe=False,
+    install_requires=['google-api-python-client', 'google-auth-httplib2', 'google-auth-oauthlib'],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'dir-sync = sync.main:main'
+        ]
+    }
+)
