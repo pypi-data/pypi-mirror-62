5ed18df10bd6d2870f3507247e5f838b0fd26ece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:13 +0000

    Add package awkward1/0.2.0/awkward1-0.2.0-cp27-cp27m-win32.whl

---
+++ packages/awkward1/awkward1-0.2.0-cp27-cp27m-win32.whl/awkward1-0.2.0.data/data/include/awkward/type/RecordType.h
@@ -0,0 +1,46 @@
+// BSD 3-Clause License; see https://github.com/jpivarski/awkward-1.0/blob/master/LICENSE
+
+#ifndef AWKWARD_RECORDTYPE_H_
+#define AWKWARD_RECORDTYPE_H_
+
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+#include "awkward/util.h"
+
+#include "awkward/type/Type.h"
+
+namespace awkward {
+  class EXPORT_SYMBOL RecordType: public Type {
+  public:
+    RecordType(const util::Parameters& parameters, const std::string& typestr, const std::vector<std::shared_ptr<Type>>& types, const std::shared_ptr<util::RecordLookup>& recordlookup);
+    RecordType(const util::Parameters& parameters, const std::string& typestr, const std::vector<std::shared_ptr<Type>>& types);
+
+    const std::vector<std::shared_ptr<Type>> types() const;
+    const std::shared_ptr<util::RecordLookup> recordlookup() const;
+    bool istuple() const;
+
+    std::string tostring_part(const std::string& indent, const std::string& pre, const std::string& post) const override;
+    const std::shared_ptr<Type> shallow_copy() const override;
+    bool equal(const std::shared_ptr<Type>& other, bool check_parameters) const override;
+    int64_t numfields() const override;
+    int64_t fieldindex(const std::string& key) const override;
+    const std::string key(int64_t fieldindex) const override;
+    bool haskey(const std::string& key) const override;
+    const std::vector<std::string> keys() const override;
+    const std::shared_ptr<Content> empty() const override;
+
+    const std::shared_ptr<Type> field(int64_t fieldindex) const;
+    const std::shared_ptr<Type> field(const std::string& key) const;
+    const std::vector<std::shared_ptr<Type>> fields() const;
+    const std::vector<std::pair<std::string, std::shared_ptr<Type>>> fielditems() const;
+    const std::shared_ptr<Type> astuple() const;
+
+  private:
+    const std::vector<std::shared_ptr<Type>> types_;
+    const std::shared_ptr<util::RecordLookup> recordlookup_;
+  };
+}
+
+#endif // AWKWARD_RECORDTYPE_H_
