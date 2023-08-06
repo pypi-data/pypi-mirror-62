44056f3febd6425524548b61fd373faa7d32f506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:51 +0000

    Add package WUDESIM-Py/0.0.1/WUDESIM_Py-0.0.1.tar.gz

---
+++ packages/WUDESIM-Py/WUDESIM_Py-0.0.1.tar.gz/WUDESIM_Py-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+from distutils.core import Extension
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+setup(
+   name='WUDESIM_Py',
+   version='0.0.1',
+   description='A model for simulating water quality in the dead-end branches of drinking water distribution networks',
+   author='Ahmed Abokifa',
+   author_email=' abokifa@uic.edu',
+   packages=find_packages()  #same as name
+)
