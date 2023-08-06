6259ee21dfae6deeee6fec23c3297030508b7d28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:23 +0000

    Add package terminus-client-python/0.0.15/terminus-client-python-0.0.15.tar.gz

---
+++ packages/terminus-client-python/terminus-client-python-0.0.15.tar.gz/terminus-client-python-0.0.15/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+requires = [
+   'requests'
+]
+
+test_requirements = [
+    'pytest>=3',
+    'pytest-cov',
+    'pytest-mock',
+    'pytest-xdist',
+]
+
+setuptools.setup(
+    name="terminus-client-python",
+    version="0.0.15",
+    author="TerminusDB group",
+    author_email="terminusdatabase@gmail.com",
+    description="Python client for Terminus DB",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_data={'': ['LICENSE']},
+    url="https://github.com/terminusdb/terminus-client-python",
+    packages=setuptools.find_packages(exclude=['tests']),
+    include_package_data=True,
+    install_requires=requires,
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=test_requirements,
+    python_requires='>=3.6',
+)
---
+++ packages/terminus-client-python/terminus-client-python-0.0.15.tar.gz/terminus-client-python-0.0.15/woqlclient/__init__.py
@@ -0,0 +1,14 @@
+"""
+    woqlClient Library
+    ~~~~~~~~~~~~~~~~~~~~~
+
+"""
+from .woqlClient import *
+from .woql import WOQLQuery
+from .__version__ import __version__
+
+__all__ = ['woqlClient', 'woql']
+
+name = "woqlclient"
+#__version__ = "0.0.4"
+# cmdclass = {'test': PyTest}
