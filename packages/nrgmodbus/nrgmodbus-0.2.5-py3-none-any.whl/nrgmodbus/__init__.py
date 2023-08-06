61df31b4a4206984259fdd3518a2a3c3cc45e3bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:29 +0000

    Add package nrgmodbus/0.2.5/nrgmodbus-0.2.5-py3-none-any.whl

---
+++ packages/nrgmodbus/nrgmodbus-0.2.5-py3-none-any.whl/nrgmodbus/__init__.py
@@ -0,0 +1,6 @@
+__name__ = "nrgmodbus"
+from .ipackaccess import ipackaccess
+from .ipackaccess import ipackaccess_registers
+from .spidar import spidar_v1
+from .spidar import spidar_registers
+from .utilities import combine_registers, combine_u32_registers, convert_hex_to_float
