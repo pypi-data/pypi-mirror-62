e84d07de7b09a281d05af14e32b5dd1d6edbf49f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:51 +0000

    Add package BioPro/0.1.0/BioPro-0.1.0.tar.gz

---
+++ packages/BioPro/BioPro-0.1.0.tar.gz/BioPro-0.1.0/README.md
@@ -0,0 +1,19 @@
+# Predicting Biochemical Pathways Involving Target RBP
+
+
+
+## Install
+
+
+### Install via source code
+
+To install via source code, clone our github repo and run:
+
+`bash install.sh`
+
+
+### Install via pip
+
+Or you can directly install our software from pip:
+
+`pip install BioPro`
---
+++ packages/BioPro/BioPro-0.1.0.tar.gz/BioPro-0.1.0/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+from distutils.util import convert_path
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+main_ns = {}
+ver_path = convert_path('version.py')
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
+
+setuptools.setup(
+    name="BioPro",
+    version=main_ns['__version__'],
+    author="Tianqin Li, Zeyuan Zuo, Jui-Chia Chung, Serena Abraham, Snigdha Agarwal",
+    author_email="jacklitianqin@gmail.com",
+    description="BioPro is a bioinformatics pipeline that predicts the functional targets of RNA-binding protein and the biological processes to which these functional targets contribute. ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Crazy-Jack/SuperdopeBioinformatics",
+    packages=['Team3'],
+    entry_points = {
+        'console_scripts':[
+            'BioPro = Team3.__main__:main',
+            ]
+        },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+    ],
+)
