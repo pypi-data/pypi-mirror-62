1fe7929f0834c0f4333a3698e4502db2a0063880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:54 +0000

    Add package Python-Test-Migration-Challenge/1.0.1/Python-Test-Migration-Challenge-1.0.1.tar.gz

---
+++ packages/Python-Test-Migration-Challenge/Python-Test-Migration-Challenge-1.0.1.tar.gz/Python-Test-Migration-Challenge-1.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import setuptools
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="Python-Test-Migration-Challenge",
+    version="1.0.1",
+    description="A Python package to do cloud migration Coding challenge",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/alhadbhadekar/python-test-migrate-challenge",
+    author="Alhad Bhadekar",
+    author_email="alhad.bhadekar@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=["dill"],
+)
