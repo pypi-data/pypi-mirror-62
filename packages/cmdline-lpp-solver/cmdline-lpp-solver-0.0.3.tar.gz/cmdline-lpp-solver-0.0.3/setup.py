e7da7d2bd0ff3e129f27659ce98be525eade84e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:30 +0000

    Add package cmdline-lpp-solver/0.0.3/cmdline-lpp-solver-0.0.3.tar.gz

---
+++ packages/cmdline-lpp-solver/cmdline-lpp-solver-0.0.3.tar.gz/cmdline-lpp-solver-0.0.3/setup.py
@@ -0,0 +1,40 @@
+"""setup.py: setuptools control"""
+
+from setuptools import setup
+import os
+
+directory_location = os.path.dirname(__file__)
+readme_location = os.path.join(directory_location, 'README.md')
+requirements_location = os.path.join(directory_location, 'requirements.txt')
+
+with open(readme_location, "rb") as f:
+    long_descr = f.read().decode("utf-8")
+
+with open(requirements_location, 'r') as requirements_file:
+    requirements = requirements_file.read().split('\n')
+
+setup(
+    name="cmdline-lpp-solver",
+    packages=["lpp_solver"],
+    entry_points={
+        "console_scripts": ['lpp_solver = lpp_solver.simplex:main']
+    },
+    version="0.0.3",
+    license='MIT',
+    description="Python command line application with solves linear programming problems.",
+    long_description=long_descr,
+    author="Maxym Fuhol, Evgenii Babin",
+    author_email="maxym.fugol@gmail.com, prototy791@gmail.com",
+    url="https://github.com/Mtrqq/LPPSolver/blob/master/lpp_solver/__init__.py",
+    download_url="https://github.com/Mtrqq/LPPSolver/archive/0.0.3.tar.gz",
+    keywords = ['LINEAR PROGRAMMING', 'CMD', 'UTILITY'],
+    install_requires=requirements,
+      classifiers=[
+    'Development Status :: 3 - Alpha',
+    'License :: OSI Approved :: MIT License', 
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    ]
+    )
