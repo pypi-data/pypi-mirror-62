68819a85d5135b4006ddff4418aa2d14b923233a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:28 +0000

    Add package spatial-efd/1.2.0/spatial_efd-1.2.0.tar.gz

---
+++ packages/spatial-efd/spatial_efd-1.2.0.tar.gz/spatial_efd-1.2.0/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(name='spatial_efd',
+      version='1.2.0',
+      description='Spatial elliptical fourier analysis',
+      url='http://github.com/sgrieve/spatial-efd',
+      long_description=readme(),
+      keywords='GIS elliptical fourier analysis shapefile',
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Intended Audience :: Science/Research',
+                   'Topic :: Scientific/Engineering :: GIS',
+                   'Operating System :: OS Independent'],
+      author='Stuart WD Grieve',
+      author_email='stuart@swdg.io',
+      license='MIT',
+      packages=['spatial_efd'],
+      setup_requires=['pytest-runner'],
+      install_requires=['matplotlib>=2.0.0', 'numpy>=1.12.0',
+                        'pyshp>=1.2.10', 'future'],
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='pytest-runner',
+      tests_require=['pytest'])
