e99502a9ab3613e473809f2969c0a1e65a7c808f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:51 +0000

    Add package aequilibrae/0.6.2/aequilibrae-0.6.2.tar.gz

---
+++ packages/aequilibrae/aequilibrae-0.6.2.tar.gz/aequilibrae-0.6.2/tests/aequilibrae/transit/test_create_gtfsdb.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+import os
+from aequilibrae.transit.gtfs import create_gtfsdb
+from ...data import gtfs_folder
+from ...data import gtfs_zip
+from ...data import gtfs_db_output
+
+
+class TestCreate_gtfsdb(TestCase):
+    def tearDown(self) -> None:
+        if os.path.isfile(gtfs_db_output):
+            os.unlink(gtfs_db_output)
+
+    def test_create_database(self):
+        # TODO: Fix Travis-ci configuration in order to properly run the test with spatialite enabled
+        self.gtfs = create_gtfsdb(gtfs_zip, save_db=gtfs_db_output, overwrite=True, spatialite_enabled=False)
+        self.gtfs.create_database()
+        self.gtfs.conn.close()
+
+    def test_import_gtfs(self):
+        # self.gtfs = create_gtfsdb(gtfs_folder, save_db=gtfs_db_output, overwrite=True, spatialite_enabled=True)
+        self.gtfs = create_gtfsdb(gtfs_folder, save_db=gtfs_db_output, overwrite=True, spatialite_enabled=False)
+        self.gtfs.import_gtfs()
+
+        self.gtfs = create_gtfsdb(gtfs_zip, save_db=gtfs_db_output, overwrite=True, spatialite_enabled=False)
+        self.gtfs.import_gtfs()
