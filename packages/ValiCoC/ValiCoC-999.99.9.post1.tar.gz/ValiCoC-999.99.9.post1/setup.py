4e696dd1cecbe4d3e0e4529e9899d7cbd35c8e38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:53 +0000

    Add package ValiCoC/999.99.9.post1/ValiCoC-999.99.9.post1.tar.gz

---
+++ packages/ValiCoC/ValiCoC-999.99.9.post1.tar.gz/ValiCoC-999.99.9.post1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ValiCoC',
+    version='999.99.9-1',
+    description='Validation code filter',
+    long_description=open('README.rst').read(),
+    author='O_O',
+    author_email='Adammiao@outlook.com',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+    url='https://github.com/Adam-Miao/ValiCoC/'
+)
