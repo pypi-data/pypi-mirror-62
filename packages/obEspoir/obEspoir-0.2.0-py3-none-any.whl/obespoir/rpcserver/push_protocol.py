67ada9885122ba876fb13f18bfcc42684fb3a3d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:26 +0000

    Add package obEspoir/0.2.0/obEspoir-0.2.0-py3-none-any.whl

---
+++ packages/obEspoir/obEspoir-0.2.0-py3-none-any.whl/obespoir/rpcserver/push_protocol.py
@@ -0,0 +1,80 @@
+# coding=utf-8
+"""
+author = jamon
+"""
+
+import asyncio
+import ujson
+import struct
+
+from obespoir.base.common_define import ConnectionStatus
+from obespoir.base.global_object import GlobalObject
+from obespoir.base.ob_protocol import ObProtocol
+from obespoir.rpcserver.connection_manager import RpcConnectionManager
+from obespoir.rpcserver.route import rpc_message_handle
+from obespoir.share.encodeutil import AesEncoder
+from obespoir.share.ob_log import logger
+
+
+class RpcPushProtocol(ObProtocol):
+
+    def __init__(self):
+        super().__init__()
+        self.host = None
+        self.port = None
+        self.handfrt = "iii"  # (int, int, int)  -> (message_length, command_id, version)
+        self.head_len = struct.calcsize(self.handfrt)
+        self.identifier = 0
+
+        self.encode_ins = AesEncoder(GlobalObject().rpc_password, encode_type=GlobalObject().rpc_encode_type)
+        self.version = 0
+
+        self._buffer = b""    # 数据缓冲buffer
+        self._head = None     # 消息头, list,   [message_length, command_id, version]
+        self.transport = None
+
+    async def send_message(self, command_id, message, session_id, to=None):
+        print("rpc push:", message, type(message))
+        data = self.pack(message, command_id, session_id, to)
+        print("rpc_push send_message:", data, type(data))
+        self.transport.write(data)
+
+    async def message_handle(self, command_id, version, data):
+        """
+        实际处理消息
+        :param command_id:
+        :param version:
+        :param data:
+        :return:
+        """
+        print("rpc push receive response message_handle:", command_id, data)
+        # result = await rpc_route.call_target(command_id, data)
+        result = await rpc_message_handle(command_id, data)
+        print("rpc result=", result)
+        # self.transport.write(self.pack(result, command_id))
+
+    def connection_made(self, transport):
+        self.transport = transport
+        address = transport.get_extra_info('peername')
+        self.host, self.port = address
+        RpcConnectionManager().store_connection(*address, self, status=ConnectionStatus.ESTABLISHED)
+        logger.debug(
+            'connected to {} port {}'.format(*address)
+        )
+
+    def data_received(self, data):
+        logger.debug('rpc_push received response {!r}'.format(data))
+        super().data_received(data)
+
+    def eof_received(self):
+        logger.debug('rpc_push received EOF')
+        if self.transport and self.transport.can_write_eof():
+            self.transport.write_eof()
+        RpcConnectionManager().lost_connection(self.host, self.port)
+
+    def connnection_lost(self, exc):
+        logger.debug('server closed connection')
+        RpcConnectionManager().lost_connection(self.host, self.port)
+        super(RpcPushProtocol, self).connection_lost(exc)
+
+
---
+++ packages/obEspoir/obEspoir-0.2.0-py3-none-any.whl/obespoir/rpcserver/route.py
@@ -0,0 +1,113 @@
+# coding=utf-8
+"""
+author = jamon
+"""
+
+import ujson
+
+from obespoir.base.ob_handler import RegisterEvent
+from obespoir.base.common_define import NodeType, DEFAULT_ID
+from obespoir.base.global_object import GlobalObject
+from obespoir.rpcserver.push_lib import push_message
+from obespoir.share.ob_log import logger
+from obespoir.websocketserver.manager import WebsocketConnectionManager
+
+
+async def call_target(command_id, data, session_id):
+    """
+    调用注册的消息处理
+    :param command_id:
+    :param data:
+    :param session_id:
+    :return:
+    """
+    obj = RegisterEvent.events.get(command_id, None)
+    obj = obj if obj else RegisterEvent.events.get(DEFAULT_ID, None)   # 如果没有，则进入默认消息处理
+    if not obj:
+        logger.error("command_id {} not register!".format(command_id))
+        return
+    handler = obj.get("handler")(data, command_id, session_id)
+    ret = await handler.execute()
+    if obj.get("need_return"):
+        to, _ = session_id.split("_")
+        await push_message(None, command_id, ret, session_id, to)
+
+
+async def rpc_message_handle(command_id, data):
+    """
+    接收到rpc消息处理
+    :param command_id: int, 消息ID
+    :param data: dict, 消息内容
+    :return:
+    """
+    if not isinstance(data, dict):
+        data = data.decode("utf-8") if isinstance(data, bytes) else data
+        data = ujson.loads(data)
+    print("rpc_message_handle:", command_id, data, type(data), GlobalObject().id)
+    session_id = data.get("src", None)
+    to = data.get("to", None)
+    info = data.get("data", {})
+    if to:
+        if to != GlobalObject().id:
+            return await forwarding_next(command_id, info, session_id, to)
+        else:
+            return await local_handle(command_id, data, session_id)
+    else:
+        if GlobalObject().rpc_route:   # 配置了路由信息（一般只有route类型节点才会配置）
+            next_node = -1
+            for node, route in GlobalObject().rpc_route.get("special", {}).items():
+                if command_id in route:
+                    next_node = node
+                    break
+
+            if not next_node:
+                for node, route in GlobalObject().rpc_route.get("range", {}).items():
+                    if next_node:
+                        break
+                    for r in route:
+                        if r[0] <= command_id <= r[1]:
+                            next_node = node
+                            break
+            print("bbbbbb:", next_node, GlobalObject().type, command_id, GlobalObject().rpc_route)
+            if -1 == next_node:
+                next_node = NodeType.ROUTE
+            if next_node == GlobalObject().type:
+                return await local_handle(command_id, data, session_id)
+            else:
+                return await push_message(next_node, command_id, data, session_id, to)
+        else:   # 没有路由信息，则默认为本地处理
+            return await local_handle(command_id, data, session_id)
+
+
+async def forwarding_next(command_id, data, session_id, to):
+    """
+    有明确目标，向其他节点转发
+    :param command_id:
+    :param data:
+    :param session_id:
+    :param to:
+    :return:None
+    """
+    return await push_message(None, command_id, data, session_id, to)
+
+
+async def local_handle(command_id, data, session_id):
+    """
+    无需转发, 本节点处理的消息
+    :param command_id:
+    :param data:
+    :param session_id:
+    :return: None
+    """
+    if NodeType.PROXY == GlobalObject().type:
+        # proxy类型节点收到rpc消息后会通过websocket推送给客户端
+        seq = int(session_id.split("_")[-1])
+        client = WebsocketConnectionManager().get_websocket(seq)
+        if not isinstance(data, str):
+            data = ujson.dumps(data)
+        print("cccccc:", data, command_id, client)
+        await client.send_message(data, command_id)
+        return
+    else:
+        await call_target(command_id, data, session_id=session_id)
+
