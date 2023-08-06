8b9c5e08b23cd3f798dd400ba2d9b7a5cf744689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:10 +0000

    Add package KGlobal/1.0.16/KGlobal-1.0.16-py3-none-any.whl

---
+++ packages/KGlobal/KGlobal-1.0.16-py3-none-any.whl/KGlobal_Core/main.py
@@ -0,0 +1,36 @@
+from getopt import GetoptError
+import sys
+
+if MYPY_CHECK_RUNNING:
+    from typing import List, Optional
+
+logger = logging.getLogger(__name__)
+
+
+def main(args=None):
+    # type: (Optional[List[str]]) -> int
+    if args is None:
+        args = sys.argv[1:]
+
+    try:
+        opts, args = parse_command(args)
+    except GetoptError as exc:
+        sys.stderr.write("ERROR: %s" % exc)
+        sys.stderr.write(os.linesep)
+        return 1
+
+    for cmd, arg in opts:
+        if cmd == '-h':
+            print('KGlobal -create_master_salt')
+        elif cmd == '-create_master_salt':
+            try:
+                print('Creating Master Salt Key....')
+                from . import create_master_salt_key
+                create_master_salt_key()
+                print('Master Salt Key has been successfully created')
+            except Exception as exc:
+                sys.stderr.write("ERROR: %s" % exc)
+                sys.stderr.write(os.linesep)
+                return 1
+
+    return 0
