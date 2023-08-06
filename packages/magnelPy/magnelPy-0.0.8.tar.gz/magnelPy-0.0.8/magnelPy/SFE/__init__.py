91cc5a0bc0a608d675391cddac4f0ccf897afac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:46 +0000

    Add package magnelPy/0.0.8/magnelPy-0.0.8.tar.gz

---
+++ packages/magnelPy/magnelPy-0.0.8.tar.gz/magnelPy-0.0.8/magnelPy/SFE/__init__.py
@@ -0,0 +1,18 @@
+"""
+magnelPy.SFE
+============
+Provides
+  1. Fire definition
+  2. Thermal calculation tools
+  3. Structural calculation tools
+
+=============== ==========================================================
+Fire definition ~ FireCurve
+==========================================================================
+ISO834          ISO834 standard fire definition
+=============== ==========================================================
+
+"""
+
+### get submodules ###
+from magnelPy.SFE import FireCurve
