6bce967817d312ef8921ea0d6fa712bc4a0fbcec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:39 +0000

    Add package pychemin/0.0.1.dev0/PyChemin-0.0.1.dev0.tar.gz

---
+++ packages/pychemin/PyChemin-0.0.1.dev0.tar.gz/PyChemin-0.0.1.dev0/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pychemin']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['colr>=0.9.1,<0.10.0',
+ 'docopt>=0.6.2,<0.7.0',
+ 'fuzzywuzzy>=0.18.0,<0.19.0',
+ 'lark-parser>=0.8.1,<0.9.0',
+ 'python-levenshtein>=0.12.0,<0.13.0',
+ 'termcolor>=1.1.0,<2.0.0',
+ 'toml>=0.10.0,<0.11.0',
+ 'unidecode>=1.1.1,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['pychemin = pychemin.cli:run']}
+
+setup_kwargs = {
+    'name': 'pychemin',
+    'version': '0.0.1.dev0',
+    'description': 'A minimal language to describe interactive, terminal-based text adventure games',
+    'long_description': None,
+    'author': 'ewen-lbh',
+    'author_email': 'ewen.lebihan7@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
