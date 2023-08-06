547ed13961a0caaf47d1230bb385b5e74b31f771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:46 +0000

    Add package computype/0.0.4/computype-0.0.4.tar.gz

---
+++ packages/computype/computype-0.0.4.tar.gz/computype-0.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+with open("DESCRIPTION.md", "r") as fh:
+    ld = fh.read()
+setuptools.setup (
+    name='computype',
+    version='0.0.4',
+    description='A library that looks like the computer is typing and stuff, see https://codeberg.org/RevelCorp/Computype',
+    author = "Revel Corp (Jason Wang specifically)",
+    packages = setuptools.find_packages(),
+    author_email="revel.corporation@mailfence.com",
+    install_requires = ['keyboard'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires = '>=3.0',
+    url = 'https://codeberg.org/RevelCorp/Computype',
+    license = ("MIT"),
+    long_description = ld,
+    long_description_content_type = 'text/markdown'
+)
