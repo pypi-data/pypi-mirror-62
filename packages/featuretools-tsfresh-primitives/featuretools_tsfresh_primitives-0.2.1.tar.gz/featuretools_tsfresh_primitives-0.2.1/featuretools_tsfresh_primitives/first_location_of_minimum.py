c5a61f256e83cc14db6a7e8b37c1ac91d0358917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:20 +0000

    Add package featuretools-tsfresh-primitives/0.2.1/featuretools_tsfresh_primitives-0.2.1.tar.gz

---
+++ packages/featuretools-tsfresh-primitives/featuretools_tsfresh_primitives-0.2.1.tar.gz/featuretools_tsfresh_primitives-0.2.1/featuretools_tsfresh_primitives/first_location_of_minimum.py
@@ -0,0 +1,25 @@
+from featuretools.primitives import AggregationPrimitive
+from featuretools.variable_types import Numeric
+from tsfresh.feature_extraction.feature_calculators import \
+    first_location_of_minimum
+
+from .utils import to_array
+
+
+class FirstLocationOfMinimum(AggregationPrimitive):
+    """Returns the first location of the minimal value of x. The position is
+    calculated relatively to the length of x.
+
+    Docstring source:
+    https://tsfresh.readthedocs.io/en/latest/api/tsfresh.feature_extraction.html#tsfresh.feature_extraction.feature_calculators.first_location_of_minimum
+    """
+    name = "first_location_of_minimum"
+    input_types = [Numeric]
+    return_type = Numeric
+    stack_on_self = False
+
+    def get_function(self):
+        def function(x):
+            return first_location_of_minimum(to_array(x))
+
+        return function
