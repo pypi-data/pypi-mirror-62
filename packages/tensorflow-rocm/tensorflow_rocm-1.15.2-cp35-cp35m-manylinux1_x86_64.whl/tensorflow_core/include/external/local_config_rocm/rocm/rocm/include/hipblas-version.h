8c5e9c9070ab6c4227c0481f3db2fcf8dc7e123d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:53 +0000

    Add package tensorflow-rocm/1.15.2/tensorflow_rocm-1.15.2-cp35-cp35m-manylinux1_x86_64.whl

---
+++ packages/tensorflow-rocm/tensorflow_rocm-1.15.2-cp35-cp35m-manylinux1_x86_64.whl/tensorflow_core/include/external/local_config_rocm/rocm/rocm/include/hipblas-version.h
@@ -0,0 +1,12 @@
+/* ************************************************************************
+ * Copyright 2016-2020 Advanced Micro Devices, Inc.
+ * ************************************************************************ */
+
+/* the configured version and settings
+ */
+// clang-format off
+#define hipblasVersionMajor 0
+#define hipblaseVersionMinor 20
+#define hipblasVersionPatch 0
+#define hipblasVersionTweak 307-rocm-rel-3.1-35-ff35c32
+// clang-format on
