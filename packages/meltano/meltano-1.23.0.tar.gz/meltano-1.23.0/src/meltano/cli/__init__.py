4bc9d9794314d76ac6e6dc613e8b25f045406d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:49 +0000

    Add package meltano/1.23.0/meltano-1.23.0.tar.gz

---
+++ packages/meltano/meltano-1.23.0.tar.gz/meltano-1.23.0/src/meltano/cli/__init__.py
@@ -0,0 +1,28 @@
+import os
+
+from .cli import cli
+from . import (
+    elt,
+    schema,
+    discovery,
+    initialize,
+    add,
+    install,
+    invoke,
+    ui,
+    upgrade,
+    permissions,
+    schedule,
+    select,
+    repl,
+    config,
+    model,
+    user,
+)
+
+
+def main():
+    # mark the current process as executed via the `cli`
+    os.environ["MELTANO_JOB_TRIGGER"] = os.getenv("MELTANO_JOB_TRIGGER", "cli")
+
+    cli(obj={"project": None})
