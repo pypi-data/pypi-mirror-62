b71427340e209918e95cd6f88a15601bf22a70ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:13 +0000

    Add package coco-puppet/0.0.7/coco-puppet-0.0.7.tar.gz

---
+++ packages/coco-puppet/coco-puppet-0.0.7.tar.gz/coco-puppet-0.0.7/setup.py
@@ -0,0 +1,32 @@
+
+from setuptools import setup
+from setuptools import find_packages
+
+long_description = '''
+Puppet is a high-level toolkit for building chatbots using conversational components.
+Try puppet if you want to make your chatbot modular using composable components.
+'''
+
+setup(name='coco-puppet',
+      version='0.0.7',
+      description='Modular composable chatbot development',
+      long_description=long_description,
+      author='Chen Buskilla',
+      author_email='chen@buskilla.com',
+      url='https://github.com/chenb67/puppet',
+      license='GPLv3',
+      install_requires=["aioconsole"],
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Education',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.7',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      packages=find_packages(),
+      python_requires=">=3.7"
+)
