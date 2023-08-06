696a4210094dca36483ade9068ea86f4c6ad8d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:41 +0000

    Add package pyconmech/0.4.1/pyconmech-0.4.1.tar.gz

---
+++ packages/pyconmech/pyconmech-0.4.1.tar.gz/pyconmech-0.4.1/src/pyconmech/database/__init__.py
@@ -0,0 +1,15 @@
+"""
+********************************************************************************
+pyconmech.database
+********************************************************************************
+
+.. currentmodule:: pyconmech.database
+
+This package contains some commonly used material properties, cross section properties
+for conmech.
+
+"""
+
+from __future__ import absolute_import
+
+from .material_properties import *
