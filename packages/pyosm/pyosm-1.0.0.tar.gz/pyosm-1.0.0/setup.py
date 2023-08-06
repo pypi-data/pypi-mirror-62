572c71c495a073f30fb03a2a4f582267f26021e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:27 +0000

    Add package pyosm/1.0.0/pyosm-1.0.0.tar.gz

---
+++ packages/pyosm/pyosm-1.0.0.tar.gz/pyosm-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='pyosm',
+    version='1.0.0',
+    author='Ian Dees',
+    author_email='ian.dees@gmail.com',
+    packages=['pyosm'],
+    url='http://github.com/iandees/pyosm',
+    license='LICENSE.txt',
+    description='Parses OSM XML files in a fast and reliable way.',
+    long_description=open('README.md').read(),
+    keywords=['osm', 'openstreetmap', 'xml', 'parsing'],
+    install_requires=[
+        'lxml',
+        'requests',
+    ]
+)
