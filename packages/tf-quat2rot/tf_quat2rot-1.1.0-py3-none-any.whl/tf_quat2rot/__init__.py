83e22d7634d8204478ba202b7299b99590fb9f26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:44 +0000

    Add package tf-quat2rot/1.1.0/tf_quat2rot-1.1.0-py3-none-any.whl

---
+++ packages/tf-quat2rot/tf_quat2rot-1.1.0-py3-none-any.whl/tf_quat2rot/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+__author__ = """Christoph Rist"""
+__email__ = "c.rist@posteo.de"
+
+from .converter import quaternion_to_rotation_matrix
+from .converter import rotation_matrix_to_quaternion
+from .generator import random_uniform_quaternion
+from .generator import random_uniform_rotation_matrix
+
+
+__all__ = [
+    "quaternion_to_rotation_matrix",
+    "rotation_matrix_to_quaternion",
+    "random_uniform_quaternion",
+    "random_uniform_rotation_matrix",
+]
