55345693df55d4eaac2369b7d70a95379e656628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:14 +0000

    Add package KGlobal/1.0.18/KGlobal-1.0.18.tar.gz

---
+++ packages/KGlobal/KGlobal-1.0.18.tar.gz/KGlobal-1.0.18/KGlobal/main.py
@@ -0,0 +1,32 @@
+from getopt import GetoptError
+import sys
+
+logger = logging.getLogger(__name__)
+
+
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+    try:
+        opts, args = parse_command(args)
+    except GetoptError as exc:
+        sys.stderr.write("ERROR: %s" % exc)
+        sys.stderr.write(os.linesep)
+        return 1
+
+    for cmd, arg in opts:
+        if cmd == '-h':
+            print('KGlobal -create_master_salt')
+        elif cmd == '-create_master_salt':
+            try:
+                print('Creating Master Salt Key....')
+                from . import create_master_salt_key
+                create_master_salt_key()
+                print('Master Salt Key has been successfully created')
+            except Exception as exc:
+                sys.stderr.write("ERROR: %s" % exc)
+                sys.stderr.write(os.linesep)
+                return 1
+
+    return 0
