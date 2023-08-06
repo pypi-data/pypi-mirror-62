73069bf4d41607efb17111be337d50896664928b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:45 +0000

    Add package django-cfdi/1.5.14/django_cfdi-1.5.14-py3-none-any.whl

---
+++ packages/django-cfdi/django_cfdi-1.5.14-py3-none-any.whl/cfdi/catalogos.py
@@ -0,0 +1,9 @@
+MOTIVOS_TRASLADO = (
+	("01", "01 Envío de mercancias facturadas con anterioridad"),
+	("02", "02 Reubicación de mercancías propias"),
+	("03", "03 Envío de mercancías objeto de contrato de consignación"),
+	("04", "04 Envío de mercancías para posterior enajenación"),
+	("05", "05 Envío de mercancías propiedad de terceros"),
+	("99", "99 Otros"),
+)
+
