b517f872ca33ffa7e467cbcb437bf0ff98a4cf37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:13 +0000

    Add package sense-push/0.0.8/sense_push-0.0.8-py3-none-any.whl

---
+++ packages/sense-push/sense_push-0.0.8-py3-none-any.whl/sense_push/core/data.py
@@ -0,0 +1,34 @@
+from sense_push.util import *
+from sense_push.common import *
+import sense_core as sd
+
+
+class PushDataHandler(object):
+
+    def __init__(self, database=None, table=None, queue_configs=None):
+        self.database = database
+        self.table = table
+        self.queue_configs = queue_configs
+
+    def insert_data(self, data):
+        message = build_push_message(PUSH_ACTION_INSERT, self.database, self.table, data)
+        if not message:
+            return
+        self.push_queue_message(message)
+
+    def update_data(self, data):
+        message = build_push_message(PUSH_ACTION_UPDATE, self.database, self.table, data)
+        if not message:
+            return
+        self.push_queue_message(message)
+
+    def delete_data(self, data):
+        message = build_push_message(PUSH_ACTION_DELETE, self.database, self.table, data)
+        if not message:
+            return
+        self.push_queue_message(message)
+
+    def push_queue_message(self, message):
+        for config in self.queue_configs:
+            producer = sd.RabbitProducer(config_info=config)
+            producer.send_safely(config['queue'], sd.dump_json(message), True, 1000000)
