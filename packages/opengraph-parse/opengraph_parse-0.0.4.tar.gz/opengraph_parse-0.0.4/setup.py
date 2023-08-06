7b6f54de49471e8e9b99f745efabba8689faef0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:48 +0000

    Add package opengraph-parse/0.0.4/opengraph_parse-0.0.4.tar.gz

---
+++ packages/opengraph-parse/opengraph_parse-0.0.4.tar.gz/opengraph_parse-0.0.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="opengraph_parse", 
+    version="0.0.4",
+    author="Ben Jacobson",
+    author_email="",
+    description="Parses Open Graph meta tags of web pages and returns a handy JSON-like object. Use this to generate preview of web pages like you see on social media",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ben-jacobson/opengraph_parse",
+    packages=setuptools.find_packages(),
+    install_requires = ['bs4', 'requests'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
