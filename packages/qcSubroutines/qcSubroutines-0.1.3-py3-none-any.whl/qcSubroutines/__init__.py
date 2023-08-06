bf67e39b5b2afa2c9cc360dbb16d3c08546c467e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:53 +0000

    Add package qcSubroutines/0.1.3/qcSubroutines-0.1.3-py3-none-any.whl

---
+++ packages/qcSubroutines/qcSubroutines-0.1.3-py3-none-any.whl/qcSubroutines/__init__.py
@@ -0,0 +1,8 @@
+from qcSubroutines._swap_test import swap_test
+from qcSubroutines._grover import grover
+from qcSubroutines._dist_calc import dist_calc, transform
+from qcSubroutines._duerr_hoyer import duerr_hoyer
+from qcSubroutines._quantum_phase_estimation import quantum_phase_estimation
+
+
+__version__ = '0.1'
