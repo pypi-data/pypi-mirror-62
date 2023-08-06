c2d00f79ce32c41e9619de9d3a50bd6a1480e03f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:47 +0000

    Add package falcon-heavy/0.1.2/falcon-heavy-0.1.2.tar.gz

---
+++ packages/falcon-heavy/falcon-heavy-0.1.2.tar.gz/falcon-heavy-0.1.2/falcon_heavy/core/openapi/utils.py
@@ -0,0 +1,23 @@
+# Copyright 2019-2020 Not Just A Toy Corp.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import typing as ty
+
+__all__ = (
+    'are_all_items_equal',
+)
+
+
+def are_all_items_equal(items: ty.Sequence) -> bool:
+    return items and items.count(items[0]) == len(items)
