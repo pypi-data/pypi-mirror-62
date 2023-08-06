7b9e14397ccf75b83c0386de279bcc6f47cfd7a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:57 +0000

    Add package artan/0.1.0/artan-0.1.0.tar.gz

---
+++ packages/artan/artan-0.1.0.tar.gz/artan-0.1.0/README.md
@@ -0,0 +1,136 @@
+# Artan
+[![Build Status](https://travis-ci.com/ozancicek/artan.svg?branch=master)](https://travis-ci.com/ozancicek/artan)
+[![codecov](https://codecov.io/gh/ozancicek/artan/branch/master/graph/badge.svg)](https://codecov.io/gh/ozancicek/artan)
+
+Model-parallel bayesian filtering with Apache Spark.
+
+- [Overview](#overview)
+- [Download](#download)
+- [Examples](#examples)
+- [Usage](#usage)
+
+## Overview
+
+This library provides supports for running various bayesian filters in parallel with Apache Spark. Uses arbitrary
+stateful transformation capabilities of Spark DataFrames to define model-parallel bayesian filters. Therefore, it
+is suitable for latent state estimation of many similar small scale systems rather than a big single system.
+
+Both structured streaming & batch processing modes are supported. Implemented filters extend SparkML Transformers, so
+you can transform a DataFrame of measurements to a DataFrame of estimated states with Kalman filters
+(extended, unscented, etc,..) and various other filters as a part of your SparkML Pipeline.
+
+Artan requires Scala 2.11, Spark 2.4+ and Python 3,6+
+
+
+## Download
+
+This project has been published to the Maven Central Repository. When submitting jobs on your cluster, you can use
+`spark-submit` with `--packages` parameter to download all required dependencies including python packages.
+
+    spark-submit --packages='com.github.ozancicek:artan_2.11:0.1.0'
+
+For SBT:
+
+    libraryDependencies += "com.github.ozancicek" %% "artan" % "0.1.0"
+
+For python:
+
+    pip install artan
+
+Note that pip will only install the python dependencies. To submit pyspark jobs, `--packages='com.github.ozancicek:artan_2.11:0.1.0'` argument should be specified in order to download necessary jars.
+
+
+## Examples
+
+See [examples](examples/src/main) for all sample scripts.
+
+### Streaming examples
+- Local linear trend filtering with Linear Kalman Filter ([python](examples/src/main/python/streaming/lkf_rate_source_llt.py), [scala](examples/src/main/scala/com/ozancicek/artan/examples/streaming/LKFRateSourceLLT.scala))
+- Recursive least squares ([python](examples/src/main/python/streaming/rls_rate_source_ols.py), [scala](examples/src/main/scala/com/ozancicek/artan/examples/streaming/RLSRateSourceOLS.scala))
+- GLM estimation with Extended Kalman Filter, gaussian noise & log link ([scala](examples/src/main/scala/com/ozancicek/artan/examples/streaming/EKFRateSourceGLMLog.scala))
+
+
+## Usage
+
+In scala, filters are located at `com.github.ozancicek.artan.ml.filter` package.
+
+```scala
+import com.github.ozancicek.artan.ml.filter.LinearKalmanFilter
+import org.apache.spark.ml.linalg._
+
+val measurements: DataFrame = ... // DataFrame of measurements
+
+// Size of the state vector
+val stateSize = 2
+// Size of the measurements vector
+val measurementsSize = 1 //
+
+val filter = new LinearKalmanFilter(stateSize, measurementSize)
+  .setStateKeyCol("stateKey")
+  .setMeasurementCol("measurement")
+  .setInitialCovariance(new DenseMatrix(2, 2, Array(10.0, 0.0, 0.0, 10.0)))
+  .setProcessModel(new DenseMatrix(2, 2, Array(1.0, 0.0, 1.0, 1.0)))
+  .setProcessNoise(new DenseMatrix(2, 2, Array(0.01, 0.0, 0.0, 0.01)))
+  .setMeasurementNoise(new DenseMatrix(1, 1, Array(1.0)))
+  .setMeasurementModel(new DenseMatrix(1, 2, Array(1.0, 0.0)))
+
+// Transform measurements DF to state estimates
+val state = filter.transform(measurements)
+
+state.show()
+// +--------+----------+--------------------+--------------------+
+// |stateKey|stateIndex|               state|     stateCovariance|
+// +--------+----------+--------------------+--------------------+
+// |       0|         1|[-0.7208397067075...|0.999500249900037...|
+// |       0|         2|[-0.4690224912583...|0.998012912493968...|
+// |       0|         3|[1.67397010773909...|0.832649076225536...|
+// |       0|         4|[3.44378338744333...|0.699687597788894...|
+// |       0|         5|[4.46146025799578...|0.599860305854393...|
+// |       1|         1|[-0.7114408860120...|0.999500249900037...|
+// |       1|         2|[0.59908327905805...|0.998012912493968...|
+// |       1|         3|[2.33770526334526...|0.832649076225536...|
+// |       1|         4|[3.70332866441643...|0.699687597788894...|
+// |       1|         5|[3.88577148836894...|0.599860305854393...|
+// +--------+----------+--------------------+--------------------+
+```
+
+The supported filters in python are located at `artan.filter` package. Some of the supported filters in scala are
+not yet supported in python.
+
+```python
+from artan.filter import LinearKalmanFilter
+from pyspark.ml.linalg import Matrices
+
+# DataFrame of measurements
+measurements = ...
+# Size of the state vector
+state_size = 2
+
+# Size of the measurements vector
+measurement_size = 1
+
+filter = LinearKalmanFilter(2, 1)\
+    .setStateKeyCol("stateKey")\
+    .setMeasurementCol("measurement")\
+    .setInitialCovariance(Matrices.dense(2, 2, [10.0, 0.0, 0.0, 10.0]))\
+    .setProcessModel(Matrices.dense(2, 2, [1.0, 0.0, 1.0, 1.0]))\
+    .setProcessNoise(Matrices.dense(2, 2, [0.01, 0.0, 0.0, 0.01]))\
+    .setMeasurementNoise(Matrices.dense(1, 1, [1.0]))\
+    .setMeasurementModel(Matrices.dense(1, 2, [1.0, 0.0]))
+
+# Transform measurements DF to state estimates
+state = filter.transform(measurements)
+
+state.show()
+# +--------+----------+--------------------+--------------------+
+# |stateKey|stateIndex|               state|     stateCovariance|
+# +--------+----------+--------------------+--------------------+
+# |       0|         1|[-0.9837995930710...|0.999950002500125...|
+# |       0|         2|[-0.8434180999763...|0.999800129920049...|
+# |       0|         3|[3.13016426536432...|0.833272243145062...|
+# |       1|         1|[0.77903611655215...|0.999950002500125...|
+# |       1|         2|[-1.7712651068330...|0.999800129920049...|
+# |       1|         3|[1.89342894288159...|0.833272243145062...|
+# +--------+----------+--------------------+--------------------+
+
+```
---
+++ packages/artan/artan-0.1.0.tar.gz/artan-0.1.0/python/artan/filter/recursive_least_squares_filter.py
@@ -0,0 +1,201 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+
+#  http://www.apache.org/licenses/LICENSE-2.0
+
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+import numpy as np
+
+from pyspark.ml.param import Params, Param, TypeConverters
+from pyspark.ml.param.shared import HasLabelCol, HasFeaturesCol
+from pyspark.ml.linalg import DenseMatrix
+from artan.state import StatefulTransformer
+from artan.filter.filter_params import HasInitialState, HasInitialStateCol
+
+
+class HasForgettingFactor(Params):
+    """
+    Mixin for param forgetting factor, between 0 and 1.
+    """
+
+    forgettingFactor = Param(
+        Params._dummy(),
+        "forgettingFactor", "Forgetting factor between 0 and 1", typeConverter=TypeConverters.toFloat)
+
+    def __init__(self):
+        super(HasForgettingFactor, self).__init__()
+
+    def getForgettingFactor(self):
+        """
+        Gets the value of forgettingFactor or its default value.
+        """
+        return self.getOrDefault(self.forgettingFactor)
+
+
+class HasRegularizationMatrix(Params):
+    """
+    Mixin for param regularization matrix.
+    """
+
+    regularizationMatrix = Param(
+        Params._dummy(),
+        "regularizationMatrix",
+        "Positive definite regularization matrix for RLS filter, typically a factor multiplied by identity matrix." +
+        "Small factors (>1) give more weight to initial state, whereas large factors (>>1) decrease regularization" +
+        "and cause RLS filter to behave like ordinary least squares",
+        typeConverter=TypeConverters.toMatrix)
+
+    def __init__(self):
+        super(HasRegularizationMatrix, self).__init__()
+
+    def getRegularizationMatrix(self):
+        """
+        Gets the value of regularization matrix or its default value.
+        """
+        return self.getOrDefault(self.regularizationMatrix)
+
+
+class HasRegularizationMatrixCol(Params):
+    """
+    Mixin for param regularization matrix column.
+    """
+
+    regularizationMatrixCol = Param(
+        Params._dummy(),
+        "regularizationMatrixCol",
+        "Regularization matrix column for specifying different reg matrices across filters",
+        typeConverter=TypeConverters.toString)
+
+    def __init__(self):
+        super(HasRegularizationMatrixCol, self).__init__()
+
+    def getRegularizationMatrixCol(self):
+        """
+        Gets the value of regularization matrix column or its default value.
+        """
+        return self.getOrDefault(self.regularizationMatrixCol)
+
+
+class RecursiveLeastSquaresFilter(StatefulTransformer, HasInitialState, HasInitialStateCol,
+                                  HasForgettingFactor, HasRegularizationMatrix, HasRegularizationMatrixCol,
+                                  HasLabelCol, HasFeaturesCol):
+    """
+    Recursive formulation of least squares with exponential weighting & regularization, implemented with
+    a stateful spark Transformer for running parallel filters /w spark dataframes. Transforms an input dataframe
+    of observations to a dataframe of model parameters using stateful spark transformations, which can be used
+    in both streaming and batch applications.
+
+    Let w denote the model parameters and w_est denote our prior belief. RLS minimizes following regularization
+    & weighted SSE terms;
+
+    (w - w_est).T * (lambda^(-N-1) * P)^(-1) * (w - w_est) + Sum(lambda(N - j)*(d_k - u_k.T * w),  k = 0,1, .. N)
+
+    Where:
+    - lambda: forgetting factor, or exponential weighting factor. Between 0 and 1.
+    - P: regularization matrix. Smaller values increseas the weight of regularization term, whereas larger values
+      increases the weight of weighted SSE term.
+    - d_k, u_k: label and features vector at time step k.
+    """
+    def __init__(self, featuresSize):
+        super(RecursiveLeastSquaresFilter, self).__init__()
+        self._java_obj = self._new_java_obj("com.github.ozancicek.artan.ml.filter.RecursiveLeastSquaresFilter",
+                                            featuresSize, self.uid)
+        self._featuresSize = featuresSize
+
+    def setLabelCol(self, value):
+        """
+        Set label column. Default is "label"
+
+        :param value: String
+        :return: RecursiveLeastSquaresFilter
+        """
+        return self._set(labelCol=value)
+
+    def setFeaturesCol(self, value):
+        """
+        Set features column. Default is "features"
+
+        :param value: String
+        :return: RecursiveLeastSquaresFilter
+        """
+        return self._set(featuresCol=value)
+
+    def setInitialEstimate(self, value):
+        """
+        Set initial estimate for model parameters. Default is zero vector.
+
+        Note that if this parameter is set through here, it will result in same initial estimate for all filters.
+        For different initial estimates across filters, set the dataframe column for corresponding to initial estimate
+        with setInitialEstimateCol.
+
+        :param value: pyspark.ml.linalg.Vector with size (featuresSize)
+        :return: RecursiveLeastSquaresFilter
+        """
+        return self._set(initialState=value)
+
+    def setInitialEstimateCol(self, value):
+        """
+        Sets the column corresponding to initial estimates
+        :param value: String
+        :return: RecursiveLeastSquaresFilter
+        """
+        return self._set(initialStateCol=value)
+
+    def setForgettingFactor(self, value):
+        """
+        Set forgetting factor, exponentially weights the measurements based on its sequence.
+
+        Default value of 1.0 weights all measurements equally. With smaller values, recent measurements will have
+        more weight. Generally set around 0.95 ~ 0.99
+
+        :param value: Float >= 1.0
+        :return: RecursiveLeastSquaresFilter
+        """
+        return self._set(forgettingFactor=value)
+
+    def setRegularizationMatrix(self, value):
+        """
+        Set regularization matrix governing the influence of the initial estimate (prior). Larger values will
+        remove regularization effect, making the filter behave like OLS.
+
+        Default is 10E5 * I
+
+        Note that if this parameter is set through here, it will result in same regularization for all filters.
+        For different regularizations across filters, set the dataframe column for corresponding to regularization
+        with setRegularizationMatrixCol.
+
+        :param value: pyspark.ml.linalg.Matrix with size (featuresSize, featuresSize)
+        :return: RecursiveLeastSquaresFilter
+        """
+        return self._set(regularizationMatrix=value)
+
+    def setRegularizationMatrixCol(self, value):
+        """
+        Sets the column corresponding to regularization matrix
+        :param value: String
+        :return: RecursiveLeastSquaresFilter
+         """
+        return self._set(regularizationMatrixCol=value)
+
+    def setRegularizationMatrixFactor(self, value):
+        """
+        Sets the regularization matrix with a float factor, which results in setting the regularization matrix as
+        factor * identity
+
+        :param value: Float
+        :return: RecursiveLeastSquaresFilter
+        """
+        regMat = np.eye(self._featuresSize) * value
+        rows, cols = regMat.shape
+        return self._set(regularizationMatrix=DenseMatrix(rows, cols, regMat.reshape(rows * cols, order="F")))
