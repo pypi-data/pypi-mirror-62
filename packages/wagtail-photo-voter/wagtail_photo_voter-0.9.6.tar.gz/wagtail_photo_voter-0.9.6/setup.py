056fd9a88d58814d505bb93e63edf818a5dd10ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:40 +0000

    Add package wagtail-photo-voter/0.9.6/wagtail_photo_voter-0.9.6.tar.gz

---
+++ packages/wagtail-photo-voter/wagtail_photo_voter-0.9.6.tar.gz/wagtail_photo_voter-0.9.6/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wagtail_photo_voter",
+    version="0.9.6",
+    author="Tomas Strand",
+    author_email="wagtail@tomas.fik1.net",
+    description="A Wagtail module for creating Photo competitions with voting",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/straend/wagtail_photo_voter",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Framework :: Wagtail",
+    ],
+    install_requires=[
+        'Django>=2.2',
+        'wagtail>=2.6',
+        'django-bootstrap4',
+    ],
+    python_requires='>=3.6',
+)
