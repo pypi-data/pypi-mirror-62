0cf037051d6308774105a86b88587edc79ef3804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:57 +0000

    Add package enroll-gcalendar/0.4/enroll_gcalendar-0.4.tar.gz

---
+++ packages/enroll-gcalendar/enroll_gcalendar-0.4.tar.gz/enroll_gcalendar-0.4/enroll_gcalendar/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    enroll_calendar.__init__
+    ~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    :copyright: (c) 2020 by Hubert Pelczarski.
+    :license: MIT, see LICENSE for more details.
+"""
+
+
+
+from .convert_plan import convert_plan
