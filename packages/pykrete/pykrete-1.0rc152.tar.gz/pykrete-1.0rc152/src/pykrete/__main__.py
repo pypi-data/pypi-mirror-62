ffd2d88558b9b0e69c39263ce7124f04a2470c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:59 +0000

    Add package pykrete/1.0rc152/pykrete-1.0rc152.tar.gz

---
+++ packages/pykrete/pykrete-1.0rc152.tar.gz/pykrete-1.0rc152/src/pykrete/__main__.py
@@ -0,0 +1,25 @@
+"""
+Pykrete main
+Author: Shai Bennathan - shai.bennathan@gmail.com
+(C) 2020
+"""
+
+import sys
+import click
+
+
+@click.group()
+def main():
+    """Pykrete's main function"""
+    print('in main')
+    print('called with: ' + str(sys.argv))
+
+
+@main.command()
+def generate():
+    """Generate something"""
+    print("Generate something")
+
+
+if __name__ == '__main__':
+    main()
