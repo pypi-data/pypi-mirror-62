c8b6fbbe20f624cf6bd7014268ae50d693407a26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:50 +0000

    Add package viabel/0.3/viabel-0.3.tar.gz

---
+++ packages/viabel/viabel-0.3.tar.gz/viabel-0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name = 'viabel',
+    version='0.3',
+    description='Efficient, lightweight, variational inference and approximation bounds',
+    author='Jonathan H. Huggins',
+    author_email='huggins@bu.edu',
+    url='https://github.com/jhuggins/viabel/',
+    packages=['viabel'],
+    install_requires=['numpy>=1.13'],
+    extras_require={'vb' : ['scipy', 'autograd', 'tqdm', 'paragami'],
+                    'examples' : ['scipy', 'autograd', 'tqdm', 'paragami',
+                                  'matplotlib', 'seaborn', 'pandas', 'pystan']},
+    python_requires='>=2.7',
+    keywords = ['Bayesian inference',
+                'variational inference',
+                'Wasserstein distance',
+                'alpha-divergence'],
+    classifiers=['Development Status :: 4 - Beta',
+                 'License :: OSI Approved :: MIT License',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.5'],
+    platforms='ALL',
+)
