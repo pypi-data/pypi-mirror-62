ce800e734c79d1cd3a2aeb9748be77e906305afe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:32 +0000

    Add package colab-ssh/0.2.19/colab_ssh-0.2.19.tar.gz

---
+++ packages/colab-ssh/colab_ssh-0.2.19.tar.gz/colab_ssh-0.2.19/colab_ssh/init_git.py
@@ -0,0 +1,14 @@
+from colab_ssh._command import run_command as _run_command
+import os
+
+def init_git(repositoryUrl, branch="master", github_personal_token=""):
+    _run_command("git clone {}".format(repositoryUrl))
+    _run_command("git checkout {}".format(branch))
+
+    # TODO: Create the folder if it doesn't exist
+    os.system("mkdir -p ~/.ssh && curl -L -f {} {}/{}/.colab_ssh/authorized_keys >> ~/.ssh/authorized_keys".format(
+        ("-H 'Authorization: token {}'".format(github_personal_token)) if github_personal_token else "",
+        repositoryUrl.rstrip(".git").replace("github.com", "raw.githubusercontent.com"), 
+        branch))
+
+    print("Successfully cloned the repository")
