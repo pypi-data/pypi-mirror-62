ef0ed13ceabc17f2b60d3b6d8a8a9c4ada05d4f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:32 +0000

    Add package Proba/1.0/Proba-1.0.tar.gz

---
+++ packages/Proba/Proba-1.0.tar.gz/Proba-1.0/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name="Proba",
+      version='1.0',
+      description='Our first Python distribution project',
+      author="JG",
+      author_email="jgrynczewski@gmail.com"
+      )
