99ee9c745e0ab267859d74a62822425de2f47428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:47 +0000

    Add package aflow/0.0.11/aflow-0.0.11-py3-none-any.whl

---
+++ packages/aflow/aflow-0.0.11-py3-none-any.whl/aflow/control.py
@@ -0,0 +1,332 @@
+"""Functions for controlling high-level search functionality for the
+AFLOW database.
+"""
+server = "http://aflowlib.duke.edu/search/API/?"
+"""str: API server address over HTTP.
+"""
+
+from aflow import msg
+def search(catalog=None, batch_size=100):
+    """Returns a :class:`aflow.control.Query` to help construct the search
+    query.
+
+    Args:
+        catalog (str): one of the catalogs supported on AFLOW: ['icsd', 'lib1',
+          'lib2', 'lib3']. Also supports a `list` of catalog names.
+        batch_size (int): number of data entries to return per HTTP request.
+    """
+    return Query(catalog, batch_size)
+
+class Query(object):
+    """Represents a search againts the AFLUX API.
+
+    Args:
+        catalog (str): one of the catalogs supported on AFLOW: ['icsd', 'lib1',
+          'lib2', 'lib3']. Also supports a `list` of catalog names.
+        batch_size (int): number of data entries to return per HTTP request.
+        step (int): step size over entries.
+
+    Attributes:
+        filters (list): of `str` filter arguments to pass to the matchbook
+          section of the API request.
+        select (list): of :class:`aflow.keywords.Keyword` to *include* in the request.
+        excludes (list): of :class:`aflow.keywords.Keyword` to *exclude* in the request.
+        orderby (str): name of the keyword to order by. AFLUX only supports a
+          single order-by parameter for now.
+        catalog (str): one of the catalogs supported on AFLOW: ['icsd', 'lib1',
+          'lib2', 'lib3']. Also supports a `list` of catalog names.
+        N (int): number of results in the current search query.
+        reverse (bool): when True, reverse the order of the results in the
+          query.
+        k (int): number of datasets per page for the current iterator. Can be
+          controlled by `batch_size`.
+        responses (dict): keys are (n,k) tuples from the pagination; values are
+          the corresponding JSON dictionaries.
+        step (int): step size over entries.
+    """
+    def __init__(self, catalog=None, batch_size=100, step=1):
+        self.filters = []
+        self.selects = []
+        self.excludes = []
+        self.order = None
+        self.catalog = catalog if isinstance(catalog, (list, tuple, type(None))) else [catalog]
+        self._N = None
+        """int: number of results in the current search query."""
+        self.reverse = False
+        self._n = 1
+        self.k = batch_size
+        self.step = step
+        self.responses = {}
+        self._iter = 0
+        """int: current integer id of the iterator in the *whole* dataset; this
+        means it can have a value greater than :attr:`k`.
+        """
+        self._max_entry = None
+        """int: index of the maximum entry that should be returned from this
+        query.
+        """
+        self._matchbook = None
+        """str: matchbook portion of the request URL.
+        """
+        self._final = False
+        """bool: when True, this query is finalized and no additional filters,
+        etc. can be added to it.
+        """
+
+    def reset_iter(self):
+        """Resets the iterator back to zero so that the collection can be
+        iterated over again *without* needing to request the data from the
+        server again.
+        """
+        self._iter = 0
+        
+    @property
+    def n(self):
+        """Current page number for the iterator.
+        """
+        if self.reverse:
+            return -1*self._n
+        else:
+            return self._n
+
+    @property
+    def N(self):
+        if self._N is None:
+            self._request(self.n, self.k)
+        return self._N
+
+    @property
+    def max_N(self):
+        """Returns the maximum integer index that will be reached by this query.
+        """
+        if self._max_entry is None:
+            return self.N
+        else:
+            return self._max_entry
+    
+    def __len__(self):
+        return self.max_N
+
+    def __getitem__(self, seq):
+        #We need to trigger the first request to make sure that the total
+        #number of entries is fixed on the parent object, and so that the
+        #pointers to the response caching are all the same.
+        assert len(self) > 0
+        
+        from copy import copy
+        result = copy(self)
+
+        if type(seq) is slice:
+            #Perform a shallow copy so that we get a new reference for the
+            #indices.
+            result._iter = seq.start if seq.start is not None else 0
+            result._max_entry = seq.stop
+            result.step = seq.step
+            return result
+        elif isinstance(seq, int):
+            result._iter = seq
+            return next(result)
+        
+    def _request(self, n, k):
+        """Constructs the query string for this :class:`Query` object for the
+        specified paging limits and then returns the response from the REST API
+        as a python object.
+
+        Args:
+            n (int): page number of the results to return.
+            k (int): number of datasets per page.
+        """
+        if len(self.responses) == 0:
+            #We are making the very first request, finalize the query.
+            self.finalize()
+        
+        import json
+        from six.moves import urllib
+        urlopen = urllib.request.urlopen
+        url = "{0}{1},{2}".format(server, self.matchbook(),
+                                  self._directives(n, k))
+        rawresp = urlopen(url).read().decode("utf-8")
+        try:
+            response = json.loads(rawresp)
+        except:# pragma: no cover
+            #We can't easily simulate network failure...
+            msg.err("{}\n\n{}".format(url, rawresp))
+            return
+
+        if not response:
+            self._N = 0
+            msg.err("Empty response from API. "
+                    "Check your query filters.\nURI: {}".format(url))
+            return
+        #If this is the first request, then save the number of results in the
+        #query.
+        if len(self.responses) == 0:
+            self._N = int(next(iter(response.keys())).split()[-1])
+        self.responses[n] = response
+
+    def finalize(self):
+        """Finalizes the current state of the query. This means that the request URL
+        will be saved, but the individual keyword objects will be
+        *reset*. Re-executing the search query will reconstruct the same object
+        and request, but any cached responses will be lost.
+        """
+        #Generate the matchbook query, this has all the filters, selects and
+        #ordering information.
+        self.matchbook()
+        #Next, reset all the keywords for the global AFLOW.
+        from aflow.keywords import reset
+        reset()
+        #Switch out all of the keyword instances for their string
+        #representations.
+        self.selects = [str(s) for s in self.selects]
+        self.filters = [str(f) for f in self.filters]
+        self.excludes = [str(x) for x in self.excludes]
+        self.order = str(self.order) if self.order is not None else None
+        #Set the finalizer flag so that this object doesn't allow mutations.
+        self._final = True
+        
+    def matchbook(self):
+        """Constructs the matchbook portion of the query.
+        """
+        if not self._final:
+            items = []
+            #AFLUX orders by the first element in the query. If we have an orderby
+            #specified, then place it first.
+            if self.order is not None:
+                excludes = [x.name for x in self.excludes]
+                selects = [v.name for v in self.selects]
+                if self.order.name in excludes:
+                    items.append("${}".format(str(self.order.name)))
+                    idx = excludes.index(self.order.name)
+                    self.excludes.pop(idx)
+                else:
+                    items.append(str(self.order.name))
+
+                if self.order.name in selects:
+                    idx = selects.index(self.order.name)
+                    self.selects.pop(idx)
+
+            items.extend(list(map(str, self.selects)))
+            items.extend(list(map(str, self.filters)))
+            items.extend(["${}".format(str(k)) for k in self.excludes]) 
+            self._matchbook = ','.join(items)
+
+        return self._matchbook
+
+    def _directives(self, n, k):
+        """Returns the directives portion of the AFLUX query.
+
+        Args:
+            n (int): page number of the results to return.
+            k (int): number of datasets per page.
+        """
+        items = []
+        if self.catalog is not None:
+            items.append("catalog({})".format(':'.join(self.catalog)))
+
+        #Next, add the paging context. This query maintains its own paging
+        #context
+        items.append("paging({0:d},{1:d})".format(n, k))
+        return ','.join(items)
+
+    def __iter__(self):
+        return self
+
+    def next(self):# pragma: no cover
+        """Yields a generator over AFLUX API request results.
+        """
+        return self.__next__()
+    
+    def __next__(self):
+        """Yields a generator over AFLUX API request results.
+        """
+        #First, find out which entry we are on.
+        n = (self._iter // self.k) + 1
+        i = self._iter % self.k
+
+        #Reverse the sign now that we have figured out the ordinal page number.
+        if self.reverse:
+            n *= -1
+
+        if self._iter < self.max_N and n not in self.responses:
+            self._n = abs(n)
+            self._request(self.n, self.k)
+
+        assert len(self.responses) > 0
+
+        from aflow.entries import Entry
+        if self._iter < self.max_N:
+            index = self.k*(abs(n)-1) + i + 1
+            key = "{} of {}".format(index, self.N)
+            raw = self.responses[n][key]
+            result = Entry(**raw)
+
+            #Increment the iterator right before we return the entry.
+            self._iter += 1
+            return result
+        else:
+            raise StopIteration()
+
+    def _final_check(self):
+        """Checks whether this object is finalized; if it is, print a friendly
+        message and return False, otherwise True.
+        """
+        if self._final:
+            msg.info("This query has been finalized. It cannot be mutated. "
+                     "Create a new query to change the matchbook.")
+        return not self._final
+        
+    def filter(self, keyword):
+        """Adds a search term to the current filter list. Calling :meth:`filter`
+        multiple times will join the final filters together using logical *and*.
+
+        Args:
+            keyword (aflow.keywords.Keyword): that encapsulates the AFLUX
+              request language logic.
+        """
+        if self._final_check():
+            self._N = None
+            self.filters.append(keyword)
+        return self
+
+    def select(self, *keywords):
+        """Adds a keyword to the list of properties to return for each material
+        in the request.
+
+        Args:
+            keywords (list): of :class:`aflow.keywords.Keyword` that
+              encapsulates the AFLUX request language logic.
+        """
+        if self._final_check():
+            self._N = None
+            for keyword in keywords:
+                if keyword is not self.order:
+                    self.selects.append(keyword)
+        return self
+
+    def orderby(self, keyword, reverse=False):
+        """Sets a keyword to be the one by which 
+
+        Args:
+            keyword (aflow.keywords.Keyword): that encapsulates the AFLUX
+              request language logic.
+            reverse (bool): when True, reverse the ordering.
+        """
+        if self._final_check():
+            self._N = None
+            self.order = keyword
+            self.reverse = reverse
+        return self
+    
+    def exclude(self, *keywords):
+        """Sets a keyword to be *excluded* from the response.
+
+        Args:
+            keywords (list): of :class:`aflow.keywords.Keyword` that
+              encapsulates the AFLUX request language logic.
+        """
+        if self._final_check():
+            self._N = None
+            for keyword in keywords:
+                self.excludes.append(keyword)
+        return self
---
+++ packages/aflow/aflow-0.0.11-py3-none-any.whl/aflow/templates/entry.py
@@ -0,0 +1,256 @@
+"""Provides class and methods for abstracting the data from AFLOW into
+python.
+"""
+from aflow.caster import cast
+import aflow.keywords as kw
+
+def _val_from_str(attr, value):
+    """Retrieves the specified attribute's value, cast to an
+    appropriate python type where possible.
+    """
+    clsname = "_{}".format(attr)
+    if hasattr(kw, clsname):
+        cls = getattr(kw, clsname)
+        atype = getattr(cls, "atype")
+        return cast(atype, attr, value)
+    else:
+        return value
+
+class AflowFile(object):
+    """Represents a single file for an entry in AFLOW and allows easy
+    access to download it.
+
+    Args:
+        aurl (str): URL for the entry in AFLOW.
+        filename (str): name of the file.
+    """
+    def __init__(self, aurl, filename):
+        self.aurl = aurl
+        self.filename = filename
+
+    def __repr__(self):
+        return "AflowFile({0}/{1})".format(self.aurl, self.filename)
+
+    def __call__(self, target=None):
+        """Download the file.
+
+        Args:
+            target (str): path to the location to save the file. If None, the
+              contents of the file are returned as a string.
+        """       
+        from six.moves import urllib
+        urlopen = urllib.request.urlopen
+        url = "http://{}/{}".format(self.aurl.replace(':', '/'), self.filename)
+        rawresp = urlopen(url).read().decode("utf-8")
+
+        if target is not None:
+            from os import path
+            tpath = path.abspath(path.expanduser(target))
+            with open(tpath, 'w') as f:
+                f.write(rawresp)
+            return tpath
+        else:
+            return rawresp
+    
+class AflowFiles(list):
+    """Represents a collection of files for an entry in AFLOW and allows easy
+    access to download them.
+
+    Args:
+        entry (Entry): database entry object that has a list of the files and
+          remote URL for accessing them.
+    """
+    def __init__(self, entry):
+        files = entry._lazy_load("files")
+        if files is not None:
+            super(AflowFiles, self).extend(files)
+        self.aurl = entry._lazy_load("aurl")
+
+    def __getitem__(self, key):
+        from six import string_types
+        from fnmatch import fnmatch
+        if isinstance(key, string_types):
+            matches = [f for f in self if fnmatch(f, key)]
+            if len(matches) == 1:
+                return AflowFile(self.aurl, matches[0])
+            else:
+                raise KeyError("Pattern matches more than one file.")
+        else:
+            match = super(AflowFiles, self).__getitem__(key).strip()
+            return AflowFile(self.aurl, match)
+            
+    
+class Entry(object):
+    """Encapsulates the result of a single material entry in the AFLOW
+    database.
+
+    .. note:: Additional keyword values will be loaded lazily as
+      requested (using additional HTTP requests). For optimization, it
+      is recommended to request *all* known keywords up front.
+
+    Args:
+        kwargs (dict): of key-value pairs obtained from the initial
+          AFLUX request.
+
+    Attributes:
+        attributes (dict): of key-value pairs requested for the given
+          material. This will only be identical to the passed in the
+          keyword arguments if no additional property requests have been
+          made.
+        raw (dict): original response dictionary (without any cast
+          values).
+    """
+    def __init__(self, **kwargs):
+        self.attributes = {a: _val_from_str(a, v) for a, v in kwargs.items()}
+        self.raw = kwargs
+        self._atoms = None
+        """ase.atoms.Atoms: atoms object for the configuration in the
+        database.
+        """
+        self._files = None
+        
+
+    def __str__(self):
+        aurl = self.attributes["aurl"].replace(".edu:", ".edu/")
+        return "http://" + aurl
+    def __eq__(self, other):
+        return self.auid == other.auid
+    def __hash__(self):
+        return hash(self.auid)
+
+    def _lazy_load(self, keyword):
+        """Loads the value of the specified keyword via HTTP request against the
+        AFLUX API, if it isn't already present on the object.
+
+        Args:
+            keyword (str): name of the keyword to retrieve for this entry.
+        """
+        if keyword in self.attributes:
+            return self.attributes[keyword]
+        else:
+            import requests
+            import json
+            aurl = self.attributes["aurl"].replace(".edu:", ".edu/")
+            url = "http://{0}?{1}".format(aurl, keyword)
+            r = requests.get(url)
+
+            if len(r.text) == 0:
+                return
+
+            #We need to coerce the string returned from aflow into the
+            #appropriate python format.
+            result = _val_from_str(keyword, r.text)
+            self.attributes[keyword] = result
+            return result
+
+    def atoms(self, pattern="CONTCAR.relax*", quippy=False, keywords=None,
+              calculator=None):
+        """Creates a :class:`ase.atoms.Atoms` or a :class:`quippy.atoms.Atoms`
+        object for this database entry.
+
+        Args:
+            pattern (str): pattern for choosing the file to generate the atomic
+              lattice and positions from. The pattern is passed to
+              :func:`~fnmatch.fnmatch` and the *last* entry in the list is
+              returned (so that `CONTCAR.relax2` would be returned
+              preferentially over `CONTCAR.relax1` or `CONTCAR.relax`).
+            quippy (bool): when True, return a :class:`quippy.atoms.Atoms`
+              object.
+            keywords (dict): keys are keyword obects accessible from `aflow.K`;
+              values are desired `str` names in the parameters dictionary of the
+              atoms object.
+            calculator (ase.calculators.Calculator): calculator to set for the
+              newly created atoms object.
+
+        Examples:
+            Generate a :class:`quippy.atoms.Atoms` object and include the total
+            energy and forces. Assume that `result` is a valid
+            :func:`aflow.search` object.
+
+            >>> entry = result[0] #Get the first result in the set.
+            >>> keywords = {K.energy_cell: "dft_energy", K.forces: "dft_force"}
+            >>> entry.atoms(quippy=True, keywords=keywords)
+        """
+        if self._atoms is not None:
+            return self._atoms
+        
+        from fnmatch import fnmatch
+        target = [f for f in self.files if fnmatch(f, pattern)][-1]
+        aurl = self.attributes["aurl"].replace(".edu:", ".edu/")
+        url = "http://{0}/{1}".format(aurl, target)
+
+        import requests
+        lines = requests.get(url).text.split('\n')
+        preline = ' '.join(self.species).strip() + ' !'
+        lines[0] = preline + lines[0]
+        contcar = '\n'.join(lines)
+
+        if quippy:# pragma: no cover
+            import quippy
+            reader = quippy.io.read
+        else:
+            from ase.io import read
+            reader = read
+
+        from six import StringIO
+        cfile = StringIO(contcar)
+        try:
+            self._atoms = reader(cfile, format="vasp")
+        finally:
+            cfile.close()
+
+        if calculator is not None:
+            self._atoms.set_calculator(calculator)
+        if keywords is None:
+            return self._atoms
+
+        self._atoms.results = {}
+        for kw, pname in keywords.items():
+            value = getattr(self, kw.name)
+            if quippy: # pragma: no cover
+                self._atoms.params.set_value(pname, value)
+            else:
+                #ASE only cares about certain values, but we'll save
+                #them all anyway.
+                self._atoms.results[pname] = value
+
+        return self._atoms
+
+    @property
+    def files(self):
+        if self._files is None:
+            self._files = AflowFiles(self)
+        return self._files
+    
+    {% for keyword, metadata in keywords.items() %}
+    {%- if keyword != "files" %}
+    @property
+    def {{keyword}}(self):
+        """{{metadata.title}} (`{{metadata.inclusion}}`). Units: `{{metadata.units}}`.
+        
+        {% if metadata.status != "production" -%}
+        .. warning:: This keyword is still listed as development level. Use it
+          knowing that it is subject to change or removal.
+
+        {%- endif %}
+        {% if "verification" in metadata -%}
+        .. note:: The following verifications are available for this
+          keyword. They are exposed as additional methods on this object.
+          {% for verify in metadata.verification %}
+          - :meth:`{{verify}}`
+          {%- endfor %}
+
+        {%- endif %}
+
+        Returns:
+            {{metadata.ptype}}: {{metadata.description|indent(12, False)}}
+        {% if "example" in metadata %}
+        Examples:
+            You can expect the *content* of the result to be something like:
+
+            `{{metadata.example}}`
+        {% endif -%}
+        """
+        return self._lazy_load("{{keyword}}")
+    {%- endif %}
+    {% endfor %}
