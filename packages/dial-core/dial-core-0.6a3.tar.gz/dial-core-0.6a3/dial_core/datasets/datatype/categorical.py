750a1865305b1aa226a60dea4973222b49f0f403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:30 +0000

    Add package dial-core/0.6a3/dial-core-0.6a3.tar.gz

---
+++ packages/dial-core/dial-core-0.6a3.tar.gz/dial-core-0.6a3/dial_core/datasets/datatype/categorical.py
@@ -0,0 +1,30 @@
+# vim: ft=python fileencoding=utf-8 sts=4 sw=4 et:
+
+from typing import List
+
+from tensorflow import keras
+
+from .datatype import DataType
+
+
+class Categorical(DataType):
+    """
+    Represents a Category.
+
+    A Category is a number indexed on an array of strings, each one representing a
+    different category.
+    """
+
+    def __init__(self, categories: List[str]):
+        super().__init__()
+
+        self.categories = categories
+
+    def process(self, data) -> List[int]:
+        return keras.utils.to_categorical(data, len(self.categories))
+
+    def display(self, data: int) -> str:
+        if self.categories is not None:
+            return self.categories[data]
+
+        return str(data)
