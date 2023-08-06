133e0af3eecd5fe66cb1537fa6dbd9e9692a1aae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:27 +0000

    Add package dynamite-nsm/0.6.5.1/dynamite_nsm-0.6.5.1-py2-none-any.whl

---
+++ packages/dynamite-nsm/dynamite_nsm-0.6.5.1-py2-none-any.whl/dynamite_nsm/updater.py
@@ -0,0 +1,49 @@
+import os
+import sys
+import time
+import shutil
+from dynamite_nsm import const
+from dynamite_nsm.utilities import download_file
+from dynamite_nsm.utilities import extract_archive
+from dynamite_nsm.utilities import create_dynamite_root_directory
+
+
+def update_default_configurations():
+    """
+    Retrieves the latest skeleton configurations for setting up ElasticSearch, LogStash, Zeek, and Suricata
+
+    :return: True, if retrieved successfully
+    """
+    shutil.rmtree(const.INSTALL_CACHE, ignore_errors=True)
+    create_dynamite_root_directory()
+    download_file(const.DEFAULT_CONFIGS_URL,
+                  const.DEFAULT_CONFIGS_ARCHIVE_NAME, stdout=True)
+    shutil.rmtree(const.DEFAULT_CONFIGS, ignore_errors=True)
+    time.sleep(1)
+    try:
+        sys.stdout.write('[+] Copying default_configs -> {}\n'.format(const.DEFAULT_CONFIGS))
+        extract_archive(os.path.join(const.INSTALL_CACHE, 'default_configs.tar.gz'), const.CONFIG_PATH)
+        return True
+    except IOError as e:
+        sys.stderr.write('[-] An error occurred while attempting to extract file. [{}]\n'.format(e))
+    return False
+
+
+def update_mirrors():
+    """
+    Retrieves the latest mirrors which contain the download locations for all components
+
+    :return: True, if retrieved successfully
+    """
+    shutil.rmtree(const.INSTALL_CACHE, ignore_errors=True)
+    create_dynamite_root_directory()
+    download_file(const.MIRRORS_CONFIG_URL,
+                  const.MIRRORS_CONFIG_ARCHIVE_NAME, stdout=True)
+    shutil.rmtree(const.MIRRORS, ignore_errors=True)
+    try:
+        sys.stdout.write('[+] Copying mirrors -> {}\n'.format(const.MIRRORS))
+        extract_archive(os.path.join(const.INSTALL_CACHE, 'mirrors.tar.gz'), const.CONFIG_PATH)
+        return True
+    except IOError as e:
+        sys.stderr.write('[-] An error occurred while attempting to extract file. [{}]\n'.format(e))
+    return False
