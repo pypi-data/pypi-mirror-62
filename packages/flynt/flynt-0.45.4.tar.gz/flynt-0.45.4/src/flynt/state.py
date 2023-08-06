934e43b978dcd24d09d22d617a08d338500b668b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:14 +0000

    Add package flynt/0.45.4/flynt-0.45.4.tar.gz

---
+++ packages/flynt/flynt-0.45.4.tar.gz/flynt-0.45.4/src/flynt/state.py
@@ -0,0 +1,13 @@
+verbose = False
+quiet = False
+
+percent_candidates = 0
+percent_transforms = 0
+
+call_candidates = 0
+call_transforms = 0
+
+invalid_conversions = 0
+
+concat_candidates = 0
+concat_changes = 0
