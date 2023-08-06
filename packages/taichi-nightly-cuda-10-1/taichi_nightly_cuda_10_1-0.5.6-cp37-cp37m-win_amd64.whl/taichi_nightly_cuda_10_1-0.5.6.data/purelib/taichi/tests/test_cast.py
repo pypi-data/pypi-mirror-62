779081ffe303ad1fc726deb8a6a394e28097217b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:10 +0000

    Add package taichi-nightly-cuda-10-1/0.5.6/taichi_nightly_cuda_10_1-0.5.6-cp37-cp37m-win_amd64.whl

---
+++ packages/taichi-nightly-cuda-10-1/taichi_nightly_cuda_10_1-0.5.6-cp37-cp37m-win_amd64.whl/taichi_nightly_cuda_10_1-0.5.6.data/purelib/taichi/tests/test_cast.py
@@ -0,0 +1,25 @@
+import taichi as ti
+
+@ti.all_archs
+def test_cast_f32():
+  z = ti.var(ti.i32, shape=())
+  
+  @ti.kernel
+  def func():
+    z[None] = ti.cast(1e9, ti.f32) / ti.cast(1e6, ti.f32) + 1e-3
+  
+  func()
+  assert z[None] == 1000
+
+
+@ti.require(ti.extension.data64)
+@ti.all_archs
+def test_cast_f64():
+  z = ti.var(ti.i32, shape=())
+  
+  @ti.kernel
+  def func():
+    z[None] = ti.cast(1e13, ti.f64) / ti.cast(1e10, ti.f64) + 1e-3
+  
+  func()
+  assert z[None] == 1000
