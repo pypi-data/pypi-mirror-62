49e3f8283300074678c42053f5541871ec695c8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:24 +0000

    Add package urlscanio/0.1.1/urlscanio-0.1.1.tar.gz

---
+++ packages/urlscanio/urlscanio-0.1.1.tar.gz/urlscanio-0.1.1/README.md
@@ -0,0 +1,61 @@
+# urlscanio
+
+## Summary
+
+[URLScan.io][urlscan-homepage] is a useful tool for scanning and obtaining information from potentially malicious websites. The creators of URLScan have very helpfully made an [API][urlscan-api] which can be used to add some automation to your workflow. `urlscanio` is a simple Python CLI utility which makes use of the aforementioned APIs to automate my own personal workflow when it comes to using URLScan.
+
+## Requirements
+
+`urlscanio` requires Python >= 3.8. You will also need a [URLScan.io][urlscan-homepage] account and/or API key.
+
+## Installation
+
+If you have a compatible Python version installed, simply run (using `pip3` if necessary):
+
+```bash
+pip install urlscanio
+```
+
+## How to use
+
+In this section, the different functions of the CLI are outlined. You may also use `urlscanio -h` or `urlscanio --help` for information within your terminal.
+
+### API key and download directory
+
+This tool requires an environment variable named `URLSCAN_API_KEY` containing your API key. Optionally, you may also set an environment variable called `URLSCAN_DATA_DIR` to specify where the screenshots and DOM should be downloaded. If not set, they will be downloaded in the directory you run `urlscanio` from.
+
+It is recommended to use `.bashrc` or `.zshrc` for this. If using PowerShell, add `URLSCAN_API_KEY` and `URLSCAN_DATA_DIR` to your user profile.
+
+### Investigate URL
+
+Provided a URL (containing the protocol and domain at minimum), will request a scan and download the corresponding screenshot and DOM, as well as the report URL.
+
+Examples:
+
+```bash
+urlscanio -i https://www.some-dodgy.website
+urlscanio --investigate http://some-dodgy.website
+```
+
+### Submit scan request
+
+Provided a URL (containing the protocol and domain at minimum), will request a scan and return the UUID generated. This can then be used to determine eg the screenshot location.
+
+Examples:
+
+```bash
+urlscan -s https://www.some-dodgy.website
+urlscan --submit http://some-dodgy.website
+```
+
+### Retrieve scan information
+
+Provided the UUID linked to the scan in question, will query the API to download the screenshot and DOM from the report, as well as return the report URL.
+
+```bash
+urlscan -r c5be1459-0a64-4751-bf25-8dd6d3c5742d
+urlscan --retrieve c5be1459-0a64-4751-bf25-8dd6d3c5742d
+```
+
+[urlscan-homepage]: https://urlscan.io
+[urlscan-api]: https://urlscan.io/about-api
---
+++ packages/urlscanio/urlscanio-0.1.1.tar.gz/urlscanio-0.1.1/src/urlscanio/utils.py
@@ -0,0 +1,69 @@
+import argparse
+import pathlib
+import re
+import urllib.parse
+
+def create_arg_parser():
+    parser = argparse.ArgumentParser(
+        prog="urlscan",
+        description=(
+            "Call URLScan.io's submission and result APIs to get information about "
+            "a website. This tool requires an environment variable named "
+            "URLSCAN_API_KEY to be set to your API key.\nOptionally, you may also set "
+            "an environment variable called URLSCAN_DATA_DIR to specify where the "
+            "screenshots and DOM should be downloaded. If not set, they will be downloaded "
+            "in your current directory."
+        ),
+    )
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument(
+        "-i", "--investigate",
+        help=(
+            "Investigate the specified URL. Returns the report URL and the locations of the "
+            "DOM and screenshots."
+        ),
+        type=str
+    )
+    group.add_argument(
+        "-s", "--submit",
+        help="Submit a scan request for the specified URL. Returns the corresponding UUID.",
+        type=str
+    )
+    group.add_argument(
+        "-r", "--retrieve",
+        help=(
+            "Retrieves the scan report for the provided UUID. Returns the report URL and the "
+            "download locations for the DOM and screenshot."
+        ),
+        type=str
+    )
+
+    return parser
+
+
+def is_url_valid(url):
+    minimum_url_attributes = ["scheme", "netloc"]
+    token = urllib.parse.urlparse(url)
+    return all([getattr(token, attribute) for attribute in minimum_url_attributes]) and \
+           len([s for s in token.netloc.split(".") if s != ""]) > 1
+
+
+def validate_arguments(args: argparse.Namespace):
+    uuid_validator = re.compile(
+        "^[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}$"
+    )
+
+    if (args.investigate and not is_url_valid(args.investigate)) or \
+       (args.submit and not is_url_valid(args.submit)):
+        raise ValueError(
+            "The URL provided does not contain the scheme (e.g. http:// or https://) "
+            "and/or a non-empty location (e.g. google.com)"
+        )
+    elif args.retrieve and not bool(uuid_validator.match(args.retrieve)):
+        raise ValueError("The UUID provided is incorrectly formatted")
+
+
+def create_data_dir(data_dir: pathlib.Path):
+    pathlib.Path(f"{data_dir}/screenshots").mkdir(exist_ok=True)
+    pathlib.Path(f"{data_dir}/doms").mkdir(exist_ok=True)
