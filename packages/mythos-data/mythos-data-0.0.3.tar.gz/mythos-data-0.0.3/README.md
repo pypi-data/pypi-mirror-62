00583907daf1537a5717f6bdf344d64f3accda90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:35 +0000

    Add package mythos-data/0.0.3/mythos-data-0.0.3.tar.gz

---
+++ packages/mythos-data/mythos-data-0.0.3.tar.gz/mythos-data-0.0.3/README.md
@@ -0,0 +1,10 @@
+Mythos Data
+===========
+
+Description
+-----------
+
+Support
+-------
+
+[TheYarek](mailto:jwojtas@yarek.io)
---
+++ packages/mythos-data/mythos-data-0.0.3.tar.gz/mythos-data-0.0.3/src/mythos/data/__version__.py
@@ -0,0 +1,7 @@
+__title__ = "mythos-data"
+__version__ = "0.0.3"
+__description__ = "Mythos Data"
+__author__ = "Jaroslaw Wojtas"
+__license__ = "Apache 2.0"
+__copyright__ = "Copyright 2020 Jaroslaw Wojtas"
+__url__ = "https://github.com/theyarek/mythos-data"
