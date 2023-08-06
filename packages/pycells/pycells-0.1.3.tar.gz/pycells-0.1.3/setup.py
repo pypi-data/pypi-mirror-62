4613d6eff80e1b6419223334af785784915b6755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:23 +0000

    Add package pycells/0.1.3/pycells-0.1.3.tar.gz

---
+++ packages/pycells/pycells-0.1.3.tar.gz/pycells-0.1.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as req:
+    requirements = "\n".split(req.read())
+
+setuptools.setup(
+    name="pycells",
+    version="v0.1.3",
+    author="Tim Fischer",
+    author_email="t.fischer98@hotmail.com",
+    description="A small package for simulating simple cellular automata.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tim-fi/pycells",
+    packages=["pycells"],
+    package_data={"pycells": ["presets/*.yml"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={'console_scripts': ['pycells = pycells.cli:cli']},
+    python_requires='>=3.7',
+    install_requires=requirements
+)
