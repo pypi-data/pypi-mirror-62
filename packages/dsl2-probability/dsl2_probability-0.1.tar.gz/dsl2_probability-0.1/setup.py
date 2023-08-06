fbeb404681f7931d62bf0924291b44f803aa5f0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:12 +0000

    Add package dsl2-probability/0.1/dsl2_probability-0.1.tar.gz

---
+++ packages/dsl2-probability/dsl2_probability-0.1.tar.gz/dsl2_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsl2_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsl2_probability'],
+      author = 'Alexandra Jaber',
+      author_email = 'alexandra.jaber@vodafone.com',
+    zip_safe=False)
