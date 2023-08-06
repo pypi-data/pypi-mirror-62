1bd62d671979d34815c4c1c51d33efdda90f53b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:32 +0000

    Add package pydata-madison/0.1/pydata-madison-0.1.tar.gz

---
+++ packages/pydata-madison/pydata-madison-0.1.tar.gz/pydata-madison-0.1/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="pydata-madison",
+    version="0.1",
+    description="PyData Madison",
+    url="https://www.meetup.com/PyData-Madison/",
+    packages=["pydata_madison"],
+)
