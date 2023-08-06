0006dbe098d24d9487eaeadef4a04ad89905b933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:35 +0000

    Add package okra/2.0.0/okra-2.0.0.tar.gz

---
+++ packages/okra/okra-2.0.0.tar.gz/okra-2.0.0/okra/cli.py
@@ -0,0 +1,20 @@
+""" Command Line Interface utilities for Okra """
+import click
+
+from okra.repo_mgmt import clone_or_fetch_repo
+from okra.populate_db import persist_repo
+
+@click.group()
+def cli():
+    pass
+
+@cli.command(name='upsert')
+@click.argument('owner')
+@click.argument('project')
+@click.argument('dburl')
+@click.argument('repopath')
+@click.argument('repourl')
+@click.option('--buffer_size', default=int(1e4), help="number of commits before db write")
+def upsert_repo(owner, project, dburl, repopath, repourl, buffer_size):
+    clone_or_fetch_repo(repopath, repourl)
+    persist_repo(owner, project, dburl, repopath, buffer_size)
