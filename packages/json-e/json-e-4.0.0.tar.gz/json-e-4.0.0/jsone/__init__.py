64c0a43eac8551ae84418d5eeb4da17c61540e44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:43 +0000

    Add package json-e/4.0.0/json-e-4.0.0.tar.gz

---
+++ packages/json-e/json-e-4.0.0.tar.gz/json-e-4.0.0/jsone/__init__.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import, print_function, unicode_literals
+
+import re
+from .render import renderValue
+from .shared import JSONTemplateError, DeleteMarker, TemplateError, fromNow
+from . import builtins
+
+_context_re = re.compile(r'[a-zA-Z_][a-zA-Z0-9_]*$')
+
+
+def render(template, context):
+    if not all(_context_re.match(c) for c in context):
+        raise TemplateError('top level keys of context must follow '
+                            '/[a-zA-Z_][a-zA-Z0-9_]*/')
+    full_context = {'now': fromNow('0 seconds', None)}
+    full_context.update(builtins.build(full_context))
+    full_context.update(context)
+    rv = renderValue(template, full_context)
+    if rv is DeleteMarker:
+        return None
+    if callable(rv):
+        raise TemplateError(('$eval {} doesn\'t get any arguments '
+                             'in template').format(template['$eval']))
+    return rv
