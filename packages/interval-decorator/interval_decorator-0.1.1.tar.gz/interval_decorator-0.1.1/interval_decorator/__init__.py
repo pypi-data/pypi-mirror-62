d855aaf4822cb9981d56ab2ef57083d9ef98a9c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:17 +0000

    Add package interval-decorator/0.1.1/interval_decorator-0.1.1.tar.gz

---
+++ packages/interval-decorator/interval_decorator-0.1.1.tar.gz/interval_decorator-0.1.1/interval_decorator/__init__.py
@@ -0,0 +1,36 @@
+__version__ = "0.1.1"
+import time
+
+
+def interval(time_interval, alternative_func=None):
+    def decorator(function):
+        function.__last_run = 0
+
+        def guard(*args, **kwargs):
+            now = time.time()
+            if now - function.__last_run >= time_interval:
+                function.__last_run = now
+                return function(*args, **kwargs)
+            else:
+                return alternative_func(*args, **kwargs)
+
+        return guard
+
+    return decorator
+
+
+def interval_or_return_first_value(time_interval):
+    def decorator(function):
+        function.__last_run = 0
+
+        def guard(*args, **kwargs):
+            now = time.time()
+            if now - function.__last_run >= time_interval:
+                function.__last_run = now
+                return function(*args, **kwargs)
+            else:
+                return args[0]
+
+        return guard
+
+    return decorator
---
+++ packages/interval-decorator/interval_decorator-0.1.1.tar.gz/interval_decorator-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['interval_decorator']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'interval-decorator',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'fx_kirin',
+    'author_email': 'fx.kirin@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
