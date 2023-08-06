cc1784e8c6bf589c0704dc83bf699bdcaa06306b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:28 +0000

    Add package more-argparse/1.0.0/more_argparse-1.0.0-py2-none-any.whl

---
+++ packages/more-argparse/more_argparse-1.0.0-py2-none-any.whl/margparse/__init__.py
@@ -0,0 +1,13 @@
+from .types import *
+from .actions import *
+
+from .types import __all__ as _types_all
+from .actions import __all__ as _actions_all
+
+__all__ = _types_all + _actions_all
+
+__title__ = 'More argparse'
+__version__ = '1.0.0'
+__license__ = 'MIT'
+__author__ = 'Gaming32'
+__contibutor__ = 'theglossy1'
