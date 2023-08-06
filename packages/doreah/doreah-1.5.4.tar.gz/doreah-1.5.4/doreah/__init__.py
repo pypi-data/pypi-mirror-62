c0d97b36c955e412a6ffce9bf0dc04d865f7285a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:31 +0000

    Add package doreah/1.5.4/doreah-1.5.4.tar.gz

---
+++ packages/doreah/doreah-1.5.4.tar.gz/doreah-1.5.4/doreah/__init__.py
@@ -0,0 +1,57 @@
+name = "doreah"
+version = 1,5,4
+versionstr = ".".join(str(n) for n in version)
+desc = "Small toolkit of utilities for python projects"
+author = {
+	"name": "Johannes Krattenmacher",
+	"email": "python@krateng.dev",
+	"github": "krateng"
+}
+requires = [
+	"lxml",
+	"mechanicalsoup",
+	"requests",
+	"pyyaml",
+	"beautifulsoup4"
+]
+resources = [
+	'res/login.pyhp'
+]
+
+commands = {
+	"pyhpserver":"pyhp2:run_testserver"
+}
+
+
+__all__ = [
+	"auth",
+	"caching",
+	"database",
+	"io",
+	"logging",
+	"persistence",
+	"pyhp",
+	"regular",
+	"scraping"
+	"settings",
+	"timing",
+	"tsv"
+
+]
+
+from ._internal import config
+
+
+
+
+# useful things for everyone
+
+
+def deprecated(func):
+	"""Function decorator to deprecate a function"""
+
+	def newfunc(*args,**kwargs):
+		print("\033[93m" + "Function " + func.__name__ + " is deprecated!" + "\033[0m")
+		return func(*args,**kwargs)
+
+	return newfunc
