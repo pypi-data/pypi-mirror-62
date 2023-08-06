29e0bdd412eaf755d2c7e52e92c0ecf0d2b6b040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:04 +0000

    Add package rasterbox/0.0.6/rasterbox-0.0.6.tar.gz

---
+++ packages/rasterbox/rasterbox-0.0.6.tar.gz/rasterbox-0.0.6/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rasterbox",
+    version="0.0.6",
+    author="Brad Crump",
+    author_email="brad@breadcrumbbuilds.com",
+    description="Semantic access of raster images and target labels associated with rasters",
+    keywords = ["RASTER","IMAGES", "MACHINE","LEARNING"],
+    install_requires=[
+        'numpy'
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/breadcrumbbuilds/rasterbox",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
