e3b669d578716cdaf593f7d58f838f648d6efbb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:20 +0000

    Add package youconfigme/0.2.2/youconfigme-0.2.2.tar.gz

---
+++ packages/youconfigme/youconfigme-0.2.2.tar.gz/youconfigme-0.2.2/setup.py
@@ -0,0 +1,31 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-function-docstring
+from setuptools import setup
+
+from youconfigme import __version__
+
+setup(
+    name='youconfigme',
+    version=__version__,
+    description='',
+    url='https://github.com/crossnox/YouConfigMe',
+    author='CrossNox',
+    install_requires=[],
+    extras_require={
+        'test': ['pytest'],
+        'dev': [
+            'precommit',
+            'mypy',
+            'flake8',
+            'isort',
+            'black',
+            'pylint',
+            'sphinx',
+            'bump',
+            'sphinx_rtd_theme',
+            'm2r',
+        ],
+    },
+    packages=['youconfigme'],
+    classifiers=['Programming Language :: Python :: 3'],
+)
