041f3ff8b9322e9f5e1b87bc67de6a438faf67fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:26 +0000

    Add package openlab/2.4.1/openlab-2.4.1.tar.gz

---
+++ packages/openlab/openlab-2.4.1.tar.gz/openlab-2.4.1/openlab/exceptions.py
@@ -0,0 +1,29 @@
+class ValidationError(Exception):
+    """
+    Raised when OpenLab Simulator returns 422 error code
+    This happens when:
+    * config/sim setpoints are out of simulator boundaries
+    * Creating a configuration with name that already exists
+    """
+    def __init__(self, message):
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
+        self.message = message
+
+class ConfigurationNotFound(Exception):
+    """
+    Raise when configuration is not found in OpenLab
+    """
+    def __init__(self, message):
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
+        self.message = message
+
+class SimulationNotFound(Exception):
+    """
+    Raise when simulation is not found in OpenLab
+    """
+    def __init__(self, message):
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
+        self.message = message
