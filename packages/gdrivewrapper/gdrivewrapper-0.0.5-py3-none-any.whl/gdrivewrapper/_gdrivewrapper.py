893334971de0ed4d445a877844705e7da581425b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:51 +0000

    Add package gdrivewrapper/0.0.5/gdrivewrapper-0.0.5-py3-none-any.whl

---
+++ packages/gdrivewrapper/gdrivewrapper-0.0.5-py3-none-any.whl/gdrivewrapper/_gdrivewrapper.py
@@ -0,0 +1,30 @@
+from multiprocessing import Lock
+
+from gdrivewrapper.core import *
+
+_lock = Lock()
+
+
+def _lock_and_call(func, *args, **kwargs):
+    _lock.acquire()
+    try:
+        return func(*args, **kwargs)
+    finally:
+        _lock.release()
+
+
+class GDriveWrapper:
+    def __init__(self, scopes: str, creds_path: str):
+        self.svc = get_service_object(scopes, creds_path)
+
+    def upload(self, *args, **kwargs):
+        return _lock_and_call(upload, self.svc, *args, **kwargs)
+
+    def download_bytes(self, *args, **kwargs):
+        return _lock_and_call(download_bytes, self.svc, *args, **kwargs)
+
+    def create_folder(self, *args, **kwargs):
+        return _lock_and_call(create_folder, self.svc, *args, **kwargs)
+
+    def create_comment(self, *args, **kwargs):
+        return _lock_and_call(create_comment, self.svc, *args, **kwargs)
