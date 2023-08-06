72d0a353bc8462d7d739924e991447a872e7c8c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:04 +0000

    Add package pybfcontrol/0.0.2/pybfcontrol-0.0.2.tar.gz

---
+++ packages/pybfcontrol/pybfcontrol-0.0.2.tar.gz/pybfcontrol-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pybfcontrol",
+    version="0.0.2",
+    author="bitfly",
+    author_email="zhangbitfly@163.com",
+    description="some controls for pygame",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zhangenter/bf_control",
+    packages=setuptools.find_packages(),
+    install_requires = ["pygame"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
