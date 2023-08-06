37918d3d8ab0a18a70f784f03475bef1dfbfd28e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:11 +0000

    Add package dciborowMMLSpark/0.0.26/dciborowMMLSpark-0.0.26.tar.gz

---
+++ packages/dciborowMMLSpark/dciborowMMLSpark-0.0.26.tar.gz/dciborowMMLSpark-0.0.26/setup.py
@@ -0,0 +1,43 @@
+# Copyright (C) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See LICENSE in project root for information.
+
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name="dciborowMMLSpark",
+    version="0.0.26",
+    description="Microsoft ML for Spark",
+    long_description="Microsoft ML for Apache Spark contains Microsoft's open source " +
+                     "contributions to the Apache Spark ecosystem",
+    license="MIT",
+    # Project's main homepage.
+    url="https://github.com/dciborow/mmlspark",
+    # Author details
+    author="Microsoft",
+    author_email="mmlspark-support@microsoft.com",
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3"
+    ],
+    
+     packages=['mmlspark',
+               'mmlspark.recommendation',
+               'mmlspark.jars'],
+    include_package_data=True,
+    package_dir={
+            'mmlspark.jars': 'deps/jars',
+    },
+    package_data={
+            'mmlspark.jars': ['*.jar']},
+
+    install_requires=[
+        "pyspark",
+        "numpy"
+    ],
+
+    zip_safe=True
+)
