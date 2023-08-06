91b9661f8d123e3aa9782ff8af3dffd031986eca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:04 +0000

    Add package former/0.2.6/former-0.2.6-py2.py3-none-any.whl

---
+++ packages/former/former-0.2.6-py2.py3-none-any.whl/former/__init__.py
@@ -0,0 +1,11 @@
+import logging
+import sys
+
+__version__ = '0.2.6'
+
+logger = logging.getLogger('former')
+handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter('%(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
---
+++ packages/former/former-0.2.6-py2.py3-none-any.whl/former/specification.py
@@ -0,0 +1,24 @@
+import os
+import json
+import platform
+import pathlib
+
+import requests
+
+if platform.system() == 'Windows':
+    CACHE_PATH = pathlib.WindowsPath(os.getenv('TEMP', '~') + '/former-spec.cached.json').expanduser()
+else:
+    CACHE_PATH = pathlib.Path('/tmp/former-spec.cached.json')
+CACHE_PATH = str(CACHE_PATH)
+
+
+def specification():
+    if os.path.exists(CACHE_PATH):
+        with open(CACHE_PATH) as f:
+            return json.load(f)
+    response = requests.get(
+        'https://d1uauaxba7bl26.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json')
+    response.raise_for_status()
+    with open(CACHE_PATH, 'w') as f:
+        f.write(response.text)
+    return json.loads(response.text)
