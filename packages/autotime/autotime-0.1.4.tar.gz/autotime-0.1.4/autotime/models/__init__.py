f0e15e4cda29b9b274377dfd16537123a9af601e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:39 +0000

    Add package autotime/0.1.4/autotime-0.1.4.tar.gz

---
+++ packages/autotime/autotime-0.1.4.tar.gz/autotime-0.1.4/autotime/models/__init__.py
@@ -0,0 +1,5 @@
+from .ar_based import build_arima_model, build_sarimax_model, build_var_model
+from .dl import run_deep_model
+from .ml import run_ensemble_model
+from .prophet import build_prophet_model
+from .pyflux import build_pyflux_model
