ba76ddfbfa06e93149b5680c8416371aeee5b6f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:37 +0000

    Add package webviz-subsurface-components/0.0.25/webviz_subsurface_components-0.0.25.tar.gz

---
+++ packages/webviz-subsurface-components/webviz_subsurface_components-0.0.25.tar.gz/webviz_subsurface_components-0.0.25/src/lib/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/prefer-default-export */
+import HistoryMatch from "./components/HistoryMatch";
+import Map from "./components/Map";
+import Morris from "./components/Morris";
+import LayeredMap from "./components/LayeredMap";
+import PriorPosteriorDistribution from "./components/PriorPosteriorDistribution";
+
+export { HistoryMatch, Morris, Map, LayeredMap, PriorPosteriorDistribution };
