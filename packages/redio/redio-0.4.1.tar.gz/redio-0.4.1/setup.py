917f7b8a0054cd09de4dff223f4e8f42731e198b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:35 +0000

    Add package redio/0.4.1/redio-0.4.1.tar.gz

---
+++ packages/redio/redio-0.4.1.tar.gz/redio-0.4.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from redio import __version__
+
+setup(
+    name="redio",
+    version=__version__,
+    author="L. Kärkkäinen",
+    author_email="tronic@noreply.users.github.com",
+    description="Redis Trio client",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Tronic/redio",
+    packages=find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+    ],
+    python_requires = ">=3.7",
+    install_requires = [
+        "trio",
+    ],
+    include_package_data = True,
+)
