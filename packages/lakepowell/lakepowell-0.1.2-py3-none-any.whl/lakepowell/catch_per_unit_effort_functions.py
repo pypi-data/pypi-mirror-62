9526c5bdfb82cf89f27efdfe0bc537fde9a1556e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:28 +0000

    Add package lakepowell/0.1.2/lakepowell-0.1.2-py3-none-any.whl

---
+++ packages/lakepowell/lakepowell-0.1.2-py3-none-any.whl/lakepowell/catch_per_unit_effort_functions.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 31 13:19:05 2020
+
+@author: aquag
+"""
+
+def calc_catch_per_unit_effort(data):
+    #pull weights
+    weights = data["Weight"]
+        #add together
+    total_weight = sum(weights)
+
+    #divide by hours = 32?? overnight???
+    #get average number of hours
+    
+    #divide by number of nets = number of sites = 32??
+    #do thsi by taking the site column
+    #pull out and coutn unique values
+    
+    #also get avearge net size
+    #have this as a sencondary caracterstic based on what gets imported
+    
+    return 
