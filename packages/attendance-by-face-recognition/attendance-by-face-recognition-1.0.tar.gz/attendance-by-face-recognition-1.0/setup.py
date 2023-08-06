d6807df6680a7d9f1ba6641df14762a4fadaa133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:10 +0000

    Add package attendance-by-face-recognition/1.0/attendance-by-face-recognition-1.0.tar.gz

---
+++ packages/attendance-by-face-recognition/attendance-by-face-recognition-1.0.tar.gz/attendance-by-face-recognition-1.0/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="attendance-by-face-recognition", # Replace with your own username
+    version="1.0",
+    author="Parimal Nandkumar Mahindrakar",
+    author_email="parimalm4653@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
