c7f721420442711b8ee1ca30175c4e6bd6877c77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:10 +0000

    Add package conformity/1.26.10/conformity-1.26.10-py27.py35.py36.py37.py38-none-any.whl

---
+++ packages/conformity/conformity-1.26.10-py27.py35.py36.py37.py38-none-any.whl/conformity/error.py
@@ -0,0 +1,73 @@
+from __future__ import (
+    absolute_import,
+    unicode_literals,
+)
+
+from typing import (
+    Hashable as HashableType,
+    Optional,
+)
+
+import attr
+import six
+
+from conformity.utils import (
+    attr_is_optional,
+    attr_is_string,
+)
+
+
+__all__ = (
+    'ERROR_CODE_INVALID',
+    'ERROR_CODE_MISSING',
+    'ERROR_CODE_UNKNOWN',
+    'Error',
+    'KeywordError',
+    'PositionalError',
+    'ValidationError',
+    'update_error_pointer',
+)
+
+
+ERROR_CODE_INVALID = 'INVALID'
+ERROR_CODE_MISSING = 'MISSING'
+ERROR_CODE_UNKNOWN = 'UNKNOWN'
+
+
+@attr.s
+class Error(object):
+    """
+    Represents an error found validating against the schema.
+    """
+    message = attr.ib(validator=attr_is_string())  # type: six.text_type
+    code = attr.ib(default=ERROR_CODE_INVALID, validator=attr_is_string())  # type: six.text_type
+    pointer = attr.ib(default=None, validator=attr_is_optional(attr_is_string()))  # type: Optional[six.text_type]
+
+
+class ValidationError(ValueError):
+    """
+    Error raised when a value fails to validate.
+    """
+
+
+class PositionalError(TypeError):
+    """
+    Error raised when you pass positional arguments into a validated function that doesn't support them.
+    """
+
+
+class KeywordError(TypeError):
+    """
+    Error raised when you pass keyword arguments into a validated function that doesn't support them.
+    """
+
+
+def update_error_pointer(error, pointer_or_prefix):  # type: (Error, HashableType) -> Error
+    """
+    Helper function to update an Error's pointer attribute with a (potentially prefixed) dictionary key or list index.
+    """
+    if error.pointer:
+        error.pointer = '{}.{}'.format(pointer_or_prefix, error.pointer)
+    else:
+        error.pointer = '{}'.format(pointer_or_prefix)
+    return error
