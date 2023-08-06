40fea680da0fe886d91f8209377ae1ed14256420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:37 +0000

    Add package aktools/0.0.5/aktools-0.0.5-py3-none-any.whl

---
+++ packages/aktools/aktools-0.0.5-py3-none-any.whl/aktools/__init__.py
@@ -0,0 +1,18 @@
+"""AkTools 是 AkShare的 HTTP server 工具, 主要用于把 AkShare 的数据接口部署到本地从而通过 HTTP 访问"""
+
+"""
+Changelog
+
+0.0.1:for test
+
+0.0.2:FastAPI test
+
+0.0.3:FastAPI deployment test
+
+0.0.4:add docs
+
+0.0.5:add automate package management
+"""
+
+__version__ = "0.0.5"
+__author__ = "Albert King"
