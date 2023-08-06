6365634ef21d20d18f4642c716a380f5a6128025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:53 +0000

    Add package streamline/1.0.4/streamline-1.0.4.tar.gz

---
+++ packages/streamline/streamline-1.0.4.tar.gz/streamline-1.0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name = 'streamline',
+    scripts=['bin/streamline'],
+    packages=['streamline'],
+    version = '1.0.4',
+    description = 'CLI tool for doing async tasks and transformations',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author = 'KJ',
+    author_email = 'jdotpy@users.noreply.github.com',
+    url = 'https://github.com/jdotpy/streamline',
+    download_url = 'https://github.com/jdotpy/streamline/tarball/master',
+    keywords = ['tools'],
+    install_requires=[
+        'pyyaml',
+    ],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+    ],
+)
