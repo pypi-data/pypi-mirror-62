0920b325e89e596f9efdc546c9cc3db7cdcc09f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:17 +0000

    Add package vjobs-ebrandon/0.1.0/vjobs-ebrandon-0.1.0.tar.gz

---
+++ packages/vjobs-ebrandon/vjobs-ebrandon-0.1.0.tar.gz/vjobs-ebrandon-0.1.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+
+setuptools.setup(
+    name='vjobs-ebrandon',
+    version='0.1.0',
+    url='https://gitlab.com/flaxking/vjobs-ebrandon',
+    author='flaxking',
+    author_email='flaxking@digitalnostril.com',
+    description='Collects jobs from ebrandon.ca for vjobs',
+    long_description='Collects jobs from ebrandon.ca for vjobs',
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=['vjobs >= 0.1.0'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
