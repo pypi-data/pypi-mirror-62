3f2b9fa2bd936655828a264b0258941c922352fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:30 +0000

    Add package jturtle/0.0.3/jturtle-0.0.3-py3-none-any.whl

---
+++ packages/jturtle/jturtle-0.0.3-py3-none-any.whl/jturtle/__init__.py
@@ -0,0 +1,7 @@
+# jturtle - Simple Turtle Library to be used
+# in Jupyter Notebooks and elsewhere
+# (c) 2018 Dmitri Soshnikov, http://twitter.com/shwars
+
+__version__ = '0.0.3'
+
+from .turtle import *
