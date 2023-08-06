6410954bab586c029792676109ef07c30b3346dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:43 +0000

    Add package KGlobal/1.0.15/KGlobal-1.0.15.tar.gz

---
+++ packages/KGlobal/KGlobal-1.0.15.tar.gz/KGlobal-1.0.15/KGlobal.py
@@ -0,0 +1,10 @@
+__package_name__ = 'KGlobal'
+__author__ = 'Kevin Russell'
+__version__ = "1.0.15"
+__description__ = '''File, encryption, SQL, XML, and etc...'''
+__url__ = 'https://github.com/KLRussell/Python_KGlobal_Package'
+
+
+if __name__ == '__main__':
+    from runpy import run_module
+    run_module('KGlobal_Core')
