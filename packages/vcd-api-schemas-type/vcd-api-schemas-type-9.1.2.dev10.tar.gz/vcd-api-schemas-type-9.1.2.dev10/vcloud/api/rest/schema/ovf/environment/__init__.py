d947e3d867562eed85852fefed851a6070671b97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:14 +0000

    Add package vcd-api-schemas-type/9.1.2.dev10/vcd-api-schemas-type-9.1.2.dev10.tar.gz

---
+++ packages/vcd-api-schemas-type/vcd-api-schemas-type-9.1.2.dev10.tar.gz/vcd-api-schemas-type-9.1.2.dev10/vcloud/api/rest/schema/ovf/environment/__init__.py
@@ -0,0 +1,9 @@
+"""
+  Copyright (c) 2020 VMware, Inc. All rights reserved.
+"""
+from .entity_type import EntityType
+from .platform_section_type import PlatformSectionType
+from .property_section_type import PropertySectionType
+from .section_type import SectionType
+from .property import Property
+from .environment_type import EnvironmentType
