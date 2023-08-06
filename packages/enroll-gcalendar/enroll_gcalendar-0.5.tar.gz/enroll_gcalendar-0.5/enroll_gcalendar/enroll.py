7c10bfc13096e10fc3fca515c16c94621094cc26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:34 +0000

    Add package enroll-gcalendar/0.5/enroll_gcalendar-0.5.tar.gz

---
+++ packages/enroll-gcalendar/enroll_gcalendar-0.5.tar.gz/enroll_gcalendar-0.5/enroll_gcalendar/enroll.py
@@ -0,0 +1,315 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    enroll-calendar.enroll
+    ~~~~~~~~~~~~~~~~~~~~~~
+
+    contains plan fetching functions
+
+    :copyright: (c) 2020 by Hubert Pelczarski
+    :license: LICENSE_NAME, see LICENSE for more details.
+"""
+
+import sys
+
+import selenium.webdriver.firefox
+import selenium.webdriver.chrome
+from selenium import webdriver
+from selenium.common.exceptions import (NoSuchElementException,
+                                        WebDriverException)
+
+DRIVER = None
+DAYS_LOCATIONS = {}
+
+
+def connect(timeout, driver_name=None):
+    """
+        establish connection to enroll-me.iiet.pl
+    """
+    global DRIVER
+    if driver_name is None:
+        try:
+            options = selenium.webdriver.firefox.options.Options()
+            options.headless = True
+            DRIVER = webdriver.Firefox(options=options)
+            print('headless Firefox initialized')
+        except WebDriverException:
+            print('firefox geckodriver not found')
+        if DRIVER is None:
+            try:
+                options = selenium.webdriver.chrome.options.Options()
+                options.headless = True
+                DRIVER = webdriver.Chrome(options=options)
+                print('headless Chrome initialized')
+            except WebDriverException:
+                print('chromedriver not found')
+    elif driver_name == 'chrome':
+        try:
+            options = selenium.webdriver.chrome.options.Options()
+            options.headless = True
+            DRIVER = webdriver.Chrome(options=options)
+            print('headless Chrome initialized')
+        except WebDriverException:
+            print('chromedriver not found')
+    elif driver_name == 'firefox':
+        try:
+            options = selenium.webdriver.firefox.options.Options()
+            options.headless = True
+            DRIVER = webdriver.Firefox(options=options)
+            print('headless Firefox initialized')
+        except WebDriverException:
+            print('firefox geckodriver not found')
+    if DRIVER is None:
+        raise Exception('no webdriver found, refer to help or README.md')
+    DRIVER.implicitly_wait(timeout)
+    print('connecting to http://enroll-me.iiet.pl/')
+    DRIVER.get('http://enroll-me.iiet.pl/')
+
+
+def get_day_locations():
+    """
+        fetches location of all weekdays from enroll-me.iiet.pl
+    """
+    global DAYS_LOCATIONS
+    DAYS_LOCATIONS['monday'] = {
+        'start': int(DRIVER.find_elements_by_class_name(
+            'fc-mon')[-1].location['x']),
+        'middle': int(DRIVER.find_elements_by_class_name(
+            'fc-mon')[-1].location['x']) + int(
+                DRIVER.find_elements_by_class_name(
+                    'fc-mon')[-1].size['width']/2)
+    }
+    DAYS_LOCATIONS['tuesday'] = {
+        'start': int(DRIVER.find_elements_by_class_name(
+            'fc-tue')[-1].location['x']),
+        'middle': int(DRIVER.find_elements_by_class_name(
+            'fc-tue')[-1].location['x']) + int(
+                DRIVER.find_elements_by_class_name(
+                    'fc-tue')[-1].size['width']/2)
+    }
+    DAYS_LOCATIONS['wednesday'] = {
+        'start': int(DRIVER.find_elements_by_class_name(
+            'fc-wed')[-1].location['x']),
+        'middle': int(DRIVER.find_elements_by_class_name(
+            'fc-wed')[-1].location['x']) + int(
+                DRIVER.find_elements_by_class_name(
+                    'fc-wed')[-1].size['width']/2)
+    }
+    DAYS_LOCATIONS['thursday'] = {
+        'start': int(DRIVER.find_elements_by_class_name(
+            'fc-thu')[-1].location['x']),
+        'middle': int(DRIVER.find_elements_by_class_name(
+            'fc-thu')[-1].location['x']) + int(
+                DRIVER.find_elements_by_class_name(
+                    'fc-thu')[-1].size['width']/2)
+    }
+    DAYS_LOCATIONS['friday'] = {
+        'start': int(DRIVER.find_elements_by_class_name(
+            'fc-fri')[-1].location['x']),
+        'middle':
+        int(DRIVER.find_elements_by_class_name(
+            'fc-fri')[-1].location['x']) + int(
+                DRIVER.find_elements_by_class_name(
+                    'fc-fri')[-1].size['width']/2)
+    }
+
+
+def find_lesson_day(lesson_location):
+    """
+        lesson loction is never the same as day location due to responsive
+        padding
+        finding lesson's day requires fidning minimum distance from
+        lesson_location to each day_location
+    """
+    min_distance = sys.maxsize
+    lesson_day = ''
+    for day in DAYS_LOCATIONS:
+        day_location_start = DAYS_LOCATIONS[day]['start']
+        day_location_middle = DAYS_LOCATIONS[day]['middle']
+        if abs(day_location_start - lesson_location) < min_distance:
+            min_distance = abs(day_location_start - lesson_location)
+            lesson_day = day
+        if abs(day_location_middle - lesson_location) < min_distance:
+            min_distance = abs(day_location_middle - lesson_location)
+            lesson_day = day
+    return lesson_day
+
+
+def format_time_to_24_format(lesson_time_block):
+    """
+        formats enroll's time format to google's 24h format
+    """
+    lesson_time = lesson_time_block.text.strip().replace(' ', '')
+    description = ''
+    if not lesson_time[-1].isdigit():
+        description = lesson_time[-1]
+        lesson_time = (lesson_time[0:-1])
+    diff = DRIVER.find_element_by_class_name('fc-slot20')
+    starting_hour = lesson_time.split('-')[0]
+    ending_hour = lesson_time.split('-')[1]
+
+    # start of enroll square
+    if lesson_time_block.location['y'] < diff.location['y']:
+        hour = starting_hour.split(':')[0]
+        if len(hour) < 2:
+            hour = f'0{hour}'
+        starting_hour = hour + ':' + starting_hour.split(':')[1]
+    else:
+        hour = starting_hour.split(':')[0]
+        hour = str(12 + int(hour))
+        starting_hour = hour + ':' + starting_hour.split(':')[1]
+
+    # end of enroll square
+    if (lesson_time_block.location['y']+lesson_time_block.size['height'] <
+            diff.location['y']):
+        hour = ending_hour.split(':')[0]
+        if len(hour) < 2:
+            hour = f'0{hour}'
+        ending_hour = hour + ':' + ending_hour.split(':')[1]
+    else:
+        hour = ending_hour.split(':')[0]
+        hour = str(12 + int(hour))
+        ending_hour = hour + ':' + ending_hour.split(':')[1]
+    lesson_time = f'{starting_hour}-{ending_hour}'
+    return lesson_time, description
+
+
+def get_lessons_from_semester(selected_semester, semesters, overdue_lessons,
+                              all_lessons):
+    """
+        fetches all lessons from selected semester
+    """
+    number = selected_semester[0].split(' ')[0]
+    for index in semesters:
+        if semesters[index][0].split(
+                ' ')[0] == number and int(semesters[index][0].split(
+                    ' ')[1]) == int(selected_semester[0].split(' ')[1])-2:
+            overdue_lessons.append((semesters[index][0], semesters[index][1]))
+    semester_buttons = selected_semester[1].find_elements_by_css_selector(
+        'td')[-1]
+    your_schedule_button = semester_buttons.find_element_by_css_selector('div')
+    your_schedule_button.click()
+    lessons = DRIVER.find_elements_by_class_name('fc-event-inner')
+    for lesson in lessons:
+        get_day_locations()
+        lesson_header = lesson.find_element_by_class_name('fc-event-head')
+        lesson_content = lesson.find_element_by_class_name('fc-event-content')
+
+        title = lesson_content.find_element_by_class_name('fc-event-title')
+        lessonname = title.text.split(',')[0].strip()
+        lecturer = title.text.split(',')[1].strip()
+        lesson_time_block = lesson_header.find_element_by_class_name(
+            'fc-event-time')
+        lesson_time, description = format_time_to_24_format(lesson_time_block)
+        try:
+            room = title.text.split(',')[2].strip()
+        except IndexError:
+            room = lecturer
+            lecturer = ''
+        try:
+            lesson_type = title.text.split(
+                ',')[3].strip().split('-')[-1].strip()
+        except IndexError:
+            lesson_type = room.split('-')[-1].strip()
+            room = room.split('-')[0].strip()
+        all_lessons.append((lesson_time, lessonname, lecturer, room,
+                            lesson_type, description,
+                            find_lesson_day(lesson.location['x'])))
+
+
+def click_enrollment_button():
+    """
+        return to enrollment page
+    """
+    try:
+        enrollment_button = DRIVER.find_element_by_xpath(
+            '/html/body/div[1]/div/div[2]/div[2]/a')
+        enrollment_button.click()
+    except NoSuchElementException:
+        print(
+            'fetching semesters failed, possibly due to wrong login info')
+        sys.exit(1)
+
+
+def login(username, password):
+    """
+        sign in  with accounts.iiet.pl
+
+        :username: user's username
+        :password: user's password
+    """
+    sign_in_with_accoutns_iet_button = DRIVER.find_element_by_xpath(
+        '/html/body/div[2]/div[2]/form/div[4]/div/div/button[2]')
+    sign_in_with_accoutns_iet_button.click()
+    login_box = DRIVER.find_element_by_xpath('//*[@id="student_username"]')
+    password_box = DRIVER.find_element_by_xpath('//*[@id="student_password"]')
+    login_box.send_keys(username)
+    password_box.send_keys(password)
+    login_button = DRIVER.find_element_by_xpath(
+        '/html/body/div/div[2]/div/form/div[2]/input')
+    login_button.click()
+
+
+def fetch_available_semesters(print_available_semesters=False):
+    """
+        fetches all available semesters from enrollment page
+    """
+    all_plans = DRIVER.find_element_by_xpath(
+        '//*[@id="mainForm:j_id_x:tbody_element"]')
+    rows = all_plans.find_elements_by_css_selector('tr')
+    semesters = {}
+    for index, row in enumerate(rows):
+        col = row.find_elements_by_css_selector('td')[0]
+        semester = col.find_element_by_css_selector('h5')
+        semesters[index] = (semester.text, row)
+        if print_available_semesters:
+            print(f'{index}) {semester.text}')
+    return semesters
+
+
+def get_lessons_from_selected_semesters(username, password):
+    """
+        fetches all lessons from selected semesters
+    """
+    all_lessons = []
+    overdue_lessons = []
+    print('fetching semesters')
+    login(username, password)
+    click_enrollment_button()
+    semesters = fetch_available_semesters(print_available_semesters=True)
+    try:
+        selected_semester_index = int(input('select semester index: '))
+    except ValueError:
+        raise Exception('no semester selected')
+    try:
+        selected_semester = list(semesters.values())[selected_semester_index]
+    except IndexError:
+        print('please select correct semester index\n')
+    get_lessons_from_semester(selected_semester, semesters, overdue_lessons,
+                              all_lessons)
+    if len(overdue_lessons) > 0:
+        for index, overdue_lesson in enumerate(overdue_lessons):
+            print(f'{index}) {overdue_lesson[0]}')
+        try:
+            selected_semester_index = int(input(
+                'select overdue semester to exclude(empty excludes none): '))
+        except ValueError:
+            selected_semester_index = None
+        if selected_semester_index is not None:
+            del overdue_lessons[selected_semester_index]
+        click_enrollment_button()
+        semesters = fetch_available_semesters()
+        for overdue_lesson in overdue_lessons:
+            for semester in semesters:
+                if overdue_lesson[0] == semesters[semester][0]:
+                    get_lessons_from_semester(semesters[semester],
+                                              semesters, overdue_lessons,
+                                              all_lessons)
+    return all_lessons
+
+
+def close_driver():
+    """
+        closes driver
+    """
+    DRIVER.close()
---
+++ packages/enroll-gcalendar/enroll_gcalendar-0.5.tar.gz/enroll_gcalendar-0.5/enroll_gcalendar/schedule_preparator.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    enroll-calendar.schedule_preparator
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    set of functions preparing for conversion to google calendar
+
+    :copyright: (c) 2020 by Hubert Pelczarski
+    :license: LICENSE_NAME, see LICENSE for more details.
+"""
+
+import datetime
+import os
+
+from enroll_gcalendar.enroll import DAYS_LOCATIONS
+
+
+def sort_lessons_by_day(all_lessons):
+    """
+        sorts lessons by day
+    """
+    sorted_lessons = []
+    for day in DAYS_LOCATIONS:
+        for lesson in all_lessons:
+            if lesson[-1] == day:
+                sorted_lessons.append(lesson)
+    return sorted_lessons
+
+
+def print_lessons_by_day(all_lessons, sort_lessons=False, print_indices=False):
+    """
+        prints lessons day by day
+    """
+    os.system('cls' if os.name == 'nt' else 'clear')  # clear screen
+    if sort_lessons:
+        all_lessons = sort_lessons_by_day(all_lessons)
+    print('current plan:\n')
+    for day in DAYS_LOCATIONS:
+        print(day.capitalize())
+        for index, lesson in enumerate(all_lessons):
+            if lesson[-1] == day:
+                lesson_view = ''
+                if print_indices:
+                    lesson_view += f'{index})'
+                lesson_view += (f'{lesson[0]} {lesson[1]}-{lesson[4]}'
+                                f' {lesson[5]} {lesson[2]} {lesson[3]}')
+                print(lesson_view)
+        print('')
+
+
+def remove_lessons(all_lessons):
+    """
+        removes all lessons selected by user
+    """
+    choice = input('select index to remove, -1 removes all lecutres(W): ')
+    if int(choice) == -1:
+        all_lessons = [lesson for lesson in all_lessons if lesson[-3] != 'W']
+        print_lessons_by_day(
+            all_lessons, sort_lessons=True, print_indices=True)
+    else:
+        if int(choice) > len(all_lessons):
+            print('provided index too big')
+        else:
+            all_lessons = [lesson for index, lesson in enumerate(all_lessons)
+                           if index != int(choice)]
+            print_lessons_by_day(
+                all_lessons, sort_lessons=True, print_indices=True)
+    continue_deleting = input('continue removing? [y/N] ')
+    if continue_deleting.lower() == 'y':
+        all_lessons = remove_lessons(all_lessons)
+    return all_lessons
+
+
+def find_next_weekday(start_date, weekday=0):
+    """
+        finds next weekday from provided start_date,
+        defaults to search for monday
+        :start_date: date to start the search from
+        :weekday: day of week to search for, 0:Monday, 1:Tueseday...
+    """
+    while start_date.weekday() != weekday:
+        start_date += datetime.timedelta(days=1)
+    return start_date
+
+
+def convert_to_gcalendar_event(start_date, end_date, lesson):
+    """
+        convert single lesson into google calendar format
+        :lesson: lesson to convert
+        :start_date: start date of lesson
+        :end_date: end date of semester
+    """
+    index = next(index for index, key in enumerate(
+        DAYS_LOCATIONS) if key == lesson[-1])
+    start_date_date = datetime.datetime.strftime((find_next_weekday(start_date,
+                                                                    index)),
+                                                 "%Y-%m-%d")
+    end_date_date = datetime.datetime.strftime(end_date, "%Y%m%d")
+    start_date_start_time = lesson[0].split('-')[0]
+    start_date_end_time = lesson[0].split('-')[1]
+    # 4
+    summary = f'{lesson[1]} - {lesson[4]}'
+    if lesson[-2] != '':
+        summary += f' ({lesson[-2]})'
+    event = {
+        'summary': f'{summary}',
+        'location': f'{lesson[-4]}',
+        'description': f'lecturer: {lesson[2]}',
+        'start': {
+            'dateTime': f'{start_date_date}T{start_date_start_time}:00',
+            'timeZone': 'Europe/Warsaw',
+        },
+        'end': {
+            'dateTime': f'{start_date_date}T{start_date_end_time}:00',
+            'timeZone': 'Europe/Warsaw'
+        },
+        'recurrence': [
+            f'RRULE:FREQ=WEEKLY;UNTIL={end_date_date}T000000Z',
+        ]
+    }
+    return event
