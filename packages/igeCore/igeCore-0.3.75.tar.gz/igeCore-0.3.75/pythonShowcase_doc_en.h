b6e96fe0e807db3325a5f25d5a55ba0c5867cdee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:40 +0000

    Add package igeCore/0.3.75/igeCore-0.3.75.tar.gz

---
+++ packages/igeCore/igeCore-0.3.75.tar.gz/igeCore-0.3.75/pythonShowcase_doc_en.h
@@ -0,0 +1,39 @@
+
+//showcase
+PyDoc_STRVAR(showcase_doc,
+	"showcase is where you put the objects \n"\
+	"you want to render.\n"\
+	"You can add figure, editableFigure, environment, camera.\n"\
+	"\n"\
+	"case = igeCore.shocase()");
+
+//add
+PyDoc_STRVAR(add_doc,
+	"Add object to showcase\n"\
+	"\n"\
+	"showcase.add(object)\n"\
+	"\n"\
+	"Parameters\n"\
+	"----------\n"\
+	"    object : igeCore.figure, igeCore.editableFigure, igeCore.environment\n"\
+	"        Object to be added to showcase");
+
+
+//remove
+PyDoc_STRVAR(remove_doc,
+	"Remove object from showcase\n"\
+	"\n"\
+	"showcase.add(object)\n"\
+	"\n"\
+	"Parameters\n"\
+	"----------\n"\
+	"    object : igeCore.figure, igeCore.editableFigure, igeCore.environment\n"\
+	"        Object to be added to showcase");
+
+
+//clear
+PyDoc_STRVAR(clear_doc,
+	"Remove all object from showcase\n"\
+	"\n"\
+	"showcase.clear()");
+
