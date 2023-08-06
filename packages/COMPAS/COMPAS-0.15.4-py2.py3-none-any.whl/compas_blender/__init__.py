20cb8515ecf9545ca9daa4dcd52e691f9ff8e5df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:34 +0000

    Add package COMPAS/0.15.4/COMPAS-0.15.4-py2.py3-none-any.whl

---
+++ packages/COMPAS/COMPAS-0.15.4-py2.py3-none-any.whl/compas_blender/__init__.py
@@ -0,0 +1,23 @@
+"""
+********************************************************************************
+compas_blender
+********************************************************************************
+
+.. currentmodule:: compas_blender
+
+.. toctree::
+    :maxdepth: 1
+
+    compas_blender.artists
+    compas_blender.geometry
+    compas_blender.ui
+    compas_blender.utilities
+
+"""
+from .utilities import *  # noqa: F401 F403
+
+
+__version__ = '0.15.4'
+
+
+__all__ = [name for name in dir() if not name.startswith('_')]
