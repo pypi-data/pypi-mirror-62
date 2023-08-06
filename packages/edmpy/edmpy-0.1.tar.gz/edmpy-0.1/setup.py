23fdafda6d0f22db4e68da0d923dab9f2eebadf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:51 +0000

    Add package edmpy/0.1/edmpy-0.1.tar.gz

---
+++ packages/edmpy/edmpy-0.1.tar.gz/edmpy-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='edmpy',
+      version='0.1',
+      description='Electrodynamics at its best',
+      url='http://github.com/pradyoth-shandilya/edmpy',
+      author='paddyforprez',
+      author_email='pradyoth@ieee.org',
+      license='MIT',
+      packages=['edmpy'],
+      zip_safe=False)
