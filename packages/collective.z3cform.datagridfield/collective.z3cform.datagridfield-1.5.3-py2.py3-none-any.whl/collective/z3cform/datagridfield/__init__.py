4dcfaa2f8b88f0b653deb706de6cf6b0a816f7ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:46 +0000

    Add package collective.z3cform.datagridfield/1.5.3/collective.z3cform.datagridfield-1.5.3-py2.py3-none-any.whl

---
+++ packages/collective.z3cform.datagridfield/collective.z3cform.datagridfield-1.5.3-py2.py3-none-any.whl/collective/z3cform/datagridfield/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from zope.i18nmessageid import MessageFactory
+
+_ = MessageFactory('collective.z3cform.datagridfield')
+
+from .blockdatagridfield import BlockDataGridField  # noqa # pylint: disable=unused-import
+from .blockdatagridfield import BlockDataGridFieldFactory  # noqa # pylint: disable=unused-import
+from .datagridfield import DataGridField  # noqa # pylint: disable=unused-import
+from .datagridfield import DataGridFieldFactory  # noqa # pylint: disable=unused-import
+from .interfaces import IDataGridField  # noqa # pylint: disable=unused-import
+from .interfaces import IRow  # noqa # pylint: disable=unused-import
+from .row import DictRow  # noqa # pylint: disable=unused-import
---
+++ packages/collective.z3cform.datagridfield/collective.z3cform.datagridfield-1.5.3-py2.py3-none-any.whl/collective/z3cform/datagridfield/registry.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from collective.z3cform.datagridfield import DictRow as BaseDictRow
+
+
+try:
+    from plone.registry.field import PersistentField
+except ImportError:
+    class PersistentField(object):
+        pass
+
+
+class DictRow(PersistentField, BaseDictRow):
+    pass
