1287dad3f2a6f5f3773a6dd7d60519ebdc4403c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:59 +0000

    Add package pyabzu/0.0.2/pyabzu-0.0.2.tar.gz

---
+++ packages/pyabzu/pyabzu-0.0.2.tar.gz/pyabzu-0.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='pyabzu',
+    version='0.0.2',
+    description='Abzu package',
+    long_description='''
+    # Abzu
+    This is just an empty package for now. 
+    Maybe you are looking for [feyn package](https://pypi.org/project/feyn/) or you can read more about us at https://www.abzu.ai
+    ''',
+    long_description_content_type='text/markdown',
+    author='Abzu',
+    author_email='support@abzu.ai',
+    include_package_data=True,
+    zip_safe=False
+)
