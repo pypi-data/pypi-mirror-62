691383bcaeea34cd74d217deecf357af926b71ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:07 +0000

    Add package mfitools/0.0.1/mfitools-0.0.1.tar.gz

---
+++ packages/mfitools/mfitools-0.0.1.tar.gz/mfitools-0.0.1/setup.py
@@ -0,0 +1,15 @@
+"""mfitools setup script"""
+
+from setuptools import setup
+
+setup(
+	name='mfitools',
+	version='0.0.1',
+	author='Minut AB',
+	author_email='glenn@minut.com',
+	url='http://minut.com',
+	description='Planned tooling',
+	package_data={
+	        "": ["*.txt", "*.rst"],
+	},
+)
