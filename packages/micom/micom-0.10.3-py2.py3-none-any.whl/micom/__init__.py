6727683b9289d9770f992cf42a7200ffc4015a67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:27 +0000

    Add package micom/0.10.3/micom-0.10.3-py2.py3-none-any.whl

---
+++ packages/micom/micom-0.10.3-py2.py3-none-any.whl/micom/__init__.py
@@ -0,0 +1,34 @@
+"""Simple init file for mico."""
+
+from micom.community import Community, load_pickle
+from micom import (
+    algorithms,
+    problems,
+    util,
+    data,
+    duality,
+    elasticity,
+    media,
+    solution,
+    workflows,
+    workflow_examples,
+)
+
+
+__all__ = (
+    "Community",
+    "algorithms",
+    "problems",
+    "optcom",
+    "util",
+    "data",
+    "duality",
+    "elasticity",
+    "media",
+    "solution",
+    "load_pickle",
+    "workflows",
+    "workflow_examples",
+)
+
+__version__ = "0.10.3"
