29b91e5e3283ef6cef362b821008deca102de122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:04 +0000

    Add package run-subprocess/0.9.1/run_subprocess-0.9.1.tar.gz

---
+++ packages/run-subprocess/run_subprocess-0.9.1.tar.gz/run_subprocess-0.9.1/test/test_run.py
@@ -0,0 +1,25 @@
+from run_subprocess import run_to_list
+import os
+import unittest
+
+
+class RunTest(unittest.TestCase):
+    def setUp(self):
+        os.chdir(os.path.dirname(os.path.dirname(__file__)))
+
+    def test_simple(self):
+        error, lines = run_to_list('ls')
+        assert error == 0
+        assert 'run_subprocess.py' in lines
+        assert len(lines) >= 10
+
+    def test_error(self):
+        error, lines = run_to_list('ls foo setup.py bar')
+        assert error == 1
+        expected = [
+            'setup.py',
+            'ls: bar: No such file or directory',
+            'ls: foo: No such file or directory',
+        ]
+
+        assert lines == expected
