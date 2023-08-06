8d007f70437f648ecc42226c4c435898e3d3d01b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:24 +0000

    Add package python-libtiepie/0.9.1/python-libtiepie-0.9.1.tar.gz

---
+++ packages/python-libtiepie/python-libtiepie-0.9.1.tar.gz/python-libtiepie-0.9.1/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='python-libtiepie',
+      version='0.9.1',
+      description='Python bindings for LibTiePie',
+      license='MIT',
+      author='TiePie engineering',
+      author_email='support@tiepie.nl',
+      url='https://github.com/TiePie/python-libtiepie',
+      packages=['libtiepie'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: Microsoft :: Windows :: Windows 7',
+          'Operating System :: Microsoft :: Windows :: Windows 8',
+          'Operating System :: Microsoft :: Windows :: Windows 8.1',
+          'Operating System :: Microsoft :: Windows :: Windows 10',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Software Development',
+      ])
