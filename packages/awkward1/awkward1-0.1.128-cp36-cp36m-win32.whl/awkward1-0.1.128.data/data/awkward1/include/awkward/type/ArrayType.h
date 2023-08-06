ff8f47e6926cddb1bab5535aa74c2981abed8302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:14 +0000

    Add package awkward1/0.1.128/awkward1-0.1.128-cp36-cp36m-win32.whl

---
+++ packages/awkward1/awkward1-0.1.128-cp36-cp36m-win32.whl/awkward1-0.1.128.data/data/awkward1/include/awkward/type/ArrayType.h
@@ -0,0 +1,32 @@
+// BSD 3-Clause License; see https://github.com/jpivarski/awkward-1.0/blob/master/LICENSE
+
+#ifndef AWKWARD_ARRAYTYPE_H_
+#define AWKWARD_ARRAYTYPE_H_
+
+#include "awkward/type/Type.h"
+
+namespace awkward {
+  class ArrayType: public Type {
+  public:
+    ArrayType(const util::Parameters& parameters, const std::shared_ptr<Type>& type, int64_t length);
+
+    std::string tostring_part(const std::string& indent, const std::string& pre, const std::string& post) const override;
+    const std::shared_ptr<Type> shallow_copy() const override;
+    bool equal(const std::shared_ptr<Type>& other, bool check_parameters) const override;
+    int64_t numfields() const override;
+    int64_t fieldindex(const std::string& key) const override;
+    const std::string key(int64_t fieldindex) const override;
+    bool haskey(const std::string& key) const override;
+    const std::vector<std::string> keys() const override;
+    const std::shared_ptr<Content> empty() const override;
+
+    const std::shared_ptr<Type> type() const;
+    int64_t length() const;
+
+  private:
+    std::shared_ptr<Type> type_;
+    int64_t length_;
+  };
+}
+
+#endif // AWKWARD_ARRAYTYPE_H_
