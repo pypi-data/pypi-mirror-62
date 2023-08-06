e3413be1c0e607c461a8985fcfd02658a0ed80fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:41 +0000

    Add package aioice/0.6.18/aioice-0.6.18.tar.gz

---
+++ packages/aioice/aioice-0.6.18.tar.gz/aioice-0.6.18/tests/utils.py
@@ -0,0 +1,26 @@
+import asyncio
+import os
+
+
+async def invite_accept(conn_a, conn_b):
+    # invite
+    await conn_a.gather_candidates()
+    conn_b.remote_candidates = conn_a.local_candidates
+    conn_b.remote_username = conn_a.local_username
+    conn_b.remote_password = conn_a.local_password
+
+    # accept
+    await conn_b.gather_candidates()
+    conn_a.remote_candidates = conn_b.local_candidates
+    conn_a.remote_username = conn_b.local_username
+    conn_a.remote_password = conn_b.local_password
+
+
+def read_message(name):
+    path = os.path.join(os.path.dirname(__file__), "data", name)
+    with open(path, "rb") as fp:
+        return fp.read()
+
+
+def run(coro):
+    return asyncio.get_event_loop().run_until_complete(coro)
