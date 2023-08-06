956b27448c23ffb016305ed9436d76d57543ea63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:56 +0000

    Add package py-random-words/0.5.3/py_random_words-0.5.3.tar.gz

---
+++ packages/py-random-words/py_random_words-0.5.3.tar.gz/py_random_words-0.5.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+      name='py_random_words',
+      version='0.5.3',
+      packages=find_packages(exclude=['tests*']),
+      url='https://github.com/andreasonny83/py_random_words',
+      license='MIT',
+      author='andreasonny83',
+      author_email='andreasonny83@gmail.com',
+      description='Generate random names',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+)
