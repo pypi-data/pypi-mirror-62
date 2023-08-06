55544c1c81c256d7d6979be96398d9add8fef8e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:55 +0000

    Add package sparcli/0.1.1/sparcli-0.1.1-py3-none-any.whl

---
+++ packages/sparcli/sparcli-0.1.1-py3-none-any.whl/sparcli/context.py
@@ -0,0 +1,16 @@
+from contextlib import AbstractContextManager
+
+
+class SparcliContext(AbstractContextManager):
+    def __init__(self, event_queue):
+        self.emit = event_queue.append
+
+    def record(self, **variables):
+        self.emit(("data_produced", self, variables))
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        del exc_type, exc_value, traceback
+        self.emit(("producer_stopped", self))
