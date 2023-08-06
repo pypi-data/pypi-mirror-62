958d753d69f504e0eff9af5e25be461d1cdb1aa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:11 +0000

    Add package encoder-decoder/0.1.4/encoder_decoder-0.1.4.tar.gz

---
+++ packages/encoder-decoder/encoder_decoder-0.1.4.tar.gz/encoder_decoder-0.1.4/encoder_decoder/dtypes_encode_decode.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pdb
+def float_ndarray_to_dict(arr):
+    return np_arr_to_dict(arr)
+
+def dict_to_float_ndarray(string):
+    return dict_to_np_arr(string)
+
+def identity(e):
+    return e
+
+def float_to_string(num):
+    return str(num)
+
+def string_to_float(string):
+    return float(string)
+
+def np_arr_to_dict(arr):
+    # pdb.set_trace()
+    return {'arr': arr.tolist(),
+        'shape':list(arr.shape),
+        'dtype':str(arr.dtype)
+    }
+
+def dict_to_np_arr(data):
+    arr, shape, dtype = data['arr'], data['shape'], data['dtype']
+    return np.array(arr, dtype=dtype).reshape(tuple(shape))
