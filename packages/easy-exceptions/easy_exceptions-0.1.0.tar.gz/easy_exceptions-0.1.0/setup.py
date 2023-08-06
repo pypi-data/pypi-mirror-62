77f9175fef597bf6f00b152dfac7838d201320b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:23 +0000

    Add package easy-exceptions/0.1.0/easy_exceptions-0.1.0.tar.gz

---
+++ packages/easy-exceptions/easy_exceptions-0.1.0.tar.gz/easy_exceptions-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+  name = 'easy_exceptions',
+  packages = ['easy_exceptions'],
+  version = '0.1.0',
+  license='MIT',
+  description = 'Throw and catch named exceptions without creating a class first',
+  author = 'Patrick Robertshaw',
+  author_email = 'patrick.robertshaw1@gmail.com',
+  url = 'https://github.com/Cruuncher/EasyExceptions',
+  download_url = 'https://github.com/Cruuncher/EasyExceptions/archive/0.1.0.tar.gz',
+  keywords = ['exceptions'],  
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',   
+  ],
+)
