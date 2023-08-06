727b6fb1984693b1932c635ae46f0ef085b65dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:02 +0000

    Add package plynx/1.0.0/plynx-1.0.0-py3.7.egg

---
+++ packages/plynx/plynx-1.0.0-py3.7.egg/plynx/db/demo_user_manager.py
@@ -0,0 +1,38 @@
+import string
+import random
+from plynx.db.user import User
+from plynx.utils.config import get_demo_config
+
+
+class DemoUserManager(object):
+    """The class contains Demo user code."""
+    demo_config = get_demo_config()
+
+    @staticmethod
+    def _id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+        return ''.join(random.choice(chars) for _ in range(size))
+
+    @staticmethod
+    def create_demo_user():
+        if not DemoUserManager.demo_config.enabled:
+            return None
+
+        user = User()
+        user.username = 'demo-{}'.format(DemoUserManager._id_generator())
+        user.hash_password(DemoUserManager._id_generator(size=8))
+        user.save()
+        return user
+
+    @staticmethod
+    def create_demo_graphs(user):
+        raise NotImplementedError()
+        """
+        res = []
+        for graph_id in DemoUserManager.demo_config.graph_ids:
+            graph = Graph.load(graph_id)
+            graph._id = ObjectId()
+            graph.author = user._id
+            graph.save()
+            res.append(graph._id)
+        """
+        return []
---
+++ packages/plynx/plynx-1.0.0-py3.7.egg/plynx/web/user.py
@@ -0,0 +1,33 @@
+from flask import g
+from plynx.db.demo_user_manager import DemoUserManager
+from plynx.web.common import app, requires_auth, make_fail_response
+from plynx.utils.common import JSONEncoder
+
+
+demo_user_manager = DemoUserManager()
+
+
+@app.route('/plynx/api/v0/token', strict_slashes=False)
+@requires_auth
+def get_auth_token():
+    access_token = g.user.generate_access_token()
+    refresh_token = g.user.generate_refresh_token()
+    return JSONEncoder().encode({
+        'access_token': access_token.decode('ascii'),
+        'refresh_token': refresh_token.decode('ascii')
+    })
+
+
+@app.route('/plynx/api/v0/demo', methods=['POST'])
+def post_demo_user():
+    user = demo_user_manager.create_demo_user()
+    if not user:
+        return make_fail_response('Failed to create demo user')
+    demo_user_manager.create_demo_graphs(user)
+
+    access_token = user.generate_access_token(expiration=1800)
+    return JSONEncoder().encode({
+        'access_token': access_token.decode('ascii'),
+        'refresh_token': 'Not assigned',
+        'username': user.username
+    })
