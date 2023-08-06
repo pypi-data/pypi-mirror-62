f21580bc787b76bba239588260123c3af49d0fd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:06 +0000

    Add package ha-distributions/0.1/ha_distributions-0.1.tar.gz

---
+++ packages/ha-distributions/ha_distributions-0.1.tar.gz/ha_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ha_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['ha_distributions'],
+      author='Hanny',
+      author_email='hanny.aly@gmail.com',
+      zip_safe=False)
