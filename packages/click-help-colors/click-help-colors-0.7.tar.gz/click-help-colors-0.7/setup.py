f0c70d2d07aef7bc6a60b0ce56841684acae5a0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:26 +0000

    Add package click-help-colors/0.7/click-help-colors-0.7.tar.gz

---
+++ packages/click-help-colors/click-help-colors-0.7.tar.gz/click-help-colors-0.7/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import io
+import re
+from setuptools import setup
+
+
+with io.open("click_help_colors/__init__.py", "rt", encoding="utf8") as f:
+    version = re.search(r"__version__ = '(.*?)'", f.read()).group(1)
+
+
+setup(
+    name='click-help-colors',
+    version=version,
+    packages=['click_help_colors'],
+    description='Colorization of help messages in Click',
+    url='https://github.com/r-m-n/click-help-colors',
+    download_url='https://github.com/r-m-n/click-help-colors/archive/0.7.tar.gz',
+    keywords=['click'],
+    license='MIT',
+    install_requires=[
+        'click>=7.0'
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+        ]
+    }
+)
