130c2ad888ca8fa6b487d5697e07a15dc4e9e78c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:13 +0000

    Add package librair/2020.2.29/librair-2020.2.29-py3-none-any.whl

---
+++ packages/librair/librair-2020.2.29-py3-none-any.whl/librair/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from . import catalogs
+from . import interfaces
+from . import protocols
+from . import queries
+from . import schemas
+from . import utilities
