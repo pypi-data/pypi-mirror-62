c44774cefeb13fac964ef6203d4c2e43ae50e886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:17 +0000

    Add package mlgw/1.0.post1/mlgw-1.0.post1.tar.gz

---
+++ packages/mlgw/mlgw-1.0.post1.tar.gz/mlgw-1.0.post1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+setup(
+    name='mlgw',
+    version='1.0.post1',
+    author='S. Schmidt',
+    author_email='stefanoschmidt1995@gmail.com',
+    packages=['mlgw'],
+    package_dir = {'mlgw':'./mlgw'},
+    url="https://github.com/stefanoschmidt1995/MLGW/",
+    license='LICENSE.txt',
+    description='ML model for generating gravitational waveforms for a BBH coalescence',
+    long_description=open('README.txt').read(),
+    include_package_data = True,
+    package_data={'mlgw': ['TD_model/*']},
+    install_requires=[
+        "numpy >= 1.16.4",
+		"scipy >= 1.3.1",
+		"lalsuite >= 6.62"
+    ],
+)
+
