28880f6896f797ef02c88e8493bf27d5e472d88b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:48 +0000

    Add package azure-functions-worker/1.0.4/azure-functions-worker-1.0.4.tar.gz

---
+++ packages/azure-functions-worker/azure-functions-worker-1.0.4.tar.gz/azure-functions-worker-1.0.4/azure_functions_worker/constants.py
@@ -0,0 +1,8 @@
+# Capabilities
+RAW_HTTP_BODY_BYTES = "RawHttpBodyBytes"
+TYPED_DATA_COLLECTION = "TypedDataCollection"
+RPC_HTTP_BODY_ONLY = "RpcHttpBodyOnly"
+RPC_HTTP_TRIGGER_METADATA_REMOVED = "RpcHttpTriggerMetadataRemoved"
+
+# Feature Flags (app settings)
+PYTHON_ROLLBACK_CWD_PATH = "PYTHON_ROLLBACK_CWD_PATH"
