42ad404e4c760886d134e505ab07eca0b52e0138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:26 +0000

    Add package pbxproj/2.10.1/pbxproj-2.10.1.tar.gz

---
+++ packages/pbxproj/pbxproj-2.10.1.tar.gz/pbxproj-2.10.1/pbxproj/pbxsections/PBXContainerItemProxy.py
@@ -0,0 +1,17 @@
+from pbxproj import PBXGenericObject
+
+
+class PBXContainerItemProxy(PBXGenericObject):
+    @classmethod
+    def create(cls, file_ref, remote_ref, proxy_type=2):
+        return cls().parse({
+            '_id': cls._generate_id(),
+            'isa': cls.__name__,
+            'containerPortal': file_ref.get_id(),
+            'proxyType': proxy_type,
+            'remoteGlobalIDString': remote_ref.productReference,
+            'remoteInfo': remote_ref.productName
+        })
+
+    def _get_comment(self):
+        return 'PBXContainerItemProxy'
