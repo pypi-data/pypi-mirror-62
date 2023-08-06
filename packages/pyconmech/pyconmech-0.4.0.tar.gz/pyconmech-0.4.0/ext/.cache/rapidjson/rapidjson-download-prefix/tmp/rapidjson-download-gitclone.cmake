57e1872a3b6b338862dbb38a895977cd3089ab08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:40 +0000

    Add package pyconmech/0.4.0/pyconmech-0.4.0.tar.gz

---
+++ packages/pyconmech/pyconmech-0.4.0.tar.gz/pyconmech-0.4.0/ext/.cache/rapidjson/rapidjson-download-prefix/tmp/rapidjson-download-gitclone.cmake
@@ -0,0 +1,108 @@
+if("b16cec1a1aff158f5002250139528cf8a4b413d8" STREQUAL "")
+  message(FATAL_ERROR "Tag for git checkout should not be empty.")
+endif()
+
+set(run 0)
+
+if("C:/Users/yijiangh/Documents/pb_ws/conmech/ext/.cache/rapidjson/rapidjson-download-prefix/src/rapidjson-download-stamp/rapidjson-download-gitinfo.txt" IS_NEWER_THAN "C:/Users/yijiangh/Documents/pb_ws/conmech/ext/.cache/rapidjson/rapidjson-download-prefix/src/rapidjson-download-stamp/rapidjson-download-gitclone-lastrun.txt")
+  set(run 1)
+endif()
+
+if(NOT run)
+  message(STATUS "Avoiding repeated git clone, stamp file is up to date: 'C:/Users/yijiangh/Documents/pb_ws/conmech/ext/.cache/rapidjson/rapidjson-download-prefix/src/rapidjson-download-stamp/rapidjson-download-gitclone-lastrun.txt'")
+  return()
+endif()
+
+execute_process(
+  COMMAND ${CMAKE_COMMAND} -E remove_directory "C:/Users/yijiangh/Documents/pb_ws/conmech/cmake/../ext/rapidjson"
+  RESULT_VARIABLE error_code
+  )
+if(error_code)
+  message(FATAL_ERROR "Failed to remove directory: 'C:/Users/yijiangh/Documents/pb_ws/conmech/cmake/../ext/rapidjson'")
+endif()
+
+set(git_options)
+
+# disable cert checking if explicitly told not to do it
+set(tls_verify "")
+if(NOT "x" STREQUAL "x" AND NOT tls_verify)
+  list(APPEND git_options
+    -c http.sslVerify=false)
+endif()
+
+set(git_clone_options)
+
+set(git_shallow "")
+if(git_shallow)
+  list(APPEND git_clone_options --depth 1 --no-single-branch)
+endif()
+
+set(git_progress "")
+if(git_progress)
+  list(APPEND git_clone_options --progress)
+endif()
+
+set(git_config "advice.detachedHead=false")
+foreach(config IN LISTS git_config)
+  list(APPEND git_clone_options --config ${config})
+endforeach()
+
+# try the clone 3 times in case there is an odd git clone issue
+set(error_code 1)
+set(number_of_tries 0)
+while(error_code AND number_of_tries LESS 3)
+  execute_process(
+    COMMAND "F:/Git/cmd/git.exe" ${git_options} clone ${git_clone_options} --origin "origin" "https://github.com/Tencent/rapidjson.git" "rapidjson"
+    WORKING_DIRECTORY "C:/Users/yijiangh/Documents/pb_ws/conmech/cmake/../ext"
+    RESULT_VARIABLE error_code
+    )
+  math(EXPR number_of_tries "${number_of_tries} + 1")
+endwhile()
+if(number_of_tries GREATER 1)
+  message(STATUS "Had to git clone more than once:
+          ${number_of_tries} times.")
+endif()
+if(error_code)
+  message(FATAL_ERROR "Failed to clone repository: 'https://github.com/Tencent/rapidjson.git'")
+endif()
+
+execute_process(
+  COMMAND "F:/Git/cmd/git.exe" ${git_options} checkout b16cec1a1aff158f5002250139528cf8a4b413d8 --
+  WORKING_DIRECTORY "C:/Users/yijiangh/Documents/pb_ws/conmech/cmake/../ext/rapidjson"
+  RESULT_VARIABLE error_code
+  )
+if(error_code)
+  message(FATAL_ERROR "Failed to checkout tag: 'b16cec1a1aff158f5002250139528cf8a4b413d8'")
+endif()
+
+execute_process(
+  COMMAND "F:/Git/cmd/git.exe" ${git_options} submodule init 
+  WORKING_DIRECTORY "C:/Users/yijiangh/Documents/pb_ws/conmech/cmake/../ext/rapidjson"
+  RESULT_VARIABLE error_code
+  )
+if(error_code)
+  message(FATAL_ERROR "Failed to init submodules in: 'C:/Users/yijiangh/Documents/pb_ws/conmech/cmake/../ext/rapidjson'")
+endif()
+
+execute_process(
+  COMMAND "F:/Git/cmd/git.exe" ${git_options} submodule update --recursive --init 
+  WORKING_DIRECTORY "C:/Users/yijiangh/Documents/pb_ws/conmech/cmake/../ext/rapidjson"
+  RESULT_VARIABLE error_code
+  )
+if(error_code)
+  message(FATAL_ERROR "Failed to update submodules in: 'C:/Users/yijiangh/Documents/pb_ws/conmech/cmake/../ext/rapidjson'")
+endif()
+
+# Complete success, update the script-last-run stamp file:
+#
+execute_process(
+  COMMAND ${CMAKE_COMMAND} -E copy
+    "C:/Users/yijiangh/Documents/pb_ws/conmech/ext/.cache/rapidjson/rapidjson-download-prefix/src/rapidjson-download-stamp/rapidjson-download-gitinfo.txt"
+    "C:/Users/yijiangh/Documents/pb_ws/conmech/ext/.cache/rapidjson/rapidjson-download-prefix/src/rapidjson-download-stamp/rapidjson-download-gitclone-lastrun.txt"
+  WORKING_DIRECTORY "C:/Users/yijiangh/Documents/pb_ws/conmech/cmake/../ext/rapidjson"
+  RESULT_VARIABLE error_code
+  )
+if(error_code)
+  message(FATAL_ERROR "Failed to copy script-last-run stamp file: 'C:/Users/yijiangh/Documents/pb_ws/conmech/ext/.cache/rapidjson/rapidjson-download-prefix/src/rapidjson-download-stamp/rapidjson-download-gitclone-lastrun.txt'")
+endif()
+
