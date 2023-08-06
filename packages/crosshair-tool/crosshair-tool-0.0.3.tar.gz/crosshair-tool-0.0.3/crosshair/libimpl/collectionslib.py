446c31c07eab77afc49b8d4731f5b43da53a2274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:46 +0000

    Add package crosshair-tool/0.0.3/crosshair-tool-0.0.3.tar.gz

---
+++ packages/crosshair-tool/crosshair-tool-0.0.3.tar.gz/crosshair-tool-0.0.3/crosshair/libimpl/collectionslib.py
@@ -0,0 +1,51 @@
+import collections
+from typing import *
+
+from crosshair import register_type
+
+T = TypeVar('T')
+class ListBasedDeque:
+    def __init__(self, contents: List[T], maxlen: Optional[int]=None):
+        self._contents = contents
+        self._maxlen = maxlen
+    def _has_room(self) -> bool:
+        maxlen = self._maxlen
+        return maxlen is None or len(self._contents) < maxlen
+    def append_left(self, item: T):
+        if self._has_room():
+            self._contents = [item] + self._contents
+    # TODO: implement remainder of deque ...
+
+
+def make_registrations():
+    # NOTE: defaultdict could be symbolic (but note the default_factory is changable/stateful):
+    register_type(collections.defaultdict, lambda p, kt=Any, vt=Any: collections.defaultdict(p(Callable[[], vt]), p(Dict[kt, vt]))) # type: ignore
+    register_type(collections.ChainMap, lambda p, kt=Any, vt=Any: collections.ChainMap(*p(Tuple[Dict[kt, vt], ...]))) # type: ignore
+    register_type(collections.abc.Mapping, lambda p, kt=Any, vt=Any: p(Dict[kt, vt]))  # type: ignore
+    register_type(collections.abc.MutableMapping, lambda p, kt=Any, vt=Any: p(Dict[kt, vt]))  # type: ignore
+    register_type(collections.OrderedDict, lambda p, kt=Any, vt=Any: collections.OrderedDict(p(Dict[kt, vt]))) # type: ignore
+    register_type(collections.Counter, lambda p, t=Any: collections.Counter(p(Dict[t, int]))) # type: ignore
+    # TODO: MappingView is missing
+    register_type(collections.abc.ItemsView, lambda p, kt=Any, vt=Any: p(Set[Tuple[kt, vt]]))  # type: ignore
+    register_type(collections.abc.KeysView, lambda p, t=Any: p(Set[t]))  # type: ignore
+    register_type(collections.abc.ValuesView, lambda p, t=Any: p(Set[t]))  # type: ignore
+
+    register_type(collections.abc.Container, lambda p, t=Any: p(Tuple[t, ...]))
+    register_type(collections.abc.Collection, lambda p, t=Any: p(Tuple[t, ...]))
+
+    register_type(collections.deque, lambda p, t=Any: collections.deque(p(Tuple[t, ...])))
+    # TODO: When ready, use custom deque class instead:
+    # register_type(collections.deque, lambda p, t=Any: ListBasedDeque(p(List[t, ...]))) # type: ignore
+
+    register_type(collections.abc.Iterable, lambda p, t=Any: p(Tuple[t, ...]))
+    register_type(collections.abc.Iterator, lambda p, t=Any: iter(p(Iterable[t])))  # type: ignore
+
+    register_type(collections.abc.MutableSequence, lambda p, t=Any: p(List[t]))  # type: ignore
+    register_type(collections.abc.Reversible, lambda p, t=Any: p(Tuple[t, ...]))
+    register_type(collections.abc.Sequence, lambda p, t=Any: p(Tuple[t, ...]))
+    register_type(collections.abc.Sized, lambda p, t=Any: p(Tuple[t, ...]))
+
+    register_type(collections.abc.MutableSet, lambda p, t=Any: p(Set[t]))  # type: ignore
+
+    register_type(collections.abc.ByteString, lambda p: bytes(b % 256 for b in p(List[int])))
+    register_type(collections.abc.Hashable, lambda p: p(int))
---
+++ packages/crosshair-tool/crosshair-tool-0.0.3.tar.gz/crosshair-tool-0.0.3/crosshair/libimpl/datetimelib.py
@@ -0,0 +1,35 @@
+import datetime
+from crosshair import register_patch, register_type
+from crosshair import realize, with_realized_args, IgnoreAttempt
+from typing import Callable
+
+def make_registrations():
+
+    def make_date(p: Callable) -> datetime.date:
+        year, month, day = p(int), p(int), p(int)
+        # This condition isn't technically required, but it develops useful
+        # symbolic inequalities before the realization below:
+        if not (1 <= year <= 9999 and 1 <= month <= 12 and 1 <= day <= 31):
+            raise IgnoreAttempt('Invalid date')
+        try:
+            return datetime.date(realize(year), realize(month), realize(day))
+        except ValueError:
+            raise IgnoreAttempt('Invalid date')
+
+    register_type(datetime.date, make_date)
+    
+    def make_timedelta(p: Callable) -> datetime.timedelta:
+        microseconds, seconds, days = p(int), p(int), p(int)
+        # the normalized ranges, per the docs:
+        if not(0 <= microseconds < 1000000 and
+               0 <= seconds < 3600*24 and
+               -999999999 <= days <= 999999999):
+            raise IgnoreAttempt('Invalid timedelta')
+        try:
+            return datetime.timedelta(
+                days=realize(days), seconds=realize(seconds),
+                microseconds=realize(microseconds))
+        except OverflowError:
+            raise IgnoreAttempt('Invalid timedelta')
+
+    register_type(datetime.timedelta, make_timedelta)
