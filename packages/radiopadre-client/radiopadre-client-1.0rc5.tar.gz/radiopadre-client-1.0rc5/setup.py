edb4525fb79359f3ec5904813c8289ea88d17ced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:49 +0000

    Add package radiopadre-client/1.0rc5/radiopadre-client-1.0rc5.tar.gz

---
+++ packages/radiopadre-client/radiopadre-client-1.0rc5.tar.gz/radiopadre-client-1.0rc5/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+import os
+
+__version__ = "1.0-pre5"
+build_root = os.path.dirname(__file__)
+
+with open("requirements.txt") as stdr:
+    install_requires = stdr.readlines()
+
+def readme():
+    """Get readme content for package long description"""
+    with open(os.path.join(build_root, 'README.rst')) as f:
+        return f.read()
+
+scripts = ["bin/" + i for i in os.listdir("bin")]
+
+setup(
+    name="radiopadre-client",
+    version=__version__,
+    install_requires=install_requires,
+    python_requires='>=2.7',
+    author="Oleg Smirnov",
+    author_email="osmirnov@gmail.com",
+    description=("Radiopadre client-side script"),
+    long_description=readme(),
+    license="MIT",
+    keywords="ipython notebook fits dataset resultset visualisation",
+    url="http://github.com/ratt-ru/radiopadre-client",
+    packages=['radiopadre_client', 'radiopadre_client.backends', 'iglesia'],
+    scripts=scripts,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
