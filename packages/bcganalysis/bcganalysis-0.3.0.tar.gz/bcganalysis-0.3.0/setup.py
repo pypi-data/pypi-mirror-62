907e78e279ba96bf81f9c858d880e553eaf5256e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:33 +0000

    Add package bcganalysis/0.3.0/bcganalysis-0.3.0.tar.gz

---
+++ packages/bcganalysis/bcganalysis-0.3.0.tar.gz/bcganalysis-0.3.0/setup.py
@@ -0,0 +1,15 @@
+# setup.py
+
+import io
+from setuptools import setup, find_packages
+
+setup(
+    name='bcganalysis',
+    version='0.3.0',
+    author='Matias Eiletz',
+    author_email='mat.eil1991@gmail.com',
+    description='Builds BCG Matrix',
+    keywords='BCG Growth Share Matrix Feature Importance',
+    packages = ['bcg_analysis'],
+    url='https://github.com/mateil04/bcg_analysis'
+)
