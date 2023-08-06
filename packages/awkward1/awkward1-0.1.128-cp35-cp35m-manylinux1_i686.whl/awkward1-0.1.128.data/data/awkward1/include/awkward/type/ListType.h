1965de9e5ec8ebead9e6c5c0b99942cf88aaf5df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:28 +0000

    Add package awkward1/0.1.128/awkward1-0.1.128-cp35-cp35m-manylinux1_i686.whl

---
+++ packages/awkward1/awkward1-0.1.128-cp35-cp35m-manylinux1_i686.whl/awkward1-0.1.128.data/data/awkward1/include/awkward/type/ListType.h
@@ -0,0 +1,30 @@
+// BSD 3-Clause License; see https://github.com/jpivarski/awkward-1.0/blob/master/LICENSE
+
+#ifndef AWKWARD_LISTTYPE_H_
+#define AWKWARD_LISTTYPE_H_
+
+#include "awkward/type/Type.h"
+
+namespace awkward {
+  class ListType: public Type {
+  public:
+    ListType(const util::Parameters& parameters, const std::shared_ptr<Type>& type);
+
+    std::string tostring_part(const std::string& indent, const std::string& pre, const std::string& post) const override;
+    const std::shared_ptr<Type> shallow_copy() const override;
+    bool equal(const std::shared_ptr<Type>& other, bool check_parameters) const override;
+    int64_t numfields() const override;
+    int64_t fieldindex(const std::string& key) const override;
+    const std::string key(int64_t fieldindex) const override;
+    bool haskey(const std::string& key) const override;
+    const std::vector<std::string> keys() const override;
+    const std::shared_ptr<Content> empty() const override;
+
+  const std::shared_ptr<Type> type() const;
+
+  private:
+    const std::shared_ptr<Type> type_;
+  };
+}
+
+#endif // AWKWARD_LISTTYPE_H_
