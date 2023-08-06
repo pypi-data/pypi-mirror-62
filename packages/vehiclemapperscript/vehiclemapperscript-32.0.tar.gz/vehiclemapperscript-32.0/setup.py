fc06661971139d1db67232cc1246c5bc4cd0d864
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:03 +0000

    Add package vehiclemapperscript/32.0/vehiclemapperscript-32.0.tar.gz

---
+++ packages/vehiclemapperscript/vehiclemapperscript-32.0.tar.gz/vehiclemapperscript-32.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'readme.md'), encoding='utf-8') as f:
+    long_description = f.read()
+setup(name='vehiclemapperscript',
+      version='32.0',
+      description='Vehicle Mapper',
+      url='',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='yzcop',
+      author_email='yadugkrishnan@gmail.com',
+      license='MIT',
+      install_requires=['pandas','fuzzywuzzy','python-Levenshtein','xlsxwriter'],
+      packages=['vehiclemapperscript'],
+      zip_safe=False)
