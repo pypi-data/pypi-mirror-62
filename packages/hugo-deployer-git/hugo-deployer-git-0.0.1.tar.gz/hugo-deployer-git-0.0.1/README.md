970a83cf9e7e72154f2b0adc4a8cae9730739eef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:17 +0000

    Add package hugo-deployer-git/0.0.1/hugo-deployer-git-0.0.1.tar.gz

---
+++ packages/hugo-deployer-git/hugo-deployer-git-0.0.1.tar.gz/hugo-deployer-git-0.0.1/README.md
@@ -0,0 +1,31 @@
+# hugo-deployer-git
+
+Deploy your hugo site with git using one command
+
+Inspired by [hexo-deployer-git](https://github.com/hexojs/hexo-deployer-git).
+
+## Installation
+
+`pip3 install hugo-deployer-git`
+
+## Setup
+
+In your `config.toml`, put in this section
+
+```toml
+[deploy]
+    # required field
+    repo = "<your github page repo>"
+    branch =  "master"
+
+    # optional_commit message, default to build time
+    message = ""
+
+    name= "testuser"
+    email= "test@testtest.com"
+    build-config= "--minify"
+```
+
+## How to use
+
+In your hugo website folder, use `hugo-deployer-git`.
---
+++ packages/hugo-deployer-git/hugo-deployer-git-0.0.1.tar.gz/hugo-deployer-git-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="hugo-deployer-git",
+    version="0.0.1",
+    license='MIT',
+    url='https://github.com/ianre657/hugo-deployer-git',
+    packages=find_packages( exclude="test-site"),
+    author="Ian Chen",
+    author_email="ianre657@gmail.com",
+    description="Deploy your hugo site with github pages using one command",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords= ["hugo", "github pages", "deploy", "git", "blogger", "utility", "static site"],
+    classifiers=[
+      "Programming Language :: Python :: 3",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+      "Development Status :: 2 - Pre-Alpha",
+      "Topic :: Utilities",
+    ],
+    entry_points={
+      'console_scripts':[
+        'hugo-deployer-git=hugo_deployer_git.cli.__main__:main'
+      ]
+    },
+)
