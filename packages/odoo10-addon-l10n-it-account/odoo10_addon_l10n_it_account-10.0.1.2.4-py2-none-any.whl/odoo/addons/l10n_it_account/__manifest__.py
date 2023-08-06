608899d675dd6a613aeeef8c122fc65f788fd3dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:42 +0000

    Add package odoo10-addon-l10n-it-account/10.0.1.2.4/odoo10_addon_l10n_it_account-10.0.1.2.4-py2-none-any.whl

---
+++ packages/odoo10-addon-l10n-it-account/odoo10_addon_l10n_it_account-10.0.1.2.4-py2-none-any.whl/odoo/addons/l10n_it_account/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015 Abstract srl (<http://www.abstract.it>)
+# Copyright 2015-2017 Agile Business Group (<http://www.agilebg.com>)
+# Copyright 2015 Link It Spa (<http://www.linkgroup.it/>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Italian Localization - Account',
+    'version': '10.0.1.2.4',
+    'category': 'Hidden',
+    'author': "Agile Business Group, Abstract, "
+              "Odoo Community Association (OCA)",
+    'website': 'http://www.odoo-italia.net',
+    'license': 'AGPL-3',
+    "depends": [
+        'account_fiscal_year',
+    ],
+    "data": [
+        'views/account_setting.xml',
+        'views/account_view.xml',
+        'reports/account_reports_view.xml',
+    ],
+    'installable': True,
+}
