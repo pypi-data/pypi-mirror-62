f0877b344facb0e8b3a9fe08e27da98886537b5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:09 +0000

    Add package alexapy/1.5.1/AlexaPy-1.5.1.tar.gz

---
+++ packages/alexapy/AlexaPy-1.5.1.tar.gz/AlexaPy-1.5.1/alexapy/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#  SPDX-License-Identifier: Apache-2.0
+"""
+Python Package for controlling Alexa devices (echo dot, etc) programmatically.
+
+For more details about this api, please refer to the documentation at
+https://gitlab.com/keatontaylor/alexapy
+"""
+from .__version__ import __version__
+from .alexaapi import AlexaAPI
+from .alexalogin import AlexaLogin
+from .alexawebsocket import WebsocketEchoClient
+from .errors import AlexapyConnectionError, AlexapyLoginError
+from .helpers import hide_email, hide_serial
+
+__all__ = [
+    "AlexaLogin",
+    "AlexaAPI",
+    "AlexapyConnectionError",
+    "AlexapyLoginError",
+    "WebsocketEchoClient",
+    "hide_email",
+    "hide_serial",
+    "__version__",
+]
