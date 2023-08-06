7785d2d29aa231ae71575e0622603eaa4ffaf106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:42 +0000

    Add package monilog/0.1.0/monilog-0.1.0.tar.gz

---
+++ packages/monilog/monilog-0.1.0.tar.gz/monilog-0.1.0/monilog/__init__.py
@@ -0,0 +1,15 @@
+from monilog.monitoring import MonilogPipeline
+from monilog.log_generator import LogGenerator
+from monilog.parser import Parser
+from monilog.statistics import Statistics
+
+
+__name__ = "monilog"
+__version__ = "0.1.0"
+
+__all__ = [
+    'MonilogPipeline',
+    'LogGenerator',
+    'Parser',
+    'Statistics'
+]
---
+++ packages/monilog/monilog-0.1.0.tar.gz/monilog-0.1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+import monilog
+
+with open('README.md') as f:
+    long_description = f.read()
+
+
+setup(name='monilog',
+      version='0.1.0',
+      description='HTTP log monitoring',
+      long_description=long_description,
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+      ],
+      keywords='HTTP log monitoring',
+      url='https://tayciryahmed.github.io/monilog/',
+      author='Taycir Yahmed',
+      author_email='taycir.yahmed@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      entry_points={
+          'console_scripts': ['monitoring=monilog.bin.run_monitoring:run',
+                              'log_generator=monilog.bin.run_log_generator:run'],
+      },
+      zip_safe=False)
