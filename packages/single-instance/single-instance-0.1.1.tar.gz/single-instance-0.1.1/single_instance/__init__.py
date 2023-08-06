73d8cd9e88d3959667c627248f8612726bd03156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:57 +0000

    Add package single-instance/0.1.1/single-instance-0.1.1.tar.gz

---
+++ packages/single-instance/single-instance-0.1.1.tar.gz/single-instance-0.1.1/single_instance/__init__.py
@@ -0,0 +1,104 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2020 fx-kirin <fx.kirin@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+
+import logging
+import os
+import sys
+import tempfile
+from multiprocessing import Process
+
+import kanilog
+
+if sys.platform != "win32":
+    import fcntl
+
+__version__ = "0.1.1"
+
+
+class SingleInstanceException(BaseException):
+    pass
+
+
+logger = kanilog.get_module_logger(__file__,)
+
+
+class SingleInstance(object):
+    def __init__(self, flavor_id="", lockfile=""):
+        self.initialized = False
+        if lockfile:
+            self.lockfile = lockfile
+        else:
+            basename = (
+                os.path.splitext(os.path.abspath(sys.argv[0]))[0]
+                .replace("/", "-")
+                .replace(":", "")
+                .replace("\\", "-")
+                + "-%s" % flavor_id
+                + ".lock"
+            )
+            self.lockfile = os.path.normpath(tempfile.gettempdir() + "/" + basename)
+
+        logger.debug("SingleInstance lockfile: " + self.lockfile)
+        if sys.platform == "win32":
+            try:
+                # file already exists, we try to remove (in case previous
+                # execution was interrupted)
+                if os.path.exists(self.lockfile):
+                    os.unlink(self.lockfile)
+                self.fd = os.open(self.lockfile, os.O_CREAT | os.O_EXCL | os.O_RDWR)
+            except OSError:
+                type, e, tb = sys.exc_info()
+                if e.errno == 13:
+                    logger.error("Another instance is already running, quitting.")
+                    raise SingleInstanceException()
+                print(e.errno)
+                raise
+        else:  # non Windows
+            self.fp = open(self.lockfile, "w")
+            self.fp.flush()
+            try:
+                fcntl.lockf(self.fp, fcntl.LOCK_EX | fcntl.LOCK_NB)
+            except IOError:
+                logger.warning("Another instance is already running, quitting.")
+                raise SingleInstanceException()
+        self.initialized = True
+
+    def __del__(self):
+        if not self.initialized:
+            return
+        try:
+            if sys.platform == "win32":
+                if hasattr(self, "fd"):
+                    os.close(self.fd)
+                    os.unlink(self.lockfile)
+            else:
+                fcntl.lockf(self.fp, fcntl.LOCK_UN)
+                # os.close(self.fp)
+                if os.path.isfile(self.lockfile):
+                    os.unlink(self.lockfile)
+        except Exception as e:
+            if logger:
+                logger.warning(e)
+            else:
+                print("Unloggable error: %s" % e)
+            sys.exit(-1)
+
+
+def f(name):
+    tmp = logger.level
+    logger.setLevel(logging.CRITICAL)  # we do not want to see the warning
+    try:
+        me2 = SingleInstance(flavor_id=name)  # noqa
+    except SingleInstanceException:
+        sys.exit(-1)
+    logger.setLevel(tmp)
+    pass
