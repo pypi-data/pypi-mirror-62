fc23547858c4dcee3edf84c161260f82757e8c22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:16 +0000

    Add package pygrading/0.2.6/pygrading-0.2.6-py3-none-any.whl

---
+++ packages/pygrading/pygrading-0.2.6-py3-none-any.whl/pygrading/__init__.py
@@ -0,0 +1,19 @@
+"""
+    Name: __init__.py
+    Author: Charles Zhang <694556046@qq.com>
+    Propose: __init__ for pygrading.
+    Coding: UTF-8
+
+    Change Log:
+        **2020.02.09**
+        Add version()!
+
+        **2020.01.26**
+        Create this file!
+"""
+
+__version__ = "0.2.6"
+
+
+def version():
+    print(__version__)
