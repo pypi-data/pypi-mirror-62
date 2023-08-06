fa183fe02bcdfb5fda3ec1c9572aec81eccdba62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:55 +0000

    Add package colab-ssh/0.1.3/colab_ssh-0.1.3-py3-none-any.whl

---
+++ packages/colab-ssh/colab_ssh-0.1.3-py3-none-any.whl/colab_ssh/init_git.py
@@ -0,0 +1,7 @@
+from colab_ssh._command import run_command as _run_command
+
+def init_git(repositoryUrl, branch="master"):
+    _run_command("git clone {} .".format(repositoryUrl))
+    _run_command("git checkout {}".format(branch))
+
+    print("Successfully cloned the repository")
