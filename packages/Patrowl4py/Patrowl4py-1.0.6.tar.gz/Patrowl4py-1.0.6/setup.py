0d63b0b75ff79599b280943604cdba5860f6727c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:21 +0000

    Add package Patrowl4py/1.0.6/Patrowl4py-1.0.6.tar.gz

---
+++ packages/Patrowl4py/Patrowl4py-1.0.6.tar.gz/Patrowl4py-1.0.6/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Setup file."""
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Patrowl4py',
+    version='1.0.6',
+    description='Python API client for Patrowl',
+    url='https://github.com/Patrowl/Patrowl4py',
+    author='Nicolas Mattiocco',
+    author_email='getsupport@patrowl.io',
+    license='AGPLv3',
+    packages=find_packages(),
+)
