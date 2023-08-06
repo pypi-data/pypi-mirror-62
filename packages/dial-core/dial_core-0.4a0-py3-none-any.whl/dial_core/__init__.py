2e8bf4d429ab44fa140f18e03f6eb20356233764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:21 +0000

    Add package dial-core/0.4a0/dial_core-0.4a0-py3-none-any.whl

---
+++ packages/dial-core/dial_core-0.4a0-py3-none-any.whl/dial_core/__init__.py
@@ -0,0 +1,16 @@
+# vim: ft=python fileencoding=utf-8 sts=4 sw=4 et:
+
+"""A Deep Learning, node-based framework"""
+
+__author__ = "David Afonso"
+__license__ = "GPL"
+__maintainer__ = __author__
+__url__ = "https://github.com/dial-app/dial-core"
+__version__ = "0.4a0"
+__description__ = "A Deep Learning, node-based framework"
+
+__requirements__ = [
+    ("tensorflow", ">=2.0.0a0"),
+    ("tensorboard", ">=2.0.1"),
+    ("dependency-injector", ">=3.14.12"),
+]
