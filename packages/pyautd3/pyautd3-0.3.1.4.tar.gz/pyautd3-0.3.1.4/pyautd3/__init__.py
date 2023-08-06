d4c63a27672d8fac574e4514115e2fa189ef6f31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:43 +0000

    Add package pyautd3/0.3.1.4/pyautd3-0.3.1.4.tar.gz

---
+++ packages/pyautd3/pyautd3-0.3.1.4.tar.gz/pyautd3-0.3.1.4/pyautd3/__init__.py
@@ -0,0 +1,37 @@
+'''
+File: __init__.py
+Project: pyautd
+Created Date: 11/02/2020
+Author: Shun Suzuki
+-----
+Last Modified: 28/02/2020
+Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
+-----
+Copyright (c) 2020 Hapis Lab. All rights reserved.
+
+'''
+
+from pyautd3.autd import LinkType
+from pyautd3.nativemethods import init_autd3
+from pyautd3.autd import AUTD
+import os.path
+import platform
+
+pf = platform.system()
+prefix = ''
+ext = ''
+if pf == 'Windows':
+    ext = '.dll'
+elif pf == 'Darwin':
+    prefix = 'lib'
+    ext = '.dylib'
+elif pf == 'Linux':
+    prefix = 'lib'
+    ext = '.so'
+
+__all__ = ['LinkType', 'AUTD']
+__version__ = '0.3.1.4'
+
+lib_path = os.path.join(os.path.dirname(__file__),
+                        'bin', prefix + 'autd3capi' + ext)
+init_autd3(lib_path)
