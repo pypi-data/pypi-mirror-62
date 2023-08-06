5e7dbddcb069e5bd3f561a55e3fc28bb88263a0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:46 +0000

    Add package mergexp/0.2.1/mergexp-0.2.1.tar.gz

---
+++ packages/mergexp/mergexp-0.2.1.tar.gz/mergexp-0.2.1/mergexp/net/elements.py
@@ -0,0 +1,8 @@
+from .. import ConstraintGenerator
+
+capacity = ConstraintGenerator("capacity")
+latency = ConstraintGenerator("latency")
+
+# Routing is a reticulator. "static" may be a type of routing.
+routing = ConstraintGenerator("routing")
+static = "static"
