8cc55642bd7956fa236a251291d24cb79f2701a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:32 +0000

    Add package awkward1/0.1.128/awkward1-0.1.128-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/awkward1/awkward1-0.1.128-cp36-cp36m-manylinux1_x86_64.whl/awkward1-0.1.128.data/data/awkward1/include/awkward/type/RegularType.h
@@ -0,0 +1,34 @@
+// BSD 3-Clause License; see https://github.com/jpivarski/awkward-1.0/blob/master/LICENSE
+
+#ifndef AWKWARD_REGULARTYPE_H_
+#define AWKWARD_REGULARTYPE_H_
+
+#include <vector>
+
+#include "awkward/type/Type.h"
+
+namespace awkward {
+  class RegularType: public Type {
+  public:
+    RegularType(const util::Parameters& parameters, const std::shared_ptr<Type>& type, int64_t size);
+
+    std::string tostring_part(const std::string& indent, const std::string& pre, const std::string& post) const override;
+    const std::shared_ptr<Type> shallow_copy() const override;
+    bool equal(const std::shared_ptr<Type>& other, bool check_parameters) const override;
+    int64_t numfields() const override;
+    int64_t fieldindex(const std::string& key) const override;
+    const std::string key(int64_t fieldindex) const override;
+    bool haskey(const std::string& key) const override;
+    const std::vector<std::string> keys() const override;
+    const std::shared_ptr<Content> empty() const override;
+
+    const std::shared_ptr<Type> type() const;
+    int64_t size() const;
+
+  private:
+    const std::shared_ptr<Type> type_;
+    const int64_t size_;
+  };
+}
+
+#endif // AWKWARD_REGULARTYPE_H_
