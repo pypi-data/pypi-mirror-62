8e72b6fdc13fb658653cc36e9ab9880f59dcbee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:10 +0000

    Add package pyscal/0.4.0/pyscal-0.4.0-py3-none-any.whl

---
+++ packages/pyscal/pyscal-0.4.0-py3-none-any.whl/pyscal/__init__.py
@@ -0,0 +1,20 @@
+"""A module for creating relative permeability input curves for
+Eclipse and Nexus.
+"""
+
+from __future__ import division, absolute_import
+from __future__ import print_function
+
+
+from .utils import interpolator  # noqa
+from .wateroil import WaterOil  # noqa
+from .wateroilgas import WaterOilGas  # noqa
+from .gasoil import GasOil  # noqa
+from .scalrecommendation import SCALrecommendation  # noqa
+from .pyscallist import PyscalList  # noqa
+from .factory import PyscalFactory  # noqa
+
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
