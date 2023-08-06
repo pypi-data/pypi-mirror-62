8a1a93657bfd3d9d84aabd93e797ef9e8fca70d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:15 +0000

    Add package cbitstruct/1.0.7/cbitstruct-1.0.7-cp37-cp37m-macosx_10_12_x86_64.whl

---
+++ packages/cbitstruct/cbitstruct-1.0.7-cp37-cp37m-macosx_10_12_x86_64.whl/cbitstruct/__init__.py
@@ -0,0 +1,19 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from ._cbitstruct import (
+    pack,
+    pack_into,
+    pack_dict,
+    pack_into_dict,
+    unpack,
+    unpack_from,
+    unpack_dict,
+    unpack_from_dict,
+    calcsize,
+    byteswap,
+    compile,
+    CompiledFormat,
+    CompiledFormatDict,
+)
