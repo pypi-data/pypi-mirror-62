b1befe83d951d7edcb6b6e92ee4cf710c13bcfff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:49 +0000

    Add package neathgeohash/0.4.0/neathgeohash-0.4.0.tar.gz

---
+++ packages/neathgeohash/neathgeohash-0.4.0.tar.gz/neathgeohash-0.4.0/neathgeohash/__init__.py
@@ -0,0 +1,17 @@
+"""
+.. module:: neathgeohash
+   :platform: Unix, Windows
+   :synopsis: A module for converting EEP95 to geohash coverage
+
+.. moduleauthor:: Marek Dwulit <marek.dwulit@gmail.com>
+
+
+"""
+
+from .Ellipse import Ellipse
+
+__author__ = 'mpdwulit'
+
+__all__ = [
+    'Ellipse'
+]
