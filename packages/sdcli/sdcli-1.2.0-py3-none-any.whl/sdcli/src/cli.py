e1ec915a767ca53eb4a2eebf5bd60ab30e9a0807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:19 +0000

    Add package sdcli/1.2.0/sdcli-1.2.0-py3-none-any.whl

---
+++ packages/sdcli/sdcli-1.2.0-py3-none-any.whl/sdcli/src/cli.py
@@ -0,0 +1,33 @@
+import click
+import colorama
+
+from sdcli.src.lib import info
+from sdcli.src.lib import generator as creator
+from sdcli.src.lib.error import print_output_error
+
+
+@click.group()
+def cli():
+    '''
+    CLI create by Streamelopers for generate our configuration on OBS.
+    '''
+
+    pass
+
+@cli.command(help='For generate new config file.')
+# @click.option('--event', prompt='Type of event: ', help='Specificate type of event.')
+@click.option('--name', '-n', nargs=1, default='speaking', required=True,
+              prompt='Name of Scenes Collection', help='Name Scnenes Collection')
+def generate(name):
+    try:
+        creator._generator(name)
+    except Exception as ex:
+        print_output_error(message='Error while creating config file...', ex=str(ex))
+
+@cli.command(help='Print information of our social medias.')
+def information():
+    info._info()
+
+@cli.command(help='Print version of SGen.')
+def version():
+    info.version()
