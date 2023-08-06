acbd8a9b321f43fe14725015f6fdea3f2ad5b61a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:32 +0000

    Add package hazelib/0.0.12/hazelib-0.0.12.tar.gz

---
+++ packages/hazelib/hazelib-0.0.12.tar.gz/hazelib-0.0.12/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="hazelib",
+    version="0.0.12",
+    packages=["src"],
+    description='My utility package.',
+    url='http://github.com/hazelpy/hazelib',
+    author='Hazelpy',
+    author_email='hazelpytv@gmail.com',
+    license='MIT',
+    zip_safe=False)
