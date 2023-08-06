251212f24e241f667833b97d405a41156241dc1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:37 +0000

    Add package carnival/0.10/carnival-0.10.tar.gz

---
+++ packages/carnival/carnival-0.10.tar.gz/carnival-0.10/carnival/cmd/__init__.py
@@ -0,0 +1,17 @@
+from carnival.cmd import (
+    cli,
+    system,
+    systemd,
+    apt,
+    transfer,
+    fs,
+)
+
+__all__ = [
+    'cli',
+    'system',
+    'systemd',
+    'apt',
+    'transfer',
+    'fs',
+]
