4a83c729a706935849eae04440732236c889040c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:26 +0000

    Add package iconify/0.0.80/iconify-0.0.80-py3-none-any.whl

---
+++ packages/iconify/iconify-0.0.80-py3-none-any.whl/iconify/browser.py
@@ -0,0 +1,197 @@
+"""
+A browser for exploring the available images and possible options.
+"""
+
+import sys
+from typing import Any, NoReturn, Optional
+
+import iconify as ico
+from iconify.qt import QtCore, QtGui, QtWidgets
+
+VIEW_COLUMNS = 5
+AUTO_SEARCH_TIMEOUT = 500
+
+
+class Browser(QtWidgets.QMainWindow):
+
+    def __init__(self, parent=None):
+        # type: (Optional[QtWidgets.QWidget]) -> None
+        super(Browser, self).__init__(parent=parent)
+        self.setMinimumSize(400, 300)
+        self.setWindowTitle('Iconify Browser')
+
+        iconNames = ico.path.listIcons()
+
+        self._filterTimer = QtCore.QTimer(self)
+        self._filterTimer.setSingleShot(True)
+        self._filterTimer.setInterval(AUTO_SEARCH_TIMEOUT)
+        self._filterTimer.timeout.connect(self._updateFilter)
+
+        model = Model()
+        model.setStringList(sorted(iconNames))
+
+        self._proxyModel = QtCore.QSortFilterProxyModel()
+        self._proxyModel.setSourceModel(model)
+        self._proxyModel.setFilterCaseSensitivity(QtCore.Qt.CaseInsensitive)
+
+        self._listView = View(self)
+        self._listView.setUniformItemSizes(True)
+        self._listView.setViewMode(QtWidgets.QListView.IconMode)
+        self._listView.setModel(self._proxyModel)
+        self._listView.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+        self._listView.doubleClicked.connect(self._copyIconText)
+
+        self._lineEdit = QtWidgets.QLineEdit(self)
+        self._lineEdit.setAlignment(QtCore.Qt.AlignCenter)
+        self._lineEdit.textChanged.connect(self._triggerDelayedUpdate)
+        self._lineEdit.returnPressed.connect(self._triggerImmediateUpdate)
+
+        lyt = QtWidgets.QHBoxLayout()
+        lyt.setContentsMargins(0, 0, 0, 0)
+        lyt.addWidget(self._lineEdit)
+
+        searchBarFrame = QtWidgets.QFrame(self)
+        searchBarFrame.setLayout(lyt)
+
+        self._copyButton = QtWidgets.QPushButton('Copy Name', self)
+        self._copyButton.clicked.connect(self._copyIconText)
+
+        lyt = QtWidgets.QVBoxLayout()
+        lyt.addWidget(searchBarFrame)
+        lyt.addWidget(self._listView)
+        lyt.addWidget(self._copyButton)
+
+        frame = QtWidgets.QFrame(self)
+        frame.setLayout(lyt)
+
+        self.setCentralWidget(frame)
+
+        QtWidgets.QShortcut(
+            QtGui.QKeySequence(QtCore.Qt.Key_Return),
+            self,
+            self._copyIconText,
+        )
+
+        self._lineEdit.setFocus()
+
+        geo = self.geometry()
+        desktop = QtWidgets.QApplication.desktop()
+        screen = desktop.screenNumber(desktop.cursor().pos())
+        centerPoint = desktop.screenGeometry(screen).center()
+        geo.moveCenter(centerPoint)
+        self.setGeometry(geo)
+
+    def _updateFilter(self):
+        # type: () -> None
+        """
+        Update the string used for filtering in the proxy model with the
+        current text from the line edit.
+        """
+        searchTerm = self._lineEdit.text()
+        if searchTerm:
+            reString = ".*%s.*$" % searchTerm
+        else:
+            reString = ""
+
+        self._proxyModel.setFilterRegExp(reString)
+
+    def _triggerDelayedUpdate(self):
+        # type: () -> None
+        """
+        Reset the timer used for committing the search term to the proxy model.
+        """
+        self._filterTimer.stop()
+        self._filterTimer.start()
+
+    def _triggerImmediateUpdate(self):
+        # type: () -> None
+        """
+        Stop the timer used for committing the search term and update the
+        proxy model immediately.
+        """
+        self._filterTimer.stop()
+        self._updateFilter()
+
+    def _copyIconText(self):
+        # type: () -> None
+        """
+        Copy the name of the currently selected icon to the clipboard.
+        """
+        indexes = self._listView.selectedIndexes()
+        if not indexes:
+            return
+
+        clipboard = QtWidgets.QApplication.instance().clipboard()
+        clipboard.setText(indexes[0].data())
+
+
+class View(QtWidgets.QListView):
+    """
+    A QListView that scales it's grid size to ensure the same number of
+    columns are always drawn.
+    """
+
+    def __init__(self, parent=None):
+        # type: (Optional[QtWidgets.QWidget]) -> None
+        super(View, self).__init__(parent)
+        self.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
+
+    def resizeEvent(self, event):
+        # type: (QtCore.QEvent) -> bool
+        """
+        Re-implemented to re-calculate the grid size to provide scaling icons
+
+        Parameters
+        ----------
+        event : QtCore.QEvent
+        """
+        width = self.viewport().width() - 30
+        # The minus 30 above ensures we don't end up with an item width that
+        # can't be drawn the expected number of times across the view without
+        # being wrapped. Without this, the view can flicker during resize
+        tileWidth = width / VIEW_COLUMNS
+        iconWidth = int(tileWidth * 0.8)
+
+        self.setGridSize(QtCore.QSize(tileWidth, tileWidth))
+        self.setIconSize(QtCore.QSize(iconWidth, iconWidth))
+
+        return super(View, self).resizeEvent(event)
+
+
+class Model(QtCore.QStringListModel):
+
+    def flags(self, index):
+        # type: (QtCore.QModelIndex) -> QtCore.QItemFlags
+        return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable
+
+    def data(self, index, role=QtCore.Qt.DisplayRole):
+        # type: (QtCore.QModelIndex, QtCore.Qt.ItemRole) -> Any
+        """
+        Re-implemented to return the icon for the current index.
+
+        Parameters
+        ----------
+        index : QtCore.QModelIndex
+        role : int
+
+        Returns
+        -------
+        Any
+        """
+        if role == QtCore.Qt.DecorationRole:
+            iconString = self.data(index, role=QtCore.Qt.DisplayRole)
+            return ico.Icon(iconString)
+        return super(Model, self).data(index, role)
+
+
+def run():
+    # type: () -> NoReturn
+    """
+    Start the Iconify Browser and block until the process exits.
+    """
+    app = QtWidgets.QApplication([])
+
+    browser = Browser()
+    browser.show()
+
+    sys.exit(app.exec_())
