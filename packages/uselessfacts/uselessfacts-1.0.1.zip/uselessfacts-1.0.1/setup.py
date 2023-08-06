fcd0a992451185e37849a96283fe9fea46554447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:54 +0000

    Add package uselessfacts/1.0.1/uselessfacts-1.0.1.zip

---
+++ packages/uselessfacts/uselessfacts-1.0.1.zip/uselessfacts-1.0.1/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup(
+    name="uselessfacts",
+    version="1.0.1",
+    description="A powerfull , flexible and modern python module to retrieve random useless facts from the mighty web . View more on the github page",
+    url="https://github.com/Jakeisbored/uselessfacts",
+    author="Jake",
+    author_email="jstyle07072004@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    include_package_data=True,
+    install_requires=["requests"]
+)
