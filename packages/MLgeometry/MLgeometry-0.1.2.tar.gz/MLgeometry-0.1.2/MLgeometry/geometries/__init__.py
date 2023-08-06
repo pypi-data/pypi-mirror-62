e348ab2fec9402a9da4ef9459a5ba5bb17a19971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:04 +0000

    Add package MLgeometry/0.1.2/MLgeometry-0.1.2.tar.gz

---
+++ packages/MLgeometry/MLgeometry-0.1.2.tar.gz/MLgeometry-0.1.2/MLgeometry/geometries/__init__.py
@@ -0,0 +1,5 @@
+# from . import BoundBox
+from MLgeometry.geometries.BoundBox import BoundBox
+from MLgeometry.geometries.Circle import Circle
+from MLgeometry.geometries.Point import Point
+from MLgeometry.geometries.Polygon import Polygon
