cff1d867a16f7a749c188dc2b2ff2eef52a94a52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:39 +0000

    Add package ycpiptest/0.1/ycpiptest-0.1.tar.gz

---
+++ packages/ycpiptest/ycpiptest-0.1.tar.gz/ycpiptest-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup,find_packages
+setup(name='ycpiptest',
+      version='0.1',
+      description='a small example',
+      classifiers=[
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+      url='https://www.python.org/',
+      author='yaoc15',
+      author_email='940609536@qq.com',
+      license='NEU',
+      packages=find_packages(),
+      zip_safe=True
+     )
+
