a8de6e9ec134912342977147528b51f80ceea7bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:18 +0000

    Add package helldiver/0.1.6/helldiver-0.1.6.tar.gz

---
+++ packages/helldiver/helldiver-0.1.6.tar.gz/helldiver-0.1.6/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['helldiver']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['argparse>=1.4.0,<2.0.0',
+ 'beautifulsoup4>=4.8.2,<5.0.0',
+ 'markdown2>=2.3.8,<3.0.0']
+
+setup_kwargs = {
+    'name': 'helldiver',
+    'version': '0.1.6',
+    'description': 'A lightweight markdown to blog-aware HTML converter.',
+    'long_description': None,
+    'author': 'Rohan Bansal',
+    'author_email': 'rohanarunbansal@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
