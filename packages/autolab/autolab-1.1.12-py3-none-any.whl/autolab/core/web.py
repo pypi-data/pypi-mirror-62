a84250d74862e546b3a0ac29bc291227161eddc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:46 +0000

    Add package autolab/1.1.12/autolab-1.1.12-py3-none-any.whl

---
+++ packages/autolab/autolab-1.1.12-py3-none-any.whl/autolab/core/web.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 20 17:11:57 2019
+
+@author: qchat
+"""
+
+import webbrowser
+
+def report():
+    webbrowser.open('https://autolab-community.ddns.net')
+    
+def doc():
+    webbrowser.open('https://autolab.readthedocs.io')
