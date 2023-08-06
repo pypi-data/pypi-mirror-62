17dfe496fd51d566b6de271eb7be9dc1c934fa08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:28 +0000

    Add package trict/0.1.1/trict-0.1.1.tar.gz

---
+++ packages/trict/trict-0.1.1.tar.gz/trict-0.1.1/README.md
@@ -0,0 +1,4 @@
+# trict
+Python dictionary with extra stuff
+
+Subclass of collections.UserDict with some recursive and other helper stuff I've sometimes felt I've needed.
---
+++ packages/trict/trict-0.1.1.tar.gz/trict-0.1.1/setup.py
@@ -0,0 +1,30 @@
+
+from setuptools import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+setup(
+  name = 'trict',
+  packages = ['trict'],
+  version = '0.1.1',
+  license='MIT',
+  description = 'UserDict subclass with extra stuff',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  author = 'Saska Karsi',
+  author_email = 'saska.karsi@gmail.com',
+  url = 'https://github.com/saskakarsi/trict',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords = ['mapping', 'map', 'dictionary', 'recursive', 'dict'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
