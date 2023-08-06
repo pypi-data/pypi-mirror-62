8e6256d24b0559449d333aedd0316f3c500ddd0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:52 +0000

    Add package keyboard-listener/0.0.6/keyboard-listener-0.0.6.tar.gz

---
+++ packages/keyboard-listener/keyboard-listener-0.0.6.tar.gz/keyboard-listener-0.0.6/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="keyboard-listener",
+    version="0.0.6",
+    author="Adib Attie",
+    author_email="adib.attie33@gmail.com",
+    description="A keyboard listener that allows for custom combinations and keywords to trigger custom functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dibsonthis/keyboard_listener",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
