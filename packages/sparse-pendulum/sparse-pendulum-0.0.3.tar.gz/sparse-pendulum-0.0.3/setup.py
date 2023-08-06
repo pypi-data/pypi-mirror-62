e2bfb799b1e6279dc7b54b54a1c47be9b5e423a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:48 +0000

    Add package sparse-pendulum/0.0.3/sparse-pendulum-0.0.3.tar.gz

---
+++ packages/sparse-pendulum/sparse-pendulum-0.0.3.tar.gz/sparse-pendulum-0.0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sparse-pendulum',
+    version='0.0.3',
+    author="Christoper Glenn Wulur",
+    author_email="christoper.glennwu@gmail.com",
+    description="Open AI gym Pendulum Env with sparse rewards",
+    install_requires=['gym']#And any other dependencies required
+)
