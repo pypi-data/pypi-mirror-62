00f456f4e5b652a8c63bd254efdc31c9c7bd143f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:45 +0000

    Add package utils4py/0.0.2/utils4py-0.0.2.tar.gz

---
+++ packages/utils4py/utils4py-0.0.2.tar.gz/utils4py-0.0.2/setup.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+from setuptools import find_packages, setup
+
+setup(
+    name='utils4py',
+    version='0.0.2',
+    packages=find_packages(),
+    url='https://github.com/LastSync/utils4py',
+    license='MPL',
+    author='last911',
+    author_email='scnjl@qq.com',
+    description='Python tools',
+    install_requires=[
+        'sqlalchemy',
+        'pycrypto'
+    ]
+)
