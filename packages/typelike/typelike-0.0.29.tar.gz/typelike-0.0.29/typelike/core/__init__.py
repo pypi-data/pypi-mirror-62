3e49e9ea921656efc45d223a6fd1669cd7e19c64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:29 +0000

    Add package typelike/0.0.29/typelike-0.0.29.tar.gz

---
+++ packages/typelike/typelike-0.0.29.tar.gz/typelike-0.0.29/typelike/core/__init__.py
@@ -0,0 +1,19 @@
+
+from .any import *
+from .core import *
+from .dict import *
+from .list import *
+from .none import *
+from .number import *
+
+__all__ = [
+    'AnyLike',
+    'Anything',
+    'DictLike',
+    'infer_type',
+    'ListLike',
+    'NoneType',
+    'nones',
+    'NumberLike',
+    'Undefined'
+]
