33d8546fd71539b1766e86c1ba8e321e437cee8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:25 +0000

    Add package lti/0.9.5/lti-0.9.5.tar.gz

---
+++ packages/lti/lti-0.9.5.tar.gz/lti-0.9.5/setup.py
@@ -0,0 +1,31 @@
+"""Set up the lti package."""
+from setuptools import setup, find_packages
+
+setup(
+    name='lti',
+    version='0.9.5',
+    description='A python library for building and/or consuming LTI apps',
+    long_description=open('README.rst', 'rb').read().decode('utf-8'),
+    maintainer='Ryan Hiebert',
+    maintainer_email='ryan@ryanhiebert.com',
+    url='https://github.com/pylti/lti',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    install_requires=[
+        'lxml',
+        'oauthlib',
+        'requests-oauthlib',
+    ],
+    license='MIT License',
+    keywords='lti',
+    zip_safe=True,
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
