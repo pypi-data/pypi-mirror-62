8c87afbfd3376814972bf587b1840cb02a628410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:18 +0000

    Add package django-check-seo/0.2.0/django-check-seo-0.2.0.tar.gz

---
+++ packages/django-check-seo/django-check-seo-0.2.0.tar.gz/django-check-seo-0.2.0/django_check_seo/conf/settings.py
@@ -0,0 +1,28 @@
+# Third party
+from django.conf import settings
+
+
+# define basic SEO settings
+SEO_SETTINGS = {
+    "content_words_number": [300, 600],
+    "internal_links": 1,
+    "external_links": 1,
+    "meta_title_length": [30, 60],
+    "meta_description_length": [50, 160],
+    "keywords_in_first_words": 50,
+    "max_link_depth": 4,
+    "max_url_length": 70,
+}
+# update SEO settings with values from projectname/settings.py
+SEO_SETTINGS.update(getattr(settings, "DJANGO_CHECK_SEO_SETTINGS", {}))
+
+
+# define auth data (for .htaccess files)
+DJANGO_CHECK_SEO_AUTH = {}
+# update auth data with values from projectname/settings.py
+DJANGO_CHECK_SEO_AUTH.update(getattr(settings, "DJANGO_CHECK_SEO_AUTH", {}))
+
+# define http(s) settings (default = use https)
+DJANGO_CHECK_SEO_FORCE_HTTP = False
+# update http(s) settings with value from projectname/settings.py
+DJANGO_CHECK_SEO_FORCE_HTTP = getattr(settings, "DJANGO_CHECK_SEO_FORCE_HTTP", False)
