cce234f77e644bf63989d59e299fbac85fe9f9fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:58 +0000

    Add package asrtoolkit/0.2.2/asrtoolkit-0.2.2.tar.gz

---
+++ packages/asrtoolkit/asrtoolkit-0.2.2.tar.gz/asrtoolkit-0.2.2/asrtoolkit/convert_transcript.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+"""
+Python class for converting file formats used in Automatic Speech Recognition
+"""
+
+import argparse
+import logging
+import sys
+
+from asrtoolkit.file_utils.script_input_validation import assign_if_valid
+
+LOGGER = logging.getLogger(__name__)
+
+
+def check_input_file_validity(input_file):
+    if input_file is None:
+        LOGGER.error("Invalid input file {}".format(input_file))
+        sys.exit(1)
+
+
+def convert(input_file, output_file):
+    input_file = assign_if_valid(input_file)
+    input_file.write(output_file)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="convert between text file formats")
+    parser.add_argument("input_file",
+                        metavar="input_file",
+                        type=str,
+                        help="input file")
+    parser.add_argument("output_file",
+                        metavar="output_file",
+                        type=str,
+                        help="output file")
+    args = parser.parse_args()
+
+    check_input_file_validity(args.input_file)
+
+    convert(args.input_file, args.output_file)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/asrtoolkit/asrtoolkit-0.2.2.tar.gz/asrtoolkit-0.2.2/asrtoolkit/data_structures/corpus.py
@@ -0,0 +1,256 @@
+#!/usr/bin/env python
+"""
+Module for organizing SPH/MP3/WAV & STM files from a corpus
+"""
+
+import glob
+import os
+from concurrent.futures import ThreadPoolExecutor
+from functools import partial
+
+from tqdm import tqdm
+
+from asrtoolkit.data_structures.audio_file import audio_file
+from asrtoolkit.data_structures.time_aligned_text import time_aligned_text
+from asrtoolkit.file_utils.name_cleaners import basename, strip_extension
+
+
+def get_files(data_dir, extension):
+    """
+    Gets all files in a data directory with given extension
+    """
+    files = []
+    if data_dir and os.path.exists(data_dir):
+        files = glob.glob(data_dir + "/*." + extension)
+    return files
+
+
+class exemplar(object):
+    """
+    Create an exemplar class to pair one audio file with one transcript file
+    """
+
+    audio_file = None
+    transcript_file = None
+
+    def __init__(self, *args, **kwargs):
+        " Instantiate using input args and kwargs "
+        for dictionary in args:
+            if isinstance(dictionary, dict):
+                for key in dictionary:
+                    setattr(self, key, dictionary[key])
+        for key in kwargs:
+            setattr(self, key, kwargs[key])
+
+    def validate(self):
+        """
+        Validates exemplar object by constraining that the filenames before the
+        extension are the same
+        """
+
+        audio_filename = basename(strip_extension(self.audio_file.location))
+        transcript_filename = basename(
+            strip_extension(self.transcript_file.location))
+
+        # Audio and transcript filename must match
+        # Audio file must not be empty
+        # Transcript file must not be empty
+        valid = (audio_filename == transcript_filename
+                 and os.path.getsize(self.audio_file.location)
+                 and os.path.getsize(self.transcript_file.location))
+
+        return valid
+
+    def prepare_for_training(self, target, sample_rate=16000, nested=False):
+        """
+        Prepare one exemplar for training
+        Returning a new exemplar object with updated file locations
+        and a resampled audio_file
+        """
+        if nested:
+            af_target_file = os.path.join(target, "sph",
+                                          basename(self.audio_file.location))
+            tf_target_file = os.path.join(
+                target, "stm", basename(self.transcript_file.location))
+        else:
+            af_target_file = os.path.join(target,
+                                          basename(self.audio_file.location))
+            tf_target_file = os.path.join(
+                target, basename(self.transcript_file.location))
+
+        af = self.audio_file.prepare_for_training(
+            af_target_file,
+            sample_rate=sample_rate,
+        )
+
+        tf = self.transcript_file.write(tf_target_file)
+
+        return (exemplar({
+            "audio_file": af,
+            "transcript_file": tf
+        }) if all([af, tf]) else None)
+
+    def hash(self):
+        """
+        Returns combined hash of two files
+        """
+        return self.audio_file.hash() + self.transcript_file.hash()
+
+
+class corpus(object):
+    """
+    Create a corpus object for storing information about
+    the location and count of files in a corpus
+    """
+
+    location = None
+    exemplars = []
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize from location and populate list of
+        SPH, WAV, or MP3 audio files
+        and STM files into segments
+        """
+        for dictionary in args:
+            if isinstance(dictionary, dict):
+                for key in dictionary:
+                    setattr(self, key, dictionary[key])
+        for key in kwargs:
+            setattr(self, key, kwargs[key])
+
+        # only if not defined above should we search for exemplars
+        # based on location
+        if not self.exemplars:
+            # instantiate exemplars for this object to override
+            # static class variable
+            self.exemplars = []
+
+            audio_extensions_to_try = ["sph", "wav", "mp3"][::-1]
+            self.exemplars += [
+                exemplar({
+                    "audio_file":
+                    audio_file(fl),
+                    "transcript_file":
+                    time_aligned_text(strip_extension(fl) + ".stm"),
+                }) for audio_extension in audio_extensions_to_try
+                for fl in (get_files(self.location, audio_extension) if self.
+                           location else [])
+                if (os.path.exists(strip_extension(fl) + ".stm"))
+            ]
+
+            # gather all exemplars from /stm and /sph subdirectories if present
+            self.exemplars += [
+                exemplar({
+                    "audio_file":
+                    audio_file(fl),
+                    "transcript_file":
+                    time_aligned_text(self.location + "/stm/" +
+                                      basename(strip_extension(fl)) + ".stm"),
+                }) for audio_extension in audio_extensions_to_try for fl in
+                (get_files(self.location +
+                           "/sph/", audio_extension) if self.location else [])
+                if (os.path.exists(self.location + "/stm/" +
+                                   basename(strip_extension(fl)) + ".stm"))
+            ]
+
+    def validate(self):
+        """
+        Check to and validate each example after sorting by audio file hash
+        since stm hash may change
+        """
+        dict_of_examples = {_.audio_file.hash(): _ for _ in self.exemplars}
+        self.exemplars = [dict_of_examples[_] for _ in set(dict_of_examples)]
+        return sum(_.validate() for _ in self.exemplars)
+
+    def log(self):
+        """
+        Log what each hashed example contains
+        """
+        return {
+            _.hash(): {
+                "audio_file": _.audio_file.location,
+                "audio_file_hash": _.audio_file.hash(),
+                "transcript_file": _.transcript_file.location,
+                "transcript_file_hash": _.transcript_file.hash(),
+            }
+            for _ in self.exemplars
+        }
+
+    def calculate_number_of_segments(self):
+        """
+        Calculate how many segments are in this corpus
+        """
+        return sum(len(eg.transcript_file.segments) for eg in self.exemplars)
+
+    def prepare_for_training(self,
+                             target=None,
+                             nested=False,
+                             sample_rate=16000):
+        """
+        Run validation and audio file preparation steps
+        """
+
+        # write corpus back in place if no target
+        target = self.location if target is None else target
+
+        executor = ThreadPoolExecutor()
+
+        # process audio files concurrently for speed
+        futures = [
+            executor.submit(
+                partial(
+                    _.prepare_for_training,
+                    target=target,
+                    sample_rate=sample_rate,
+                    nested=nested,
+                )) for _ in self.exemplars
+        ]
+
+        # trigger conversion and gather results
+        new_exemplars = [future.result() for future in tqdm(futures)]
+
+        new_corpus = corpus({
+            "location":
+            target,
+            "exemplars": [eg for eg in new_exemplars if eg is not None],
+        })
+        new_corpus.validate()
+        return new_corpus.log()
+
+    def __add__(self, other):
+        """ Allow addition of corpora via + operator """
+        return corpus({
+            "location": None,
+            "exemplars": self.exemplars + other.exemplars
+        })
+
+    def __iadd__(self, other):
+        """ Allow addition of corpora via += operator """
+        self.exemplars = self.exemplars + other.exemplars
+        return self
+
+    def __sub__(self, other):
+        """ Allow addition of corpora via - operator """
+        return corpus({
+            "location":
+            None,
+            "exemplars":
+            [_ for _ in self.exemplars if _ not in other.exemplars],
+        })
+
+    def __isub__(self, other):
+        """ Allow subtraction of corpora via -= operator """
+        self.exemplars = [
+            _ for _ in self.exemplars if _ not in other.exemplars
+        ]
+        return self
+
+    def __getitem__(self, given):
+        """ Allow slicing of corpora via [] """
+        return corpus({
+            "location":
+            self.location,
+            "exemplars": [self.exemplars[given]]
+            if not isinstance(given, slice) else self.exemplars[given],
+        })
