3f0188b093f3a396522ae115447ff6c9c9a05320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:31 +0000

    Add package yamcs-client/1.4.4/yamcs-client-1.4.4.tar.gz

---
+++ packages/yamcs-client/yamcs-client-1.4.4.tar.gz/yamcs-client-1.4.4/README.md
@@ -0,0 +1,33 @@
+# Yamcs Client Library for Python
+[![Website](https://img.shields.io/website/http/shields.io.svg?label=docs)](https://www.yamcs.org/yamcs-python/yamcs-client)
+[![Build Status](https://travis-ci.org/yamcs/yamcs-python.svg?branch=master)](https://travis-ci.org/yamcs/yamcs-python)
+[![PyPI](https://img.shields.io/pypi/v/yamcs-client.svg)](https://pypi.python.org/pypi/yamcs-client)
+[![PyPI pyversions](https://img.shields.io/pypi/pyversions/yamcs-client.svg)](https://pypi.python.org/pypi/yamcs-client/)
+
+
+## Features
+
+* MDB retrieval
+* Telemetry updates
+* Command History updates
+* Alarm updates
+* Event updates
+* Archive retrieval
+* Histogram retrieval
+
+
+## Installation
+
+The easiest way to install is via pip
+
+    pip install yamcs-client
+
+
+## Documentation
+
+All available operations are documented at https://www.yamcs.org/yamcs-python/yamcs-client
+
+
+## License
+
+LGPL-3.0. See [LICENSE](https://github.com/yamcs/yamcs-python/blob/master/LICENSE)
---
+++ packages/yamcs-client/yamcs-client-1.4.4.tar.gz/yamcs-client-1.4.4/yamcs/core/helpers.py
@@ -0,0 +1,76 @@
+from __future__ import print_function
+
+import logging
+from collections import OrderedDict
+from datetime import datetime, timedelta
+
+
+def to_isostring(dt):
+    """
+    Converts the given datetime to an ISO String.
+    This assumes the datetime is UTC.
+    """
+    if dt.tzinfo is not None and dt.tzinfo.utcoffset(dt) > timedelta(0):
+        logging.warning('Warning: aware datetimes are interpreted as if they were naive')
+
+    # -3 to change microseconds to milliseconds
+    return dt.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
+
+
+def parse_isostring(isostring):
+    """
+    Parse the ISO String to a native ``datetime``.
+    """
+    if not isostring:
+        return None
+    return datetime.strptime(isostring.replace('Z', 'GMT'),
+                             '%Y-%m-%dT%H:%M:%S.%f%Z')
+
+
+def parse_value(proto):
+    """
+    Convers a Protobuf `Value` from the API into a python native value
+    """
+    if proto.HasField('floatValue'):
+        return proto.floatValue
+    elif proto.HasField('doubleValue'):
+        return proto.doubleValue
+    elif proto.HasField('sint32Value'):
+        return proto.sint32Value
+    elif proto.HasField('uint32Value'):
+        return proto.uint32Value
+    elif proto.HasField('binaryValue'):
+        return proto.binaryValue
+    elif proto.HasField('timestampValue'):
+        # Don't use the actual 'timestampValue' field, it contains a number
+        # that is difficult to interpret on the client. Instead parse from
+        # the ISO String also set by Yamcs.
+        return parse_isostring(proto.stringValue)
+    elif proto.HasField('stringValue'):
+        return proto.stringValue
+    elif proto.HasField('uint64Value'):
+        return proto.uint64Value
+    elif proto.HasField('sint64Value'):
+        return proto.sint64Value
+    elif proto.HasField('booleanValue'):
+        return proto.booleanValue
+    elif proto.HasField('arrayValue'):
+        return [parse_value(v) for v in proto.arrayValue]
+    elif proto.HasField('aggregateValue'):
+        return OrderedDict(zip(proto.aggregateValue.name, proto.aggregateValue.value))
+    else:
+        logging.warning('Unrecognized value type for update %s', proto)
+        return None
+
+
+def adapt_name_for_rest(name):
+    """
+    Modifies a user-provided name for use in API calls.
+    Basically we want an alias like 'MDB:OPS Name/SIMULATOR_BatteryVoltage2'
+    to be prepended with a slash.
+    """
+    if name.startswith('/'):
+        return name
+    elif not '/' in name:
+        raise ValueError('Provided name is not a fully-qualified XTCE name.')
+    return '/' + name
