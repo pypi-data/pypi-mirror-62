753cedac398d04f06a660f26dfe3b3dda18aa520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:06 +0000

    Add package luadata/0.1.1/luadata-0.1.1.tar.gz

---
+++ packages/luadata/luadata-0.1.1.tar.gz/luadata-0.1.1/luadata/__init__.py
@@ -0,0 +1,9 @@
+
+from luadata.io.write import serialize
+from luadata.io.read import unserialize
+
+name = "luadata"
+
+__doc__ = """
+a Python package that can serialize Python list & dictionary to Lua table.
+"""
