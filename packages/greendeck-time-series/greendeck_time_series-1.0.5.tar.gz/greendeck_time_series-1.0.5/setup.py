089bd4ad4f6256b7f7121773a07f5599fc88b9a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:28 +0000

    Add package greendeck-time-series/1.0.5/greendeck_time_series-1.0.5.tar.gz

---
+++ packages/greendeck-time-series/greendeck_time_series-1.0.5.tar.gz/greendeck_time_series-1.0.5/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="greendeck_time_series",
+    version="1.0.5",
+    author="sambal shikhar",
+    author_email="sambal@greendeck.com",
+    description="Greendeck Time Series",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=['greendeck_time_series', 'greendeck_time_series.src', 'greendeck_time_series.src.time_series'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'timesynth'
+    ],
+    include_package_data=True,
+    zip_safe=False
+)
