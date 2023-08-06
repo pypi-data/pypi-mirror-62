5e831dabcd1435b5f3444dcc5b1d94b429734292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:55 +0000

    Add package rais/0.0.5/rais-0.0.5.tar.gz

---
+++ packages/rais/rais-0.0.5.tar.gz/rais-0.0.5/rais/rais.py
@@ -0,0 +1,21 @@
+__author__ = 'v.denisov'
+
+from rais.parameter_initialization import ParameterInitialization as pi
+
+class Rais:
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def initialization(self, url_host, prefix):
+        pi.set_url_host(url_host=url_host)
+        pi.set_prefix(prefix=prefix)
+
+    @classmethod
+    def get_parameters(self):
+        parameters = {
+            "url_host": pi.get_url_host(),
+            "prefix": pi.get_prefix()
+        }
+        return parameters
