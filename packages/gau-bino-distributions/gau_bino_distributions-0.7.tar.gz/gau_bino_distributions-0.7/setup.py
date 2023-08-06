635cd24185db535378e321c413e4e0a570b3d9f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:22 +0000

    Add package gau-bino-distributions/0.7/gau_bino_distributions-0.7.tar.gz

---
+++ packages/gau-bino-distributions/gau_bino_distributions-0.7.tar.gz/gau_bino_distributions-0.7/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gau_bino_distributions',
+      version='0.7',
+      description='Gaussian and Binomial Distributions',
+      packages=['gau_bino_distributions'],
+      author = 'Hao Xu',
+      author_email = 'edifierxuhao123@gmail.com',
+      zip_safe=False)
