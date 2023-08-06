939aadab75b5fa2af572670059261fdfeac3a8b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:52 +0000

    Add package mythos-home/0.0.3/mythos-home-0.0.3.tar.gz

---
+++ packages/mythos-home/mythos-home-0.0.3.tar.gz/mythos-home-0.0.3/src/mythos/home/__version__.py
@@ -0,0 +1,7 @@
+__title__ = "mythos-home"
+__version__ = "0.0.3"
+__description__ = "Mythos Home"
+__author__ = "Jaroslaw Wojtas"
+__license__ = "Apache 2.0"
+__copyright__ = "Copyright 2020 Jaroslaw Wojtas"
+__url__ = "https://github.com/theyarek/mythos-home"
