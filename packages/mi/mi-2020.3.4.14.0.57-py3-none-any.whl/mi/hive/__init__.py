51a2c30f5eb9778ee0776ecf4b5a6a308e7bc370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:17 +0000

    Add package mi/2020.3.4.14.0.57/mi-2020.3.4.14.0.57-py3-none-any.whl

---
+++ packages/mi/mi-2020.3.4.14.0.57-py3-none-any.whl/mi/hive/__init__.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : mi.
+# @File         : __init__.py
+# @Time         : 2020-03-04 13:31
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : 
+
+
+import os
+from pyhive import hive
+
+
+def kinit(keytab="/fds/data/h_browser.keytab"):
+    cmd = f"kinit -k -t {keytab} h_browser@XIAOMI.HADOOP && klist"
+    _ = os.popen(cmd).read()
+    print(_)
+
+
+def hive_conn():
+    """
+    conn = hive_conn()
+    sql = 'select deviceid, reachitems from browser.push_callback_log where date=20200202 limit 20'
+    df = pd.read_sql(sql, conn, chunksize=None)
+
+    """
+
+    conn = hive.connect(
+        host="zjyprc-hadoop.hive.srv", port=10000,
+        auth="KERBEROS", kerberos_service_name="sql_prc",
+        configuration={
+            'mapreduce.map.memory.mb': '4096',
+            'mapreduce.reduce.memory.mb': '4096',
+            'mapreduce.map.java.opts': '-Xmx3072m',
+            'mapreduce.reduce.java.opts': '-Xmx3072m',
+            'hive.input.format': 'org.apache.hadoop.hive.ql.io.HiveInputFormat',
+            'hive.limit.optimize.enable': 'false',
+            'mapreduce.job.queuename': 'root.production.miui_group.browser.miui_browser_zjy_1',  # zjy
+        },
+    )
+    return conn
