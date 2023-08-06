ef6e85877b905e6815578bac10a1e96bd8360b3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:45 +0000

    Add package irflow-client/1.6.1/irflow_client-1.6.1.tar.gz

---
+++ packages/irflow-client/irflow_client-1.6.1.tar.gz/irflow_client-1.6.1/irflow_client/__version__.py
@@ -0,0 +1,13 @@
+"""Syncurity IR-Flow SDK Version File"""
+
+__title__ = 'irflow_client'
+__description__ = 'A python client for Syncurity IR-Flow'
+__url__ = 'https://syncurity.net'
+__version__ = '1.6.1'
+__build__ = ''
+__author__ = 'Syncurity'
+__author_email__ = 'support@syncurity.net'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2019 Syncurity'
+__cake__ = u''
+
