ab75672f63a1aa548db36ccdfca9ad1703266a22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:12 +0000

    Add package hobart-svg/0.2.2/hobart-svg-0.2.2.tar.gz

---
+++ packages/hobart-svg/hobart-svg-0.2.2.tar.gz/hobart-svg-0.2.2/hobart_svg/test_core.py
@@ -0,0 +1,19 @@
+from lace.mesh import Mesh
+import numpy as np
+from polliwog import Plane
+import vg
+from .core import render_longest_xsection_to_svg
+
+
+def test_render_longest_xsection_to_svg():
+    mesh = Mesh(filename="examples/vitra/vitra_without_materials.obj")
+    plane = Plane(
+        point_on_plane=np.array([-0.869231, 60.8882, -20.1071]),
+        unit_normal=vg.normalize(np.array([0.0, 0.1, -1.0])),
+    )
+    xs = render_longest_xsection_to_svg(
+        mesh=mesh, plane=plane, filename="vitra_cross_section.svg"
+    )
+
+    mesh.add_lines([xs])
+    mesh.write("vitra_with_cross_section.dae")
