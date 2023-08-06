d9ba25871a335062cd0a5dda5f6167a313846c8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:14 +0000

    Add package import-watch/2.0.1/import-watch-2.0.1.tar.gz

---
+++ packages/import-watch/import-watch-2.0.1.tar.gz/import-watch-2.0.1/demo.py
@@ -0,0 +1,41 @@
+# Copyright (C) 2019 Sebastian Pipping <sebastian@pipping.org>
+# Licensed under the MIT license
+
+from __future__ import print_function
+
+import import_watch
+
+
+print('BEGIN')
+import_watch.trace_imports(depth=2)
+import yaml  # noqa: E402
+import yaml  # noqa: E402, F811
+import_watch.reset()
+print('END')
+print()
+
+
+import yaml  # noqa: E402, F811
+
+
+print('BEGIN')
+import_watch.deny_cyclic_imports()
+try:
+    from import_watch._cyclic_import_package import a
+except ImportError as e:
+    print('Exception caught:', e)
+else:
+    assert False, 'Cyclic import went undetected'
+import_watch.reset()
+print('END')
+print()
+
+
+import yaml  # noqa: E402, F401, F811
+
+
+print('BEGIN')
+import_watch.warn_about_cyclic_imports()
+from import_watch._cyclic_import_package import a  # noqa: E402, F401, F811
+import_watch.reset()
+print('END')
---
+++ packages/import-watch/import-watch-2.0.1.tar.gz/import-watch-2.0.1/setup.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python
+# Copyright (C) 2019 Sebastian Pipping <sebastian@pipping.org>
+# Licensed under the MIT license
+
+from textwrap import dedent
+
+from setuptools import find_packages, setup
+
+_classifiers = dedent("""\
+    Development Status :: 3 - Alpha
+
+    Intended Audience :: Developers
+
+    License :: OSI Approved :: MIT License
+
+    Natural Language :: English
+
+    Programming Language :: Python :: 2.7
+    Programming Language :: Python :: 3.5
+    Programming Language :: Python :: 3.6
+    Programming Language :: Python :: 3.7
+    Programming Language :: Python :: 3.8
+
+    Programming Language :: Python :: Implementation :: CPython
+""")
+
+_description = 'Trace module imports and detect/deny cyclic imports at runtime'
+
+
+if __name__ == '__main__':
+    setup(
+            name='import-watch',
+            url='https://github.com/hartwork/import-watch',
+            description=_description,
+            long_description=open('README.md').read(),
+            long_description_content_type='text/markdown',
+            license='MIT',
+            version='2.0.1',
+            author='Sebastian Pipping',
+            author_email='sebastian@pipping.org',
+            setup_requires=[
+                'setuptools>=38.6.0',  # for long_description_content_type
+            ],
+            packages=find_packages(),
+            classifiers=[c for c in _classifiers.split('\n') if c],
+            )
