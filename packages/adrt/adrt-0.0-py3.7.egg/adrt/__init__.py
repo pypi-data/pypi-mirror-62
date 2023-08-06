80833bc4ac9cd52baa43e4ac98c2b5570d0f9d60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:51 +0000

    Add package adrt/0.0/adrt-0.0-py3.7.egg

---
+++ packages/adrt/adrt-0.0-py3.7.egg/adrt/__init__.py
@@ -0,0 +1,32 @@
+"""
+ADRT
+===============
+
+``adrt`` is a software package written in ``Python`` for Discrete Random Transform.
+
+Example
+-------
+Examples can be given using either the ``Example`` or ``Examples``
+sections. Sections support any reStructuredText formatting, including
+literal blocks.
+
+	$ python example_numpy.py
+
+
+Section breaks are created with two blank lines. Section breaks are also
+implicitly created anytime a new section starts. Section bodies *may* be
+indented.
+
+Notes
+-----
+	This is an example of an indented section. It's like any other section,
+	but the body is indented to help it stand out from surrounding text.
+
+If a section is indented, then a section break is created simply by
+resuming unindented text.
+
+"""
+__author__ = ("Don Rim <dr1653@nyu.edu>",
+	"Yoon-gu Hwang <yoongu.hwang@gmail.com>")
+
+from .adrt import *
