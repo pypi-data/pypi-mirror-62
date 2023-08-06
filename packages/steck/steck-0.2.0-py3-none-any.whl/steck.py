07901fe922e5a50fdea8673cf5f7e4b145685cff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:09 +0000

    Add package steck/0.2.0/steck-0.2.0-py3-none-any.whl

---
+++ packages/steck/steck-0.2.0-py3-none-any.whl/steck.py
@@ -0,0 +1,69 @@
+import pathlib
+
+from typing import Tuple
+
+import click
+import requests
+import magic
+
+
+mime_map = {
+    "text/plain": "text",
+    "text/x-python": "python",
+}
+
+
+@click.group()
+def main() -> None:
+    """Steck, a pastebin client for pinnwand."""
+    return
+
+
+@main.command()
+@click.argument("files", nargs=-1)
+def paste(files: Tuple[str]) -> None:
+    """Paste some files matching a pattern."""
+
+    if not files:
+        print("No files, did you forget to pass any?")
+        return
+
+    print(f"You are about to paste the following {len(files)} files. Do you want to continue?")
+
+    for file in files:
+        if pathlib.Path(file).is_file():
+            print(f" - {file}")
+
+    if input("Continue? [y/N] ").lower() != "y":
+        return 0
+
+    guesser = magic.Magic(mime=True)
+
+    collect = [
+        (
+            pathlib.Path(file).name,
+            open(file).read(),
+            mime_map.get(guesser.from_file(file), "text"),
+        )
+        for file in files
+        if pathlib.Path(file).is_file()
+    ]
+
+    data = {
+        "expiry": "1day",
+        "files": [
+            {"name": name, "content": content, "lexer": lexer}
+            for name, content, lexer in collect
+        ],
+    }
+
+    response = requests.post(
+        "https://bpaste.net/api/v1/paste", json=data
+    ).json()
+
+    print("View Paste", response["link"])
+    print("Remove Paste", response["removal"])
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
