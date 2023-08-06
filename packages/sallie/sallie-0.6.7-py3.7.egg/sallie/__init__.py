d022059175e38f49508ed5c915977698c6019fb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:13 +0000

    Add package sallie/0.6.7/sallie-0.6.7-py3.7.egg

---
+++ packages/sallie/sallie-0.6.7-py3.7.egg/sallie/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: UTF-8 -*-
+"""
+TV information related tasks
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+__author__ = "the01"
+__email__ = "jungflor@gmail.com"
+__copyright__ = "Copyright (C) 2014-20, Florian JUNG"
+__license__ = "MIT"
+__version__ = "0.6.7"
+__date__ = "2020-02-29"
+# Created: 2015-02-26 03:39
+
+import logging
+
+from .tv_next import TVNext
+from .next_tvdb import TVNextTVDB
+from .cli_main import main
+
+logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+__all__ = [
+    "tv_next", "next_tvdb", "cli_main",
+    "TVNext", "TVNextTVDB", "main",
+]
