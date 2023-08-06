313cdb3c0a99115f434b5250c807593c2b52ec0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:39 +0000

    Add package rowgenerators/0.9.7/rowgenerators-0.9.7.tar.gz

---
+++ packages/rowgenerators/rowgenerators-0.9.7.tar.gz/rowgenerators-0.9.7/rowgenerators/test/test_issues.py
@@ -0,0 +1,28 @@
+import unittest
+from rowgenerators import get_generator
+from rowgenerators import parse_app_url, get_cache
+
+class TestIssues(unittest.TestCase):
+
+
+    def x_test_pass_target_format(self):
+
+        us = 'file:///Users/eric/Downloads/7908485365090507159.zip#VictimRecords.txt&target_format=csv'
+
+        u = parse_app_url(us, target_format=None)
+
+        print(u)
+        r = u.get_resource()
+        print(r)
+        t = r.get_target()
+        print(t)
+
+        g = t.generator
+
+        print(len(list(g)))
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
