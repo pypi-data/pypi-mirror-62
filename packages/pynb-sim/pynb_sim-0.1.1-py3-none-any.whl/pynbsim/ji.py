92f51559c17e2789995c72fdd5207fbdbeaddf93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:07 +0000

    Add package pynb-sim/0.1.1/pynb_sim-0.1.1-py3-none-any.whl

---
+++ packages/pynb-sim/pynb_sim-0.1.1-py3-none-any.whl/pynbsim/ji.py
@@ -0,0 +1,10 @@
+"""
+    Jupyter Interface:: This class contains the core methods to interface with the
+    Jupyter notebook.
+"""
+
+class Cell:
+    """
+        Represents a cell in the Jupyter notebook.
+    """
+    pass
