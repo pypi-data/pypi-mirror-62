400c1309b27a1199794f687b0a38e06b45d4da7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:04 +0000

    Add package datasette/0.37.1/datasette-0.37.1-py3-none-any.whl

---
+++ packages/datasette/datasette-0.37.1-py3-none-any.whl/datasette/plugins.py
@@ -0,0 +1,24 @@
+import importlib
+import pluggy
+import sys
+from . import hookspecs
+
+DEFAULT_PLUGINS = (
+    "datasette.publish.heroku",
+    "datasette.publish.now",
+    "datasette.publish.cloudrun",
+    "datasette.facets",
+    "datasette.sql_functions",
+)
+
+pm = pluggy.PluginManager("datasette")
+pm.add_hookspecs(hookspecs)
+
+if not hasattr(sys, "_called_from_test"):
+    # Only load plugins if not running tests
+    pm.load_setuptools_entrypoints("datasette")
+
+# Load default plugins
+for plugin in DEFAULT_PLUGINS:
+    mod = importlib.import_module(plugin)
+    pm.register(mod, plugin)
