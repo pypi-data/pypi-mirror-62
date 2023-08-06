a002da21c1eda519c2c2bf3a5a1c5799918a6680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:20 +0000

    Add package pierwszy-pakiet-pajtona/0.1/pierwszy_pakiet_pajtona-0.1.tar.gz

---
+++ packages/pierwszy-pakiet-pajtona/pierwszy_pakiet_pajtona-0.1.tar.gz/pierwszy_pakiet_pajtona-0.1/setup.py
@@ -0,0 +1,13 @@
+#!/user/bin/env python
+from distutils.core import setup
+import setuptools
+
+setup(
+    name="pierwszy_pakiet_pajtona",
+    version="0.1",
+    packages=setuptools.find_packages(),
+    description="First PythonPackage",
+    author="Unknown",
+    author_email="unknown@gmail.com",
+
+)
