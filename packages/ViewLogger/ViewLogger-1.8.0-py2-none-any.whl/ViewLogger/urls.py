ec94019df6771b097dd9b880b484b7af6786643d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:59 +0000

    Add package ViewLogger/1.8.0/ViewLogger-1.8.0-py2-none-any.whl

---
+++ packages/ViewLogger/ViewLogger-1.8.0-py2-none-any.whl/ViewLogger/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.conf.urls import url
+
+
+urlpatterns = [
+    url('^$', views.mainViewLogger, name="LogMain"),
+    url('^search$', views.search_in_archives, name="search_in_archives"),
+]
