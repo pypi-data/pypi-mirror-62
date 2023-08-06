b61f88ee61b7ecdd3ab5085758b650d59b51429e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:56 +0000

    Add package mortgage-boi/0.0.3/mortgage-boi-0.0.3.tar.gz

---
+++ packages/mortgage-boi/mortgage-boi-0.0.3.tar.gz/mortgage-boi-0.0.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mortgage-boi",
+    version="0.0.3",
+    author="Charlie Reese",
+    author_email="j.charles.reese@gmail.com",
+    description="A package for calculating mortgage data and cash flows",
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/charliereese/mortgage_boi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+    "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Financial and Insurance Industry",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha",	
+    ],
+    python_requires='>=3.6',
+)
