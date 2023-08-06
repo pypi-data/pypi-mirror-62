924c28990b3ba2e0dac7f43a92b2922c2c227da7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:43 +0000

    Add package enroll-gcalendar/0.3/enroll_gcalendar-0.3.tar.gz

---
+++ packages/enroll-gcalendar/enroll_gcalendar-0.3.tar.gz/enroll_gcalendar-0.3/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name='enroll_gcalendar',
+    packages=['enroll_gcalendar'],
+    version='0.3',
+    license='MIT',
+    description='Convert enroll-me.iiet.pl schedules to google calendar for CS IET students',
+    author='Hubert Pelczarski',
+    author_email='pelczarskihubert@gmail.com',
+    url='https://github.com/hoob3rt/enroll-gcalendar',
+    download_url='https://github.com/hoob3rt/enroll-gcalendar/archive/0.1.tar.gz',
+    keywords=['agh', 'google-calendar-api', 'enroll-me.iiet.pl'],
+    install_requires=[
+        'selenium',
+        'google_auth_oauthlib',
+        'google_api_python_client',
+        'google-auth-httplib2',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Education',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
