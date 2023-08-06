e766d8056e123cb27ec70ace09fbe513cd6f7ce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:35 +0000

    Add package higgsboom/0.1.0/higgsboom-0.1.0.tar.gz

---
+++ packages/higgsboom/higgsboom-0.1.0.tar.gz/higgsboom-0.1.0/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+	name = 'higgsboom',
+	version = '0.1.0',
+	description = 'Higgs Asset Management Boom Strategy Platform API',
+	author = 'Veidt',
+	author_email = 'veidt1988@live.com',
+	url = 'https://github.com/veidt1988/higgs-boom-master',
+	python_requires = '>=3.5',
+	packages = find_packages(),
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+		'Topic :: Software Development :: Build Tools',
+		'License :: OSI Approved :: MIT License',
+		],
+)
