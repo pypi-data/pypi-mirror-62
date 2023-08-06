06c238ffa5e45488e4f7240d61629b23b2582e72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:36 +0000

    Add package tablewriter/0.0.1.5/tablewriter-0.0.1.5.tar.gz

---
+++ packages/tablewriter/tablewriter-0.0.1.5.tar.gz/tablewriter-0.0.1.5/README.md
@@ -0,0 +1,7 @@
+**author** : Philippe COTTE, cottephi@gmail.com
+
+Package to compile pandas DataFrame to pdf, using LateX.
+
+Example of use, assuming *df* is some pandas.DataFrame can be found in *example.py*
+
+**new in 0.0.1.5** : Removed useless print
---
+++ packages/tablewriter/tablewriter-0.0.1.5.tar.gz/tablewriter-0.0.1.5/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tablewriter",
+    version="0.0.1.5",
+    author="cottephi",
+    author_email="cottephi@gmail.com",
+    description="latex table generator",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://gitlab.com/cottephi/latextablegenerator",
+    py_modules=["tablewriter"],
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: Unix",
+    ],
+)
+
