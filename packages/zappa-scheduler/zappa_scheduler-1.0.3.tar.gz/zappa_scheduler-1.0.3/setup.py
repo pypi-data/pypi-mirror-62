e7992fe9ea4d03ea23cb6d0fd21e01dbf15d24bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:20 +0000

    Add package zappa-scheduler/1.0.3/zappa_scheduler-1.0.3.tar.gz

---
+++ packages/zappa-scheduler/zappa_scheduler-1.0.3.tar.gz/zappa_scheduler-1.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='zappa_scheduler',
+    version='1.0.3',
+    packages=['zappa_scheduler'],
+    url='https://github.com/wisaac407/zappa-call-later',
+    license='MIT License',
+    author='andytwoods, Isaac Weaver',
+    author_email='andytwoods@gmail.com, wisaac407@gmail.com',
+    description='store future tasks in the db and call them after set delays',
+    install_requires=['django-picklefield']
+)
