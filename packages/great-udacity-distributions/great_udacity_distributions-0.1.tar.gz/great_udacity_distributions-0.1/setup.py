a98d3282db51bd350572ac30fd98533fa9fff984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:49 +0000

    Add package great-udacity-distributions/0.1/great_udacity_distributions-0.1.tar.gz

---
+++ packages/great-udacity-distributions/great_udacity_distributions-0.1.tar.gz/great_udacity_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='great_udacity_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['great_udacity_distributions'],
+      zip_safe=False)
