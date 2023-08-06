a3c093ea0520228ae21ad29f9d74d338fec05f0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:58 +0000

    Add package alfa-sdk/0.1.16/alfa-sdk-0.1.16.tar.gz

---
+++ packages/alfa-sdk/alfa-sdk-0.1.16.tar.gz/alfa-sdk-0.1.16/alfa_sdk/data.py
@@ -0,0 +1,24 @@
+import requests
+
+from alfa_sdk.common.base import BaseClient
+
+
+class DataClient(BaseClient):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def fetch_data_file(self, data_file_id):
+        url = self.session.request(
+            "get", "data", "/api/Storages/download/{}".format(data_file_id)
+        )
+
+        res = requests.get(url["downloadLink"], allow_redirects=True)
+        return res.content
+
+    def list_data_files(self, prefix="", skip=0, limit=100, order="name ASC"):
+        return self.session.request(
+            "get",
+            "data",
+            "/api/Storages/list",
+            params={"prefix": prefix, "skip": skip, "limit": limit, "order": order},
+        )
