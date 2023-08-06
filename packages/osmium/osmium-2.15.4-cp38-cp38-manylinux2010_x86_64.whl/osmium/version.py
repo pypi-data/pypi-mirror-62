b4c750c590bc27e57012f88d31c6ad442afba3ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:38 +0000

    Add package osmium/2.15.4/osmium-2.15.4-cp38-cp38-manylinux2010_x86_64.whl

---
+++ packages/osmium/osmium-2.15.4-cp38-cp38-manylinux2010_x86_64.whl/osmium/version.py
@@ -0,0 +1,15 @@
+"""
+Version information.
+"""
+
+# the major version
+pyosmium_major = '2.15'
+# current release (Pip version)
+pyosmium_release = '2.15.4'
+
+# libosmium version shipped with the Pip release
+libosmium_version = '2.15.4'
+# protozero version shipped with the Pip release
+protozero_version = '1.6.8'
+# pybind11 version shipped with the Pip release
+pybind11_version = '2.4.3'
