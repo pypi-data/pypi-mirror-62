30419218bb2443ad3b37378e662867e5d056ec5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:46 +0000

    Add package cloc/0.0.2/cloc-0.0.2.tar.gz

---
+++ packages/cloc/cloc-0.0.2.tar.gz/cloc-0.0.2/cloc/decorators.py
@@ -0,0 +1,77 @@
+
+from typing import Any, Callable, Union
+
+from cloc.core import Arg, Cmd, Grp, Opt, Flg, Params
+
+
+'''
+decorators
+'''
+
+def _create_cmd(name:str, fn:Callable, param:Union[Arg, Flg, Opt], hidden:bool=False):
+    if param:
+        cmd = Cmd(name, fn, params=Params([param]), hidden=hidden)
+    else:
+        cmd = Cmd(name, fn, hidden=hidden)
+    cmd.__doc__ = fn.__doc__
+    return cmd
+
+class opt(object):
+
+    def __init__(self, name:str, short_name: str, type: Any= None, help: str= None):
+        self.Opt = Opt(name, short_name, type, help)
+
+    def __call__(self, f):
+        if isinstance(f, Cmd):
+            f.params.order.insert(0, self.Opt)
+            return f
+        else:
+            return _create_cmd(None, f, self.Opt)
+
+class flg(object):
+
+    def __init__(self, name:str, short_name: str, help: str= None):
+        self.Flg = Flg(name, short_name, help)
+
+    def __call__(self, f):
+        if isinstance(f, Cmd):
+            f.params.order.insert(0, self.Flg)
+            return f
+        else:
+            return _create_cmd(None, f, self.Flg)
+
+class arg(object):
+
+    def __init__(self, name:str, type: Any= None, help: str= None):
+        self.Arg = Arg(name, type, help)
+
+    def __call__(self, f):
+        if isinstance(f, Cmd):
+            f.params.order.insert(0, self.Arg)
+            return f
+        else:
+            return _create_cmd(None, f, self.Arg)
+
+class cmd(object):
+
+    def __init__(self, name:str = None, hidden:bool = False):
+        self.name = name
+        self.hidden = hidden
+
+    def __call__(self, f):
+        if isinstance(f, Cmd):
+            f.name = self.name
+            f.hidden = self.hidden
+            return f
+        else:
+            return _create_cmd(None, f, None, hidden=self.hidden)
+
+class grp(object):
+    def __init__(self, name:str = None):
+        self.name = name
+
+    def __call__(self, f):
+        grp = Grp(self.name)
+        grp.__doc__ = f.__doc__
+        return grp
+
