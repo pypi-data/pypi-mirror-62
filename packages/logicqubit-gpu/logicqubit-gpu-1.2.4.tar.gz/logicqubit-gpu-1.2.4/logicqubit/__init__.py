4245b68d2e1560bd9251fa469243961a456da1b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:36 +0000

    Add package logicqubit-gpu/1.2.4/logicqubit-gpu-1.2.4.tar.gz

---
+++ packages/logicqubit-gpu/logicqubit-gpu-1.2.4.tar.gz/logicqubit-gpu-1.2.4/logicqubit/__init__.py
@@ -0,0 +1,9 @@
+from logicqubit import logic
+from logicqubit import gates
+from logicqubit import hilbert
+from logicqubit import utils
+from logicqubit import circuit
+
+__version__ = "1.2.4"
+
+print("logicqubit-gpu version "+__version__)
