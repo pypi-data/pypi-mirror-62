e69b5f0e570147189fbe8629e57bbe03ed32bc13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:17 +0000

    Add package airflow-livy-plugins/0.1/airflow-livy-plugins-0.1.tar.gz

---
+++ packages/airflow-livy-plugins/airflow-livy-plugins-0.1.tar.gz/airflow-livy-plugins-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README.md") as fh:
+    long_description = fh.read()
+
+setup(
+    name="airflow-livy-plugins",
+    version="0.1",
+    author="Vadim Panov",
+    author_email="headcra6@gmail.com",
+    description="Plugins for Airflow to run Spark jobs via Livy: sessions and batches",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/panovvv/aivrflow-livy-plugins",
+    license="MIT License",
+    packages=["airflow_home.plugins"],
+    python_requires=">=3.7",
+)
