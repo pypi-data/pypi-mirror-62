29e1b0e45c7b84b53ab0cec977ad2ad736c04ee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:47 +0000

    Add package azure-functions-worker/1.0.3/azure_functions_worker-1.0.3-py3-none-any.whl

---
+++ packages/azure-functions-worker/azure_functions_worker-1.0.3-py3-none-any.whl/azure_functions_worker/bindings/__init__.py
@@ -0,0 +1,15 @@
+from .tracecontext import TraceContext
+from .context import Context
+from .meta import check_input_type_annotation
+from .meta import check_output_type_annotation
+from .meta import is_trigger_binding
+from .meta import from_incoming_proto, to_outgoing_proto
+from .out import Out
+
+
+__all__ = (
+    'Out', 'Context',
+    'is_trigger_binding',
+    'check_input_type_annotation', 'check_output_type_annotation',
+    'from_incoming_proto', 'to_outgoing_proto', 'TraceContext',
+)
