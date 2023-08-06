c2995ab0eb430a0af08d195d6af84edea7096d17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:15 +0000

    Add package watch-diff/1.0.4/watch-diff-1.0.4.tar.gz

---
+++ packages/watch-diff/watch-diff-1.0.4.tar.gz/watch-diff-1.0.4/watch_diff/__init__.py
@@ -0,0 +1,10 @@
+"""
+"""
+
+__version__ = "1.0.4"
+
+
+from .command import Command
+from .diff import Diff
+from .email import Email
+from .format import DefaultFormatter, ConsoleFormatter, HTMLFormatter, OutputFormatting
