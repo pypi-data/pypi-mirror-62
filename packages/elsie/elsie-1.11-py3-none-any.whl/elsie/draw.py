6bd14950102e7960b7926616217c99ded98dc463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:19 +0000

    Add package elsie/1.11/elsie-1.11-py3-none-any.whl

---
+++ packages/elsie/elsie-1.11-py3-none-any.whl/elsie/draw.py
@@ -0,0 +1,93 @@
+def set_font_from_style(xml, style):
+    if "font" in style:
+        xml.set("font-family", style["font"])
+    if "size" in style:
+        xml.set("font-size", style["size"])
+
+    s = ""
+    if "color" in style:
+        s += "fill:{};".format(style["color"])
+    if style.get("bold", False):
+        s += "font-weight: bold;"
+    if style.get("italic", False):
+        s += "font-style: italic;"
+    if s:
+        xml.set("style", s)
+
+
+def draw_text(xml, x, y, parsed_text, style, styles, id=None, id_index=None, transform=None):
+    xml.element("text")
+
+    if id is not None and id_index is None:
+        xml.set("id", id)
+
+    xml.set("x", x)
+    xml.set("y", y)
+
+    if transform is not None:
+        xml.set("transform", transform)
+
+    anchor = {
+        "left": "start",
+        "middle": "middle",
+        "right": "end"
+    }
+
+    xml.set("text-anchor", anchor[style["align"]])
+
+    set_font_from_style(xml, style)
+
+    line_size = style["size"] * style["line_spacing"]
+    active_styles = [style]
+
+    xml.element("tspan")
+    if "variant-numeric" in style:
+        xml.set("font-variant-numeric", style["variant-numeric"])
+
+    for i, (token_type, value) in enumerate(parsed_text):
+        if token_type == "text":
+            xml.text(value)
+        elif token_type == "newline":
+            for s in active_styles:
+                xml.close("tspan")  # tspan
+            for i, s in enumerate(active_styles):
+                if s is None:
+                    continue
+                xml.element("tspan")
+                xml.set("xml:space", "preserve")
+                if i == 0:
+                    xml.set("x", x)
+                    xml.set("dy", line_size * value)
+                set_font_from_style(xml, s)
+        elif token_type == "begin":
+            is_dummy = value and value[0] == "#"
+            s = styles.get(value, None)
+            if s is None and not is_dummy:
+                raise Exception("Style '{}' not found".format(value))
+            active_styles.append(s)
+            xml.element("tspan")
+            if id is not None and id_index == i:
+                xml.set("id", id)
+            xml.set("xml:space", "preserve")
+            if not is_dummy:
+                set_font_from_style(xml, s)
+        elif token_type == "end":
+            xml.close("tspan")
+            active_styles.pop()
+        else:
+            raise Exception("Invalid token")
+
+    for s in active_styles:
+        if s is not None:
+            xml.close("tspan")
+    xml.close("text")
+
+
+def draw_bitmap(xml, x, y, width, height, mime, data):
+    xml.element("image")
+    xml.set("x", x)
+    xml.set("y", y)
+    xml.set("width", width)
+    xml.set("height", height)
+    xml.set("xlink:href", "data:{};base64,{}".format(mime, data), escape=False)
+    xml.close("image")
---
+++ packages/elsie/elsie-1.11-py3-none-any.whl/elsie/slidecls.py
@@ -0,0 +1,94 @@
+# The file is not named "slide.py" because of class of "slide" import in __init__.py
+
+import hashlib
+import os.path
+
+from .box import Box
+from .geom import Rect
+from .rcontext import RenderingContext
+from .show import ShowInfo
+from .svg import svg_begin, svg_end, convert_to_pdf
+from .sxml import Xml
+
+
+class Slide:
+
+    def __init__(self, index, width, height, styles, temp_cache, view_box, name, debug_boxes):
+        self.width = width
+        self.height = height
+        self.index = index
+        self.view_box = view_box
+        self.debug_boxes = debug_boxes
+        self._box = Box(self, 0, 0, width, height, styles, ShowInfo(), name=name)
+        self._box._styles = styles
+        self.max_step = 1
+        self.temp_cache = temp_cache
+
+    def box(self):
+        return self._box
+
+    def queries(self):
+        queries = []
+        self._box._traverse(lambda box: queries.extend(box._queries))
+        return queries
+
+    def prepare(self):
+        rect = Rect(0, 0, self.width, self.height)
+        self._box._set_rect(rect)
+
+    def steps(self):
+        shows = [1]
+        self._box._traverse(lambda box: shows.append(box._min_steps()))
+        return max(value for value in shows if value)
+
+    def make_svg(self, step):
+        xml = Xml()
+        svg_begin(xml, self.width, self.height, self.view_box)
+        ctx = RenderingContext(xml, step, self.debug_boxes)
+        painters = self._box._get_painters(ctx, 0)
+        painters.sort(key=lambda p: p.z_level)
+        for p in painters:
+            p.render(ctx)
+        svg_end(xml)
+        return xml.to_string()
+
+    def render(self, step, cache_dir, pdfs_in_dir, debug):
+        svg = self.make_svg(step)
+
+        h = hashlib.sha1()
+        h.update(svg.encode())
+        pdf_name = h.hexdigest() + ".pdf"
+
+        if pdf_name in pdfs_in_dir:
+            return pdf_name
+
+        if debug:
+            svg_file = os.path.join(
+                cache_dir, "{}-{}.svg".format(self.index, step))
+            with open(svg_file, "w") as f:
+                f.write(svg)
+
+        full_name = os.path.join(cache_dir, pdf_name)
+        convert_to_pdf(svg, full_name)
+        return pdf_name
+
+
+class DummyPdfSlide:
+
+    def __init__(self, filename):
+        self.filename = os.path.abspath(filename)
+
+    def queries(self):
+        return []
+
+    def prepare(self):
+        pass
+
+    def steps(self):
+        return 1
+
+    def make_svg(self):
+        return None
+
+    def render(self, step, cache_dir, pdfs_in_dir, debug):
+        return self.filename
