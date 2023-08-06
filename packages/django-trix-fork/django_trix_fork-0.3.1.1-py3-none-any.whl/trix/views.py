e831fe583328b48d85c7690997613fec45c1d4f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:17 +0000

    Add package django-trix-fork/0.3.1.1/django_trix_fork-0.3.1.1-py3-none-any.whl

---
+++ packages/django-trix-fork/django_trix_fork-0.3.1.1-py3-none-any.whl/trix/views.py
@@ -0,0 +1,24 @@
+from django.http import JsonResponse
+from django.views.generic import View
+
+from trix.forms import AttachmentForm
+from .utils import is_valid_image_extension
+
+
+class AttachmentView(View):
+    def post(self, request, *args, **kwargs):
+        form = AttachmentForm(self.request.POST, self.request.FILES)
+
+        uploaded_attachment = request.FILES['file']
+
+        if is_valid_image_extension(uploaded_attachment):
+            if form.is_valid():
+                photo = form.save()
+                data = {'url': photo.file.url}
+            else:
+                data = {}
+            return JsonResponse(data)
+        else:
+            print("not valid")
+            data = {}
+            return JsonResponse(data)
