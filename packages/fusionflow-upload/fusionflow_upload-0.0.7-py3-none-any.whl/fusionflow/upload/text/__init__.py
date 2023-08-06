674e114b44c9a91840dc25027d0e7e517d33d224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:24 +0000

    Add package fusionflow-upload/0.0.7/fusionflow_upload-0.0.7-py3-none-any.whl

---
+++ packages/fusionflow-upload/fusionflow_upload-0.0.7-py3-none-any.whl/fusionflow/upload/text/__init__.py
@@ -0,0 +1,7 @@
+"""table template for semi-structure local files like csv, excel, ini, json etc.
+
+these files are like configure table 
+or tables that changes not very often
+
+use file path glob seach or re to get refreshed table content
+"""
