2043a6748ad82e6b1c5c773da7e1f69cecabc861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:32 +0000

    Add package mlrun/0.4.5/mlrun-0.4.5-py3-none-any.whl

---
+++ packages/mlrun/mlrun-0.4.5-py3-none-any.whl/mlrun/__init__.py
@@ -0,0 +1,40 @@
+# Copyright 2018 Iguazio
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__version__ = '0.4.5'
+
+from .run import (get_or_create_ctx, new_function, code_to_function,
+                  import_function, run_pipeline, run_local)
+from .db import get_run_db
+from .model import RunTemplate, NewTask, RunObject
+from .kfpops import mlrun_op
+from .config import config as mlconf
+from .runtimes import new_model_server
+from .platforms import mount_v3io, v3io_cred
+from .datastore import get_object
+from .projects import load_project, new_project
+
+
+from os import environ
+
+
+def get_version():
+    return __version__
+
+
+if 'IGZ_NAMESPACE_DOMAIN' in environ:
+    igz_domain = environ['IGZ_NAMESPACE_DOMAIN']
+    kfp_ep = 'https://dashboard.{}/pipelines'.format(igz_domain)
+    environ['KF_PIPELINES_UI_ENDPOINT'] = kfp_ep
+    mlconf.remote_host = mlconf.remote_host or igz_domain
---
+++ packages/mlrun/mlrun-0.4.5-py3-none-any.whl/mlrun/secrets.py
@@ -0,0 +1,66 @@
+# Copyright 2018 Iguazio
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ast import literal_eval
+from .utils import list2dict, run_keys
+from os import environ
+
+
+class SecretsStore:
+    def __init__(self):
+        self._secrets = {}
+
+    @classmethod
+    def from_dict(cls, struct: dict):
+        store = cls()
+        src_list = struct.get(run_keys.secrets)
+        if src_list and isinstance(src_list, list):
+            for src in src_list:
+                store._add_source(src['kind'], src.get('source'), src.get('prefix', ''))
+        return store
+
+    def to_dict(self, struct):
+        pass
+
+    def _add_source(self, kind, source='', prefix=''):
+
+        if kind == 'inline':
+            if isinstance(source, str):
+                source = literal_eval(source)
+            if not isinstance(source, dict):
+                raise ValueError("inline secrets must be of type dict")
+            for k, v in source.items():
+                self._secrets[prefix + k] = str(v)
+
+        elif kind == 'file':
+            with open(source) as fp:
+                lines = fp.read().splitlines()
+                secrets_dict = list2dict(lines)
+                for k, v in secrets_dict.items():
+                    self._secrets[prefix + k] = str(v)
+
+        elif kind == 'env':
+            for key in source.split(','):
+                k = key.strip()
+                self._secrets[prefix + k] = environ.get(k)
+
+    def get(self, key):
+        return self._secrets.get(key)
+
+    def get_all(self):
+        return self._secrets.copy()
+
+    def to_serial(self):
+        # todo: use encryption
+        return [{'kind': 'inline', 'source': self._secrets.copy()}]
