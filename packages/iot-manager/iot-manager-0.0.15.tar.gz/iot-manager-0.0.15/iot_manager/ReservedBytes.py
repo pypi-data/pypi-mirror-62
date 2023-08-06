06869bb57dfdd5075dc024c305d614cb909e8f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:05 +0000

    Add package iot-manager/0.0.15/iot-manager-0.0.15.tar.gz

---
+++ packages/iot-manager/iot-manager-0.0.15.tar.gz/iot-manager-0.0.15/iot_manager/ReservedBytes.py
@@ -0,0 +1,10 @@
+# default lib imports
+import enum
+
+
+# a list of reserved bytes which the Manager needs
+class ReservedBytes(enum.Enum):
+    # tells the device
+    GET_DATA = b'\x01'
+    HEARTBEAT = b'\x02'
+    END_CONNECTION = b'\x03'
