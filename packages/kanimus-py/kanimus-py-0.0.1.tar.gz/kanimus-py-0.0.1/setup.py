20f2342f5b9d778e8dea618553b948ed4d6966e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:12 +0000

    Add package kanimus-py/0.0.1/kanimus-py-0.0.1.tar.gz

---
+++ packages/kanimus-py/kanimus-py-0.0.1.tar.gz/kanimus-py-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kanimus-py",
+    version="0.0.1",
+    author="Bob Gold",
+    author_email="kanimus.developers@gmail.com",
+    description="Python common library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/spacecloud.kanimus/backend/kanimus-py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
