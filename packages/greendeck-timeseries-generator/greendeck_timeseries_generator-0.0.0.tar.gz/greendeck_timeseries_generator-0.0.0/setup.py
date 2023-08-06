b4e9f77ab3474df223d24407185fdfaa94675a0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:41 +0000

    Add package greendeck-timeseries-generator/0.0.0/greendeck_timeseries_generator-0.0.0.tar.gz

---
+++ packages/greendeck-timeseries-generator/greendeck_timeseries_generator-0.0.0.tar.gz/greendeck_timeseries_generator-0.0.0/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+setuptools.setup(
+    name = "greendeck_timeseries_generator",
+    __version__ = "1.0.0",
+    author = "sambal shikhar",
+    author_email = "sambal@greendeck.co",
+    url = "",
+)
+
