616dc16bff0becce5fefb57ec605d33b379a9cc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:34 +0000

    Add package highresnet/0.10.2/highresnet-0.10.2.tar.gz

---
+++ packages/highresnet/highresnet-0.10.2.tar.gz/highresnet-0.10.2/tests/test_highresnet.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `highresnet` package."""
+
+
+import unittest
+from click.testing import CliRunner
+
+from highresnet.cli import deepgif
+
+
+class TestHighresnet(unittest.TestCase):
+    """Tests for `highresnet` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
+
+    def test_command_line_interface(self):
+        """Test the CLI."""
