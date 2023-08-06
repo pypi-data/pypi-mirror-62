f0dc14b55a30a6de9e40c9f05c820f0bdaad022e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:49 +0000

    Add package Blogstrap/0.5.0/Blogstrap-0.5.0-py3-none-any.whl

---
+++ packages/Blogstrap/Blogstrap-0.5.0-py3-none-any.whl/blogstrap/context.py
@@ -0,0 +1,36 @@
+# Copyright 2019 Joe H. Rahme <joehakimrahme@gmail.com>
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+import os
+
+
+def context(app, message=None):
+    def toc():
+        result = []
+        for entry in os.listdir(app.config['BLOGROOT']):
+            if os.path.isfile(entry) and not entry.startswith("."):
+                result.append("* [{}](../{})".format(entry, entry))
+        return "\n".join(result)
+
+    context = {
+        "author": app.config['AUTHOR'],
+        "description": app.config['DESCRIPTION'],
+        "lang": app.config['DEFAULT_LANG'],
+        "title": app.config['BLOGTITLE'],
+        "toc": toc()
+    }
+    if message:
+        context['text'] = message['content']
+        context.update(message['metadata'])
+    return context
