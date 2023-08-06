7909b026b1b34b241fd133b44bda1ee0961d5a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:17 +0000

    Add package awkward1/0.2.0/awkward1-0.2.0-cp37-cp37m-win32.whl

---
+++ packages/awkward1/awkward1-0.2.0-cp37-cp37m-win32.whl/awkward1-0.2.0.data/data/include/awkward/array/IndexedArray.h
@@ -0,0 +1,94 @@
+// BSD 3-Clause License; see https://github.com/jpivarski/awkward-1.0/blob/master/LICENSE
+
+#ifndef AWKWARD_INDEXEDARRAY_H_
+#define AWKWARD_INDEXEDARRAY_H_
+
+#include <string>
+#include <memory>
+#include <vector>
+
+#include "awkward/cpu-kernels/util.h"
+#include "awkward/Slice.h"
+#include "awkward/Index.h"
+#include "awkward/Content.h"
+
+namespace awkward {
+  template <typename T, bool ISOPTION>
+  class EXPORT_SYMBOL IndexedArrayOf: public Content {
+  public:
+    IndexedArrayOf<T, ISOPTION>(const std::shared_ptr<Identities>& identities, const util::Parameters& parameters, const IndexOf<T>& index, const std::shared_ptr<Content>& content);
+    const IndexOf<T> index() const;
+    const std::shared_ptr<Content> content() const;
+    bool isoption() const;
+    const std::shared_ptr<Content> project() const;
+    const std::shared_ptr<Content> project(const Index8& mask) const;
+    const Index8 bytemask() const;
+    const std::shared_ptr<Content> simplify() const;
+    T index_at_nowrap(int64_t at) const;
+
+    const std::string classname() const override;
+    void setidentities() override;
+    void setidentities(const std::shared_ptr<Identities>& identities) override;
+    const std::shared_ptr<Type> type(const std::map<std::string, std::string>& typestrs) const override;
+    const std::string tostring_part(const std::string& indent, const std::string& pre, const std::string& post) const override;
+    void tojson_part(ToJson& builder) const override;
+    void nbytes_part(std::map<size_t, int64_t>& largest) const override;
+    int64_t length() const override;
+    const std::shared_ptr<Content> shallow_copy() const override;
+    const std::shared_ptr<Content> deep_copy(bool copyarrays, bool copyindexes, bool copyidentities) const override;
+    void check_for_iteration() const override;
+    const std::shared_ptr<Content> getitem_nothing() const override;
+    const std::shared_ptr<Content> getitem_at(int64_t at) const override;
+    const std::shared_ptr<Content> getitem_at_nowrap(int64_t at) const override;
+    const std::shared_ptr<Content> getitem_range(int64_t start, int64_t stop) const override;
+    const std::shared_ptr<Content> getitem_range_nowrap(int64_t start, int64_t stop) const override;
+    const std::shared_ptr<Content> getitem_field(const std::string& key) const override;
+    const std::shared_ptr<Content> getitem_fields(const std::vector<std::string>& keys) const override;
+    const std::shared_ptr<Content> getitem_next(const std::shared_ptr<SliceItem>& head, const Slice& tail, const Index64& advanced) const override;
+    const std::shared_ptr<Content> carry(const Index64& carry) const override;
+    const std::string purelist_parameter(const std::string& key) const override;
+    bool purelist_isregular() const override;
+    int64_t purelist_depth() const override;
+    const std::pair<int64_t, int64_t> minmax_depth() const override;
+    const std::pair<bool, int64_t> branch_depth() const override;
+    int64_t numfields() const override;
+    int64_t fieldindex(const std::string& key) const override;
+    const std::string key(int64_t fieldindex) const override;
+    bool haskey(const std::string& key) const override;
+    const std::vector<std::string> keys() const override;
+
+    // operations
+    const Index64 count64() const override;
+    const std::shared_ptr<Content> count(int64_t axis) const override;
+    const std::shared_ptr<Content> flatten(int64_t axis) const override;
+    bool mergeable(const std::shared_ptr<Content>& other, bool mergebool) const override;
+    const std::shared_ptr<Content> reverse_merge(const std::shared_ptr<Content>& other) const;
+    const std::shared_ptr<Content> merge(const std::shared_ptr<Content>& other) const override;
+    const std::shared_ptr<SliceItem> asslice() const override;
+    const std::shared_ptr<Content> reduce_next(const Reducer& reducer, int64_t negaxis, const Index64& parents, int64_t outlength, bool mask, bool keepdims) const override;
+
+    const std::shared_ptr<Content> getitem_next(const SliceAt& at, const Slice& tail, const Index64& advanced) const override;
+    const std::shared_ptr<Content> getitem_next(const SliceRange& range, const Slice& tail, const Index64& advanced) const override;
+    const std::shared_ptr<Content> getitem_next(const SliceArray64& array, const Slice& tail, const Index64& advanced) const override;
+    const std::shared_ptr<Content> getitem_next(const SliceJagged64& jagged, const Slice& tail, const Index64& advanced) const override;
+    const std::shared_ptr<Content> getitem_next_jagged(const Index64& slicestarts, const Index64& slicestops, const SliceArray64& slicecontent, const Slice& tail) const override;
+    const std::shared_ptr<Content> getitem_next_jagged(const Index64& slicestarts, const Index64& slicestops, const SliceMissing64& slicecontent, const Slice& tail) const override;
+    const std::shared_ptr<Content> getitem_next_jagged(const Index64& slicestarts, const Index64& slicestops, const SliceJagged64& slicecontent, const Slice& tail) const override;
+
+  protected:
+    template <typename S>
+    const std::shared_ptr<Content> getitem_next_jagged_generic(const Index64& slicestarts, const Index64& slicestops, const S& slicecontent, const Slice& tail) const;
+
+  private:
+    const IndexOf<T> index_;
+    const std::shared_ptr<Content> content_;
+  };
+
+  typedef IndexedArrayOf<int32_t, false>  IndexedArray32;
+  typedef IndexedArrayOf<uint32_t, false> IndexedArrayU32;
+  typedef IndexedArrayOf<int64_t, false>  IndexedArray64;
+  typedef IndexedArrayOf<int32_t, true>   IndexedOptionArray32;
+  typedef IndexedArrayOf<int64_t, true>   IndexedOptionArray64;
+}
+
+#endif // AWKWARD_INDEXEDARRAY_H_
---
+++ packages/awkward1/awkward1-0.2.0-cp37-cp37m-win32.whl/awkward1-0.2.0.data/data/include/awkward/io/json.h
@@ -0,0 +1,112 @@
+// BSD 3-Clause License; see https://github.com/jpivarski/awkward-1.0/blob/master/LICENSE
+
+#ifndef AWKWARD_IO_JSON_H_
+#define AWKWARD_IO_JSON_H_
+
+#include <cstdio>
+#include <string>
+
+#include "awkward/builder/ArrayBuilderOptions.h"
+#include "awkward/cpu-kernels/util.h"
+#include "awkward/util.h"
+
+namespace awkward {
+  class Content;
+
+  EXPORT_SYMBOL const std::shared_ptr<Content> FromJsonString(const char* source, const ArrayBuilderOptions& options);
+  EXPORT_SYMBOL const std::shared_ptr<Content> FromJsonFile(FILE* source, const ArrayBuilderOptions& options, int64_t buffersize);
+
+  class EXPORT_SYMBOL ToJson {
+  public:
+    virtual void null() = 0;
+    virtual void boolean(bool x) = 0;
+    virtual void integer(int64_t x) = 0;
+    virtual void real(double x) = 0;
+    virtual void string(const char* x, int64_t length) = 0;
+    virtual void beginlist() = 0;
+    virtual void endlist() = 0;
+    virtual void beginrecord() = 0;
+    virtual void field(const char* x) = 0;
+    virtual void endrecord() = 0;
+  };
+
+  class EXPORT_SYMBOL ToJsonString: public ToJson {
+  public:
+    ToJsonString(int64_t maxdecimals);
+    ~ToJsonString();
+    void null() override;
+    void boolean(bool x) override;
+    void integer(int64_t x) override;
+    void real(double x) override;
+    void string(const char* x, int64_t length) override;
+    void beginlist() override;
+    void endlist() override;
+    void beginrecord() override;
+    void field(const char* x) override;
+    void endrecord() override;
+    const std::string tostring();
+  private:
+    class Impl;
+    Impl* impl_;
+  };
+
+  class EXPORT_SYMBOL ToJsonPrettyString: public ToJson {
+  public:
+    ToJsonPrettyString(int64_t maxdecimals);
+    ~ToJsonPrettyString();
+    void null() override;
+    void boolean(bool x) override;
+    void integer(int64_t x) override;
+    void real(double x) override;
+    void string(const char* x, int64_t length) override;
+    void beginlist() override;
+    void endlist() override;
+    void beginrecord() override;
+    void field(const char* x) override;
+    void endrecord() override;
+    const std::string tostring();
+  private:
+    class Impl;
+    Impl* impl_;
+  };
+
+  class EXPORT_SYMBOL ToJsonFile: public ToJson {
+  public:
+    ToJsonFile(FILE* destination, int64_t maxdecimals, int64_t buffersize);
+    ~ToJsonFile();
+    void null() override;
+    void boolean(bool x) override;
+    void integer(int64_t x) override;
+    void real(double x) override;
+    void string(const char* x, int64_t length) override;
+    void beginlist() override;
+    void endlist() override;
+    void beginrecord() override;
+    void field(const char* x) override;
+    void endrecord() override;
+  private:
+    class Impl;
+    Impl* impl_;
+  };
+
+  class EXPORT_SYMBOL ToJsonPrettyFile: public ToJson {
+  public:
+    ToJsonPrettyFile(FILE* destination, int64_t maxdecimals, int64_t buffersize);
+    ~ToJsonPrettyFile();
+    void null() override;
+    void boolean(bool x) override;
+    void integer(int64_t x) override;
+    void real(double x) override;
+    void string(const char* x, int64_t length) override;
+    void beginlist() override;
+    void endlist() override;
+    void beginrecord() override;
+    void field(const char* x) override;
+    void endrecord() override;
+  private:
+    class Impl;
+    Impl* impl_;
+  };
+}
+
+#endif // AWKWARD_IO_JSON_H_
