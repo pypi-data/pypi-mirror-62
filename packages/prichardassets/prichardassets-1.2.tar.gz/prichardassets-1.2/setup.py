5e1aca2338403918aca92899a172dc1f15ba9d14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:45 +0000

    Add package prichardassets/1.2/prichardassets-1.2.tar.gz

---
+++ packages/prichardassets/prichardassets-1.2.tar.gz/prichardassets-1.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="prichardassets",
+    version="1.2",
+    description="A module with a static file in it",
+    author="prichard",
+    author_email="philippe.richard93@gmail.com",
+    include_package_data=True,
+    packages=['prichardassets'],
+    package_data={'prichardassets': ['*.txt']},
+    install_requires=[],
+)
