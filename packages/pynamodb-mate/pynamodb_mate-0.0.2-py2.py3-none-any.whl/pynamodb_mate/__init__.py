7f3ed7968a485bb9a6972e66e19902faf71cddf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:39 +0000

    Add package pynamodb-mate/0.0.2/pynamodb_mate-0.0.2-py2.py3-none-any.whl

---
+++ packages/pynamodb-mate/pynamodb_mate-0.0.2-py2.py3-none-any.whl/pynamodb_mate/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from ._version import __version__
+
+__short_description__ = "Provide Addtional Features for pynamodb"
+__license__ = "MIT"
+__author__ = "Sanhe Hu"
+__author_email__ = "husanhe@gmail.com"
+__github_username__ = "MacHu-GWU"
