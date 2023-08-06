34dfb65edf8a69409a4e9278bbb4f0a325aa4bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:21 +0000

    Add package conan-package-tools/0.31.2/conan_package_tools-0.31.2.tar.gz

---
+++ packages/conan-package-tools/conan_package_tools-0.31.2.tar.gz/conan_package_tools-0.31.2/cpt/__init__.py
@@ -0,0 +1,9 @@
+
+__version__ = '0.31.2'
+NEWEST_CONAN_SUPPORTED = "1.22.300"
+
+def get_client_version():
+    from conans.model.version import Version
+    from conans import __version__ as client_version
+    # It is a mess comparing dev versions, lets assume that the -dev is the further release
+    return Version(client_version.replace("-dev", ""))
