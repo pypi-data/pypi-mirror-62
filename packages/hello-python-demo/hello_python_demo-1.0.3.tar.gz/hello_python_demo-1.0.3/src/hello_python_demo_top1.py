50d3754b461a240ad1ac27b3c2c3774136db36cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:19 +0000

    Add package hello-python-demo/1.0.3/hello_python_demo-1.0.3.tar.gz

---
+++ packages/hello-python-demo/hello_python_demo-1.0.3.tar.gz/hello_python_demo-1.0.3/src/hello_python_demo_top1.py
@@ -0,0 +1,12 @@
+import request
+
+
+def hello_python_demo():
+    print("hello python!")
+
+
+def hello_python_demo_request():
+    print(request)
+
+def hello_python_demo22():
+    print("hello python!")
---
+++ packages/hello-python-demo/hello_python_demo-1.0.3.tar.gz/hello_python_demo-1.0.3/src/hello_python_demo_top2.py
@@ -0,0 +1,5 @@
+def hello_python_demo():
+    print("hello python old!")
+
+def hello_python_demo22():
+    print("hello python!")
