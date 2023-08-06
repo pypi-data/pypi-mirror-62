bb1497eaff7fce8a1eb619e3c1b97476bab55ac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:11 +0000

    Add package django-video-transcoding/0.0.9/django_video_transcoding-0.0.9.tar.gz

---
+++ packages/django-video-transcoding/django_video_transcoding-0.0.9.tar.gz/django_video_transcoding-0.0.9/src/video_transcoding/helpers.py
@@ -0,0 +1,21 @@
+from celery.result import AsyncResult
+
+from video_transcoding import tasks
+from video_transcoding import models
+
+
+def send_transcode_task(video: models.Video) -> AsyncResult:
+    """
+    Send a video transcoding task.
+
+    If task is successfully sent to broker, Video status is changed to QUEUED
+    and Celery task identifier is saved.
+
+    :param video: video object
+    :type video: video.models.Video
+    :returns: Celery task result
+    :rtype: celery.result.AsyncResult
+    """
+    result = tasks.transcode_video.apply_async(args=(video.pk,), countdown=10)
+    video.change_status(video.QUEUED, task_id=result.task_id)
+    return result
