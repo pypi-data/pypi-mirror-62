68fa7091fd5f3c3ac6e1750cb76a861324103baf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:18 +0000

    Add package HelmState/1.0.6/HelmState-1.0.6-py2.py3-none-any.whl

---
+++ packages/HelmState/HelmState-1.0.6-py2.py3-none-any.whl/HelmState/__main__.py
@@ -0,0 +1,12 @@
+from HelmState import Main
+import sys
+
+
+def main():
+    """Entry point for the application script"""
+    return Main.Main()
+
+
+if __name__ == "__main__":
+    sys.stdout.write(main())
+    sys.exit(0)
