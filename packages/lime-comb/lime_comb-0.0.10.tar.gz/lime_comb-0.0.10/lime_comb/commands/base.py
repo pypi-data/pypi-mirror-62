b890e31fcd6af65bee565cc257a2078a8c2e6f76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:55 +0000

    Add package lime-comb/0.0.10/lime_comb-0.0.10.tar.gz

---
+++ packages/lime-comb/lime_comb-0.0.10.tar.gz/lime_comb-0.0.10/lime_comb/commands/base.py
@@ -0,0 +1,28 @@
+import abc
+import argparse
+import os
+
+import email_validator
+
+from lime_comb.firestore.database import get_gpgs
+from lime_comb.gpg import import_gpg_key
+
+
+class Command(metaclass=abc.ABCMeta):
+    pass
+
+
+def import_keys(email, cred, *, key_type="pub"):
+    for key_str in get_gpgs(cred, email, key_type=key_type):
+        import_gpg_key(key_str)
+
+
+def validate_filepath(fp):
+    if not os.path.isfile(fp):
+        raise argparse.ArgumentTypeError(f"have to be file")
+    return fp
+
+
+def validate_email(email):
+    email_validator.validate_email(email)
+    return email
