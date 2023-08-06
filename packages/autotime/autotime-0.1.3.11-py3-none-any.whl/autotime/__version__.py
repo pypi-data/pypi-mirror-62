7f8fcdbefbcd231ff03d4344461b0da1dcd68414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:36 +0000

    Add package autotime/0.1.3.11/autotime-0.1.3.11-py3-none-any.whl

---
+++ packages/autotime/autotime-0.1.3.11-py3-none-any.whl/autotime/__version__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Specifies the version of the Deep Time-Series package."""
+
+__title__ = "autotime"
+__author__ = "Kevin R. Chen"
+__description__ = "Automated ML-based predictive analytics framework for time-series data."
+__url__ = "https://github.com/Kevin-Chen0/auto-time-series.git"
+__version__ = "0.1.3.11"
+__license__ = "Apache License 2.0"
+__copyright__ = "2020 Kevin R. Chen"
