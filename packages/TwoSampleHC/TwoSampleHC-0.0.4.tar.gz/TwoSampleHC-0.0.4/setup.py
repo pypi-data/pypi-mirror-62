8d3972e24833bad7cc09bcb5fc4f399a2699652f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:25 +0000

    Add package TwoSampleHC/0.0.4/TwoSampleHC-0.0.4.tar.gz

---
+++ packages/TwoSampleHC/TwoSampleHC-0.0.4.tar.gz/TwoSampleHC-0.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="TwoSampleHC", 
+    version="0.0.4",
+    author="Alon Kipnis",
+    author_email="alonkipnis@gmail.com",
+    description="Two-sample Higher Criticism",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alonkipnis/TwoSampleHC",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
