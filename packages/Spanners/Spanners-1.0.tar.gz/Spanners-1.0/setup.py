5043894b501c66bfc537996b4faa6e089730f1b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:45 +0000

    Add package Spanners/1.0/Spanners-1.0.tar.gz

---
+++ packages/Spanners/Spanners-1.0.tar.gz/Spanners-1.0/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python2
+
+import codecs
+from os import path
+from setuptools import setup
+
+pwd = path.abspath(path.dirname(__file__))
+with codecs.open(path.join(pwd, 'README.md'), 'r', encoding='utf8') as input:
+    long_description = input.read()
+
+version='1.0'
+	
+setup(
+	name='Spanners',
+	version=version,
+	license='MIT',
+    long_description=long_description,
+	long_description_content_type="text/markdown",
+	url='https://github.com/eddo888/Spanners',
+	download_url='https://github.com/eddo888/Spanners/archive/%s.tar.gz'%version,
+	author='David Edson',
+	author_email='eddo888@tpg.com.au',
+	packages=[
+		'Spanners',
+	],
+	install_requires=[
+		'asciitree',
+		'bs4',
+		'arrow',
+		'Baubles',
+		'Perdy',
+		'Argumental',
+	],
+	scripts=[
+		'bin/squirrel.py',
+		'bin/treeify.py',
+	],
+)
