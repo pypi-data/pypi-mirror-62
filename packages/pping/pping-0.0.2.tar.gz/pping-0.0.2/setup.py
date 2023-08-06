8fad46a07d7ae9c9e24e2699006993cb35633c2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:23 +0000

    Add package pping/0.0.2/pping-0.0.2.tar.gz

---
+++ packages/pping/pping-0.0.2.tar.gz/pping-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pping',
+    version='0.0.2',
+    author='JLMin',
+    description='ping in python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/JLMin/pping',
+    license='MIT',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+    python_requires='>=3.6',
+)
