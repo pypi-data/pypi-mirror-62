64f8d67d65bcd76cd72884b36fba237968c24650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:22 +0000

    Add package emmental/0.0.5/emmental-0.0.5-py3-none-any.whl

---
+++ packages/emmental/emmental-0.0.5-py3-none-any.whl/emmental/metrics/roc_auc.py
@@ -0,0 +1,43 @@
+import logging
+from typing import Dict, List, Optional
+
+from numpy import ndarray
+from sklearn.metrics import roc_auc_score
+
+from emmental.utils.utils import pred_to_prob
+
+logger = logging.getLogger(__name__)
+
+
+def roc_auc_scorer(
+    golds: ndarray,
+    probs: ndarray,
+    preds: Optional[ndarray],
+    uids: Optional[List[str]] = None,
+) -> Dict[str, float]:
+    """ROC AUC.
+
+    Args:
+      golds(ndarray): Ground truth values.
+      probs(ndarray): Predicted probabilities.
+      preds(ndarray or None): Predicted values.
+      uids(list, optional): Unique ids, defaults to None.
+      pos_label(int, optional): The positive class label, defaults to 1.
+
+    Returns:
+      dict: ROC AUC score.
+
+    """
+
+    gold_probs = pred_to_prob(golds, n_classes=probs.shape[1])
+
+    try:
+        roc_auc = roc_auc_score(gold_probs, probs)
+    except ValueError:
+        logger.warning(
+            "Only one class present in golds."
+            "ROC AUC score is not defined in that case, set as nan instead."
+        )
+        roc_auc = float("nan")
+
+    return {"roc_auc": roc_auc}
