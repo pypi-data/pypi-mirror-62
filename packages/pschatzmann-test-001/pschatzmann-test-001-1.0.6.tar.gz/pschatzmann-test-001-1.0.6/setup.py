a2bf7c77e7b0014a3eb703f2a5fba0145d4984c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:19 +0000

    Add package pschatzmann-test-001/1.0.6/pschatzmann-test-001-1.0.6.tar.gz

---
+++ packages/pschatzmann-test-001/pschatzmann-test-001-1.0.6.tar.gz/pschatzmann-test-001-1.0.6/setup.py
@@ -0,0 +1,61 @@
+###
+# Installs the iopenscad module and then the kernel description
+#
+
+import pathlib, os, distutils.core, setuptools 
+from setuptools import setup
+from setuptools.command import easy_install
+from setuptools.command.install import install
+import setuptools.command.build_py
+import pkg_resources
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# Extended install which also installs the kernel
+class InstallKernel(setuptools.command.build_py.build_py):
+    def run(self):
+        setuptools.command.build_py.build_py.run(self)
+        self.installKernel()
+
+    def installKernel(self):
+        kernel = pkg_resources.resource_filename("iopenscad","kernel.json")
+        if not os.path.exists(kernel):
+            raise Exception('The kernel does not exist {}'.format(kernel))
+
+        directory = str(pathlib.Path(kernel).parent)
+        if not os.path.isdir(directory):
+            raise Exception('The directory does not exist {}'.format(directory))
+
+        cmd = 'jupyter kernelspec install ' + directory
+        if os.system(cmd) != 0:
+            raise Exception('Could not install kernelspec {}'.format(cmd))
+
+# This call to setup() does all the work
+setup(
+    name="pschatzmann-test-001",
+    version="1.0.6",
+    description="Jupyter kernel for OpenSCAD",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pschatzmann/openscad-kernel",
+    author="Phil Schatzmann",
+    author_email="phil.schatzmann@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["iopenscad"],
+    include_package_data=True,
+    install_requires=["jupyter"],
+    cmdclass={'build_py': InstallKernel}
+
+)
+
+
+
