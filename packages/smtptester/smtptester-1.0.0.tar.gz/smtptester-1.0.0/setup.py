d59e92c1b4f1d8f888aa72eca8bc4fa7f811dcc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:24 +0000

    Add package smtptester/1.0.0/smtptester-1.0.0.tar.gz

---
+++ packages/smtptester/smtptester-1.0.0.tar.gz/smtptester-1.0.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import os
+import setuptools
+import smtptester
+
+setuptools.setup(
+    name="smtptester",
+    version=smtptester.VERSION,
+    author=smtptester.AUTHOR,
+    author_email="mike@conigliaro.org",
+    description="A graphical and command line SMTP diagnostic tool",
+    long_description=open(os.path.join(os.path.dirname(__file__), "README.md")).read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/mconigliaro/smtptester",
+    packages=setuptools.find_packages(),
+    package_data={"smtptester": ["data/*"]},
+    scripts=["bin/smtptester", "bin/smtptester-gui"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Environment :: X11 Applications :: Qt",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Communications :: Email",
+        "Topic :: System :: Systems Administration",
+        "Topic :: Utilities",
+    ],
+    python_requires=">=3.7",
+    requires=["dnspython (<2.0, >=1.16)", "pyside2 (< 6.0, >=5.14)"],
+)
