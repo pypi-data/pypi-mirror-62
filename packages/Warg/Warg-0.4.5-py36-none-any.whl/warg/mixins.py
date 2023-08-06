6156cbaa7bd98dc83e7087d468a8226d80bc66e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:19 +0000

    Add package Warg/0.4.5/Warg-0.4.5-py36-none-any.whl

---
+++ packages/Warg/Warg-0.4.5-py36-none-any.whl/warg/mixins.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import numpy
+
+__author__ = "Christian Heider Nielsen"
+__doc__ = ""
+
+__all__ = ["IterItemsMixin", "IterKeysMixin", "IterValuesMixin", "IndexDictTuplesMixin"]
+
+
+class IterItemsMixin(object):
+  def __iter__(self):
+    for attr, value in self.__dict__.items():
+      yield attr, value
+
+
+class IterKeysMixin(object):
+  def __iter__(self):
+    for attr in self.__dict__.keys():
+      yield attr
+
+
+class IterValuesMixin(object):
+  def __iter__(self):
+    a = self.__dict__
+    for value in a.values():
+      yield value
+
+
+class IndexDictTuplesMixin(object):
+  def __getitem__(self, item):
+    if isinstance(item, int):
+      a = self.__dict__
+      b = numpy.array(list(a.values()))
+      return b[item]
+    else:
+      return self.__dict__[item]
+
+
+if __name__ == "__main__":
+
+  def asd():
+    class IDTM(IndexDictTuplesMixin):
+      pass
+
+    a = IDTM()
+    a.a = 2
+    a.b = 3
+    assert a[0] == 2
