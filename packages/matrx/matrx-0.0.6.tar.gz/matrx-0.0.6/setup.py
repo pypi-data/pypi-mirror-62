e4b92c7f7f53d9ebbfc96d35f29dd5eecb016161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:41 +0000

    Add package matrx/0.0.6/matrx-0.0.6.tar.gz

---
+++ packages/matrx/matrx-0.0.6.tar.gz/matrx-0.0.6/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="matrx",
+    version="0.0.6",
+    author="The MATRX Development team at TNO.nl",
+    author_email="info@matrx-software.com",
+    description="A Python package for the rapid development of autonomous systems and human-agent teaming concepts.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/matrx-software/matrx",
+    packages=setuptools.find_packages(),
+    license="MIT License",
+    classifiers=[
+        "Development Status :: 4 - Beta",  # https://pypi.org/pypi?%3Aaction=list_classifiers
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+        "Intended Audience :: Science/Research",
+        "Environment :: Web Environment"
+    ],
+)
