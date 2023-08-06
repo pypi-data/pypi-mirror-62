fe1f0bb699dd4ede82fc7ddf5e8d9a2c8ef44cae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:31 +0000

    Add package andes/0.8.1/andes-0.8.1.tar.gz

---
+++ packages/andes/andes-0.8.1.tar.gz/andes-0.8.1/andes/models/__init__.py
@@ -0,0 +1,19 @@
+from .jit import JIT  # NOQA
+from collections import OrderedDict  # NOQA
+
+
+non_jit = OrderedDict([
+    ('bus', ['Bus']),
+    ('dcbase', ['Node', 'Ground', 'R', 'L', 'C', 'RCp', 'RCs', 'RLs', 'RLCs', 'RLCp']),
+    ('pq', ['PQ']),
+    ('pv', ['PV', 'Slack']),
+    ('shunt', ['Shunt']),
+    ('line', ['Line']),
+    ('area', ['Area']),
+    ('synchronous', ['GENCLS', 'GENROU']),
+    ('governor', ['TG2', 'TGOV1']),
+    ('exciter', ['EXDC2']),
+    ('vsc', ['VSCShunt']),
+    ('timer', ['Toggler', 'Fault']),
+    ('experimental', ['PI2']),
+])
