49821801aed7c74ceded9f6c7b11ac3f8bc7e55d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:01 +0000

    Add package summarizer4u/0.3.3/summarizer4u-0.3.3.tar.gz

---
+++ packages/summarizer4u/summarizer4u-0.3.3.tar.gz/summarizer4u-0.3.3/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python\r\n
+import setuptools
+from setuptools import setup
+setup(name='summarizer4u',
+version='0.3.3',
+description='Get your text summarized',
+url='https://github.com/Navan0',
+author='Navaneeth KT',
+author_email='nktclt@gmail.com',
+long_description=""" Get your text summarized ! \n \n from summarizer4u import summary \n \n text = summary("your text go here") \n \n print(text) """,
+long_description_content_type='text/markdown',
+packages=setuptools.find_packages(),
+classifiers=[
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.7",
+],
+scripts=["con.py"],
+install_requires=['spacy'],
+zip_safe=False)
+
+
+#python setup.py sdist upload -r pypi
