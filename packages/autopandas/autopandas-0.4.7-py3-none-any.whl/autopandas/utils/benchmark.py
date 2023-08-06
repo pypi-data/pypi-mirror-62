7dbd5a7b53d2b88f9d5e4fec151da7823315ce1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:12 +0000

    Add package autopandas/0.4.7/autopandas-0.4.7-py3-none-any.whl

---
+++ packages/autopandas/autopandas-0.4.7-py3-none-any.whl/autopandas/utils/benchmark.py
@@ -0,0 +1,88 @@
+# Benchmark Functions
+
+import numpy as np
+from sklearn.metrics import accuracy_score, r2_score, classification_report
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+#from autosklearn.classification import AutoSklearnClassifier
+#from autosklearn.regression import AutoSklearnRegressor
+
+# TODO:
+# - Train error bars
+# - Test error bars
+
+def score(data, model=None, metric=None, method='baseline', fit=True, test=None, verbose=False):
+    """ Benchmark, a.k.a. Utility.
+
+        Return the metric score of a model trained and tested on data.
+        If a test set is defined ('test' parameter), the model is trained on 'data' and tested on 'test'.
+
+        Parameters
+        ----------
+        model : Model to fit and test on data.
+        metric : scoring function.
+        method : 'baseline' or 'auto'. Useful only if model is None.
+        fit : If True, fit the model.
+        test : Test is an optional DataFrame to use as the test set.
+        verbose : If True, prints model information, classification report and metric function.
+
+        Returns
+        -------
+        float
+            Metric score of the model trained and tested on data.
+    """
+    if 'y' not in data.indexes:
+        raise Exception('No class defined. Please use set_class method before calling score.')
+    if model is None:
+        # Select model
+        if method == 'baseline':
+            clf = RandomForestClassifier()
+            reg = RandomForestRegressor()
+        elif method in ['auto', 'autosklearn', 'automl', 'automatic']:
+            raise Exception('autosklearn got removed from requirements. This method is currently not implemented.')
+            #clf = AutoSklearnClassifier()
+            #reg = AutoSklearnRegressor() # multi-ouput ??
+        else:
+            raise Exception('Unknown method: {}'.format(method))
+        # Select task
+        task = data.get_task()
+        if  task == 'classification':
+            model = clf
+            if metric is None:
+                metric = accuracy_score
+        elif task == 'regression':
+            model = reg
+            if metric is None:
+                metric = r2_score
+        else:
+            raise Exception('Unknown task: {}.'.format(task))
+    if test is not None: # test set is defined
+        X_train = data.get_data('X')
+        y_train = data.get_data('y')
+        X_test = test.get_data('X')
+        y_test = test.get_data('y')
+    else:
+        if 'test' not in data.indexes:
+            raise Exception('No train/test split. Please use train_test_split method before calling score.')
+        else:
+            X_train = data.get_data('X_train')
+            y_train = data.get_data('y_train')
+            X_test = data.get_data('X_test')
+            y_test = data.get_data('y_test')
+    # mono-class
+    if y_train.shape[1] == 1:
+        y_train = y_train.values.ravel()
+        y_test = y_test.values.ravel()
+    if fit:
+        model.fit(X_train, y_train)
+    # Let's go!
+    y_pred = model.predict(X_test)
+    if verbose:
+        print(model)
+        print(classification_report(y_test, y_pred))
+        print('Metric: {}'.format(metric))
+    try:
+        score = metric(y_test, y_pred, average='weighted', labels=np.unique(y_pred))
+    except:
+        score = metric(y_test, y_pred)
+    return score
+    #return model.score(X_test, y_test)
---
+++ packages/autopandas/autopandas-0.4.7-py3-none-any.whl/autopandas/utils/imputation.py
@@ -0,0 +1,48 @@
+# Functions for missing data imputation
+
+# TODO: add more complex methods (e.g. using predictive modeling)
+
+def mean(data, column):
+    """ Replace missing values by the mean of the column.
+
+        :param data: AutoData data
+        :param column: Column to impute
+        :return: Imputed data
+        :rtype: autopandas.AutoData
+    """
+    data[column] = data[column].fillna(data[column].mean())
+    return data
+
+def median(data, column):
+    """ Replace missing values by the median of the column.
+
+        :param data: AutoData data
+        :param column: Column to impute
+        :return: Imputed data
+        :rtype: autopandas.AutoData
+    """
+    data[column] = data[column].fillna(data[column].median())
+    return data
+
+def remove(data, columns):
+    """ Simply remove columns containing missing values.
+
+        :param data: AutoData data
+        :param column: Column to impute
+        :return: Imputed data
+        :rtype: autopandas.AutoData
+    """
+    data = data.dropna(axis=0, subset=columns)
+    return data
+
+def most(data, column):
+    """ Replace missing values by the most frequent value of the column.
+
+        :param data: AutoData data
+        :param column: Column to impute
+        :return: Imputed data
+        :rtype: autopandas.AutoData
+    """
+    most_frequent_value = data[column].value_counts().idxmax()
+    data[column] = data[column].fillna(most_frequent_value)
+    return data
