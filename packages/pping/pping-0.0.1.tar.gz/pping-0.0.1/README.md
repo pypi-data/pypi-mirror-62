9505ee0ea6d5d86f8e586932f5feebffa5a7809f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:13 +0000

    Add package pping/0.0.1/pping-0.0.1.tar.gz

---
+++ packages/pping/pping-0.0.1.tar.gz/pping-0.0.1/README.md
@@ -0,0 +1,49 @@
+# pping
+
+**ping** in python
+
+## Installing
+
+```python
+pip install pping
+```
+
+## Usage
+
+```python
+from pping import ping
+
+# simple usage
+result = ping('www.example.com')
+
+# full arguments (default value)
+result = ping('www.example.com',
+              repeat=4,    # Number of echo requests to send.
+              interval=1,  # Time in seconds interval between each request.
+              size=32,     # Send buffer size in bytes.
+              timeout=1,   # Timeout in seconds to wait for each reply.
+              ttl=128)     # Time To Live.
+
+# result properties
+print(result)
+result[index]
+# host
+result.hostname
+result.aliaslist
+result.ipaddrlist
+# route trip time
+result.times      # timed out are not inclued
+result.all_times  # timed out as None
+result.avg
+result.min
+result.max
+result.stdev
+# packets
+result.sent
+result.recv
+result.lost
+```
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE file for details
---
+++ packages/pping/pping-0.0.1.tar.gz/pping-0.0.1/pping/result.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+
+import statistics
+import socket
+from collections import namedtuple
+
+from .packet import Icmp, IPv4
+
+
+class Result:
+
+    def __init__(self, address):
+        (self.hostname,
+         self.aliaslist,
+         self.ipaddrlist) = socket.gethostbyname_ex(address)
+        self.responses = list()
+        self.times = list()
+        self.all_times = list()
+
+    def __getitem__(self, key):
+        return self.responses[key]
+
+    def __str__(self):
+        return '\n'.join(Result._prettify(r) for r in self.responses) + (
+            f'\n\nPing statistics for {self.hostname}:\n'
+            f'\tPackets: Sent = {self.sent}, '
+            f'Received = {self.recv}, '
+            f'Lost = {self.lost} ({round(self.lost / self.sent * 100)}% loss)\n'
+            f'Approximate round trip times in milli-seconds:\n'
+            f'\tAverage = {round(self.avg * 1000)}ms, '
+            f'Minimum = {round(self.min * 1000)}ms, '
+            f'Maximum = {round(self.max * 1000)}ms, '
+            f'Stdev = {round(self.stdev * 1000,1)}'
+        )
+
+    @staticmethod
+    def _prettify(resp):
+        if resp.status == Response.OK:
+            return (
+                f'Reply from {resp.src}: bytes={resp.size} '
+                f'time={round(resp.rtt * 1000)}ms TTL={resp.ttl}'
+            )
+        else:
+            return 'Request timed out.'
+
+    @property
+    def max(self):
+        try:
+            return max(self.times)
+        except ValueError:
+            return 0
+
+    @property
+    def min(self):
+        try:
+            return min(self.times)
+        except ValueError:
+            return 0
+
+    @property
+    def avg(self):
+        try:
+            return statistics.mean(self.times)
+        except statistics.StatisticsError:
+            return 0
+
+    @property
+    def stdev(self):
+        try:
+            return statistics.pstdev(self.times)
+        except statistics.StatisticsError:
+            return 0
+
+    @property
+    def sent(self):
+        return len(self.responses)
+
+    @property
+    def recv(self):
+        return len(self.times)
+
+    @property
+    def lost(self):
+        return len(self.responses) - len(self.times)
+
+    def append(self, response):
+        if response.status == Response.OK:
+            self.times.append(response.rtt)
+            self.all_times.append(response.rtt)
+        else:
+            self.all_times.append(None)
+        self.responses.append(response)
+
+
+class Response:
+    OK, TIMEDOUT = 'ok', 'timedout'
+
+    _Valid = namedtuple('Response', ['status', 'src', 'dst', 'ttl',
+                                     'size', 'seq', 'rtt'])
+
+    @staticmethod
+    def valid(*, packet, rtt):
+        ipv4 = IPv4.unpack(packet[:20])
+        icmp = Icmp.unpack(packet[20:])
+        return Response._Valid(Response.OK, ipv4.src, ipv4.dst, ipv4.ttl,
+                               len(icmp.payload), icmp.seq, rtt)
+
+    _Error = namedtuple('Response', ['status'])
+
+    @staticmethod
+    def timeout():
+        return Response._Error(Response.TIMEDOUT)
