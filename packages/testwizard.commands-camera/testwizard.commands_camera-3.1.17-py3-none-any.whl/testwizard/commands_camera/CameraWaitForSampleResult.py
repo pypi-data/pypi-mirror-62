2b6d72f75fcedf6b913c53f29dbf9b49e5400ae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:27 +0000

    Add package testwizard.commands-camera/3.1.17/testwizard.commands_camera-3.1.17-py3-none-any.whl

---
+++ packages/testwizard.commands-camera/testwizard.commands_camera-3.1.17-py3-none-any.whl/testwizard/commands_camera/CameraWaitForSampleResult.py
@@ -0,0 +1,20 @@
+import json
+import sys
+
+from testwizard.commands_core.ResultBase import ResultBase
+
+
+class CameraWaitForSampleResult(ResultBase):
+    def __init__(self, result, successMessage, failMessage):
+        ResultBase.__init__(
+            self, result["time"] >= 0 and result["errorCode"] == 0, successMessage, failMessage)
+
+        self.time = result["time"]
+        self.distance = result["distance"]
+        self.hasMatch = result["hasMatch"]
+        self.hasTimeout = result["hasTimeout"]
+
+        if self.success is True:
+            return
+
+        self.message = self.getMessageForErrorCode(self.message, result["errorCode"])
