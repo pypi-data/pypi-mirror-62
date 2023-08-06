951962ed74532ab9fd134f07309ed254c70da9aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:57 +0000

    Add package HotelLite/0.0.2/HotelLite-0.0.2.tar.gz

---
+++ packages/HotelLite/HotelLite-0.0.2.tar.gz/HotelLite-0.0.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+from setuptools import setup
+
+setuptools.setup(
+    name="HotelLite",  # Replace with your own username
+    version="0.0.2",
+    author="a18andrespv",
+    author_email="a18andrespv@iesteis.es",
+    url="https://github.com/andrespervil/Empresa",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
