19a608efe874090fb492f636d50906198117a2f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:28 +0000

    Add package fiduswriter/3.7.29/fiduswriter-3.7.29.tar.gz

---
+++ packages/fiduswriter/fiduswriter-3.7.29.tar.gz/fiduswriter-3.7.29/fiduswriter/base/django_handler_mixin.py
@@ -0,0 +1,44 @@
+import base64
+
+from builtins import object
+from django.contrib import auth
+from django.conf import settings
+from importlib import import_module
+
+
+class DjangoHandlerMixin(object):
+    def get_django_session(self):
+        if not hasattr(self, '_session'):
+            engine = \
+                import_module(settings.SESSION_ENGINE)
+            session_key = \
+                self.get_cookie(settings.SESSION_COOKIE_NAME)
+            self._session = engine.SessionStore(session_key)
+        return self._session
+
+    def get_current_user(self):
+        # get_user needs a django request object, but only looks at the session
+
+        class Dummy(object):
+            pass
+
+        django_request = Dummy()
+        django_request.session = self.get_django_session()
+        user = auth.get_user(django_request)
+        if user.is_authenticated:
+            return user
+        else:
+            # try basic auth
+            if 'Authorization' not in self.request.headers:
+                return None
+            (kind, data) = self.request.headers['Authorization'].split(' ')
+            if kind != 'Basic':
+                return None
+            data += "=" * ((4 - len(data) % 4) % 4)
+            (username, password) = base64.b64decode(
+                data
+            ).decode('utf-8').split(':')
+            user = auth.authenticate(username=username, password=password)
+            if user is not None and user.is_authenticated:
+                return user
+            return None
