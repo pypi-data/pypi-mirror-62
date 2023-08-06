01c57ab9fc6e59c16bb57f147ee95d85e4621689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:12 +0000

    Add package compas-rcf/0.2.0/compas_rcf-0.2.0.tar.gz

---
+++ packages/compas-rcf/compas_rcf-0.2.0.tar.gz/compas_rcf-0.2.0/src/compas_rcf/abb/__init__.py
@@ -0,0 +1,16 @@
+"""
+********************************************************************************
+compas_rcf.abb
+********************************************************************************
+.. currentmodule:: compas_rcf.abb
+
+ABB utilities & helpers
+-----------------------
+Fabrication communication is setup using ROS and compas_rrc with the controller.
+
+This package should be used for any helpers or utilities we might require.
+
+"""
+
+from .helpers import *  # noqa: F401,F403
+from .programs import *  # noqa: F401,F403
