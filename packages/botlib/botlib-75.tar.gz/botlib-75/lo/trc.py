d02d1d8df884780265efa0615dd15fd39b95fd41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:37 +0000

    Add package botlib/75/botlib-75.tar.gz

---
+++ packages/botlib/botlib-75.tar.gz/botlib-75/lo/trc.py
@@ -0,0 +1,26 @@
+# LIBOBJ - object library.
+#
+# trace
+
+import sys
+import traceback
+
+def get_exception(txt="", sep=""):
+    exctype, excvalue, tb = sys.exc_info()
+    trace = traceback.extract_tb(tb)
+    result = ""
+    for elem in trace:
+        fname = elem[0]
+        linenr = elem[1]
+        func = elem[2]
+        plugfile = fname[:-3].split(os.sep)
+        mod = []
+        for elememt in plugfile[::-1]:
+            mod.append(elememt)
+            if elememt == "lo" or element == "bot":
+                break
+        ownname = '.'.join(mod[::-1])
+        result += "%s:%s %s %s " % (ownname, linenr, func, sep)
+    res = "%s%s: %s %s" % (result, exctype, excvalue, str(txt))
+    del trace
+    return res
