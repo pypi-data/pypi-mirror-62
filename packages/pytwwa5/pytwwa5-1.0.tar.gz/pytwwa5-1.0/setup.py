37e86ad7c104db291d0ec32d1702f100d8930b5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:23 +0000

    Add package pytwwa5/1.0/pytwwa5-1.0.tar.gz

---
+++ packages/pytwwa5/pytwwa5-1.0.tar.gz/pytwwa5-1.0/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="pytwwa5",
+    version='1.0',
+    packages = setuptools.find_packages(),
+    install_requires = ['request'],
+    description="Our first Python distribution package",
+    author="Unknown",
+    author_email = "unknown@gmail.com"
+)
