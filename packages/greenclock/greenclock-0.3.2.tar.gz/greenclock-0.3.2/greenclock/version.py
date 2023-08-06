9371408e328b3b71667bc18afcd2194d187d9f6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:32 +0000

    Add package greenclock/0.3.2/greenclock-0.3.2.tar.gz

---
+++ packages/greenclock/greenclock-0.3.2.tar.gz/greenclock-0.3.2/greenclock/version.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+# license: MIT
+version_info = (0, 3, 2, 'final', None)
+__title__ = 'greenclock'
+__version__ = '0.3.2-final'
+__author__ = 'pcdinh'
+__license__ = 'MIT'
---
+++ packages/greenclock/greenclock-0.3.2.tar.gz/greenclock-0.3.2/setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+from setuptools import setup
+
+from greenclock.version import __version__
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload -r pypi')
+    sys.exit()
+
+VERSION = __version__[:__version__.index('-')]
+
+
+def long_description():
+    """Get the long description from the README"""
+    return open(os.path.join(sys.path[0], 'README.rst')).read()
+
+
+setup(
+    name='greenclock',
+    packages=['greenclock'],
+    version=VERSION,
+    description='A library that provides time-based task scheduling using green threads via gevent.',
+    license='MIT',
+    long_description=long_description(),
+    long_description_content_type="text/x-rst",
+    install_requires=[
+        'gevent>=1.4.0',
+    ],
+    author='Pham Cong Dinh',
+    author_email='pcdinh@gmail.com',
+    url='https://github.com/pcdinh/greenclock',
+    download_url='https://github.com/pcdinh/greenclock/tarball/' + VERSION,
+    keywords=[
+        'cron', 'scheduling', 'schedule', 'periodic', 'jobs', 'clockwork', 'gevent'
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Natural Language :: English',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
