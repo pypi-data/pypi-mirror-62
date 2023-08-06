2411dba4cec8da9aa555373e7b411a2e7315f8e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:37 +0000

    Add package napoleontoolbox/2.3.8.168/napoleontoolbox-2.3.8.168.tar.gz

---
+++ packages/napoleontoolbox/napoleontoolbox-2.3.8.168.tar.gz/napoleontoolbox-2.3.8.168/napoleontoolbox/signal/signal_utility.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+def convert_to_sql_column_format(run):
+    run = run.replace('[', 'ccg')
+    run = run.replace(']', 'ccd')
+    run = run.replace(',', 'comma')
+    run = run.replace(' ', 'space')
+    run = run.replace('.', 'dot')
+    run = run.replace('-', 'minus')
+    run = run.replace('"', 'dqq')
+    run = run.replace("'", 'sqq')
+    run = run.replace('{', 'aag')
+    run = run.replace('}', 'aad')
+    run = run.replace(':', 'dodo')
+    return run
+
+def recover_to_sql_column_format(run):
+    run = run.replace('ccg','[')
+    run = run.replace('ccd',']')
+    run = run.replace('comma',',')
+    run = run.replace('space',' ')
+    run = run.replace('dot','.')
+    run = run.replace('minus','-')
+    run = run.replace('dqq','"')
+    run = run.replace('sqq',"'")
+    run = run.replace('aag','{')
+    run = run.replace('aad','}')
+    run = run.replace('dodo',':')
+    return run
