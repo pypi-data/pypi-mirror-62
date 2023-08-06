9da86bde28c4fe16d310877ac9e5999af29ab4e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:24 +0000

    Add package dsbox/0.0.0/dsbox-0.0.0.tar.gz

---
+++ packages/dsbox/dsbox-0.0.0.tar.gz/dsbox-0.0.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'dsbox',
+    packages = ['dsbox'],
+    version = '0.0.0',  # Ideally should be same as your GitHub release tag varsion
+    description = '',
+    author = 'Kedar Dabhadkar',
+    author_email = 'kdabhadk@gmail.com',
+    url = 'https://github.com/dkedar7/dsbox',
+    download_url = 'https://github.com/dkedar7/dsbox/archive/alpha.tar.gz',
+    keywords = ['Data Science', 'Machine Learning'],
+    classifiers = [],
+)
