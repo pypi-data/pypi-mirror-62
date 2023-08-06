ede232ca3b773b65b72f3c7a1680b665d846a795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:57 +0000

    Add package image-pattern/0.0.15/image-pattern-0.0.15.tar.gz

---
+++ packages/image-pattern/image-pattern-0.0.15.tar.gz/image-pattern-0.0.15/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['image_pattern', 'image_pattern.contrib', 'image_pattern.elements']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pillow>=7.0,<8.0', 'pydantic>=1.4,<2.0']
+
+setup_kwargs = {
+    'name': 'image-pattern',
+    'version': '0.0.15',
+    'description': '',
+    'long_description': None,
+    'author': 'acrius',
+    'author_email': 'acrius@mail.ru',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
