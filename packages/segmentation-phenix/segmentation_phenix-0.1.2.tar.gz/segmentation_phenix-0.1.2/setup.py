e70050278b4621acac66bc95f031eb8fac7ed402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:40 +0000

    Add package segmentation-phenix/0.1.2/segmentation_phenix-0.1.2.tar.gz

---
+++ packages/segmentation-phenix/segmentation_phenix-0.1.2.tar.gz/segmentation_phenix-0.1.2/setup.py
@@ -0,0 +1,32 @@
+import sysconfig
+
+import numpy as np
+from Cython.Distutils import build_ext
+from setuptools import setup, Extension
+
+our_flags = ["-march=native", "-std=c++11", "-DNDEBUG", "-Ofast", "-Wcpp"]
+
+ext_modules = [
+    Extension("segmentation_phenix.segmentation", ["segmentation_phenix/segmentation.pyx", ],
+              language="c++",
+              include_dirs=[np.get_include(), sysconfig.get_config_var('INCLUDEDIR')],
+              extra_compile_args=our_flags,
+              extra_link_args=["-std=c++11"],
+              ),
+]
+
+setup(
+    name="segmentation_phenix",
+    packages=["segmentation_phenix"],
+    version='0.1.2',
+    description='Mixture model segmentation for Phenix images',
+    author='Matej Usaj',
+    author_email='m.usaj@utoronto.ca',
+    url='https://github.com/mmasinas/segmentation_phenix',
+    download_url='https://github.com/mmasinas/segmentation_phenix/archive/master.zip',
+    keywords=['mixture', 'model', 'segmentation'],
+    classifiers=[],
+    cmdclass={"build_ext": build_ext},
+    ext_modules=ext_modules,
+    install_requires=['cython', 'scikit-image', 'numpy'],
+)
