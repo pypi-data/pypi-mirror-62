75f2bc514f3ab8515e62d14156b69f75c784c76f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:00 +0000

    Add package Cherrydoor/0.4.dev2/Cherrydoor-0.4.dev2-py3-none-any.whl

---
+++ packages/Cherrydoor/Cherrydoor-0.4.dev2-py3-none-any.whl/cherrydoor/run.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Run server"""
+from multiprocessing import Process
+from server import app, socket, config
+from interface.commands import Commands
+
+interface = Commands()
+interface_run = Process(target=interface.start)
+server = Process(
+    target=socket.run,
+    kwargs={
+        "app": app,
+        "log_output": True,
+        "host": config["host"],
+        "port": config["port"],
+    },
+)
+
+
+def exit():
+    interface_run.terminate()
+    server.terminate()
+
+
+if __name__ == "__main__":
+    import atexit
+
+    atexit.register(exit)
+    interface_run.start()
+    server.run()
