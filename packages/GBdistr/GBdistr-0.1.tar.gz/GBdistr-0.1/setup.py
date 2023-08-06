2fd2e6a22a7522d14c0995312f62597bf67e25d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:14 +0000

    Add package GBdistr/0.1/GBdistr-0.1.tar.gz

---
+++ packages/GBdistr/GBdistr-0.1.tar.gz/GBdistr-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='GBdistr',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['GBdistr'],
+      zip_safe=False)
