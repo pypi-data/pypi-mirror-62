545b51e11c316c8fa2c179b8c4ae4f85cc229309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:44 +0000

    Add package secure-aiohttp/0.0.2.2/secure-aiohttp-0.0.2.2.tar.gz

---
+++ packages/secure-aiohttp/secure-aiohttp-0.0.2.2.tar.gz/secure-aiohttp-0.0.2.2/secure_aiohttp/exceptions.py
@@ -0,0 +1,3 @@
+
+class CSRFtokenError(Exception):
+    pass
---
+++ packages/secure-aiohttp/secure-aiohttp-0.0.2.2.tar.gz/secure-aiohttp-0.0.2.2/secure_aiohttp/security_middlewares.py
@@ -0,0 +1,31 @@
+from aiohttp import web
+from secure_aiohttp.defaults import CSP_DEFAULT
+from secure_aiohttp.utils import csp_builder
+from secure_aiohttp.exceptions import CSRFtokenError
+
+
+
+def security_middleware(xss='default'):
+    """Middleware that includes handling of XSS via CSP and SCRF
+
+    Parameters:
+        xss(str/dict/None): If not specified than default CSP is enabled.
+            Accepts either 'default'/'secure' as string as predifined set of CSP rules,
+            or dict with custom rules. Example can be found in documentation.
+            You can also pass None to deactivate xss at all.
+
+    Returns:
+        @web.middlware wrapped middleware
+
+    """
+    @web.middleware
+    async def return_middleware(request, handler):
+        if xss:
+            # if xss - string -> looking in predifined data. Else -> this should be custom dict.
+            csp_dict = CSP_DEFAULT.get(str(xss)) or xss
+            csp_string = csp_builder(xss_dict)
+            request.headers['Content-Security-Policy'] = csp_string
+
+        return await handler(request)
+
+    return return_middleware
