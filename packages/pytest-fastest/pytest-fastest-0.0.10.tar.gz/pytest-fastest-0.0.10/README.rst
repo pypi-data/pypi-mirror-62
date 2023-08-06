29f6e8b4cd9a1dbe9df216f8029dde07b7b7ea89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:46 +0000

    Add package pytest-fastest/0.0.10/pytest-fastest-0.0.10.tar.gz

---
+++ packages/pytest-fastest/pytest-fastest-0.0.10.tar.gz/pytest-fastest-0.0.10/README.rst
@@ -0,0 +1,115 @@
+==============
+pytest-fastest
+==============
+
+.. image:: https://img.shields.io/pypi/v/pytest-fastest.svg
+    :target: https://pypi.org/project/pytest-fastest
+    :alt: PyPI version
+
+.. image:: https://img.shields.io/pypi/pyversions/pytest-fastest.svg
+    :target: https://pypi.org/project/pytest-fastest
+    :alt: Python versions
+
+.. image:: https://travis-ci.com/kstrauser/pytest-fastest.svg?branch=dev
+    :target: https://travis-ci.com/kstrauser/pytest-fastest
+    :alt: See Build Status on Travis CI
+
+.. image:: https://ci.appveyor.com/api/projects/status/github/kstrauser/pytest-fastest?branch=dev
+    :target: https://ci.appveyor.com/project/kstrauser/pytest-fastest/branch/dev
+    :alt: See Build Status on AppVeyor
+
+Use SCM and coverage to run only needed tests
+
+----
+
+This `pytest`_ plugin was generated with `Cookiecutter`_ along with `@hackebrot`_'s `cookiecutter-pytest-plugin`_ template.
+
+
+Features
+--------
+
+* Gathers coverage data from tests to track which tests call functions from which modules.
+* Uses Git to track changes from a given commit to find the minimum set of tests that need to run to test new changes, then skips everything else.
+
+
+Requirements
+------------
+
+* Python 3.5+
+* pytest 3.4.0+
+
+
+Installation
+------------
+
+You can install "pytest-fastest" via `pip`_ from `PyPI`_::
+
+    $ pip install pytest-fastest
+
+
+Usage
+-----
+
+pytest-fastest can be set to run only tests:
+
+* That test modules that have changed in Git,
+* Tests that we don't already have coverage data for, and
+* Tests that we've added or changed.
+
+In most common development workflows where you make short-lived branches
+off a main "master" or "dev" branch, the amount of code that actually
+changes while fixing a bug or writing a feature is usually just a small
+portion of the whole codebase. Instead of running thousands of tests
+after each change, pytest-fastest can identify the relevant ones that
+thoroughly test your work but skip all the things you *haven't* changed.
+
+To use it:
+
+* In ``pytest.ini``, set ``fastest_commit`` to the name of a Git commit to
+  compare your current work against. (You can also set or override it on the
+  command line with ``--fastest-commit``). This is required if you want to
+  skip tests, which is the main reason for using this plugin.
+
+* Use the command line argument ``--fastest-mode`` to choice the appropriate
+  running mode:
+
+  - ``all`` (default): Run all tests without collecting coverage data. This
+    emulates normal pytest behavior and has no effect on performance.
+  - ``skip``: Skip tests that don't need to be run, but update coverage data
+    on the ones that do run. This will usually be faster than ``all``, but
+    because collecting coverage information takes some time, as the number
+    of un-skippable tests grows very large it may actually become slower.
+  - ``gather``: Don't skip any tests, but do gather coverage data. This is
+    slower than ``all`` but can be used to seed the coverage cache.
+  - ``cache``: This is a fast mode for fixing existing tests. It skips tests
+    but doesn't update the coverage cache. It will never be slower than
+    ``all`` and will always be faster than ``skip``.
+
+Contributing
+------------
+Contributions are very welcome. Tests can be run with `tox`_, please ensure
+the coverage at least stays the same before you submit a pull request.
+
+License
+-------
+
+Distributed under the terms of the `MIT`_ license, "pytest-fastest" is free and open source software
+
+
+Issues
+------
+
+If you encounter any problems, please `file an issue`_ along with a detailed description.
+
+.. _`Cookiecutter`: https://github.com/audreyr/cookiecutter
+.. _`@hackebrot`: https://github.com/hackebrot
+.. _`MIT`: http://opensource.org/licenses/MIT
+.. _`BSD-3`: http://opensource.org/licenses/BSD-3-Clause
+.. _`GNU GPL v3.0`: http://www.gnu.org/licenses/gpl-3.0.txt
+.. _`Apache Software License 2.0`: http://www.apache.org/licenses/LICENSE-2.0
+.. _`cookiecutter-pytest-plugin`: https://github.com/pytest-dev/cookiecutter-pytest-plugin
+.. _`file an issue`: https://github.com/kstrauser/pytest-fastest/issues
+.. _`pytest`: https://github.com/pytest-dev/pytest
+.. _`tox`: https://tox.readthedocs.io/en/latest/
+.. _`pip`: https://pypi.org/project/pip/
+.. _`PyPI`: https://pypi.org/project
---
+++ packages/pytest-fastest/pytest-fastest-0.0.10.tar.gz/pytest-fastest-0.0.10/pytest_fastest/__init__.py
@@ -0,0 +1,220 @@
+# -*- coding: utf-8 -*-
+
+"""Use coverage data and Git to determine which tests may be skipped."""
+
+import contextlib
+import enum
+import json
+import pathlib
+import sys
+from typing import Dict, List, Set, Tuple  # noqa: F401, pylint: disable=unused-import
+
+import pytest
+
+try:
+    from _pytest.config import ArgumentError
+except ImportError:
+    from _pytest.config.argparsing import ArgumentError
+from _pytest.runner import runtestprotocol
+
+from . import git
+
+STOREFILE = ".fastest.coverage"
+COVERAGE = {}  # type: Dict[str, List[str]]
+STOREVERSION = 1
+
+
+# Configuration
+
+
+class Mode(enum.Enum):
+    """Enumerated running modes."""
+
+    # Run all tests without collecting coverage data: normal pytest behavior
+    ALL = "all"
+    # Skip tests that don't need to be run, but update coverage data on the ones that do
+    SKIP = "skip"
+    # Don't skip tests, but gather coverage data
+    GATHER = "gather"
+    # Skip tests, but don't gather coverage data
+    CACHE = "cache"
+
+
+def pytest_addoption(parser):
+    """Add command line options."""
+
+    group = parser.getgroup("fastest")
+    group.addoption(
+        "--fastest-mode",
+        default="all",
+        choices=[mode.value for mode in Mode],
+        action="store",
+        dest="fastest_mode",
+        help=(
+            "Set the running mode."
+            " `all` runs all tests but does not collect coverage data."
+            " `skip` skips tests that can be skipped, and updates coverage data on the rest."
+            " `gather` runs all tests and gathers coverage data on them."
+            " `cache` skips tests and does not collect coverage data."
+        ),
+    )
+    group.addoption(
+        "--fastest-commit",
+        action="store",
+        dest="fastest_commit",
+        help="Git commit to compare current work against.",
+    )
+
+    parser.addini("fastest_commit", "Git commit to compare current work against")
+
+
+# Helpers
+
+
+@contextlib.contextmanager
+def tracer(rootdir: str, own_file: str):
+    """Collect call graphs for modules within the rootdir."""
+
+    result = set()
+    base_path = str(pathlib.Path(rootdir))
+
+    def trace_calls(frame, event, arg):  # pylint: disable=unused-argument
+        """settrace calls this every time something interesting happens."""
+
+        if event != "call":
+            return
+
+        func_filename = frame.f_code.co_filename
+        if func_filename == own_file:
+            return
+        if not func_filename.endswith(".py"):
+            return
+        if not func_filename.startswith(base_path):
+            return
+
+        result.add(func_filename)
+
+    oldtrace = sys.settrace(trace_calls)
+    try:
+        yield result
+    finally:
+        sys.settrace(oldtrace)  # type: ignore
+
+
+def load_coverage():
+    """Load the coverage data from disk."""
+
+    try:
+        with open(STOREFILE, "r") as infile:
+            data = json.load(infile)
+    except FileNotFoundError:
+        return {}
+
+    try:
+        if data["version"] != STOREVERSION:
+            return {}
+    except KeyError:
+        return {}
+
+    return data["coverage"]
+
+
+def save_coverage(coverage):
+    """Save the coverage data to disk."""
+
+    with open(STOREFILE, "w") as outfile:
+        json.dump({"coverage": coverage, "version": STOREVERSION}, outfile, indent=2)
+
+
+# Hooks
+
+
+def pytest_configure(config):
+    """Process the configuration."""
+
+    config.cache.fastest_commit = config.getoption("fastest_commit") or config.getini(
+        "fastest_commit"
+    )
+    config.cache.fastest_mode = config.getoption("fastest_mode")
+
+    config.cache.fastest_skip, config.cache.fastest_gather = {
+        Mode.ALL.value: (False, False),
+        Mode.SKIP.value: (True, True),
+        Mode.GATHER.value: (False, True),
+        Mode.CACHE.value: (True, False),
+    }[config.cache.fastest_mode]
+
+    if config.cache.fastest_skip and not config.cache.fastest_commit:
+        raise ArgumentError(
+            "fastest_mode",
+            "Mode {} requires fastest_commit to be set.".format(config.cache.fastest_mode),
+        )
+
+    COVERAGE.clear()
+    if config.cache.fastest_gather or config.cache.fastest_skip:
+        COVERAGE.update(load_coverage())
+
+
+def pytest_collection_modifyitems(config, items):
+    """Mark unaffected tests as skippable."""
+
+    if not config.cache.fastest_skip:
+        return None
+
+    covered_test_files = {covdata["fspath"] for covdata in COVERAGE.values()}
+    changed_files, changed_tests = git.changes_since(config.cache.fastest_commit)
+
+    affected_nodes = set()
+    for nodeid, covdata in COVERAGE.items():
+        if any(fname in changed_files for fname in covdata["files"]):
+            affected_nodes.add(nodeid)
+
+    skip = pytest.mark.skip(reason="skipper")
+
+    for item in items:
+        if not any(
+            (
+                # Tests refer to modules that have changed
+                item.nodeid in affected_nodes,
+                # Tests that we don't already have coverage data for
+                str(item.fspath) not in covered_test_files,
+                # Tests that have themselves been changed
+                (str(item.fspath), item.name) in changed_tests,
+            )
+        ):
+            item.add_marker(skip)
+
+    return True
+
+
+def pytest_runtest_protocol(item, nextitem):
+    """Gather coverage data for the item."""
+
+    if not item.config.cache.fastest_gather:
+        return None
+
+    with tracer(item.config.rootdir, str(item.fspath)) as coverage:
+        reports = runtestprotocol(item, nextitem=nextitem)
+
+    item.ihook.pytest_runtest_logfinish(nodeid=item.nodeid, location=item.location)
+
+    outcomes = {report.when: report.outcome for report in reports}
+    if outcomes["setup"] in {"failed", "skipped"}:
+        return True
+
+    if outcomes["call"] == "passed":
+        COVERAGE[item.nodeid] = {"files": sorted(coverage), "fspath": str(item.fspath)}
+    else:
+        try:
+            del COVERAGE[item.nodeid]
+        except KeyError:
+            pass
+
+    return True
+
+
+def pytest_terminal_summary(terminalreporter, exitstatus):  # pylint: disable=unused-argument
+    """Save the coverage data we've collected."""
+
+    if COVERAGE:
+        save_coverage(COVERAGE)
