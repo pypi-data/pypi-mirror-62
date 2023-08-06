4682d9b5349dd5f6801dbf51d3ae763c90553a70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:37 +0000

    Add package falcon-confiante/0.2.7/falcon_confiante-0.2.7-py3-none-any.whl

---
+++ packages/falcon-confiante/falcon_confiante-0.2.7-py3-none-any.whl/falcon_confiante/__init__.py
@@ -0,0 +1,8 @@
+from .router import OpenApiRouter
+from .middleware import (
+    OpenApiSchemaValidationMiddleware,
+    OpenApiAuthenticationMiddleware,
+)
+
+__version__ = "0.2.7"
+__author__ = "Erle Carrara <carrara.erle@gmail.com>"
