0e1d428c0349cd0f122f7bf000ae7c56aaf2544b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:10 +0000

    Add package sdcli/1.1.5/sdcli-1.1.5-py3-none-any.whl

---
+++ packages/sdcli/sdcli-1.1.5-py3-none-any.whl/sdcli/config/_config.py
@@ -0,0 +1,23 @@
+'''
+...File config os this project STOBS...
+'''
+import os
+
+
+class Config(object):
+    NAME = 'sdcli'
+    VERSION = '1.1.5'
+    LICENSE = 'MIT'
+    AUTHOR = 'Jose Segura'
+    EMAIL = 'j0s3s3gur4@gmail.com'
+    DESCRIPTION = '''
+    sdcli -> Is a CLI for generate a simple scenes collection that we using in ours streamings.
+    '''
+    SOCIAL_MEDIAS = {
+        'medium': '...',
+        'github': 'https://github.com/DarkCode01/streamelopers-cli',
+        'patron': '...'
+    }
+
+    # System Configuration.
+    HOME = os.environ.get('HOME')
