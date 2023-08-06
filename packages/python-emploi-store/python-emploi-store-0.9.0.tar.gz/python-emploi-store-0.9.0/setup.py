fece9115f6573b36073c8366ae998fb45ed7a730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:57 +0000

    Add package python-emploi-store/0.9.0/python-emploi-store-0.9.0.tar.gz

---
+++ packages/python-emploi-store/python-emploi-store-0.9.0.tar.gz/python-emploi-store-0.9.0/setup.py
@@ -0,0 +1,32 @@
+# encoding: utf-8
+"""Python client library for Pôle Emploi's "Emploi Store Dev"."""
+
+import setuptools
+
+__version__ = '0.9.0'
+
+setuptools.setup(
+    name='python-emploi-store',
+    packages=['emploi_store'],
+    package_data={'emploi_store': ['py.typed']},
+    version=__version__,
+    description=__doc__,
+    author='Pascal Corpet',
+    author_email='pascal@bayesimpact.org',
+    url='https://github.com/bayesimpact/python-emploi-store',
+    download_url='https://github.com/bayesimpact/python-emploi-store/tarball/' + __version__,
+    license='The MIT License (MIT)',
+    keywords=['Pôle Emploi', 'France', 'emploi', 'OpenData'],
+    install_requires=['requests', 'six'],
+    extras_require={'dev': [
+        'codecov',
+        'coverage',
+        'mock',
+        'nose',
+        'nose-watch',
+        'pylint',
+        'pylint-quotes',
+        'requests-mock',
+    ]},
+    classifiers=[],
+)
