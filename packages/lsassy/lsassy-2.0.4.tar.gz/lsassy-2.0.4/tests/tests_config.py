7cce03e68111477b521273f0e8f97ff83362d804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:22 +0000

    Add package lsassy/2.0.4/lsassy-2.0.4.tar.gz

---
+++ packages/lsassy/lsassy-2.0.4.tar.gz/lsassy-2.0.4/tests/tests_config.py
@@ -0,0 +1,14 @@
+# Author:
+#  Romain Bentz (pixis - @hackanddo)
+# Website:
+#  https://beta.hackndo.com
+
+ip_address = "192.168.1.101"
+ip_address_protected = "192.168.1.121"
+domain = "adsec.local"
+da_login = "jsnow"
+da_password = "Winter_is_coming_!"
+usr_login = "jlannister"
+usr_password = "summer4ever!"
+procdump_path = "/home/pixis/Tools/Windows/Sysinternals/procdump.exe"
+dumpert_path = "/home/pixis/Tools/Windows/Dumpert/Outflank-Dumpert.exe"
