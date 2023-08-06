1c33fd06b41acd3fe62a80a7bd9c61bd10f80150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:47 +0000

    Add package pytwwa/0.4/pytwwa-0.4.tar.gz

---
+++ packages/pytwwa/pytwwa-0.4.tar.gz/pytwwa-0.4/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name="pytwwa",
+    version='0.4',
+    description="Our first Python distribution package",
+    author="Unknown",
+    author_email = "unknown@gmail.com"
+)
