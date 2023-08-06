e6a72aaa6452e1d7cd9786c8365ae805e3b4bc59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:01 +0000

    Add package pygametext/0.1.0.2/pygametext-0.1.0.2.tar.gz

---
+++ packages/pygametext/pygametext-0.1.0.2.tar.gz/pygametext-0.1.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pygametext",
+    version="0.1.0.2",
+    author="LeBogo",
+    author_email="lebogomc@gmail.com",
+    description="This Package adds UI Elements like Buttons and Textboxes to PyGame.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LeBogoo/pygametext",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
