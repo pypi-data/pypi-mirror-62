fe806031cc48546a989700075f7da184dc72f1c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:28 +0000

    Add package flynt/0.45.4b0/flynt-0.45.4b0.tar.gz

---
+++ packages/flynt/flynt-0.45.4b0.tar.gz/flynt-0.45.4b0/config.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+__version__ = "0.45.4b"
+
+home = os.path.dirname(__file__)
+
+
+def add_src_to_path():
+    sys.path.append(home)
+    sys.path.append(os.path.join(home, "src"))
---
+++ packages/flynt/flynt-0.45.4b0.tar.gz/flynt-0.45.4b0/src/flynt/__init__.py
@@ -0,0 +1,5 @@
+"""`flynt` is a command line tool to automatically convert a project's Python code
+from old "%-formatted" and .format(...) strings into Python 3.6+'s f-strings.
+Learn more about f-strings at https://www.python.org/dev/peps/pep-0498/"""
+
+from flynt.cli import main
