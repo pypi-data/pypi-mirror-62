27dbfae528a544006f51255270868fc138cf7ce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:55 +0000

    Add package heritagego-core-library/1.0.0/heritagego_core_library-1.0.0-py3-none-any.whl

---
+++ packages/heritagego-core-library/heritagego_core_library-1.0.0-py3-none-any.whl/majormode/heritagego/model/heritagego.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2019 Heritage Observatory.  All rights reserved.
+#
+# This software is the confidential and proprietary information of
+# Heritage Observatory or one of its subsidiaries.  You shall not
+# disclose this confidential information and shall use it only in
+# accordance with the terms of the license agreement or other applicable
+# agreement you entered into with Heritage Observatory.
+#
+# HERITAGE OBSERVATORY MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE
+# SUITABILITY OF THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT
+# NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR
+# A PARTICULAR PURPOSE, OR NON-INFRINGEMENT.  HERITAGE OBSERVATORY SHALL
+# NOT BE LIABLE FOR ANY LOSSES OR DAMAGES SUFFERED BY LICENSEE AS A
+# RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS
+# DERIVATIVES.
+
+# Canonical name of the property defining a users' national
+# identification number used by the governments of many countries as a
+# means of tracking their citizens, permanent residents, and temporary
+# residents for the purposes of work, taxation, government benefits,
+# health care, and other governmentally-related functions.  The number
+# appears on identity documents issued by several of the countries.
+PROPERTY_NATIONAL_IDENTIFICATION_NUMBER = 'common.identity.national_identification_number'
