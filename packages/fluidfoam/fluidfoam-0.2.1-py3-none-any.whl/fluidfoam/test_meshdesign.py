44043991a20145b4b5750a65998bd13a091d6a99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:00 +0000

    Add package fluidfoam/0.2.1/fluidfoam-0.2.1-py3-none-any.whl

---
+++ packages/fluidfoam/fluidfoam-0.2.1-py3-none-any.whl/fluidfoam/test_meshdesign.py
@@ -0,0 +1,12 @@
+import unittest
+
+import fluidfoam
+
+
+class SimpleTestCase(unittest.TestCase):
+    def test_meshdesign(self):
+        print("meshdesign TestCase")
+        dummy, dummy, dummy = fluidfoam.getgz(1.0, 0.001, 20)
+        dummy, dummy, dummy = fluidfoam.getgz(1.0, 0.1, 20)
+        dummy, dummy = fluidfoam.getdzs(1.0, 0.1, 20)
+        dummy, dummy = fluidfoam.getdzs(1.0, 10, 20)
---
+++ packages/fluidfoam/fluidfoam-0.2.1-py3-none-any.whl/fluidfoam/test_readpostpro.py
@@ -0,0 +1,24 @@
+import unittest
+
+import fluidfoam
+
+case = "output_samples/ascii"
+probes_name = "probes"
+
+
+class SimpleTestCase(unittest.TestCase):
+    def test_read_forces(self):
+        dummy = fluidfoam.readforce(case)
+        dummy = fluidfoam.readforce(case, time_name="latestTime")
+        dummy = fluidfoam.readforce(case, time_name="mergeTime")
+        # self.assertEqual(1, len(postpro.forcedirs))
+        print("create force object")
+
+    def test_read_probes(self):
+        time_vect, dummy = fluidfoam.readprobes(case)
+        self.assertEqual(10, len(time_vect))
+        time_vect, dummy = fluidfoam.readprobes(case, time_name="latestTime")
+        self.assertEqual(6, len(time_vect))
+        time_vect, dummy = fluidfoam.readprobes(case, time_name="mergeTime")
+        self.assertEqual(13, len(time_vect))
+        time_vect, dummy = fluidfoam.readprobes(case, time_name="0", name="p")
