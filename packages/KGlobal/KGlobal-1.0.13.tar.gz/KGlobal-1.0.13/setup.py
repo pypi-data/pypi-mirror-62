afca7c373d7060ad1f0d335cac771f8670735286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:25 +0000

    Add package KGlobal/1.0.13/KGlobal-1.0.13.tar.gz

---
+++ packages/KGlobal/KGlobal-1.0.13.tar.gz/KGlobal-1.0.13/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+
+if __name__ == '__main__':
+    setup(
+        name='KGlobal',
+        version='1.0.13',
+        author='Kevin Russell',
+        packages=['KGlobal_Core', 'KGlobal_Core.data', 'KGlobal_Core.sql'],
+        py_modules=['KGlobal'],
+        url='https://github.com/KLRussell/Python_KGlobal_Package',
+        description='SQL Handling, Object Shelving, Data Encryption, XML Parsing/Writing, E-mail Parsing, Logging',
+        install_requires=[
+            'pandas',
+            'future',
+            'sqlalchemy',
+            'pyodbc',
+            'portalocker',
+            'cryptography',
+            'independentsoft.msg',
+            'exchangelib',
+            'bs4',
+            'six',
+        ],
+        package_data={
+            "": ["*.txt", "*.md"],
+        },
+        zip_safe=False,
+    )
