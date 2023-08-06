d710be1fdabd15cd0b5cbcd79f81d9745b50d681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:10 +0000

    Add package azure-iot-device/2.1.0/azure-iot-device-2.1.0.tar.gz

---
+++ packages/azure-iot-device/azure-iot-device-2.1.0.tar.gz/azure-iot-device-2.1.0/azure/iot/device/iothub/pipeline/__init__.py
@@ -0,0 +1,10 @@
+"""Azure IoT Hub Device SDK Pipeline
+
+This package provides a protocol pipeline for use with the Azure IoT Hub Device SDK.
+
+INTERNAL USAGE ONLY
+"""
+
+from .iothub_pipeline import IoTHubPipeline
+from .http_pipeline import HTTPPipeline
+from .config import IoTHubPipelineConfig
