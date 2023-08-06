46de6873e7db41deb0998944cc3f5e1732b886ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:31 +0000

    Add package hellogrpc/0.1.2/hellogrpc-0.1.2-py3-none-any.whl

---
+++ packages/hellogrpc/hellogrpc-0.1.2-py3-none-any.whl/hellogrpc/__init__.py
@@ -0,0 +1,8 @@
+import grpc
+
+from hellogrpc.hellogrpc_pb2 import *
+from hellogrpc.hellogrpc_pb2_grpc import *
+
+def Client():
+    channel = grpc.secure_channel('hello.rpc.stackmachine.com:443')
+    return GreeterStub(channel)
