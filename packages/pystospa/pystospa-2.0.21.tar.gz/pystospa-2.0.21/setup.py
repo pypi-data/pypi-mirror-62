248713a57c1fe7629a8e80708823e6fd87c2da5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:13 +0000

    Add package pystospa/2.0.21/pystospa-2.0.21.tar.gz

---
+++ packages/pystospa/pystospa-2.0.21.tar.gz/pystospa-2.0.21/setup.py
@@ -0,0 +1,20 @@
+
+from os import path
+from skbuild import setup
+
+with open(path.join("pystospa", "README.md"), "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pystospa',
+    version='2.0.21',
+    author='Bartosz Bartmanski',
+    author_email='bartoszbartmanski@gmail.com',
+    description='A python binding of C++ package for stochastic simulations of spatially extended systems',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    platforms=["Linux"],
+    packages=['pystospa'],
+    setup_requires=["setuptools", "wheel", "scikit-build >= 0.10.0", "cmake >= 3.5"],
+    zip_safe=False,
+)
