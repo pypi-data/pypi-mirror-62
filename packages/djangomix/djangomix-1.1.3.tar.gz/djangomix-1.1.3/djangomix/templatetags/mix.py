8dd77ef249ebde4310c653811306ed8c9be7a90b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:06 +0000

    Add package djangomix/1.1.3/djangomix-1.1.3.tar.gz

---
+++ packages/djangomix/djangomix-1.1.3.tar.gz/djangomix-1.1.3/djangomix/templatetags/mix.py
@@ -0,0 +1,42 @@
+import json
+import os
+from django.conf import settings
+from django import template
+
+register = template.Library()
+
+MANIFEST_DIRECTORY = getattr(settings, 'LARAVELMIX_MANIFEST_DIRECTORY', '')
+PUBLIC_URL = getattr(settings, 'LARAVELMIX_PUBLIC_URL', settings.STATIC_URL)
+
+
+@register.simple_tag
+def mix(path, manifest_directory=None):
+    if manifest_directory is None:
+        manifest_directory = MANIFEST_DIRECTORY
+
+    # laravel-mix generate / on the path in manifest.json
+    if path[0] != '/':  # url separator
+        path = f'/{path}'
+
+    if os.path.exists(os.path.join(manifest_directory, 'hot')):
+        # taken from https://github.com/laravel/framework/blob/master/src/Illuminate/Foundation/Mix.php
+        url = open(os.path.join(manifest_directory, 'hot')).read().strip()
+        if 'http://' or 'https://' in url:
+            return ':'.join(url.split(':')[1:]) + path
+        else:
+            return f'//localhost:8080{path}'
+
+    manifest_path = os.path.join(manifest_directory, 'mix-manifest.json')
+
+    if not os.path.exists(manifest_path):
+        raise FileNotFoundError('Unable to locate manifest file: ' + manifest_path)
+
+    with open(manifest_path, 'r') as f:
+        manifest = json.load(f)
+
+    for key, value in manifest.items():
+        if path in key:
+            path = key
+            full_path = f"{PUBLIC_URL.rstrip('/')}/{manifest[path].lstrip('/')}"
+            return full_path
+    raise Exception('Unable to locate mix file: ' + path)
---
+++ packages/djangomix/djangomix-1.1.3.tar.gz/djangomix-1.1.3/setup.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+version = '1.1.3'
+
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+
+setup(
+    name='djangomix',
+    version=version,
+    keywords='Django Mix',
+    description='Django integration for Laravel Mix',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    url='https://github.com/m-a-k-o/django-mix',
+
+    author='Marek Racik',
+    author_email='marek@idea-loop.com',
+
+    packages=find_packages(),
+    requires=[
+        'Django (>=1.11)',
+    ],
+    install_requires=[
+        'Django>=1.11',
+    ],
+
+    include_package_data=True,
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
