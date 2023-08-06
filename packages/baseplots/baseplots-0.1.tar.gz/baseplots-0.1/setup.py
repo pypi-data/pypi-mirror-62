cdfa343a52b69790df713e37e13a867a084b3f9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:06 +0000

    Add package baseplots/0.1/baseplots-0.1.tar.gz

---
+++ packages/baseplots/baseplots-0.1.tar.gz/baseplots-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='baseplots',
+      version='0.1',
+      description='standardized plots from matplotlib and seaborn',
+      packages=['baseplots'],
+      zip_safe=False)
