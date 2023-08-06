13a5d429835c9b5dae5112e5ad76a4b870d41259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:02 +0000

    Add package agilicus/1.4.2/agilicus-1.4.2-py3-none-any.whl

---
+++ packages/agilicus/agilicus-1.4.2-py3-none-any.whl/agilicus/api/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from agilicus.api.application_services_api import ApplicationServicesApi
+from agilicus.api.applications_api import ApplicationsApi
+from agilicus.api.files_api import FilesApi
+from agilicus.api.groups_api import GroupsApi
+from agilicus.api.logs_query_api import LogsQueryApi
+from agilicus.api.metrics_api import MetricsApi
+from agilicus.api.organisations_api import OrganisationsApi
+from agilicus.api.services_api import ServicesApi
+from agilicus.api.tokens_api import TokensApi
+from agilicus.api.users_api import UsersApi
+from agilicus.api.view_audit_records_api import ViewAuditRecordsApi
+from agilicus.api.whoami_api import WhoamiApi
