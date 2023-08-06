8d465bdbe31e65069236b3a1a55803f30f952f23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:27 +0000

    Add package crystaldb/1.1.0/crystaldb-1.1.0.tar.gz

---
+++ packages/crystaldb/crystaldb-1.1.0.tar.gz/crystaldb-1.1.0/setup.py
@@ -0,0 +1,33 @@
+# !/usr/bin/python
+# -*- coding:utf-8 -*-
+# Author: Zhichang Fu
+# Created Time: 2018-10-06 12:33:04
+
+from setuptools import setup
+import os
+
+try:
+    f = open(os.path.join(os.path.dirname(__file__), 'README.md'))
+    readme = f.read()
+    f.close()
+except Exception:
+    readme = "CrystalDB is a simple and small ORM and  no need to provide a model."
+
+install_requires = ["mysqlclient", "DBUtils==1.3"]
+
+setup(
+    name='crystaldb',
+    description=
+    'CrystalDB is a simple and small ORM and  no need to provide a model.',
+    long_description=readme,
+    author='Zhichang Fu',
+    author_email='fuzctc@gmail.com',
+    long_description_content_type="text/markdown",
+    url='https://github.com/fuzctc/crystaldb.py.git',
+    version=__import__('crystaldb').__version__,
+    packages=['crystaldb'],
+    install_require=install_requires,
+    include_package_data=True,
+    exclude_package_date={'': ['.gitignore']},
+    install_requires=[],
+)
