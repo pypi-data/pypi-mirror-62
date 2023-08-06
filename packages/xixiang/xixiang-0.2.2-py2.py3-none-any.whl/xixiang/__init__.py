ef4161e66858992f6c2b0677c1a7054f6497b116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:02 +0000

    Add package xixiang/0.2.2/xixiang-0.2.2-py2.py3-none-any.whl

---
+++ packages/xixiang/xixiang-0.2.2-py2.py3-none-any.whl/xixiang/__init__.py
@@ -0,0 +1,7 @@
+__author__ = "Lirian Su"
+__version__ = "0.2.2"
+
+from . import urls as urls  # NOQA
+from .exceptions import *  # NOQA
+from .models import *  # NOQA
+from .services import XiXiang  # NOQA
