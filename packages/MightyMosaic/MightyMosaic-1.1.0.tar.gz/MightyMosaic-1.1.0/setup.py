708dc12bd2c19b5af58aadcaad11e56a1eeb15ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:40 +0000

    Add package MightyMosaic/1.1.0/MightyMosaic-1.1.0.tar.gz

---
+++ packages/MightyMosaic/MightyMosaic-1.1.0.tar.gz/MightyMosaic-1.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["tqdm>=4.32.1"]
+
+setup(
+    name="MightyMosaic",
+    python_requires='>=3.6',
+    version="1.1.0",
+    author="Aurélien COLIN",
+    author_email="aureliencolin@hotmail.com",
+    description="Create mosaics with overlapping tiles",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Rignak/MightyMosaic",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
